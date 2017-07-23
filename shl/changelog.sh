bac512303cb9abcc5cae636d828c1575cde4bb8f
sebma <sebma@users.noreply.github.com>
2017-07-23 19:36:16 +0200

    Updated shl/changelog.sh

--- shl/changelog.sh
+++ shl/changelog.sh
@@ -2,9 +2,14 @@
 
 package_name=$1
 ppa_user_name=$(apt-cache policy $package_name | grep -m1 500.http | awk -F/ '{print$4}')
-ppa_name=$(apt-cache policy $package_name | grep -m1 500.http | awk -F/ '{print$5}')
-package_version=$(apt-cache policy $package_name | awk -F"[ :]" '/Candidate:/{print$NF}')
-URL=https://launchpad.net/~$ppa_user_name/+archive/ubuntu/$ppa_name/+files/${package_name}_${package_version}_source.changes
-#echo "=> URL = $URL"
-grep -q launchpad.net <<< "$URL" || { echo "=> ERROR: The package is not installed from <launchpad.net>" >&2 ; exit 1; }
-\wget -q -O - $URL | awk '/Changes:/{f=1;next}/Checksums/{f=0}f'
+ppa_name=$(apt-cache policy $package_name | grep -m1 500.http | awk -F/ '{print$5}' | awk '{print$NF}')
+if [ $ppa_name = "$(lsb_release -sc)-updates" ]
+then
+	apt-get changelog $package_name
+else
+	package_version=$(apt-cache policy $package_name | awk -F"[ :]" '/Candidate:/{print$NF}')
+	URL=https://launchpad.net/~$ppa_user_name/+archive/ubuntu/$ppa_name/+files/${package_name}_${package_version}_source.changes
+	#echo "=> URL = $URL"
+	grep -q launchpad.net <<< "$URL" || { echo "=> ERROR: The package is not installed from <launchpad.net>" >&2 ; exit 1; }
+	\wget -q -O - $URL | awk '/Changes:/{f=1;next}/Checksums/{f=0}f'
+fi
