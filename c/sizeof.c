100b672aa85ad5c752a81262a6e5acba8548bdae
sebma <sebma@users.noreply.github.com>
2017-09-14 20:29:37 +0200

    Updated c/sizeof.c

--- c/sizeof.c
+++ c/sizeof.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdint.h> //uint8_t, etc...
 
-int len_int(int *x) {
+int len_uint8_t(uint8_t *x) {
 //	printf("=> __func__ = %s\n", __func__ );
-	return (int)(sizeof(x) / sizeof(int));
+	return (int)(sizeof(x) / sizeof(uint8_t));
 }
 
 int main (int argc, char **argv) {
@@ -30,13 +30,13 @@ int main (int argc, char **argv) {
 
 	printf("sizeof(int64_t): %d bits\n", 8 * (int) sizeof(int64_t));
 
-	int a[15];
+	uint8_t a[15];
 
 	printf("=> sizeof(a) = %lu\n", sizeof(a));
 
-	printf("=> sizeof(a)/sizeof(int) = %lu\n", sizeof(a)/sizeof(int));
+	printf("=> sizeof(a)/sizeof(int) = %lu\n", sizeof(a)/sizeof(uint8_t));
 
-	printf("=> len_int = %d\n", len_int(a));
+	printf("=> len_uint8_t = %d\n", len_uint8_t(a));
 
 //  printf("Hit enter to exit.\n");
 //  char *scannedText;
