6395251be5fcfcc86bc49db2f252f85757fea087
sebma <sebma@users.noreply.github.com>
2017-09-14 15:37:39 +0200

    Updated c/sizeof.c

--- c/sizeof.c
+++ c/sizeof.c
@@ -1,34 +1,47 @@
 #include <stdio.h>
 #include <stdint.h> //uint8_t, etc...
 
+int len_int(int *x) {
+//	printf("=> __func__ = %s\n", __func__ );
+	return (int)(sizeof(x) / sizeof(int));
+}
+
 int main (int argc, char **argv) {
 
-  printf("sizeof(char):   %d bits\n", 8 * (int) sizeof(char));
+	printf("sizeof(char):   %d bits\n", 8 * (int) sizeof(char));
 
-  printf("sizeof(short): %d bits\n", 8 * (int) sizeof(short));
+	printf("sizeof(short): %d bits\n", 8 * (int) sizeof(short));
 
-  printf("sizeof(int):   %d bits\n", 8 * (int) sizeof(int));
+	printf("sizeof(int):   %d bits\n", 8 * (int) sizeof(int));
 
-  printf("sizeof(long):  %d bits\n", 8 * (int) sizeof(long));
+	printf("sizeof(long):  %d bits\n", 8 * (int) sizeof(long));
 
-  printf("sizeof(void *):%d bits\n", 8 * (int) sizeof(void *));
+	printf("sizeof(void *):%d bits\n", 8 * (int) sizeof(void *));
 
-  printf("sizeof(long long): %d bits\n", 8 * (int) sizeof(long long));
+	printf("sizeof(long long): %d bits\n", 8 * (int) sizeof(long long));
 
-  printf("=> Standard types from stdint.h :\n");
+	printf("=> Standard types from stdint.h :\n");
 
-  printf("sizeof(uint8_t): %d bits\n", 8 * (int) sizeof(uint8_t));
+	printf("sizeof(uint8_t): %d bits\n", 8 * (int) sizeof(uint8_t));
 
-  printf("sizeof(int16_t): %d bits\n", 8 * (int) sizeof(int16_t));
+	printf("sizeof(int16_t): %d bits\n", 8 * (int) sizeof(int16_t));
 
-  printf("sizeof(int32_t): %d bits\n", 8 * (int) sizeof(int32_t));
+	printf("sizeof(int32_t): %d bits\n", 8 * (int) sizeof(int32_t));
 
-  printf("sizeof(int64_t): %d bits\n", 8 * (int) sizeof(int64_t));
+	printf("sizeof(int64_t): %d bits\n", 8 * (int) sizeof(int64_t));
+
+	int a[15];
+
+	printf("=> sizeof(a) = %lu\n", sizeof(a));
+
+	printf("=> sizeof(a)/sizeof(int) = %lu\n", sizeof(a)/sizeof(int));
+
+	printf("=> len_int = %d\n", len_int(a));
 
 //  printf("Hit enter to exit.\n");
 //  char *scannedText;
 //  scanf("%s", scannedText);
 
-  return 0;
+	return 0;
 
 }
