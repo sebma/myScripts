9e4c6534c2173d1b8a817feb4c9a4d08ad60cc2c
sebma <sebma@users.noreply.github.com>
2017-09-21 22:21:57 +0200

    Updated shl/brewInstall.sh

--- shl/brewInstall.sh
+++ shl/brewInstall.sh
@@ -3,7 +3,9 @@
 case $(uname) in 
 	Darwin)
 		command -v brew >/dev/null || $(which ruby) -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-		set -x;$(which brew) tap caskroom/cask;set +x ;;
+		set -x
+		$(which brew) tap caskroom/cask
+		$(which brew) tap caskroom/versions;set +x ;;
 	Linux)
 		command -v brew >/dev/null || $(which ruby) -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)" ;;
 	*) echo "=> ERROR : brew does not support $(uname)." >&2; exit 1;;
