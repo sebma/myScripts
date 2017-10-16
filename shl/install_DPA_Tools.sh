b80bcea35902c2fde497c14ddca788873dffe5b0
sebma <sebma@users.noreply.github.com>
2017-10-16 20:45:13 +0200

    Updated shl/install_DPA_Tools.sh

--- shl/install_DPA_Tools.sh
+++ shl/install_DPA_Tools.sh
@@ -1,1 +1,17 @@
-/Users/mansfeld/projects/AES_Correlation_Analysis_Side_Channel_Attack/src/shl/install_DPA_Tools.sh
+#!/usr/bin/env bash
+
+brew=$(which brew)
+if [ $(uname -s) = Darwin ]
+then
+	set -x
+	$brew update
+	$brew install watch remake cgdb
+	$brew tap caskroom/cask
+	$brew tap caskroom/drivers
+	$brew cask install https://raw.githubusercontent.com/caskroom/homebrew-cask/84b7491d6a2c7124fd54ac177cb55f61192018bf/Casks/mono-mdk.rb #Installation de mono-mdk v5.0.1.1
+	$brew cask install picoscope arduino
+	gem install iStats
+elif [ $(uname -s) = Linux ]
+then
+	echo "=> TO BE DONE."
+fi
