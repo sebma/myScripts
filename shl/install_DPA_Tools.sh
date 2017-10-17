4a6f1ce817118523d8dba2682d568a1b6f576e12
sebma <sebma@users.noreply.github.com>
2017-10-17 14:26:15 +0200

    Updated shl/install_DPA_Tools.sh

--- shl/install_DPA_Tools.sh
+++ shl/install_DPA_Tools.sh
@@ -1,16 +1,28 @@
 #!/usr/bin/env bash
 
 brew=$(which brew)
+brewCask="$(which brew) cask"
 if [ $(uname -s) = Darwin ]
 then
-	set -x
 	$brew update
-	$brew install watch remake cgdb
+	formulas="watch remake cgdb"
+	for formula in $formulas
+	do
+#		set -x
+		$brew list | grep -wq $formula || $brew install $formula
+		set +x
+	done
 	$brew tap caskroom/cask
 	$brew tap caskroom/drivers
-	$brew cask install https://raw.githubusercontent.com/caskroom/homebrew-cask/84b7491d6a2c7124fd54ac177cb55f61192018bf/Casks/mono-mdk.rb #Installation de mono-mdk v5.0.1.1
-	$brew cask install picoscope arduino
-	gem install iStats
+	$brewCask list | grep -wq mono-mdk || $brewCask install https://raw.githubusercontent.com/caskroom/homebrew-cask/84b7491d6a2c7124fd54ac177cb55f61192018bf/Casks/mono-mdk.rb #Installation de mono-mdk v5.0.1.1
+	casks="picoscope arduino"
+	for cask in $casks
+	do
+#		set -x
+		$brewCask list | grep -wq $cask || $brewCask install $cask
+		set +x
+	done
+	gem list --local | grep -wq iStats || gem install iStats
 elif [ $(uname -s) = Linux ]
 then
 	echo "=> TO BE DONE."
