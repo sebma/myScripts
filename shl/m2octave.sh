07958eead2354db10fed94dd2f5dd5fe178e3f5c
sebma <sebma@users.noreply.github.com>
2017-10-20 01:08:35 +0200

    Updated shl/m2octave.sh

--- shl/m2octave.sh
+++ shl/m2octave.sh
@@ -3,6 +3,13 @@
 for matlabScript
 do
 	octaveScript=${matlabScript/.m/.octave}
+	if test -s $octaveScript
+	then
+		test -w $octaveScript || {
+			echo "=> $0 ERROR : <$octaveScript> is readonly." >&2
+			continue
+		}
+	fi
 	echo "#!/usr/bin/env octave-cli" > $octaveScript
 	egrep -vw "^main|/usr/bin/.*methlabs" $matlabScript >> $octaveScript
 	mainFunctionName=$(awk -F "[ (=]" '/function.(\w+ =)?\w+/{print$(NF-3);exit}' $octaveScript)
