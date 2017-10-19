10d74590ba4c84b96b730376233c1ddd6c69d6cf
sebma <sebma@users.noreply.github.com>
2017-10-19 14:14:14 +0200

    Updated m/show3DSurf.m

--- m/show3DSurf.m
+++ m/show3DSurf.m
@@ -1,12 +1,14 @@
 %#!/usr/bin/env methlabs.py
 
 function show3DSurf( varargin )
-	matFile = varargin{1};
+	matFile = varargin{1}
 	matFileStructure = load(matFile);
-	matFileFieldNames = fieldnames(matFileStructure);
+	matFileFieldNames = fieldnames(matFileStructure)
 	X = matFileStructure.(matFileFieldNames{1});
 	Y = matFileStructure.(matFileFieldNames{2});
 	Z = matFileStructure.(matFileFieldNames{3});
 	whos
 	surf(X,Y,Z);
+	pause
+	return
 end
