ae85e8eed8d956fe830305c0bc8d95fdce41d919
sebma <sebma@users.noreply.github.com>
2017-10-20 18:05:30 +0200

    Updated shl/seb_Acquire.sh

--- shl/seb_Acquire.sh
+++ shl/seb_Acquire.sh
@@ -10,10 +10,8 @@ then
 			#Read [System Integrity Protection Guide: Runtime Protections](https://developer.apple.com/library/content/documentation/Security/Conceptual/System_Integrity_Protection_Guide/RuntimeProtections/RuntimeProtections.html)
 			test -d /Applications/PicoScope6.app/Contents/Resources/lib && export DYLD_LIBRARY_PATH=/Applications/PicoScope6.app/Contents/Resources/lib
 		fi
-	elif [ $(uname -s) = Linux ]
-	then
-		:
 	fi
+	test [ $(uname -s) = Linux ] && :
 	
 	$(which python2) ${0/.sh/.py} $@
 else
@@ -26,10 +24,10 @@ else
 			echo "=> INFO: Installing the picoscope Python module because it's not installed ..." >&2
 			if groups | egrep -wq "sudo|adm|admin"
 			then
-		#		sudo -H $(which·$pipCmd)·install·picoscope==0.6.2
+		#		sudo -H $(which $pipCmd) install picoscope==0.6.2
 				sudo -H $(which $pipCmd) install git+https://github.com/colinoflynn/pico-python.git@0.6.2
 			else
-		#		$(which·$pipCmd)·install·picoscope==0.6.2 --user
+		#		$(which $pipCmd) install picoscope==0.6.2 --user
 				$(which $pipCmd) install git+https://github.com/colinoflynn/pico-python.git@0.6.2 --user
 			fi
 			echo "=> INFO: Please re-run $0 $@ after that installation." >&2
