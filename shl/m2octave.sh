be06cb4aae53c589d1ba10aeef0f9c3329c9ad51
sebma <sebma@users.noreply.github.com>
2017-10-20 15:58:44 +0200

    Updated shl/m2octave.sh

--- shl/m2octave.sh
+++ shl/m2octave.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-octaveShebang='#!'"$(which octave-cli)"
+[ $(uname -s) = Darwin ] && octaveShebang='#!'"$(which env) octave-cli -q"
+[ $(uname -s) = Linux ]  && octaveShebang='#!'"$(which octave-cli)"
+
 matlab2D_3D_PlottingFunctions="animatedline|area|bar|bar3|bar3h|barh|comet|comet3|compass|coneplot|contour|contour3|contourf|contourslice|errorbar|ezpolar|fcontour|feather|fimplicit|fimplicit3|fmesh|fplot|fplot3|fsurf|heatmap|histogram|histogram2|image|imagesc|loglog|mesh|meshc|meshz|pareto|pcolor|pie|pie3|plot|plot3|plotmatrix|polarhistogram|polarplot|polarscatter|quiver|quiver3|ribbon|scatter|scatter3|semilogx|semilogy|slice|spy|stairs|stem|stem3|streamline|streamparticles|streamribbon|streamslice|streamtube|surf|surfc|surfl|waterfall"
 
 for matlabScript
