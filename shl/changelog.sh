795c26eed566f586382690165e6d4c01ba125cdd
sebma <sebma@users.noreply.github.com>
2018-01-29 22:13:59 +0100

    Updated shl/changelog.sh

--- shl/changelog.sh
+++ shl/changelog.sh
@@ -8,7 +8,7 @@ then
 else
 	ppa_user_name=$(apt-cache policy $package_name | grep -m1 500.http | awk -F/ '{print$4}')
 	ppa_name=$(apt-cache policy $package_name | grep -m1 500.http | awk -F/ '{print$5}' | awk '{print$NF}')
-	package_version=$(apt-cache policy $package_name | awk -F"[ :]" '/Candidate:/{print$NF}')
+	package_version=$(LANG=C apt-cache policy $package_name | awk -F"[ :]" '/Candidate:/{print$NF}')
 	URL=https://launchpad.net/~$ppa_user_name/+archive/ubuntu/$ppa_name/+files/${package_name}_${package_version}_source.changes
 	#echo "=> URL = $URL"
 	grep -q launchpad.net <<< "$URL" || { echo "=> ERROR: The package is not installed from <launchpad.net>" >&2 ; exit 1; }
