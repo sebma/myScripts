5d2264919641b10d8a719031bcef034a07a1153d
sebma <sebma@users.noreply.github.com>
2018-03-13 11:03:52 +0100

    Updated shl/checkModulePython3.sh

---
+++ shl/checkModulePython3.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+for module
+do
+		if ! python3 -c "import $module" 2>&1 | grep ModuleNotFoundError
+		then
+				printf "=> The module $module\tv%s is installed.\n" $(python3 -c "import $module;print($module.__version__)")
+		fi
+done
