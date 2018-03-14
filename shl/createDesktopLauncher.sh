70cdb8cc04027238f5b2ec224b841c5584b11d11
sebma <sebma@users.noreply.github.com>
2018-03-14 19:41:15 +0100

    Updated shl/createDesktopLauncher.sh

--- shl/createDesktopLauncher.sh
+++ shl/createDesktopLauncher.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
+[ $# = 0 ] && {
+	echo  "=> Usage: $0 programName"
+	exit 1
+}
+
 FORM=$(zenity --forms \ --title="Simple shortcut maker" --text="Create new .desktop file" \
         --add-entry="Program Name" \
         --add-entry="Command or path to file" \
-        --add-entry="Terminal app(true/false)" \
+        --add-entry="Terminal app (true/false)" \
         --add-entry="Icon (path)")
 
-[ $? = 0 ] || exit 1
+[ $? = 0 ] || exit 2
 
-awk -F'|' -v home="$HOME" '{
-    FILE = home"/Desktop/"$1".desktop"
+awk -F'|' -v home="$HOME" -v programName="$1" 'BEGIN {
+    FILE = home"/.local/share/applications/"programName".desktop"
+}{
         print "[Desktop Entry]" >> FILE
         print "Type=Application" >> FILE
         print "Name="$1 >> FILE
@@ -18,5 +24,4 @@ awk -F'|' -v home="$HOME" '{
         if ($4 !~ /^[ ]*$/)
             print "Icon="$4 >> FILE ;
     system("chmod 755 " FILE);
-
-}' <<< "$FORM"
+}' <<< "$FORM" && ls -l ~/.local/share/applications/$1.desktop
