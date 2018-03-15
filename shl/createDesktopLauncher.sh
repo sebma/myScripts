ef1011de4b3b86ad281065feedea0251597f84eb
sebma <sebma@users.noreply.github.com>
2018-03-15 16:28:12 +0100

    Updated shl/createDesktopLauncher.sh

--- shl/createDesktopLauncher.sh
+++ shl/createDesktopLauncher.sh
@@ -1,27 +1,31 @@
 #!/usr/bin/env bash
 
-[ $# = 0 ] && {
-	echo  "=> Usage: $0 programName"
-	exit 1
-}
-
-FORM=$(zenity --forms \ --title="Simple shortcut maker" --text="Create new .desktop file" \
-        --add-entry="Program Name" \
-        --add-entry="Command or path to file" \
-        --add-entry="Terminal app (true/false)" \
-        --add-entry="Icon (path)")
-
-[ $? = 0 ] || exit 2
-
-awk -F'|' -v home="$HOME" -v programName="$1" 'BEGIN {
-    FILE = home"/.local/share/applications/"programName".desktop"
-}{
-        print "[Desktop Entry]" >> FILE
-        print "Type=Application" >> FILE
-        print "Name="$1 >> FILE
-        print "Exec="$2 >> FILE
-        print "Terminal="$3 >> FILE
-        if ($4 !~ /^[ ]*$/)
-            print "Icon="$4 >> FILE ;
-    system("chmod 755 " FILE);
-}' <<< "$FORM" && ls -l ~/.local/share/applications/$1.desktop
+os=$(uname -s)
+if [ $os = Linux ]
+then
+	[ $# = 0 ] && {
+		echo  "=> Usage: $0 programName"
+		exit 1
+	}
+	
+	programName="$1"
+	appName="$(echo $programName | sed 's/^./\U&/')"
+	destopFilePath=~/.local/share/applications/$1.desktop
+	FORM=$(zenity --forms --title="Simple shortcut maker" --text="Create new .desktop file" \
+	        --add-entry="Program Name" \
+	        --add-entry="Command or path to file" \
+		--add-combo="Terminal app (true/false)" --combo-values "false|true" \
+	        --add-entry="Icon (path)")
+	
+	[ $? = 0 ] || exit 2
+	
+	awk -F'|' '{
+		print "#!/usr/bin/env xdg-open"
+	        print "[Desktop Entry]"
+	        print "Type=Application"
+	        print "Name="$1
+	        print "Exec="$2
+	        print "Terminal="$3
+	        if ($4 !~ /^[ ]*$/) print "Icon="$4
+	}' <<< "$FORM" > $destopFilePath && chmod -v +x $destopFilePath && ls -l $destopFilePath
+fi
