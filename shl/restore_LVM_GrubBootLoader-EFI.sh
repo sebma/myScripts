ad2243e0908b7c1b6b6871ea2ee5c8d4566f6d14
sebma <sebma@users.noreply.github.com>
2018-03-16 18:59:15 +0100

    Updated shl/restore_LVM_GrubBootLoader-EFI.sh

--- shl/restore_LVM_GrubBootLoader-EFI.sh
+++ shl/restore_LVM_GrubBootLoader-EFI.sh
@@ -1,8 +1,15 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-type sudo >/dev/null 2>&1 && sudo=$(which sudo) || sudo=""
+type sudo >/dev/null 2>&1 && [ $(id -u) != 0 ] && groups | egrep -wq "sudo|adm|admin|root" && sudo=$(which sudo) || sudo=""
 #set -o nounset
 set -o errexit
+
+[ $# != 1 ] && {
+	echo "=> ERROR : $0 diskDeviceName" >&2
+	exit 1
+}
+
+diskDeviceName=$1
 
 $sudo lvs | grep -q root || {
 	$sudo pvscan
@@ -15,18 +22,18 @@ $sudo lvs | grep -q root || {
 }
 rootFSDevice=$($sudo lvs | awk '/root/{print$2"-"$1}')
 #mount -t proc /proc /mnt/proc # Pour que Grub2 trouve /proc/mounts
-$sudo mkdir -p /mnt/{dev/pts,proc,sys}
+$sudo mkdir -p /mnt/dev/pts /mnt/proc /mnt/sys
 mount | grep -q $rootFSDevice || $sudo mount /dev/mapper/$rootFSDevice /mnt          # montage de celle-ci en remplacant le X par le bon numero de partition
 for i in dev dev/pts proc sys ; do $sudo mount --bind /$i /mnt/$i ; done
 $sudo chroot /mnt /bin/bash <<-EOF # mise a la racine du disque monte
 	mount -av                      # montage des partitions dans le chroot
 	dpkg -S x86_64-efi/modinfo.sh
-#	apt install grub-efi-amd64-bin -y
+	host archive.ubuntu.com >/dev/null && apt install -V -y grub-efi-amd64-bin grub-efi-amd64-signed linux-image-generic linux-signed-image-generic
 	test -s /boot/grub/grub.cfg || update-grub # creation d'un nouveau fichier de configuration : grub.cfg
 	set -x
-	grub-install --target=x86_64-efi --efi-directory=/boot/efi /dev/sda || grub-install --force --target=x86_64-efi --efi-directory=/boot/efi /dev/sda        # installation de grub
+	grub-install --target=x86_64-efi --efi-directory=/boot/efi $diskDeviceName || grub-install --force --target=x86_64-efi --efi-directory=/boot/efi $diskDeviceName        # installation de grub
 	set +x
 	sync
 	umount -av
 EOF
-$sudo umount -v /mnt/{sys,proc,dev/pts,dev,}
+$sudo umount -v /mnt/sys /mnt/proc /mnt/dev/pts /mnt/dev
