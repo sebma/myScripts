17a0a804d3b9c809f17f646ce40de718b3ebf7b3
sebma <sebma@users.noreply.github.com>
2018-03-22 15:08:30 +0100

    Updated shl/hddInfo.sh

--- shl/hddInfo.sh
+++ shl/hddInfo.sh
@@ -4,8 +4,20 @@
 set -o nounset
 type smartctl >/dev/null || exit
 type sudo >/dev/null 2>&1 && sudo=$(which sudo) || sudo=""
+diskDevice=""
+os=$(uname -s)
 
-test $# = 0 && diskDevice=sda || diskDevice=$1
+if [ $# = 0 ] 
+then
+	[ $os = Linux  ] && diskDevice=sda
+	[ $os = Darwin ] && diskDevice=disk0
+else
+	[ "$1" = "-h" ] && {
+		echo "=> Usage: $0 [disk device name]" >&2
+		exit 1
+	} || diskDevice=$1
+fi
+
 if ! echo $diskDevice | grep -q /dev/; then 
 	diskDevice=/dev/$diskDevice
 fi
@@ -22,7 +34,7 @@ fi
 
 diskModel=$($sudo smartctl -i $diskDevice | awk '/Model:/{print$NF}')
 test -z $diskModel && exit
-diskFamily="$($sudo smartctl -i $diskDevice |  awk '/Family:/{for(i=3;i<NF;++i)printf $i" ";print$i}')"
+diskFamily="$($sudo smartctl -i $diskDevice |  awk '/Family:/{gsub("/","_");for(i=3;i<NF;++i)printf $i"_";print$i}')"
 logDir=$HOME/log
 logFile=$logDir/$(echo smartctl_$diskFamily $diskModel | sed "s/[ .]/_/g").log
 mkdir -p $logDir
@@ -30,8 +42,7 @@ mkdir -p $logDir
 {
 	echo "=> Enabling SMART on disk $diskFamily $diskModel on $diskDevice :"
 	$sudo smartctl --smart=on --offlineauto=on --saveauto=on $diskDevice
-	echo "=> hdparm info for disk $diskFamily $diskModel on $diskDevice :"
-	$sudo hdparm -I $diskDevice
+	which hddparm >/dev/null 2>&1 && echo "=> hdparm info for disk $diskFamily $diskModel on $diskDevice :" && $sudo hdparm -I $diskDevice
 	echo "=> Printing all SMART and non-SMART information about the disk $diskFamily $diskModel on $diskDevice :"
 	$sudo smartctl $allInformation $diskDevice
 	#echo "=> Running a short self test for one minute for disk $diskFamily $diskModel on $diskDevice :"
@@ -42,8 +53,7 @@ mkdir -p $logDir
 	$sudo smartctl -q errorsonly -H -l selftest $diskDevice
 	echo "=> Disk temperature using smartctl :"
 	$sudo smartctl $allInformation $diskDevice | grep Temperature | head -5
-	echo "=> Disk temperature using hddtemp :"
-	$sudo hddtemp $diskDevice
+	which hddtemp >/dev/null 2>&1 && echo "=> Disk temperature using hddtemp :" && $sudo hddtemp $diskDevice
 } 2>&1 | tee $logFile
 
 echo
