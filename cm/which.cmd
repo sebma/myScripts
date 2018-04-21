88fb1a8a3788cb02705783354d946ef9022ff25a
sebma <sebma@users.noreply.github.com>
2018-04-21 19:49:28 +0200

    Updated cm/which.cmd

--- cm/which.cmd
+++ cm/which.cmd
@@ -1,42 +1,29 @@
 @echo off
 
-set command=%~n1
+set command=%1
 if not defined command (
   echo =^> Usage: %0 ^<command to find in path^>
   exit/b -1
 )
 
-::setlocal enabledelayedexpansion
-
-REM call :simple_which %*
-call :which %~n1 %~n2 %~n3 %~n4 %~n5 %~n6 %~n7 %~n8 %~n9
+call :which %*
+echo Suite et fin.
 
 endlocal
 
 exit/b %errorlevel%
-goto :eof
 
 :which
-	echo off
-	set extensionList=exe com dll cpl cmd bat msc pl py rb vbs
-	setlocal enabledelayedexpansion
-	set not_found=1
-	for %%a in (%*) do (
-		for %%e in (%extensionList%) do (
-			for %%c in (%%a.%%e) do (
-				set command=%%c
-				echo.%%~$PATH:c | findstr -i !command! && (
-					set not_found=0
-				)
-			)
-		)
-	)
-
-	exit/b !not_found!
-
-	goto :eof
-
-:simple_which
-	set extensionList=exe com dll cpl cmd bat msc pl py rb vbs
-	for %%a in (%*) do @for %%e in (%extensionList%) do @for %%c in (%%a.%%e) do @echo.%%~$PATH:c | findstr -i %%c
-	goto :eof
+setlocal enabledelayedexpansion
+for %%a in (%*) do (
+  set command=%%a
+  set not_found=1
+  for %%p in ("%path:;=" "%") do @(
+    set curr_path=%%p
+    set new_path=!curr_path:"=!\!command!
+    if exist !new_path! echo !new_path! && set not_found=0
+  )
+  if !not_found!==1 echo =^> ERROR: The command ^< !command! ^> was not_found not in the path. >&2
+)
+exit/b !not_found!
+goto :eof
