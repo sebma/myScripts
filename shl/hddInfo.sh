be96f129ef436f0fdc0831640453f0426761e740
sebma <sebma@users.noreply.github.com>
2018-04-21 22:50:19 +0200

    Updated shl/hddInfo.sh

--- shl/hddInfo.sh
+++ shl/hddInfo.sh
@@ -40,22 +40,36 @@ logFile=$logDir/$(echo smartctl_$diskFamily $diskModel | sed "s/[ .]/_/g").log
 mkdir -p $logDir
 
 {
-	echo "=> Enabling SMART on disk $diskFamily $diskModel on $diskDevice :"
-	$sudo smartctl --smart=on --offlineauto=on --saveauto=on $diskDevice
-	which hddparm >/dev/null 2>&1 && echo "=> hdparm info for disk $diskFamily $diskModel on $diskDevice :" && $sudo hdparm -I $diskDevice
-#	echo "=> Printing all SMART and non-SMART information about the disk $diskFamily $diskModel on $diskDevice :"
+	echo "=> Disk general info for $diskFamily model $diskModel on $diskDevice :"
+	echo
+	$sudo smartctl -i $diskDevice
+	echo "=> Enabling SMART on disk $diskFamily model $diskModel on $diskDevice ..."
+	echo
+	$sudo smartctl --smart=on --offlineauto=on --saveauto=on $diskDevice >/dev/null
+	which hddparm >/dev/null 2>&1 && echo "=> hdparm info for disk $diskFamily model $diskModel on $diskDevice :" && $sudo hdparm -I $diskDevice
+#	echo "=> Printing all SMART and non-SMART information about the disk $diskFamily model $diskModel on $diskDevice :"
+#	echo
 #	$sudo smartctl $allInformation $diskDevice
-	#echo "=> Running a short self test for one minute for disk $diskFamily $diskModel on $diskDevice :"
-	#$sudo smartctl -t short $diskDevice && sleep 60
-	echo "=> Disk tests results for disk $diskFamily $diskModel on $diskDevice :"
+#	echo "=> Running a short self test for one minute for disk $diskFamily model $diskModel on $diskDevice :"
+#	echo
+#	$sudo smartctl -t short $diskDevice && sleep 60
+	echo "=> Disk Self-Test results for $diskFamily model $diskModel on $diskDevice :"
+	echo
 	$sudo smartctl -H -l selftest $diskDevice
-	echo "=> Disk Errors for disk $diskFamily $diskModel on $diskDevice for the SMART Self-Test Log :"
+	echo "=> Disk Errors for disk $diskFamily model $diskModel on $diskDevice for the SMART Self-Test Log :"
+	echo
 	$sudo smartctl -q errorsonly -H -l selftest $diskDevice
-	echo "=> Disk Errors for disk $diskFamily $diskModel on $diskDevice for the SMART Error Log :"
+	echo "=> Disk Errors for disk $diskFamily model $diskModel on $diskDevice for the SMART Error Log :"
+	echo
 	$sudo smartctl -q errorsonly -H -l error $diskDevice
+	echo "=> SMART Attributes Data for $diskFamily model $diskModel on $diskDevice :"
+	echo
+	$sudo smartctl -A $diskDevice
 	echo "=> Disk temperature using smartctl :"
+	echo
 	$sudo smartctl $allInformation $diskDevice | grep Temperature | head -5
-	which hddtemp >/dev/null 2>&1 && echo "=> Disk temperature using hddtemp :" && $sudo hddtemp $diskDevice
+	echo
+	which hddtemp >/dev/null 2>&1 && echo "=> Disk temperature using hddtemp :" && echo && $sudo hddtemp $diskDevice
 } 2>&1 | tee $logFile
 
 echo
