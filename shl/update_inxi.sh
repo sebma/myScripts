bb7c92f128bf58ba0eddaeb55701e37c577e888c
sebma <sebma@users.noreply.github.com>
2018-04-21 14:10:22 +0200

    Updated shl/update_inxi.sh

--- shl/update_inxi.sh
+++ shl/update_inxi.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 LANG=C wget --no-check-certificate --content-disposition --no-continue -N -P ~/.local/share/man/man1/ https://raw.githubusercontent.com/smxi/inxi/master/inxi.1
-#LANG=C wget --no-check-certificate --content-disposition --no-continue -N -P ../py/not_mine/ https://raw.githubusercontent.com/smxi/inxi/master/inxi
-LANG=C curl -q --no-progress-bar -C - -R -z ../py/not_mine/inxi -o ../py/not_mine/inxi https://raw.githubusercontent.com/smxi/inxi/master/inxi
+#LANG=C wget --no-check-certificate --content-disposition --no-continue -N -P ../pl/not_mine/ https://raw.githubusercontent.com/smxi/inxi/master/inxi
+LANG=C curl -q --no-progress-bar -C - -R -z ../pl/not_mine/inxi -o ../pl/not_mine/inxi https://raw.githubusercontent.com/smxi/inxi/master/inxi
