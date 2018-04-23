b1fe85547772dbb807d7370e0f70ee86d809aab2
sebma <sebma@users.noreply.github.com>
2018-04-23 20:57:42 +0200

    Updated shl/user_exits.sh

---
+++ shl/user_exits.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+test $1 && userList=$@ || userList=$USER
+
+serverList="pingoin01 pingoin02"
+for server in $serverList
+do
+	echo "=> server = $server"
+	netcat -v -z -w 5 $server 22 2>&1 | egrep -v "succeeded|open" || ssh -q $server "groups $userList"
+done
