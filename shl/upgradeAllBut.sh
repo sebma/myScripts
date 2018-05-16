a400fafc22068aa6cafac30eb2692e88f888802c
sebma <sebma@users.noreply.github.com>
2018-05-16 14:42:38 +0200

    Updated upgradeAllBut.sh

---
+++ shl/upgradeAllBut.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+function upgradeAllBut {
+	local packagesNotUpgraded="$@"
+	packagesNotUpgraded=$(echo $packagesNotUpgraded | tr ' ' '|')
+#	packagesToBeUpgraded=$($(which apt) list --upgradable 2>/dev/null | cut -d/ -f1 | egrep -v "$packagesNotUpgraded" | grep -v "Listing...")
+	packagesToBeUpgraded=$($(which apt-get) upgrade --dry-run | awk '/^Inst/{print$2}' | egrep -v "$packagesNotUpgraded" | grep -v "Listing...")
+	echo "=> packagesToBeUpgraded = <$packagesToBeUpgraded>"
+	set -x
+	sudo apt install -v $packagesToBeUpgraded
+	set +x
+}
+
+function main {
+	local os=$(uname -s)
+	if [ $os = Linux ]
+	then
+		if [ -e /etc/debian_version ]; then	
+			upgradeAllBut "$@"
+		else
+			echo "=> The distribution $(\lsb_release -si) is not supported." >&2
+			return 1
+		fi
+	else
+		echo "=> $os is not supported." >&2
+		return 2
+	fi
+}
+
+main "$@"
