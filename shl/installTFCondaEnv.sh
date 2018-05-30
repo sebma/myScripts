e97501f80f4721ec7a8abc8234345d8151469fe5
sebma <sebma@users.noreply.github.com>
2018-05-30 19:41:51 +0200

    Updated shl/installTFCondaEnv.sh

--- shl/installTFCondaEnv.sh
+++ shl/installTFCondaEnv.sh
@@ -37,7 +37,6 @@ function installTFCondaEnv {
 
 		echo $PATH | grep -q $CONDA_HOME || echo 'export PATH=$CONDA_HOME/bin${PATH:+:${PATH}}' >> $shellInitFileName
 		conda=$(which conda)
-		pip=$(which pip)
 		conda list | grep -q argcomplete || $sudo $conda install argcomplete
 
 		echo
@@ -62,8 +61,11 @@ function installTFCondaEnv {
 
 		conda list -n $tensorFlowEnvName | grep gpustat || {
 			set -x
-			$sudo $pip install --prefix $CONDA_ENVS/$tensorFlowEnvName gpustat
-			sudo sed -i '1s|#!.*python|#!'"$CONDA_ENVS/$tensorFlowEnvName/bin/python|" $CONDA_ENVS/$tensorFlowEnvName/bin/gpustat
+#			pip=$(which pip)
+#			$sudo $pip install --prefix $CONDA_ENVS/$tensorFlowEnvName gpustat
+#			$sudo sed -i '1s|#!.*python|#!'"$CONDA_ENVS/$tensorFlowEnvName/bin/python|" $CONDA_ENVS/$tensorFlowEnvName/bin/gpustat
+			pip=$($conda env list | awk "/$tensorFlowEnvName/"'{print$NF}'/bin/pip)
+			$sudo -H $pip install gpustat livelossplot # TESTER SI CES MODULES SONT INSTALLES AU BON ENDROIT
 			set +x
 		}
 
