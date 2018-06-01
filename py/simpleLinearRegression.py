2b67c0a31bac0607366fdb7d3a742082419d2ce1
sebma <sebma@users.noreply.github.com>
2018-06-01 20:03:24 +0200

    Updated py/simpleLinearRegression.py

--- py/simpleLinearRegression.py
+++ py/simpleLinearRegression.py
@@ -1,50 +1,12 @@
 #!/usr/bin/env python3
 
 orig_keys = set(globals().keys())
+from seb_ML import *
 
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
-from sys import stdout, stderr, exit
-from ipdb import set_trace
-
 import pandas as pda
 import numpy as np
-import matplotlib as mpl
 import matplotlib.pyplot as plt
-
-def isnotebook() :
-	try :
-		shell = get_ipython().__class__.__name__
-		if shell == 'ZMQInteractiveShell':
-			return True   # Jupyter notebook or qtconsole
-		elif shell == 'TerminalInteractiveShell':
-			return False  # Terminal running IPython
-		else:
-			return False  # Other type (?)
-	except NameError :
-		return False      # Probably standard Python interpreter
-
-def setJupyterBackend( newBackend = 'nbAgg' ) : # Set the "notebook" backend as default or other when newBackend is given
-	# If the script is not run by python but by jupyter and is using a different backend then "notebook"
-	#if mpl.get_backend() != 'Qt5Agg' and mpl.get_backend() != 'nbAgg' :
-	#	print("=> BEFORE: matplotlib backend = <%s>" % mpl.get_backend() )
-	#	mpl.use('nbAgg',warn=False, force=True) # <=> %matplotlib notebook
-
-	# If the script is not run by python but by jupyter and is using a different backend then "inline"
-	if mpl.get_backend() != 'Qt5Agg' and mpl.get_backend() != newBackend :
-		print("=> BEFORE: matplotlib backend = <%s>" % mpl.get_backend() )
-		mpl.use( newBackend ,warn=False, force=True ) # <=> %matplotlib inline
-	#	import matplotlib.pyplot as plt
-		import matplotlib.pyplot
-		print("=> AFTER: matplotlib backend = <%s>" % mpl.get_backend() )
-	else :
-		print("=> matplotlib backend = <%s>" % mpl.get_backend() )
-
-def Print(*args, **kwargs) :
-	if not arguments.quiet : print(*args, **kwargs)
-
-def Exit(retCode=0) :
-	if arguments.md : Print("</code></pre>")
-	exit(retCode)
 
 def initArgs() :
 	global arguments, scriptBaseName, parser, __version__
@@ -57,11 +19,13 @@ def initArgs() :
 	parser.add_argument( "-b", "--batch_size", help="batchSize taken from the whole dataSet.", default=-1, type=float )
 	parser.add_argument( "-e", "--epochs", help="Number of epochs to go through the NN.", default=5, type=float )
 	parser.add_argument( "-E", "--EarlyStopping", help="Number of epochs before stopping once your loss starts to increase (disabled by default).", default=-1, type=int )
-	parser.add_argument( "-P", "--PlotMetrics", help="Enables the live ploting of the trained model metrics.", action='store_true', default = False )
+	parser.add_argument( "-P", "--PlotMetrics", help="Enables the live ploting of the trained model metrics in Jupyter NoteBook.", action='store_true', default = False )
+	parser.add_argument( "-D", "--DumpedModelFileName", help="Dump the model image to fileName", default = None )
 	parser.add_argument( "-S", "--Shuffle", help="Shuffle the data along the way.", action='store_true', default = False )
 	parser.add_argument( "-v", "--validation_split", help="Validation split ratio of the whole dataset.", default=0.2, type=float )
 	parser.add_argument( "-a", "--activationFunction", help="NN Layer activation function.", default="linear", choices = ['linear','relu','sigmoid'] )
 	parser.add_argument( "-l", "--lossFunction", help="NN model loss function.", default="mse", choices = ['mse','mae','rmse'] )
+	parser.add_argument( "-k", "--kernel_initializer", help="NN Model kernel initializer.", default="glorot_uniform", choices = ['glorot_uniform','random_normal','random_uniform'] )
 	parser.add_argument( "-o", "--optimizer", help="NN model optimizer algo.", default="sgd", choices = ['sgd', 'rmsprop','adam'] )
 
 	parser.add_argument( "--Lr", help="Set the learning rate of the NN.", default=None, type=float )
@@ -79,6 +43,7 @@ def initArgs() :
 		parser.print_usage()
 		exit()
 
+	q = arguments.quiet
 	if arguments.mdu :
 		Print("<pre><code>")
 		parser.print_usage()
@@ -91,173 +56,166 @@ def initArgs() :
 		Print("</code></pre>")
 		exit()
 
-	if arguments.lossFunction.lower() == 'mse' and arguments.epochs < 10 : arguments.epochs = 15
-	
-	if arguments.batch_size == -1 :
-		if arguments.nbSamples > 1e2 :
-			arguments.batch_size = int(arguments.nbSamples / arguments.epochs)
-		else :
-			arguments.batch_size = arguments.nbSamples
-#			arguments.epochs = int(arguments.nbSamples / 4)
-
 	if arguments.md : Print("<pre><code>")
 
 	return arguments
 
-global arguments
-arguments = initArgs()
+def plotDataAndPrediction(df, lossFunctionName, optimizerName) :
+	plt.figure()
+	plt.clf()
+	
+	#subplot(nrows, ncols, plot_number)
+	#plt.subplot(1,2,1)
+	plt.title('Linear regression with <'+lossFunctionName+'> loss and <'+optimizerName+'> optimizer')
+	plt.scatter( df['X_train'], df['y_train'], label='Line' )
+	plt.plot( df['X_train'], df['y_predicted'], 'r-.', label='Prediction')
+	plt.legend(loc='best')
+	
+	#subplot(nrows, ncols, plot_number)
+	#plt.subplot(1,2,2)
+	
+	plt.show()
 
-import keras
+def initScript() :
+#	global myArgs, arguments, nbSamples, lastSample, epochs, df, lossFunction, optimizer, activation, Lr, dumpedModelFileName, rmse, batch_size, validation_split, shuffle, earlyStoppingPatience, plotMetrics
+	global myArgs, df
+	global optimizerName, lossFunctionName, myMetrics, modelTrainingCallbacks, dataIsNormalized
 
-def Allow_GPU_Memory_Growth() : #cf. https://github.com/keras-team/keras/issues/1538
-	from keras import backend as K
+	arguments = initArgs()
+	Allow_GPU_Memory_Growth()
+	pda.options.display.max_rows = 20 #Prints the first max_rows/2 and the last max_rows/2 of each dataframe
 
-	if 'tensorflow' == K.backend():
-		import tensorflow as tf
-		config = tf.ConfigProto()
-		config.gpu_options.allow_growth = True
-		config.gpu_options.visible_device_list = "0"
-		#session = tf.Session(config=config)
-		from keras.backend.tensorflow_backend import set_session
-		set_session(tf.Session(config=config))
+#	from collections import namedtuple # namedtuples are not mutable
+	from namedlist import namedlist
+	myStruct = namedlist( "myStruct", "nbSamples lastSample epochs lossFunction optimizer activation Lr dumpedModelFileName batch_size validation_split shuffle earlyStoppingPatience plotMetrics kernel_initializer" )
 
-Allow_GPU_Memory_Growth()
+	rmse = RMSE = root_mean_squared_error
 
-from keras.models import Sequential
-from keras.layers import Dense, BatchNormalization, Activation
+	myArgs = myStruct(
+						nbSamples = int(arguments.nbSamples),
+						lastSample = arguments.lastSample,
+						epochs = int(arguments.epochs),
+						lossFunction = arguments.lossFunction.lower(),
+						optimizer = arguments.optimizer.lower(),
+						activation = arguments.activationFunction,
+						Lr = arguments.Lr,
+						dumpedModelFileName = arguments.DumpedModelFileName,
+						batch_size = int(arguments.batch_size),
+						validation_split = arguments.validation_split,
+						shuffle = arguments.Shuffle,
+						earlyStoppingPatience = arguments.EarlyStopping,
+						plotMetrics = arguments.PlotMetrics,
+						kernel_initializer = arguments.kernel_initializer
+					)
 
-nbSamples = int(arguments.nbSamples)
-lastSample = arguments.lastSample
-epochs = int(arguments.epochs)
+	df = pda.DataFrame()
+	df['X_train'] = np.linspace(0, myArgs.lastSample, myArgs.nbSamples)
+	df['y_train'] = -5 * df['X_train'] + 10
 
-pda.options.display.max_rows = 20
-df = pda.DataFrame()
-df['X_train'] = np.linspace(0, lastSample, nbSamples)
-df['y_train'] = -5 * df['X_train'] + 10
+	dataIsNormalized = False
+	if myArgs.lossFunction == 'mse' :
+		# MSE needs NORMALIZATION
+		print( "=> Doing Pandas dataframe normalization ...", file=stderr )
+	#	df['X_train'] = keras.utils.normalize( df.values )[:,0]
+	#	df['y_train'] = keras.utils.normalize( df.values )[:,1]
+		df = ( df-df.mean() ) / df.std()
+		print( "=> DONE.", file=stderr )
+		dataIsNormalized = True
 
-model = Sequential()
-#model.add(Dense(units=1, input_dim=1, activation="relu", kernel_initializer="normal"))
-model.add( Dense( units=1, input_dim=1, activation = arguments.activationFunction ) )
+	optimizerName = myArgs.optimizer
+	lossFunctionName = myArgs.lossFunction.lower()
 
-#M.A.E. = Mean Absolute Error
-lossFunction = arguments.lossFunction
-optimizer = arguments.optimizer
+	if myArgs.lossFunction == 'mse' and myArgs.epochs < 10 : myArgs.epochs = 15
 
-if lossFunction.lower() == 'mse' :
-#	model.add(BatchNormalization()); # MSE needs NORMALIZATION
-	print( "=> Doing Pandas dataframe normalization ..." )
-#	df['X_train'] = keras.utils.normalize( df.values )[:,0]
-#	df['y_train'] = keras.utils.normalize( df.values )[:,1]
-	df = ( df-df.mean() ) / df.std()
-	print( "=> DONE." )
+	if myArgs.batch_size == -1 :
+		if myArgs.nbSamples > 1e2 :
+			myArgs.batch_size = int(myArgs.nbSamples / myArgs.epochs)
+		else :
+			myArgs.batch_size = myArgs.nbSamples
+#			myArgs.epochs = int(myArgs.nbSamples / 4)
 
-optimizer = optimizer.lower()
-Lr = arguments.Lr
-optimizerName = optimizer
-if Lr :
-	if   optimizer == 'sgd' :
-		optimizer = keras.optimizers.sgd(Lr)
-	elif optimizer == 'rmsprop' :
-		optimizer = keras.optimizers.RMSProp(Lr)
-	elif optimizer == 'adam' :
-		optimizer = keras.optimizers.Adam(Lr)
+	import keras.optimizers
+	if myArgs.Lr :
+		if   myArgs.optimizer == 'sgd' :
+			myArgs.optimizer = keras.optimizers.sgd(myArgs.Lr)
+		elif myArgs.optimizer == 'rmsprop' :
+			myArgs.optimizer = keras.optimizers.RMSProp(myArgs.Lr)
+		elif myArgs.optimizer == 'adam' :
+			myArgs.optimizer = keras.optimizers.Adam(myArgs.Lr)
+	
+	myMetrics = []
+	if   myArgs.lossFunction == 'mae' :
+		myMetrics += [ 'mse' ]
+		myMetrics += [ 'rmse' ]
+	elif myArgs.lossFunction == 'mse' :
+		myMetrics += [ rmse ]
+		myMetrics += [ 'mae' ]
+	elif myArgs.lossFunction == 'rmse' :
+		myArgs.lossFunction = rmse
+		myMetrics += [ 'mse' ]
+		myMetrics += [ 'mae' ]
+	#myMetrics += [ 'accuracy' ]
 
-from keras import backend as K, metrics
-def root_mean_squared_error(y_true, y_pred):
-	return K.sqrt(K.mean(K.square(y_pred - y_true), axis=-1))
-rmse = RMSE = root_mean_squared_error
-
-myMetrics = []
-lossFunctionName = lossFunction.lower()
-if   lossFunction.lower() == 'mae' :
-	myMetrics += [ 'mse' ]
-	myMetrics += [ 'rmse' ]
-elif lossFunction.lower() == 'mse' :
-	myMetrics += [ rmse ]
-	myMetrics += [ 'mae' ]
-elif lossFunction.lower() == 'rmse' :
-	lossFunction = rmse
-	myMetrics += [ 'mse' ]
-	myMetrics += [ 'mae' ]
-
-#myMetrics += [ 'accuracy' ]
-
-model.compile(loss=lossFunction, optimizer=optimizer, metrics = myMetrics)
-
-def showModel(model, rankdir) :
-	if isnotebook() :
-		print("=> DISPLAYING MODEL IN SVG :")
-#		%matplotlib notebook
-		from IPython.display import SVG, display
-		from keras.utils.vis_utils import model_to_dot
-		display( SVG( model_to_dot( model, show_shapes=True, show_layer_names=True, rankdir = rankdir ).create( prog='dot', format='svg' ) ) )
+	if myArgs.earlyStoppingPatience == -1 and not myArgs.plotMetrics :
+		modelTrainingCallbacks = None
 	else :
-		modelFileName = 'model.svg'
-		print("=> DUMPING MODEL TO : " + modelFileName)
-		from keras.utils import plot_model
-		plot_model(model, to_file=modelFileName, show_shapes=True, show_layer_names=True, rankdir = rankdir)
+		modelTrainingCallbacks = []
+	
+	if myArgs.earlyStoppingPatience != -1 :
+		from keras.callbacks import EarlyStopping
+		modelTrainingCallbacks += [ EarlyStopping( monitor='val_loss', patience = myArgs.earlyStoppingPatience ) ]
+	#	modelTrainingCallbacks += [ EarlyStopping( monitor='loss', patience = myArgs.earlyStoppingPatience ) ]
+	if isnotebook() and myArgs.plotMetrics : # The metrics can only be plotted in a jupyter notebook
+		from livelossplot import PlotLossesKeras
+		modelTrainingCallbacks += [ PlotLossesKeras() ]
 
-batch_size = int(arguments.batch_size)
-validation_split = arguments.validation_split
+def main() :
+	global df
+	initScript()
 
-if arguments.EarlyStopping == -1 and not arguments.PlotMetrics :
-	callbacks = None
-else :
-	callbacks = []
+	from keras.models import Sequential
+	from keras.layers import Dense
+	import keras.utils, keras.optimizers, keras.initializers
+	
+	# MODEL DEFINITION
+	model = Sequential()
+	model.add( Dense( units=1, input_dim=1, activation = myArgs.activation, kernel_initializer = myArgs.kernel_initializer ) )
 
-if arguments.EarlyStopping != -1 :
-	from keras.callbacks import EarlyStopping
-	callbacks += [ EarlyStopping( monitor='val_loss', patience = arguments.EarlyStopping ) ]
-#	callbacks += [ EarlyStopping( monitor='loss', patience = arguments.EarlyStopping ) ]
-if isnotebook() and arguments.PlotMetrics : # The metrics can only be plotted in a jupyter notebook
-	from livelossplot import PlotLossesKeras
-	callbacks += [ PlotLossesKeras() ]
+	model.compile(loss=myArgs.lossFunction, optimizer=myArgs.optimizer, metrics = myMetrics)
 
-shuffle = arguments.Shuffle
+	print( "\n=> myArgs.nbSamples = %d \t myArgs.batch_size = %d \t myArgs.epochs = %d and myArgs.validation_split = %d %%" % (myArgs.nbSamples,myArgs.batch_size,myArgs.epochs,int(myArgs.validation_split*100)) )
 
-print( "\n=> nbSamples = %d \t batch_size = %d \t epochs = %d and validation_split = %d %%" % (nbSamples,batch_size,epochs,int(validation_split*100)) )
+	if isnotebook() : setJupyterBackend( newBackend = 'module://ipykernel.pylab.backend_inline' )
 
-if isnotebook() : setJupyterBackend( newBackend = 'module://ipykernel.pylab.backend_inline' )
-#mpl.pyplot.ioff()
-history = model.fit( df['X_train'], df['y_train'], batch_size=batch_size, epochs=epochs, validation_split=validation_split, callbacks = callbacks, shuffle = shuffle )
-#mpl.pyplot.ion()
-print( "=> mpl.is_interactive() = %s" % mpl.is_interactive() )
+	#MODEL TRAINING
+	history = model.fit( df['X_train'], df['y_train'], batch_size=myArgs.batch_size, epochs=myArgs.epochs, validation_split=myArgs.validation_split, callbacks = modelTrainingCallbacks, shuffle = myArgs.shuffle )
+	
+#	mpl.pyplot.ion()
+#	print( "=> mpl.is_interactive() = %s" % mpl.is_interactive() )
+#	print( "=> matplotlib backend = <%s>" % mpl.get_backend() )
+	
+	print( "\n=> myArgs.nbSamples = %d \t myArgs.batch_size = %d \t myArgs.epochs = %d and myArgs.validation_split = %d %%" % (myArgs.nbSamples,myArgs.batch_size,myArgs.epochs,int(myArgs.validation_split*100)) )
+	
+	print( "\n=> Loss function = <" +lossFunctionName+">" + " myArgs.optimizer = <"+optimizerName+">" )
+	
+	slope = model.layers[-1].get_weights()[0].item()
+	y_Intercept = model.layers[-1].get_weights()[1].item()
+	if dataIsNormalized :
+		print("\n=> THE DATA WAS NORMALIZED, hence slope=%.2f\ty_Intercept=%.2f\n" % (slope, y_Intercept))
+		df['y_predicted'] = model.predict( df['X_train'] ) # MODEL PREDICTION
+	else :
+		print("\n=> slope=%.2f\ty_Intercept=%.2f\n" % (slope, y_Intercept))
+		df['y_predicted'] = slope*df['X_train'] + y_Intercept
+	
+	showModel(model = model, modelFileName = myArgs.dumpedModelFileName, rankdir = 'TB')
+	
+	if myArgs.Lr : print("\n=> lr = ",myArgs.Lr)
+	
+	plotDataAndPrediction(df, lossFunctionName, optimizerName)
 
-print("=> matplotlib backend = <%s>" % mpl.get_backend() )
+	my_keys = sorted( set( globals().keys() ) - orig_keys )
+	#print(my_keys)
+	Exit(0)
 
-print( "\n=> nbSamples = %d \t batch_size = %d \t epochs = %d and validation_split = %d %%" % (nbSamples,batch_size,epochs,int(validation_split*100)) )
-
-print( "\n=> Loss function = <" +lossFunctionName+">" + " optimizer = <"+optimizerName+">" )
-
-slope = model.layers[-1].get_weights()[0].item()
-y_Intercept = model.layers[-1].get_weights()[1].item()
-
-if lossFunctionName.lower() == 'mae' :
-	print("\n=> slope=%.2f\ty_Intercept=%.2f\n" % (slope, y_Intercept))
-	df['y_predicted'] = slope*df['X_train'] + y_Intercept
-else :
-	print("\n=> slope=%.2f\ty_Intercept=%.2f\n" % (slope, y_Intercept))
-	df['y_predicted'] = model.predict( df['X_train'] )
-
-showModel(model, rankdir = 'TB')
-
-if Lr : print("\n=> lr = ",Lr)
-
-plt.figure()
-plt.clf()
-
-#subplot(nrows, ncols, plot_number)
-#plt.subplot(1,2,1)
-plt.title('Linear regression with <'+lossFunctionName+'> loss and <'+optimizerName+'> optimizer')
-plt.scatter( df['X_train'], df['y_train'], label='Line' )
-plt.plot( df['X_train'], df['y_predicted'], 'r-.', label='Prediction')
-plt.legend(loc='best')
-
-#subplot(nrows, ncols, plot_number)
-#plt.subplot(1,2,2)
-
-plt.show()
-
-my_keys = sorted(set(globals().keys()) - orig_keys)
-#print(my_keys)
-Exit(0)
+if __name__ == '__main__' : # Calls the main function if (and only if) this script is not imported
+	main()
