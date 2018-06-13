1580aaf000c97e15ea312beb3fc6583692b7b130
sebma <sebma@users.noreply.github.com>
2018-06-13 19:51:38 +0200

    Updated py/test_ML_Jupyter_Env.py

--- py/test_ML_Jupyter_Env.py
+++ py/test_ML_Jupyter_Env.py
@@ -6,8 +6,11 @@ from seb_ML import *
 
 import platform, os, datetime, dateutil
 
-print( datetime.datetime.now( tz=dateutil.tz.tzlocal() ).strftime('%d/%m/%Y %H:%M:%S %Z %z') )
-print( "\n=> hostname = " + platform.node() )
-print( "\n=> uname Data = " +  str(platform.uname() ) )
-print( "\n=> pwd = " + os.getcwd() )
-print( "\n=> CONDA_DEFAULT_ENV = "+os.environ['CONDA_DEFAULT_ENV'] )
+print( "\n=> date/time = " + datetime.datetime.now( tz=dateutil.tz.tzlocal() ).strftime('%d/%m/%Y %H:%M:%S %Z %z') )
+print( "\n=> hostname = <%s>" % platform.node() )
+print( "\n=> uname data = <%s>" % str(platform.uname() ) )
+print( "\n=> pwd = <%s>" % os.getcwd() )
+print( "\n=> mpl.get_backend() = <%s>" % mpl.get_backend() )
+
+if   os.environ.get('CONDA_DEFAULT_ENV') :  print( "\n=> CONDA_DEFAULT_ENV = <%s>" % os.environ['CONDA_DEFAULT_ENV'] )
+elif os.environ.get('VIRTUAL_ENV') :		print( "\n=> VIRTUAL_ENV = <%s>" % os.environ['VIRTUAL_ENV'] )
