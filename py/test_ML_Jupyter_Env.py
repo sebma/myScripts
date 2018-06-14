1550093ea997af492bb38b89580ae090bc311e49
sebma <sebma@users.noreply.github.com>
2018-06-14 17:35:00 +0200

    Updated py/test_ML_Jupyter_Env.py

--- py/test_ML_Jupyter_Env.py
+++ py/test_ML_Jupyter_Env.py
@@ -2,15 +2,24 @@
 
 orig_keys = set(globals().keys())
 
-from seb_ML import *
-
 import platform, os, datetime, dateutil
 
-print( "\n=> date/time = " + datetime.datetime.now( tz=dateutil.tz.tzlocal() ).strftime('%d/%m/%Y %H:%M:%S %Z %z') )
 print( "\n=> hostname = <%s>" % platform.node() )
 print( "\n=> uname data = <%s>" % str(platform.uname() ) )
 print( "\n=> pwd = <%s>" % os.getcwd() )
-print( "\n=> mpl.get_backend() = <%s>" % mpl.get_backend() )
+
+try :
+	import pylab # workaround for bug: https://github.com/dateutil/dateutil/issues/770
+except ModuleNotFoundError :
+	from seb_ML import insideCondaEnv
+
+print( "\n=> date/time = " + datetime.datetime.now( tz=dateutil.tz.tzlocal() ).strftime('%d/%m/%Y %H:%M:%S %Z %z') )
 
 if   os.environ.get('CONDA_DEFAULT_ENV') :  print( "\n=> CONDA_DEFAULT_ENV = <%s>" % os.environ['CONDA_DEFAULT_ENV'] )
 elif os.environ.get('VIRTUAL_ENV') :		print( "\n=> VIRTUAL_ENV = <%s>" % os.environ['VIRTUAL_ENV'] )
+
+try :
+	import matplotlib as mpl
+	print( "\n=> mpl.get_backend() = <%s>" % mpl.get_backend() )
+finally :	
+	from seb_ML import insideCondaEnv
