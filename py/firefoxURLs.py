12d5f90d45973a5fd870870ccb29e73d8a97bfd6
sebma <sebma@users.noreply.github.com>
2018-06-14 18:55:29 +0200

    Updated py/firefoxURLs.py

--- py/firefoxURLs.py
+++ py/firefoxURLs.py
@@ -1,8 +1,11 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
-import json
+from __future__ import print_function
+import json, lz4
 import platform, os
+from os.path import exists
 from sys import argv
+from os.path import exists
 
 firefoxProfileName = argv[1]
 HOME = os.environ["HOME"]
@@ -11,10 +14,19 @@ if   platform.system() == 'Darwin' :
 elif platform.system() == 'Linux' :
 	firefoxProfilesDIR = HOME + "/.mozilla/firefox"
 
-f = open( firefoxProfilesDIR + os.sep + firefoxProfileName + os.sep + "sessionstore-backups" + os.sep + "recovery.js", "r")
-jdata = json.loads(f.read())
+firefoxOpenedTabsFile = firefoxProfilesDIR + os.sep + firefoxProfileName + os.sep + "sessionstore-backups" + os.sep + "recovery.js"
+if exists( firefoxOpenedTabsFile ) :
+	f = open( firefoxOpenedTabsFile, "r" )
+	jdata = json.loads(f.read())
+else :
+	firefoxOpenedTabsFile = firefoxProfilesDIR + os.sep + firefoxProfileName + os.sep + "sessionstore-backups" + os.sep + "recovery.jsonlz4"	
+	if exists( firefoxOpenedTabsFile ) :
+		f = open( firefoxOpenedTabsFile, "r" )
+		jdata = json.loads(lz4.block.decompress(f.read()).decode("utf-8"))
+
 f.close()
+
 for win in jdata.get("windows"):
 	for tab in win.get("tabs"):
 		i = tab.get("index") - 1
-		print tab.get("entries")[i].get("url")
+		print( tab.get("entries")[i].get("url") )
