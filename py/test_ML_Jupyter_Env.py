e4c5c2fd7a8256b99b04338fbf0ea78a48c376b1
sebma <sebma@users.noreply.github.com>
2018-06-15 19:14:53 +0200

    Updated py/test_ML_Jupyter_Env.py

--- py/test_ML_Jupyter_Env.py
+++ py/test_ML_Jupyter_Env.py
@@ -4,17 +4,17 @@ orig_keys = set(globals().keys())
 
 import platform, os, datetime, dateutil
 
-print( "\n=> hostname = <%s>" % platform.node() )
-print( "\n=> uname data = <%s>" % str(platform.uname() ) )
-print( "\n=> pwd = <%s>" % os.getcwd() )
+print( "\n=> hostname = <%s>\n" % platform.node() )
+print( "=> uname data = <%s>\n" % str(platform.uname() ) )
+print( "=> pwd = <%s>\n" % os.getcwd() )
 from dateutil import tz # workaround for dateutil bug #770
-print( "\n=> date/time = " + datetime.datetime.now( tz=dateutil.tz.tzlocal() ).strftime('%d/%m/%Y %H:%M:%S %Z %z') )
+print( "=> date/time = " + datetime.datetime.now( tz=dateutil.tz.tzlocal() ).strftime('%d/%m/%Y %H:%M:%S %Z %z') )
 
-if   os.environ.get('CONDA_DEFAULT_ENV') :  print( "\n=> CONDA_DEFAULT_ENV = <%s>" % os.environ['CONDA_DEFAULT_ENV'] )
-elif os.environ.get('VIRTUAL_ENV') :		print( "\n=> VIRTUAL_ENV = <%s>" % os.environ['VIRTUAL_ENV'] )
+if   os.environ.get('CONDA_DEFAULT_ENV') :  print( "\n=> CONDA_DEFAULT_ENV = <%s>\n" % os.environ['CONDA_DEFAULT_ENV'] )
+elif os.environ.get('VIRTUAL_ENV') :		print( "\n=> VIRTUAL_ENV = <%s>\n" % os.environ['VIRTUAL_ENV'] )
 
 try :
 	import matplotlib as mpl
-	print( "\n=> mpl.get_backend() = <%s>" % mpl.get_backend() )
+	print( "=> mpl.get_backend() = <%s>\n" % mpl.get_backend() )
 finally :	
 	from seb_ML import insideCondaEnv
