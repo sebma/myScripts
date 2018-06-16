5859bba4573d893206866bd4681dd67d1fdbfb64
sebma <sebma@users.noreply.github.com>
2018-06-16 17:58:12 +0200

    Updated shl/connect2WifiFromQRCode.sh

--- shl/connect2WifiFromQRCode.sh
+++ shl/connect2WifiFromQRCode.sh
@@ -7,7 +7,22 @@ test "$qrPictureFile" || {
 	exit 1
 }
 
+os=$(uname -s)
 qrdecode="zbarimg -q --raw"
 ssid=$($qrdecode "$qrPictureFile" | awk -F "[:;]" '{printf$3}')
 pass=$($qrdecode "$qrPictureFile" | awk -F "[:;]" '{printf$7}')
-test $ssid && test $pass && nmcli dev wifi connect "$ssid" password "$pass"
+
+if [ -z $ssid ] || [ -z $pass ]
+then
+	echo "=> ERROR: The <ssid> or the <pass> is an empty string." >&2
+	exit 2
+fi
+
+if   [ $os = Linux ]
+then
+	nmcli dev wifi connect "$ssid" password "$pass"
+elif [ $os = Darwin ]
+then
+	interFace=$(networksetup -listallhardwareports | awk '/Wi-Fi/{found=1}/Device/&&found{print$NF;exit}')
+	networksetup -setairportnetwork $interFace "$ssid" "$pass"
+fi
