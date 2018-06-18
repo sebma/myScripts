bc6218854c794b542c71d9052b7b1e1109cb392e
sebma <sebma@users.noreply.github.com>
2018-06-18 20:56:28 +0200

    Updated shl/jupyterNB_ExtensionsInstall.sh

--- shl/jupyterNB_ExtensionsInstall.sh
+++ shl/jupyterNB_ExtensionsInstall.sh
@@ -2,14 +2,16 @@
 
 sudo="sudo -H"
 conda=$(which conda)
-pip=$($conda env list | awk "/$tensorFlowEnvName/"'{print$NF}'/bin/pip)
+#pip=$($conda env list | awk "/$tensorFlowEnvName/"'{print$NF}'/bin/pip)
+pip=$(which pip)
 
 tensorFlowEnvName=$1
 test $tensorFlowEnvName || tensorFlowEnvName=tensorFlow-GPU
-condaForgeModulesList="jupyter ipython ipdb jupyter_contrib_nbextensions jupyter_nbextensions_configurator"
+condaForgeModulesList="jupyter ipython ipdb jupyter_contrib_nbextensions jupyter_nbextensions_configurator jupyter_latex_envs"
 $sudo $conda install -n $tensorFlowEnvName -c conda-forge $condaForgeModulesList
 jupyter-contrib nbextension install --user
 jupyter-nbextension list
 jupyter-nbextension enable toc2/main
 jupyter-nbextension enable equation-numbering/main
 jupyter-nbextension enable latex_envs/latex_envs
+$sudo $pip install git+git://github.com/mkrphys/ipython-tikzmagic.git
