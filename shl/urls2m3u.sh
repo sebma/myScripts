78eb35de23b8e78c95b847adfe10de6cddf317bf
sebma <sebma@users.noreply.github.com>
2018-07-16 22:51:39 +0200

    Updated shl/urls2m3u.sh

--- shl/urls2m3u.sh
+++ shl/urls2m3u.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env sh
 
-multiMediaFormats="\\.(wav|wma|aac|ac3|mp2|mp3|ogg|oga|ogm|m4a|m4b|spx|opus|asf|avi|wmv|mpg|mpeg|mp4|divx|flv|mov|ogv|webm|vob|3gp|mkv|m2t|mts|m2ts|asx|m3u|m3u8|pla|pls|smil|vlc|wpl|xspf)"
-
-mainURL="$1"
+urlsFile="$1"
 
 echo "#EXTM3U"
-hxwls "$mainURL" | egrep $multiMediaFormats | uniq | while read url
+uniq $urlsFile | while read url
 do
 	printf "#EXTINF:-1,"
-	echo $(basename "$url") | sed -r 's/(\.[a-z]{3}).*$/\1/'
-	echo "$url" | sed "/dropbox.com/s/dl=0$/dl=1/"
+#	youtube-dl -e $url
+	\curl -Ls $url | awk -F'"' /og:title/'{print$4}'
+	echo $url
 done
