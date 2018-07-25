299921f674ab7664e2c0ea18fe8fd50bbc55f482
sebma <sebma@users.noreply.github.com>
2018-07-25 20:29:48 +0200

    Updated shl/brewInstall.sh

--- shl/brewInstall.sh
+++ shl/brewInstall.sh
@@ -3,18 +3,27 @@
 brew=$(which brew)
 case $(uname) in 
 	Darwin)
-		test -z $brew && $(which ruby) -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-		brew=$(which brew) || exit 1
+		brewName=Homebrew
+		if ! which brew > /dev/null 2>&1; then
+			if groups | grep --color -E -wq "adm|admin|sudo"; then
+				$(which ruby) -e "$(\curl -fsSL https://raw.githubusercontent.com/$brewName/install/master/install)" ||  return
+				brew=/usr/local/bin/brew
+			else
+				brewPrefix=$HOME/homebrew
+				\mkdir -pv -p $brewPrefix && \curl -L https://github.com/Homebrew/brew/tarball/master | \tar xz --strip 1 -C $brewPrefix || return
+				brew=$brewPrefix/bin/brew
+			fi
+		fi
 		set -x
 		$brew update
-#		$brew tap homebrew/versions # deprecated : See https://docs.brew.sh/Versions.html
-		$brew tap caskroom/versions
 		$brew tap caskroom/cask
 		$brew tap caskroom/drivers
+		$brew tap caskroom/versions
 		$brew tap buo/cask-upgrade
-		set +x ;;
+		set +x
 	Linux)
-		test -z $brew && $(which ruby) -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+		brewName=Linuxbrew
+		test -z $brew && $(which ruby) -e "$(\curl -fsSL https://raw.githubusercontent.com/$brewName/install/master/install)"
 		brew=$(which brew) || exit 1
 		$brew update ;;
 	*) echo "=> ERROR : brew does not support <$(uname)> operating system." >&2; exit 1;;
