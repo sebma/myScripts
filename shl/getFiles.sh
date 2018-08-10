b34fe7bb418ec1fc57567808ca92138f0971a8c7
sebma <sebma@users.noreply.github.com>
2018-08-10 16:36:53 +0200

    Updated shl/getFiles.sh

--- shl/getFiles.sh
+++ shl/getFiles.sh
@@ -5,7 +5,7 @@ test $# = 0 && {
 	exit 1
 }
 
-lastArg="$(eval echo \$$#)"
+lastArg="$(eval echo \${$#})"
 baseUrl="$(echo $lastArg | sed -r "s,(https?|s?ftp)://,," | cut -d/ -f1)"
 #wget --output-file=$0-$(date +%Y%m%d-%H_%M_%S).log --no-verbose --no-parent --continue --timestamping --server-response --random-wait --no-directories --directory-prefix=$baseUrl/ --user-agent=Mozilla --content-disposition --convert-links --page-requisites --recursive --level=1 --reject index.html --accept $@
 set -x
