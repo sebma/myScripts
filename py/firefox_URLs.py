823bbb06b691645e9202d555272a2107849488df
sebma <sebma@users.noreply.github.com>
2018-08-28 16:17:53 +0200

    Updated py/firefox_URLs.py

--- py/firefox_URLs.py
+++ py/firefox_URLs.py
@@ -47,8 +47,7 @@ with open( firefoxOpenedTabsFile, "r" ) as f :
 # Thanks to : https://unix.stackexchange.com/questions/385023/firefox-reading-out-urls-of-opened-tabs-from-the-command-line/389360#389360
 	if "jsonlz4" in firefoxOpenedTabsFile :
 		import lz4.block
-#		jdata = json.loads(lz4.block.decompress(f.read()).decode("utf-8"))
-		jdata = json.loads(lz4.block.decompress(f.read()).decode())
+		jdata = json.loads(lz4.block.decompress(f.read()).decode("utf-8"))
 	else :
 		jdata = json.loads(f.read())
 
