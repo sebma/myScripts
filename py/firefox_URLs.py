a8806f76ac136ff8cb2f7a0eb21dbb10f673375e
sebma <sebma@users.noreply.github.com>
2018-08-28 14:17:02 +0200

    Updated py/firefox_URLs.py

--- py/firefox_URLs.py
+++ py/firefox_URLs.py
@@ -6,24 +6,30 @@ import platform, os
 from os.path import exists, basename
 from sys import argv, stderr
 
-firefoxProfileName = argv[1]
 scriptBaseName = basename( argv[0] )
 HOME = os.environ["HOME"]
 if   platform.system() == 'Darwin' :
 	if 'firefox' in scriptBaseName.lower() :
 		firefoxProfilesDIR = HOME + "/Library/Application Support/Firefox/Profiles"
-	elif 'palemoon' in scriptBaseName.lower() :	
+	elif 'palemoon' in scriptBaseName.lower() :
 		firefoxProfilesDIR = HOME + "/Library/Application Support/Pale Moon/Profiles"
 elif platform.system() == 'Linux' :
 	if 'firefox-esr' in scriptBaseName.lower() :
 		firefoxProfilesDIR = HOME + "/.mozilla/firefox-esr"
 	elif 'palemoon' in scriptBaseName.lower() :
 		firefoxProfilesDIR = HOME + "/.moonchild productions/pale moon"
-	elif 'firefox' in scriptBaseName.lower() :	
+	elif 'firefox' in scriptBaseName.lower() :
 		firefoxProfilesDIR = HOME + "/.mozilla/firefox"
 elif platform.system() == 'Windows' :
 	APPDATA = os.environ["APPDATA"]
 	firefoxProfilesDIR = APPDATA + os.sep + "Mozilla" + os.sep +"Firefox" + os.sep + "Profiles"
+
+argc = len(argv)
+if argc == 1 :
+	print( " ".join( os.listdir( firefoxProfilesDIR ) ) )
+	exit(0)
+else :
+	firefoxProfileName = argv[1]
 
 chosenFirefoxProfileDIR = firefoxProfilesDIR + os.sep + firefoxProfileName
 sessionstoreBackupsDIR  = chosenFirefoxProfileDIR + os.sep + "sessionstore-backups"
