ad39e7cfdca4976fbc2cc5050e5a091207386746
sebma <sebma@users.noreply.github.com>
2018-11-01 23:50:25 +0100

    Updated shl/getRestrictedFilenamesHD.sh

--- shl/getRestrictedFilenamesHD.sh
+++ shl/getRestrictedFilenamesHD.sh
@@ -9,10 +9,10 @@ ytgetRestrictedFilenamesHD ()
     for url in "$@"
     do
         if echo $url | \egrep -vq "www"; then
-            if $youtube_dl -e $youtubeURLPrefix$url > /dev/null 2>&1; then
+            if LANG=C.UTF-8 $youtube_dl -e $youtubeURLPrefix$url > /dev/null 2>&1; then
                 url=$youtubeURLPrefix$url
             else
-                if $youtube_dl -e $dailymotionURLPrefix$url > /dev/null 2>&1; then
+                if LANG=C.UTF-8 $youtube_dl -e $dailymotionURLPrefix$url > /dev/null 2>&1; then
                     url=$dailymotionURLPrefix$url
                 fi
             fi
@@ -26,7 +26,7 @@ ytgetRestrictedFilenamesHD ()
         fi
         echo "=> url = $url"
         echo
-        fileName=$($youtube_dl -f $format --get-filename "$url" --restrict-filenames || $youtube_dl --get-filename "$url" --restrict-filenames)
+        fileName=$(LANG=C.UTF-8 $youtube_dl -f $format --get-filename "$url" --restrict-filenames || LANG=C.UTF-8 $youtube_dl --get-filename "$url" --restrict-filenames)
         echo "=> fileName= <$fileName>"
         echo
         if [ -f "$fileName" ] && [ ! -w "$fileName" ]; then
@@ -34,7 +34,7 @@ ytgetRestrictedFilenamesHD ()
             echo
             continue
         fi
-        $youtube_dl --embed-thumbnail -f $format "$url" --restrict-filenames || $youtube_dl --embed-thumbnail "$url" --restrict-filenames || continue
+        LANG=C.UTF-8 $youtube_dl --embed-thumbnail -f $format "$url" --restrict-filenames || LANG=C.UTF-8 $youtube_dl --embed-thumbnail "$url" --restrict-filenames || continue
         mp4tags -m "$url" "$fileName"
         chmod -w "$fileName"
         echo
