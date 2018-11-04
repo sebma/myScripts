fa8c430d6697735c207a0f629a2e55ffb51324e1
sebma <sebma@users.noreply.github.com>
2018-11-04 11:12:24 +0100

    Updated shl

--- shl/getRestrictedFilenamesSD.sh
+++ shl/getRestrictedFilenamesSD.sh
@@ -1,41 +1,52 @@
 #!/usr/bin/env bash
 
-getRestrictedFilenamesSD () 
-{ 
+declare -A | grep -wq colors || source $initDir/.colors
+function getRestrictedFilenamesSD {
+	trap 'rc=$?;set +x;echo "=> $FUNCNAME: CTRL+C Interruption trapped.">&2;return $rc' INT
 	youtube_dl=$(which youtube-dl)
 	youtubeURLPrefix=https://www.youtube.com/watch?v=
 	dailymotionURLPrefix=https://www.dailymotion.com/video/
 	format="mp4"
-	test $# != 0 && { 
-		echo "=> There are $# urls to download ..."
-	}
+	test $# != 0 && echo "=> There are $# urls to download ..."
 	i=0
 	for url in "$@"
 	do
 		let i++
-		echo "=> Downloading url #$i/$# ..."
-		if echo $url | \egrep -vq "www"; then
-			if LANG=C.UTF-8 $youtube_dl -qs $url 2> /dev/null; then
-				:
+		echo "=> Downloading url # $i/$# ..."
+		echo "=> url = $url"
+		echo "=> Testing if $url still exists ..."
+		time LANG=C.UTF-8 $youtube_dl -f "$format" -qs "$url" 2>&1 | grep --color=auto --color -A1 ^ERROR: && continue
+		if ! echo $url | \egrep -wq "www"; then
+			fileName=$(basename $( $locate -er "$url" | \egrep -v "\.part|AUDIO" | sort -rt. | head -1) 2>/dev/null)
+			if test -s "$fileName"; then
+				echo "${colors[yellowOnBlue]}=> The file <$fileName> is already downloaded, skipping ...$normal" 1>&2
+				echo
+				continue
 			else
-				if LANG=C.UTF-8 $youtube_dl -qs $youtubeURLPrefix$url 2> /dev/null; then
+				if LANG=C.UTF-8 $youtube_dl -f "$format" -qs $youtubeURLPrefix$url 2> /dev/null; then
 					url=$youtubeURLPrefix$url
 				else
-					if LANG=C.UTF-8 $youtube_dl -qs $dailymotionURLPrefix$url 2> /dev/null; then
+					if LANG=C.UTF-8 $youtube_dl -f "$format" -qs $dailymotionURLPrefix$url 2> /dev/null; then
 						url=$dailymotionURLPrefix$url
 					fi
 				fi
 			fi
+		else
+			echo $url | grep --color=auto -q youtube.com/ && urlSuffix="$(echo $url | cut -d= -f2 | sed 's/^-/\\&/')"
+			if test "$urlSuffix" && fileName=$(basename $( $locate -er "$urlSuffix.*mp4$" | \egrep -v "\.part|AUDIO" | sort -rt. | head -1) 2>/dev/null) && test -s "$fileName"; then
+				echo "${colors[yellowOnBlue]}=> The file <$fileName> is already downloaded, skipping ...$normal" 1>&2
+				echo
+				continue
+			fi
 		fi
-		LANG=C.UTF-8 $youtube_dl -qs $url 2>&1 | grep --color ^ERROR: && continue
-		format=$(LANG=C.UTF-8 $youtube_dl -F $url | egrep -vw "only|hls-[0-9]+"  | egrep '(webm|mp4|flv) .*([0-9]+x[0-9]+)|(unknown)' | egrep -wv "22|hd|http-720" | sort -k 2,2 -k 3,3rn | awk '{printf$1"/"}')
-		echo "=> url = $url"
+		set +x
+		format="mp4[height<=?480]"
 		echo
 		fileName=$(LANG=C.UTF-8 $youtube_dl -f $format --get-filename "$url" --restrict-filenames || LANG=C.UTF-8 $youtube_dl -f $fallback --get-filename "$url" --restrict-filenames)
 		echo "=> fileName = <$fileName>"
 		echo
 		if [ -f "$fileName" ] && [ ! -w "$fileName" ]; then
-			echo "${color[yellowOnBlue]}=> The file <$fileName> is already downloaded, skipping ...$normal" 1>&2
+			echo "${colors[yellowOnBlue]}=> The file <$fileName> is already downloaded, skipping ...$normal" 1>&2
 			echo
 			continue
 		fi
@@ -43,6 +54,7 @@ getRestrictedFilenamesSD ()
 		echo
 	done
 	sync
+	trap - INT
 }
 
 getRestrictedFilenamesSD $@
