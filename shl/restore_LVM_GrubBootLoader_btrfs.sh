8af2823b9365fdcab4b44f80628e9f26ff8f8da2
sebma <sebma@users.noreply.github.com>
2018-11-19 16:32:31 +0100

    Updated shl/restore_LVM_GrubBootLoader_btrfs.sh

--- shl/restore_LVM_GrubBootLoader_btrfs.sh
+++ shl/restore_LVM_GrubBootLoader_btrfs.sh
@@ -9,11 +9,12 @@ $sudo lvs | grep -q root || {
 	$sudo vgscan
 	$sudo lvscan
 }
+
 rootFSDevice=$($sudo lvs | awk '/root/{print$2"-"$1}')
-#mount -t proc /proc /mnt/proc # Pour que Grub2 trouve /proc/mounts
 $sudo mkdir -p /mnt/{dev/pts,proc,sys}
 mount | grep -q $rootFSDevice || $sudo mount -o subvol=@ /dev/mapper/$rootFSDevice /mnt          # montage de celle-ci en remplacant le X par le bon numero de partition
 for i in dev dev/pts proc sys ; do $sudo mount --bind /$i /mnt/$i ; done
+set +o errexit
 $sudo chroot /mnt /bin/bash <<-EOF # mise a la racine du disque monte
 	mount -av                      # montage des partitions dans le chroot
 	test -s /boot/grub/grub.cfg || update-grub # creation d'un nouveau fichier de configuration : grub.cfg
