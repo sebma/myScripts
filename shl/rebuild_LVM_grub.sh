862ab9579ea0ffcf8822035a8e4b607fdc18270c
sebma <sebma@users.noreply.github.com>
2018-11-19 16:32:31 +0100

    Updated shl/rebuild_LVM_grub.sh

--- shl/rebuild_LVM_grub.sh
+++ shl/rebuild_LVM_grub.sh
@@ -9,13 +9,14 @@ $sudo lvs | grep -q root || {
 	$sudo vgscan
 	$sudo lvscan
 }
+
 rootFSLogicalVolume=$($sudo lvs | awk '/root/{print$2"-"$1}')
 osVGName=$($sudo lvs | awk '/root/{print$2}')
 diskDevice=$(pvs | awk "/$osVGName/"'{print substr($1,1,8)}')
-#mount -t proc /proc /mnt/proc # Pour que Grub2 trouve /proc/mounts
 $sudo mkdir -p /mnt/{dev/pts,proc,sys}
 mount | grep -q $rootFSLogicalVolume || $sudo mount /dev/mapper/$rootFSLogicalVolume /mnt          # montage de celle-ci en remplacant le X par le bon numero de partition
 for i in dev dev/pts proc sys ; do $sudo mount --bind /$i /mnt/$i ; done
+set +o errexit
 $sudo chroot /mnt /bin/bash <<-EOF # mise a la racine du disque monte
 	mount -av                      # montage des partitions dans le chroot
 	update-grub                   # creation d'un nouveau fichier de configuration : grub.cfg
@@ -23,3 +24,4 @@ $sudo chroot /mnt /bin/bash <<-EOF # mise a la racine du disque monte
 	sync
 	umount -av
 EOF
+$sudo umount -v /mnt/{sys,proc,dev/pts,dev,}
