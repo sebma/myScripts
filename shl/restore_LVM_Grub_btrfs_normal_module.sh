f61b2c3a48031dcb8c24a0f88d4fed80494b7391
sebma <sebma@users.noreply.github.com>
2018-11-19 16:32:31 +0100

    Updated shl/restore_LVM_Grub_btrfs_normal_module.sh

--- shl/restore_LVM_Grub_btrfs_normal_module.sh
+++ shl/restore_LVM_Grub_btrfs_normal_module.sh
@@ -9,11 +9,12 @@ $sudo lvs | grep -q root || {
 	$sudo vgscan
 	$sudo lvscan
 }
+
 rootFSDevice=$($sudo lvs | awk '/root/{print$2"-"$1}')
-#mount -t proc /proc /mnt/proc # Pour que Grub2 trouve /proc/mounts
 $sudo mkdir -p /mnt/{dev/pts,proc,sys}
 mount | grep -q $rootFSDevice || $sudo mount -o subvol=@ /dev/mapper/$rootFSDevice /mnt          # montage de celle-ci en remplacant le X par le bon numero de partition
 for i in dev dev/pts proc sys ; do $sudo mount --bind /$i /mnt/$i ; done
+set +o errexit
 $sudo chroot /mnt /bin/bash <<-EOF
 	mount -av
 	cp -puv /usr/lib/grub/x86_64-efi/*.mod /boot/grub/x86_64-efi/
