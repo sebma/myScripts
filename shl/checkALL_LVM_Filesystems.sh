14c1715a9ce416d1afe75c5cc2267dee3bf8af87
sebma <sebma@users.noreply.github.com>
2018-11-19 18:46:09 +0100

    Updated shl/checkALL_LVM_Filesystems.sh

--- shl/checkALL_LVM_Filesystems.sh
+++ shl/checkALL_LVM_Filesystems.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-fsck -N /dev/mapper/* 2>/dev/null | egrep -v "/control|^fsck" | awk '/btrfs/{print"btrfsck -p "$NF}!/btrfs/{print$(NF-1)" -p "$NF}' | sh -x
+fsck -N /dev/mapper/* 2>/dev/null | egrep -v "/control|^fsck\>" | awk '/btrfs/{print"btrfsck -p "$NF}!/btrfs/{print$(NF-1)" -p "$NF}' | sh -x
