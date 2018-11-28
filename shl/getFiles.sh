a91ffc06497b30f2429e7d7bdb1aee9eaa159828
sebma <sebma@users.noreply.github.com>
2018-11-28 21:55:32 +0100

    Updated shl/getFiles.sh

--- shl/getFiles.sh
+++ shl/getFiles.sh
@@ -7,9 +7,10 @@ test $# = 0 && {
 
 lastArg="$(eval echo \${$#})"
 baseUrl="$(echo $lastArg | sed -r "s,(https?|s?ftp)://,," | cut -d/ -f1)"
-#wget --output-file=$0-$(date +%Y%m%d-%H_%M_%S).log --no-verbose --no-parent --continue --timestamping --server-response --random-wait --no-directories --directory-prefix=$baseUrl/ --user-agent=Mozilla --content-disposition --convert-links --page-requisites --recursive --level=1 --reject index.html --accept $@
+wget=$(which wget)
+#$wget --output-file=$0-$(date +%Y%m%d-%H_%M_%S).log --no-verbose --no-parent --continue --timestamping --server-response --random-wait --no-directories --directory-prefix=$baseUrl/ --user-agent=Mozilla --content-disposition --convert-links --page-requisites --recursive --level=1 --reject index.html --accept $@
 set -x
-#wget --no-verbose --no-parent --continue --timestamping --server-response --random-wait --no-directories --directory-prefix=$baseUrl/ --user-agent=Mozilla --content-disposition --convert-links --page-requisites --recursive --level=1 --reject index.html --accept "$@"
-#wget --no-parent --continue --timestamping --server-response --random-wait --no-directories --directory-prefix=$baseUrl/ --user-agent=Mozilla --content-disposition --convert-links --page-requisites --recursive --level=1 --reject index.html --accept "$@"
-#wget --no-parent --continue --timestamping --random-wait --no-directories --directory-prefix=$baseUrl/ --user-agent=Mozilla --content-disposition --convert-links --page-requisites --recursive --level=1 --reject index.html --accept "$@"
-wget --no-parent --continue --timestamping --random-wait --directory-prefix=$baseUrl/ --user-agent=Mozilla --content-disposition --convert-links --page-requisites --recursive --reject index.html --accept "$@"
+#$wget --no-verbose --no-parent --continue --timestamping --server-response --random-wait --no-directories --directory-prefix=$baseUrl/ --user-agent=Mozilla --content-disposition --convert-links --page-requisites --recursive --level=1 --reject index.html --accept "$@"
+#$wget --no-parent --continue --timestamping --server-response --random-wait --no-directories --directory-prefix=$baseUrl/ --user-agent=Mozilla --content-disposition --convert-links --page-requisites --recursive --level=1 --reject index.html --accept "$@"
+#$wget --no-parent --continue --timestamping --random-wait --no-directories --directory-prefix=$baseUrl/ --user-agent=Mozilla --content-disposition --convert-links --page-requisites --recursive --level=1 --reject index.html --accept "$@"
+$wget --no-parent --continue --timestamping --random-wait --directory-prefix=$baseUrl/ --user-agent=Mozilla --content-disposition --convert-links --page-requisites --recursive --reject index.html --accept "$@"
