9afe471060ff2d91b539e5d7172efff6eca52235
sebma <sebma@users.noreply.github.com>
2018-12-19 09:27:25 +0100

    Updated cm/activate1stPart.cmd

--- cm/activate1stPart.cmd
+++ cm/activate1stPart.cmd
@@ -1,9 +1,12 @@
 @echo off
 REM Make first partition (Windows Recovery) bootable
-echo select disk 0 > activate1stPart.dpt
-echo select partition 1 >> activate1stPart.dpt
-echo active >> activate1stPart.dpt
-echo list partition >> activate1stPart.dpt
+(
+echo select disk 0
+echo select partition 1
+echo active
+echo list partition
+) > activate1stPart.dpt
+
 diskpart -s activate1stPart.dpt
 del activate1stPart.dpt
 echo.
