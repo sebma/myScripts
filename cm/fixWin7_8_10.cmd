f77e6abe02caed9d1acccbecec03347bdc4c75a7
sebma <sebma@users.noreply.github.com>
2018-12-19 09:27:25 +0100

    Updated cm/fixWin7_8_10.cmd

--- cm/fixWin7_8_10.cmd
+++ cm/fixWin7_8_10.cmd
@@ -1,13 +1,17 @@
 @echo off
 REM Make first partition (Windows Recovery) bootable
-echo select disk 0 > activate_C_drive.dpt
-echo select volume C: >> activate_C_drive.dpt
-echo active >> activate_C_drive.dpt
-echo list volume >> activate_C_drive.dpt
+(
+echo select disk 0
+echo select volume C:
+echo active
+echo list volume
+) > activate_C_drive.dpt
+
 diskpart -s activate_C_drive.dpt
 del activate_C_drive.dpt
 echo.
 echo on
+
 bootrec /FixMbr && bootrec /FixBoot && bootrec /ScanOs
 bootrec /RebuildBcd || chkdsk /f c:
 bootrec /RebuildBcd || chkdsk /r c:
