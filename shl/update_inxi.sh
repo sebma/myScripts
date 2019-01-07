84817b1020ae84a2780773d299cc15a00ffe17d8
sebma <sebma@users.noreply.github.com>
2019-01-07 03:36:01 +0100

    Updated shl/update_inxi.sh

--- shl/update_inxi.sh
+++ shl/update_inxi.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
-LANG=C wget --no-check-certificate --content-disposition --no-continue -N -P ~/.local/share/man/man1/ https://raw.githubusercontent.com/smxi/inxi/master/inxi.1
-#LANG=C wget --no-check-certificate --content-disposition --no-continue -N -P ../pl/not_mine/ https://raw.githubusercontent.com/smxi/inxi/master/inxi
-LANG=C curl -q --no-progress-bar -C - -R -z ../pl/not_mine/inxi -o ../pl/not_mine/inxi https://raw.githubusercontent.com/smxi/inxi/master/inxi
+rm -v ../pl/not_mine/inxi
+LANG=C \wget --no-check-certificate --content-disposition --no-continue -N -P ~/.local/share/man/man1/ https://raw.githubusercontent.com/smxi/inxi/master/inxi.1
+#LANG=C \wget --no-check-certificate --content-disposition --no-continue -N -P ../pl/not_mine/ https://raw.githubusercontent.com/smxi/inxi/master/inxi
+LANG=C \curl -q --no-progress-bar -C - -R -z ../pl/not_mine/inxi -o ../pl/not_mine/inxi https://raw.githubusercontent.com/smxi/inxi/master/inxi
+chmod +x ../pl/not_mine/inxi
