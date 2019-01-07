f1d6be115ef0d5973f33092d95488628eb9d7756
sebma <sebma@users.noreply.github.com>
2019-01-07 03:40:06 +0100

    Updated shl/update_binxi.sh

--- shl/update_binxi.sh
+++ shl/update_binxi.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
-LANG=C wget --no-check-certificate --content-disposition --no-continue -N -P ~/.local/share/man/man1/ https://raw.githubusercontent.com/smxi/inxi/master/binxi.1
-#LANG=C wget --no-check-certificate --content-disposition --no-continue -N -P ../shl/not_mine/ https://raw.githubusercontent.com/smxi/inxi/master/binxi
-LANG=C curl -q --no-progress-bar -C - -R -z ../shl/not_mine/inxi -o ../shl/not_mine/inxi https://raw.githubusercontent.com/smxi/inxi/master/binxi
+rm -v ../shl/not_mine/binxi
+LANG=C \wget --no-check-certificate --content-disposition --no-continue -N -P ~/.local/share/man/man1/ https://raw.githubusercontent.com/smxi/inxi/inxi-legacy/binxi.1
+#LANG=C \wget --no-check-certificate --content-disposition --no-continue -N -P ../shl/not_mine/ https://raw.githubusercontent.com/smxi/inxi/inxi-legacy/binxi
+LANG=C \curl -q --no-progress-bar -C - -R -z ../shl/not_mine/binxi -o ../shl/not_mine/binxi https://raw.githubusercontent.com/smxi/inxi/inxi-legacy/binxi
+chmod +x ../shl/not_mine/binxi
