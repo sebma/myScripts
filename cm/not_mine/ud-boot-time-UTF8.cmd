db6500fe7c1c4c550f872f895bdc92661f2e20b7
sebma <sebma@users.noreply.github.com>
2019-02-04 20:33:00 +0100

    Updated -a

---
+++ cm/not_mine/ud-boot-time-UTF8.cmd
@@ -0,0 +1,27 @@
+﻿@echo off
+;--------------------------------------------------------------------
+;                UltraDefrag Boot Time Shell Script
+;--------------------------------------------------------------------
+; !!! NOTE: THIS FILE MUST BE SAVED IN UNICODE (UTF-16) ENCODING !!!
+;--------------------------------------------------------------------
+
+set UD_IN_FILTER=*windows*;*winnt*;*ntuser*;*pagefile.sys;*hiberfil.sys
+set UD_EX_FILTER=*temp*;*tmp*;*dllcache*;*ServicePackFiles*
+
+; to exclude archives too uncomment the follwing lines
+; set UD_EX_FILTER=%UD_EX_FILTER%;*.7z;*.7z.*;*.arj;*.bz2;*.bzip2;*.cab;*.cpio
+; set UD_EX_FILTER=%UD_EX_FILTER%;*.deb;*.dmg;*.gz;*.gzip;*.lha;*.lzh;*.lzma
+; set UD_EX_FILTER=%UD_EX_FILTER%;*.rar;*.rpm;*.swm;*.tar;*.taz;*.tbz;*.tbz2
+; set UD_EX_FILTER=%UD_EX_FILTER%;*.tgz;*.tpz;*.txz;*.xar;*.xz;*.z;*.zip
+
+; uncomment the following line to create debugging output
+; set UD_DBGPRINT_LEVEL=DETAILED
+
+; uncomment the following line to save debugging information to a log file
+; set UD_LOG_FILE_PATH=%UD_INSTALL_DIR%\Logs\defrag_native.log
+
+udefrag %SystemDrive%
+
+boot-off
+
+exit
