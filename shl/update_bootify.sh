05a454e0a1613c05fd9d6043e768f23a807c67ed
sebma <sebma@users.noreply.github.com>
2019-02-06 04:29:45 +0100

    Updated -a

--- shl/update_bootify.sh
+++ shl/update_bootify.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-LANG=C wget -P not_mine/ https://raw.githubusercontent.com/oneohthree/bootify/master/bootify.sh
+LANG=C wget -P not_mine/ https://raw.githubusercontent.com/oneohthree/bootify/master/bootify
