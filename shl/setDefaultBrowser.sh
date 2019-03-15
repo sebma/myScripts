fd00d33971b965affbda297d0e2a69d9fd93922c
sebma <sebma@users.noreply.github.com>
2019-03-15 16:14:31 +0100

    Updated shl/setDefaultBrowser.sh

--- shl/setDefaultBrowser.sh
+++ shl/setDefaultBrowser.sh
@@ -3,7 +3,7 @@ echo "=> Association du protocole apt:, ssh: avec les applications adequoites ..
 xdg-mime default apturl.desktop x-scheme-handler/apt
 xdg-mime default putty.desktop x-scheme-handler/ssh
 
-for browser in midori qupzilla firefox chromim-browser konqueror
+for browser in palemoon firefox-esr firefox midori qupzilla chromim-browser konqueror
 do
 	which $browser >/dev/null 2>&1 && defaultBrowser=$browser && break
 done
