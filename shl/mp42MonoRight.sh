d3875184d0acf5eb4768a47d7a771af19eb77d6c
sebma <sebma@users.noreply.github.com>
2019-03-20 01:37:45 +0100

    Updated shl/mp42MonoRight.sh

--- shl/mp42MonoRight.sh
+++ shl/mp42MonoRight.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
 echo
+ffprobe="command ffprobe -hide_banner -probesize 400M -analyzeduration 400M"
+#movflags="+frag_keyframe"
+movflags="+faststart"
 for file
 do
 	extension=${file/*./}
-#	audioStreamSpecifier=$(ffprobe -hide_banner "$file" 2>&1 | sed -n "/Audio:/s/^[^0]*\|(.*//gp" | sed "s/:/./")
-#	audioStreamSpecifier=$(ffprobe -hide_banner "$file" 2>&1 | awk -F " *|#|[(]" '/Audio:/{sub(":",".",$4);print$4".0"}' | sed "s/\([0-9].[0-9]\).*/\1/")
-	audioStreamSpecifier=$(ffprobe "$file" -show_entries stream=codec_type -of flat -v 0 | awk -F "." '/audio/{print "0."$3".1"}')
-	time ffmpeg -hide_banner -i "$file" -map 0 -c copy -map_channel $audioStreamSpecifier -acodec aac -aq 1.0 -movflags +faststart "${file/.$extension/_MONO.$extension}" -y
+#	audioStreamSpecifier=$($ffprobe "$file" -show_entries stream=codec_type -of flat -v 0 | awk -F "." '/="audio"/{print "0."$3".1"}')
+#	time ffmpeg -hide_banner -i "$file" -map 0 -c copy -map_channel $audioStreamSpecifier -acodec aac -aq 1.0 -movflags $movflags "${file/.$extension/_MONO.$extension}" -y
+	time ffmpeg -hide_banner -i "$file" -map 0 -c copy -af "pan=mono|c0=FR" -acodec aac -aq 1.5 -movflags $movflags "${file/.$extension/_MONO.$extension}" -y
 	sync
 	echo "=> Output File = <${file/.$extension/_MONO.$extension}>"
 	echo
