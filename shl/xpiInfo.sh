946173840c347c3a40bb2bfac66f18cf14787f15
sebma <sebma@users.noreply.github.com>
2019-04-05 18:51:35 +0200

    Updated shl/xpiInfo.sh

--- shl/xpiInfo.sh
+++ shl/xpiInfo.sh
@@ -1,18 +1,27 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-xpiInfo () 
+xpiInfo()
 { 
-    for xpiFile
-    do
-        echo "=> xpiFile = $xpiFile"
-        printf "em:id = "
-        unzip -q -p $xpiFile install.rdf | egrep --color=auto -m1 "em:id" | awk -F "<|>" '{print$3}'
-        printf "em:name = "
-        unzip -q -p $xpiFile install.rdf | egrep --color=auto -m1 "em:name" | awk -F "<|>" '{print$3}'
-        printf "em:version = "
-        unzip -q -p $xpiFile install.rdf | egrep --color=auto -m1 "em:version" | awk -F "<|>" '{print$3}'
-        echo
-    done
+	for xpiFile
+	do
+		echo "=> xpiFile = $xpiFile"
+		if unzip -q -l $xpiFile | \grep -q install.rdf
+		then
+			printf "em:id = "
+			unzip -q -p $xpiFile install.rdf | egrep --color=auto -m1 "em:id" | awk -F "<|>" '{print$3}'
+			printf "em:name = "
+			unzip -q -p $xpiFile install.rdf | egrep --color=auto -m1 "em:name" | awk -F "<|>" '{print$3}'
+			printf "em:version = "
+			unzip -q -p $xpiFile install.rdf | egrep --color=auto -m1 "em:version" | awk -F "<|>" '{print$3}'
+		elif unzip -q -l $xpiFile | \grep -q manifest.json
+		then
+			printf "name = "
+			unzip -q -p $xpiFile manifest.json | jq .name
+			printf "version = "
+			unzip -q -p $xpiFile manifest.json | jq .version
+		fi
+		echo
+	done
 }
 
 xpiInfo $@
