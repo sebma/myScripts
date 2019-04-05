fe21e08809404813a189db7ab72ab8e62144004b
sebma <sebma@users.noreply.github.com>
2019-04-05 19:00:09 +0200

    Updated shl/xpiInfo.sh

--- shl/xpiInfo.sh
+++ shl/xpiInfo.sh
@@ -5,7 +5,7 @@ xpiInfo()
 	for xpiFile
 	do
 		echo "=> xpiFile = $xpiFile"
-		if unzip -q -l $xpiFile | \grep -q install.rdf
+		if unzip -t $xpiFile | \grep -wq install.rdf
 		then
 			printf "em:id = "
 			unzip -q -p $xpiFile install.rdf | egrep --color=auto -m1 "em:id" | awk -F "<|>" '{print$3}'
@@ -13,7 +13,7 @@ xpiInfo()
 			unzip -q -p $xpiFile install.rdf | egrep --color=auto -m1 "em:name" | awk -F "<|>" '{print$3}'
 			printf "em:version = "
 			unzip -q -p $xpiFile install.rdf | egrep --color=auto -m1 "em:version" | awk -F "<|>" '{print$3}'
-		elif unzip -q -l $xpiFile | \grep -q manifest.json
+		elif unzip -t $xpiFile | \grep -wq manifest.json
 		then
 			printf "name = "
 			unzip -q -p $xpiFile manifest.json | jq .name
