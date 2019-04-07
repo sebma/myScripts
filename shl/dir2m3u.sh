c6797809091c1780cf866be93408f8f1e2893309
sebma <sebma@users.noreply.github.com>
2019-04-07 17:50:07 +0200

    Updated shl/dir2m3u.sh

--- shl/dir2m3u.sh
+++ shl/dir2m3u.sh
@@ -10,7 +10,7 @@ echo "#EXTM3U"
 $find "$inputDIR" -xdev -type f | \egrep "$multiMediaFormats" | sort -V | while read fileName
 do
 	printf "#EXTINF:-1,"
-#	title=$(mediainfo --Inform="General;%Movie%" "$fileName")
+#	title=$(mediainfo --Inform="General;%Movie%" "$fileName") # 1.33 slower than ffprobe
 	title=$($ffprobe -v error -show_entries format_tags=title -of default=noprint_wrappers=1:nokey=1 "$fileName")
 	test $? = 0 && echo $title && echo $fileName || echo
 done | sed "s|^\./||"
