49b83fc9ce31c83b10ac3db1b93987cd30bafbb5
sebma <sebma@users.noreply.github.com>
2019-04-07 17:50:07 +0200

    Updated shl/files2m3u.sh

--- shl/files2m3u.sh
+++ shl/files2m3u.sh
@@ -7,7 +7,7 @@ echo "#EXTM3U"
 awk '{print$1}' $inputPlaylist | uniq | while read fileName
 do
 	printf "#EXTINF:-1,"
+#	title=$(mediainfo --Inform="General;%Movie%" "$fileName") # 1.33 slower than ffprobe
 	title=$($ffprobe -v error -show_entries format_tags=title -of default=noprint_wrappers=1:nokey=1 "$fileName")
-	echo $title
-	echo $fileName
-done
+	test $? = 0 && echo $title && echo $fileName || echo
+done | sed "s|^\./||"
