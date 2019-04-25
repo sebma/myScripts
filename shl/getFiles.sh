9382a222a57c7e2bf58d3300a3c068fe0e37e8d1
sebma <sebma@users.noreply.github.com>
2019-04-25 13:01:12 +0200

    Updated shl/getFiles.sh

--- shl/getFiles.sh
+++ shl/getFiles.sh
@@ -6,11 +6,11 @@ test $# = 0 && {
 }
 
 lastArg="$(eval echo \${$#})"
-baseUrl="$(echo $lastArg | sed -r "s,(https?|s?ftp)://,," | cut -d/ -f1)"
-wget=$(which wget)
-#$wget --output-file=$0-$(date +%Y%m%d-%H_%M_%S).log --no-verbose --no-parent --continue --timestamping --server-response --random-wait --no-directories --directory-prefix=$baseUrl/ --user-agent=Mozilla --content-disposition --convert-links --page-requisites --recursive --level=1 --reject index.html --accept $@
+baseUrl="$(echo $lastArg | \sed -r "s,(https?|s?ftp)://,," | cut -d/ -f1)"
+wget="$(which wget2 2>/dev/null || which wget)"
+#time $wget --output-file=$0-$(date +%Y%m%d-%H_%M_%S).log --no-verbose --no-parent --continue --timestamping --server-response --random-wait --no-directories --directory-prefix=$baseUrl/ --user-agent=Mozilla --content-disposition --convert-links --page-requisites --recursive --level=1 --reject index.html --accept $@
 set -x
-#$wget --no-verbose --no-parent --continue --timestamping --server-response --random-wait --no-directories --directory-prefix=$baseUrl/ --user-agent=Mozilla --content-disposition --convert-links --page-requisites --recursive --level=1 --reject index.html --accept "$@"
-#$wget --no-parent --continue --timestamping --server-response --random-wait --no-directories --directory-prefix=$baseUrl/ --user-agent=Mozilla --content-disposition --convert-links --page-requisites --recursive --level=1 --reject index.html --accept "$@"
-#$wget --no-parent --continue --timestamping --random-wait --no-directories --directory-prefix=$baseUrl/ --user-agent=Mozilla --content-disposition --convert-links --page-requisites --recursive --level=1 --reject index.html --accept "$@"
-$wget --no-parent --continue --timestamping --random-wait --directory-prefix=$baseUrl/ --user-agent=Mozilla --content-disposition --convert-links --page-requisites --recursive --reject index.html --accept "$@"
+#time $wget --no-verbose --no-parent --continue --timestamping --server-response --random-wait --no-directories --directory-prefix=$baseUrl/ --user-agent=Mozilla --content-disposition --convert-links --page-requisites --recursive --level=1 --reject index.html --accept "$@"
+#time $wget --no-parent --continue --timestamping --server-response --random-wait --no-directories --directory-prefix=$baseUrl/ --user-agent=Mozilla --content-disposition --convert-links --page-requisites --recursive --level=1 --reject index.html --accept "$@"
+#time $wget --no-parent --continue --timestamping --random-wait --no-directories --directory-prefix=$baseUrl/ --user-agent=Mozilla --content-disposition --convert-links --page-requisites --recursive --level=1 --reject index.html --accept "$@"
+time $wget --no-parent --continue --timestamping --random-wait --directory-prefix=$baseUrl/ --user-agent=Mozilla --content-disposition --convert-links --page-requisites --recursive --reject index.html --accept "$@"
