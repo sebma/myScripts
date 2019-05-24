57ee1b7b9ff94b87c3e954e5e186c4a090eff45b
sebma <sebma@users.noreply.github.com>
2019-05-24 22:46:32 +0200

    Updated xsetResolution.sh

--- shl/xsetResolution.sh
+++ shl/xsetResolution.sh
@@ -1,27 +1,28 @@
 #!/usr/bin/env bash
 
 xsetResolution () { 
-	local awk="command awk"
-	local xrandr="command xrandr"
-	local output=$($xrandr | $awk  '/^.+ connected/{print$1}')
-	local oldResolution=$($xrandr | $awk '/[0-9].*\*/{print$1}')
-	local newResolution=$1
-	echo "=> Reset current resolution command :"
-	echo "xrandr --output $output --mode $oldResolution"
-	if [ -z $newResolution ]; then
-		$xrandr
-		echo "=> Usage: $FUNCNAME XResxYRes"
-		return 1
-	else
-		if ! $xrandr | grep --color=auto -wq $newResolution; then
-			echo "=> $FUNCNAME ERROR : The resolution <$newResolution> is not supported." 1>&2
-			return 2
-		fi
-		echo "=> Setting new resolution command :"
-		echo "xrandr --output $output --mode $newResolution"
-		$xrandr --output $output --mode $newResolution
-	fi
+    local output=$(\xrandr | \awk  '/^.+ connected/{print$1}')
+    local oldResolution=$(\xrandr | \awk '/[0-9].*\*/{print$1}')
+    echo "=> Reset current resolution command :"
+    echo "xrandr --output $output --mode $oldResolution"
+    if [ $# = 0 ]; then
+        echo "=> Usage: $FUNCNAME XResxYRes"
+        echo
+        echo "=> Here are the possible resolutions :"
+        \xrandr | awk '/^ +[0-9]+x[0-9]+/{printf$1" "}'
+        echo
+        return 1
+    else
+        local newResolution=$1
+        if ! \xrandr | grep --color=auto -wq $newResolution; then
+            echo "=> $FUNCNAME ERROR : The resolution <$newResolution> is not supported." 1>&2
+            return 2
+        fi
+        echo "=> Setting new resolution command :"
+        echo "xrandr --output $output --mode $newResolution"
+        \xrandr --output $output --mode $newResolution
+    fi
 }
 
-xsetResolution $@
+xsetResolution "$@"
 lxrandr || xfce4-display-settings &
