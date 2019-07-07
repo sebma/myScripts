f4fa1a37acc77a991567524ded3dde1a4ee69bb6
sebma <sebma@users.noreply.github.com>
2019-07-07 17:53:53 +0200

    Updated shl/calc_montant_total.sh

--- shl/calc_montant_total.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-[ $# -ne 1 ] && {
-  echo "=> Usage: <$0> <Swift|CMI OrderFilename>"
-  exit 1
-}
-
-egrep ":32B:" $1 | tr ',' '.' | gawk -F":32B:EUR" 'BEGIN{print"0.00"}/32B/{print".+"$2}' | bc -l
