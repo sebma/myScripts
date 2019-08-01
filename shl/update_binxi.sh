2818dbf91bd160560d8e0285618287e710e736e1
sebma <sebma@users.noreply.github.com>
2019-08-01 07:37:28 +0200

    Updated update_binxi.sh

--- shl/update_binxi.sh
+++ shl/update_binxi.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env sh
 
-rm -v ../shl/not_mine/binxi
-LANG=C \wget --no-check-certificate --content-disposition --no-continue -N -P ~/.local/share/man/man1/ https://raw.githubusercontent.com/smxi/inxi/inxi-legacy/binxi.1
-#LANG=C \wget --no-check-certificate --content-disposition --no-continue -N -P ../shl/not_mine/ https://raw.githubusercontent.com/smxi/inxi/inxi-legacy/binxi
-LANG=C \curl -q --no-progress-bar -C - -R -z ../shl/not_mine/binxi -o ../shl/not_mine/binxi https://raw.githubusercontent.com/smxi/inxi/inxi-legacy/binxi
-chmod +x ../shl/not_mine/binxi
+update_binxi() {
+	local scriptDir=$(dirname $0)
+	scriptDir=$(cd $scriptDir;pwd)
+	
+	mkdir -pv $scriptDir/not_mine/
+	rm -v $scriptDir/not_mine/binxi
+	LANG=C \wget --no-check-certificate --content-disposition --no-continue -N -P ~/.local/share/man/man1/ https://raw.githubusercontent.com/smxi/inxi/inxi-legacy/binxi.1
+	#LANG=C \wget --no-check-certificate --content-disposition --no-continue -N -P $scriptDir/not_mine/ https://raw.githubusercontent.com/smxi/inxi/inxi-legacy/binxi
+	LANG=C \curl -q --no-progress-bar -C - -R -z $scriptDir/not_mine/binxi -o $scriptDir/not_mine/binxi https://raw.githubusercontent.com/smxi/inxi/inxi-legacy/binxi
+	chmod +x $scriptDir/not_mine/binxi
+}
+
+update_binxi
