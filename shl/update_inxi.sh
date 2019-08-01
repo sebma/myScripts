4fde4201bc84753009ed404559ee96837540f4f1
sebma <sebma@users.noreply.github.com>
2019-08-01 07:37:28 +0200

    Updated update_inxi.sh

--- shl/update_inxi.sh
+++ shl/update_inxi.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env sh
 
-rm -v ../pl/not_mine/inxi
-LANG=C \wget --no-check-certificate --content-disposition --no-continue -N -P ~/.local/share/man/man1/ https://raw.githubusercontent.com/smxi/inxi/master/inxi.1
-#LANG=C \wget --no-check-certificate --content-disposition --no-continue -N -P ../pl/not_mine/ https://raw.githubusercontent.com/smxi/inxi/master/inxi
-LANG=C \curl -q --no-progress-bar -C - -R -z ../pl/not_mine/inxi -o ../pl/not_mine/inxi https://raw.githubusercontent.com/smxi/inxi/master/inxi
-chmod +x ../pl/not_mine/inxi
+update_inxi() {
+	local scriptDir=$(dirname $0)
+	scriptDir=$(cd $scriptDir;pwd)
+	
+	mkdir -pv $scriptDir/../pl/not_mine/
+	rm -v $scriptDir/../pl/not_mine/inxi
+	LANG=C \wget --no-check-certificate --content-disposition --no-continue -N -P ~/.local/share/man/man1/ https://raw.githubusercontent.com/smxi/inxi/master/inxi.1
+	#LANG=C \wget --no-check-certificate --content-disposition --no-continue -N -P $scriptDir/../pl/not_mine/ https://raw.githubusercontent.com/smxi/inxi/master/inxi
+	LANG=C \curl -q --no-progress-bar -C - -R -z $scriptDir/../pl/not_mine/inxi -o $scriptDir/../pl/not_mine/inxi https://raw.githubusercontent.com/smxi/inxi/master/inxi
+	chmod +x $scriptDir/../pl/not_mine/inxi
+}
+
+update_inxi
