69e83c1bb28031729473cc2d7f44a85cdb750459
sebma <sebma@users.noreply.github.com>
2019-08-01 10:50:29 +0200

    Updated not_mine/get_dimensions2.py

---
+++ py/not_mine/get_dimensions2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import gi
+gi.require_version('Gdk', '3.0')
+from gi.repository import Gdk
+
+dsp = Gdk.Display.get_default()
+n_mons = dsp.get_n_monitors()
+
+print(("\t").join(["Screen", "width", "height", "diagonal\n"+32*"-"]))
+
+for i in range(n_mons):
+	mon = dsp.get_monitor(i)
+	mon_name = mon.get_model()
+	w = mon.get_width_mm()/25.4
+	h = mon.get_height_mm()/25.4
+	d = ((w**2)+(h**2))**(0.5)
+	print(("\t").join([mon_name]+[str(round(n, 1)) for n in [w, h, d]]))
