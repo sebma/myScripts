634a708a3d51c61ea19d960479b1bc1861704e48
sebma <sebma@users.noreply.github.com>
2019-08-09 16:13:01 +0200

    Updated seb-os-info.py

---
+++ py/seb-os-info.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3.5
+#coding: latin1
+
+from __future__ import print_function
+import os,sys,platform
+import warnings
+
+def osinfo() :
+	print()
+	print( "=> OS platform.system = " , platform.system() )
+	print( "=> OS platform.release = " , platform.release() )
+	print( "=> OS platform.processor = " , platform.processor() )
+	print( "=> OS platform.architecture = " , platform.architecture() )
+	print( "=> OS platform.node = " , platform.node() )
+	print()
+	
+	if platform.system() == "Linux" :
+		try :
+			warnings.simplefilter("error",category=DeprecationWarning)
+			print( "=> OS = "  , " ".join( platform.dist() ) )
+		except DeprecationWarning :
+			import distro
+			print( "=> OS = "  , " ".join( distro.linux_distribution() ) )
+			warnings.resetwarnings()
+	elif platform.system() == "Darwin" :
+		print( "=> OS = "  , " ".join( platform.mac_ver() ) )
+	elif platform.system() == "Windows" :
+		print( "=> OS = "  , " ".join( platform.win32_ver() ) )
+		raw_input("Press Enter to continue...")
+	
+osinfo()
+
+exit(0)
