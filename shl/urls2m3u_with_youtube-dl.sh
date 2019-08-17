af7b3472f54864413d7d0c3d86a1293c65756047
sebma <sebma@users.noreply.github.com>
2019-08-17 21:02:37 +0200

    Updated urls2m3u_with_youtube-dl.sh

---
+++ shl/urls2m3u_with_youtube-dl.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+urlsFile="$1"
+test $# = 0 && {
+	echo "=> Usage: $(basename $0) urlsFile" >&2
+	exit 1
+}
+
+alias youtube-dl='LANG=C.UTF-8 command youtube-dl'
+
+echo "#EXTM3U"
+time awk '{print$1}' $urlsFile | uniq | while read url
+do
+	printf "#EXTINF:-1,"
+	youtube-dl -e $url
+	echo $url
+done
