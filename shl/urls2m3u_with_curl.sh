535212c7ea8cc84df0b7314a85e75dd91709a041
sebma <sebma@users.noreply.github.com>
2019-08-22 16:48:19 +0200

    Updated urls2m3u_with_curl.sh

--- shl/urls2m3u_with_curl.sh
+++ shl/urls2m3u_with_curl.sh
@@ -10,6 +10,7 @@ echo "#EXTM3U"
 time awk '{print$1}' $urlsFile | uniq | while read url
 do
 	printf "#EXTINF:-1,"
-	curl -qs $url | pup --charset utf8 'title text{}'
+	title=$(\curl -qs $url | pup --charset utf8 'title text{}')
+	echo $title
 	echo $url
 done
