e3afd656a92050b73a373b2a70519b1a3f17b3ca
sebma <sebma@users.noreply.github.com>
2019-11-28 21:32:24 +0100

    Updated wifiInfos.sh

--- shl/wifiInfos.sh
+++ shl/wifiInfos.sh
@@ -1,50 +1,40 @@
 #!/usr/bin/env sh
 
-wifiInfos() {
+getprop 2>/dev/null | grep -q ro.build.version.release && export osFamily=Android || export osFamily=$(uname -s)
+wifiInfos ()
+{
 	export LANG=C
-	if [ $(uname -s) = Darwin ]
-	then
-		echo "=> Darwin/macOS operating systems are not supported yet." >&2
-		exit 1
+	local physicalDevice wiFiDevice
+	if [ $osFamily = Darwin ]; then
+		echo "=> Darwin/macOS operating systems are not supported yet." 1>&2
+		return 1
 	fi
-	
-	if which iw >/dev/null 
-	then
-		wiFiDevice=$(iw dev | awk '/Interface/{lastInterface=$NF}END{print lastInterface}')
-	elif which iwconfig >/dev/null 
-	then
-		wiFiDevice="$(iwconfig 2>/dev/null | awk '/^[^ \t]/ { if ($1 ~ /^[0-9]+:/) { lastInterface=$2 } else { lastInterface=$1 } }END{print lastInterface}')"
-	fi
-	
+	wiFiDevice=$(iw dev 2>/dev/null | awk '/Interface/{lastInterface=$NF}END{print lastInterface}')
+	test "$wiFiDevice" || wiFiDevice="$(iwconfig 2>/dev/null | awk '/^[^ \t]/ { if ($1 ~ /^[0-9]+:/) { lastInterface=$2 } else { lastInterface=$1 } }END{print lastInterface}')"
 	[ "$wiFiDevice" ] || {
-		echo "=> ERROR : Could not find any wireless network card." >&2
-		exit 1
+		echo "=> ERROR : Could not find any wireless network card." 1>&2
+		return 1
 	}
-	
-	if which iw >/dev/null; then
+	if which iw > /dev/null; then
 		physicalDevice=$(iw dev | \sed -n "1s/#//;1p")
-	
 		echo "=> Wifi physical device capabilities :"
 		iw phy $physicalDevice info
-	
 		echo "=> Stations infos. :"
 		iw dev $wiFiDevice station dump
-	
 		echo "=> Wifi networks list :"
-		which nmcli && nmcli dev wifi list || sudo iw dev $wiFiDevice scan
-	
+		which nmcli && nmcli dev wifi list || iw dev $wiFiDevice scan || \sudo iw dev $wiFiDevice scan
 		echo "=> Connected network link status :"
-		sudo iw dev $wiFiDevice link
-	else
-		which iwlist >/dev/null && {
-			sudo iwlist $wiFiDevice bitrate 
-			iwlist $wiFiDevice frequency
-			sudo iwlist $wiFiDevice encryption
-			iwlist $wiFiDevice event
-			iwlist $wiFiDevice retry
-			iwlist $wiFiDevice scan | more
-		}
+		iw dev $wiFiDevice link || \sudo iw dev $wiFiDevice link
 	fi
+	which iwlist > /dev/null && {
+		echo "========================================================================"
+		iwlist $wiFiDevice bitrate || \sudo iwlist $wiFiDevice bitrate
+		iwlist $wiFiDevice encryption || \sudo iwlist $wiFiDevice encryption
+		iwlist $wiFiDevice frequency
+		iwlist $wiFiDevice event
+		iwlist $wiFiDevice retry
+		iwlist $wiFiDevice scan | more
+	}
 }
 
 wifiInfos
