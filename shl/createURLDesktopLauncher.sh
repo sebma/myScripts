81e083963186b55346cdf17aaab3c6ebe2bd8743
sebma <sebma@users.noreply.github.com>
2019-11-30 00:13:17 +0100

    Updated createURLDesktopLauncher.sh

---
+++ shl/createURLDesktopLauncher.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+osFamily=$(uname -s)
+if [ $osFamily = Linux ]
+then
+	[ $# = 0 ] && {
+		echo  "=> Usage: $0 URLDesktopFileName"
+		exit 1
+	}
+	
+	URLDesktopFileName="$1"
+	appName="$(echo $URLDesktopFileName | sed 's/^./\U&/')"
+	destopFilePath=~/.local/share/applications/$URLDesktopFileName.desktop
+	FORM=$(zenity --forms --title="Simple URL shortcut maker" --text="Create new URL .desktop file" \
+			--add-entry="Name of the Web Page" \
+			--add-entry="URL of the Web Page" \
+		  )
+	
+	[ $? = 0 ] || exit 2
+	
+	echo
+	awk -F'|' '{
+		print "#!/usr/bin/env xdg-open"
+		print "[Desktop Entry]"
+		print "Type=Link"
+		print "Name="$1
+		print "URL="$2
+		print "Icon=Icon=text-html"
+	}' <<< "$FORM" | tee $destopFilePath && echo && chmod -v +x $destopFilePath && echo && ls -l $destopFilePath
+fi
