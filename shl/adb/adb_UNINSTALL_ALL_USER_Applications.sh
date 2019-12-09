8ef64a247686cf34c729f21c0ef8d8d0503e3b30
sebma <sebma@users.noreply.github.com>
2019-12-09 16:30:32 +0100

    Updated adb_UNINSTALL_ALL_USER_Applications.sh

--- shl/adb/adb_UNINSTALL_ALL_USER_Applications.sh
+++ shl/adb/adb_UNINSTALL_ALL_USER_Applications.sh
@@ -6,27 +6,28 @@
 	exit 1
 }
 
-typeset applicationPattern=$1
-typeset answer=no
+declare applicationPattern=$1
+declare answer=no
 
 if [ -z "$applicationPattern" ];then
 	read -p "Are you REALLY sure you want to uninstall ALL USER applications ? (YES): " answer
 fi
 
-if [ -z "$applicationPattern" ]; then
-	if [ -z "$answer" ] || [ $answer != YES ];then
-		echo "=> INFO : Doing nothing, just exiting ..." >&2
-		exit
-	fi
+if [ -z "$applicationPattern" ] && ( [ -z "$answer" ] || [ $answer != YES ] );then
+	echo "=> INFO : Doing nothing, just exiting ..." >&2
+	exit
 fi
 
-typeset adb=$(which adb)
-typeset dos2unix="$(which tr) -d '\r'"
+declare adb=$(which adb)
+declare dos2unix="$(which tr) -d '\r'"
 
-typeset packageList=$($adb shell pm list packages -3 $applicationPattern | $dos2unix | cut -d: -f2)
+declare machingPackagesNumber=$($adb shell pm list packages $applicationPattern | $dos2unix | wc -l)
+declare packageList=$($adb shell pm list packages -3 $applicationPattern | $dos2unix | cut -d: -f2)
 #echo "=> packageList = <$packageList>"
-for package in $packageList
+declare -i remainingPackages=$machingPackagesNumber
+time for package in $packageList
 do
-	echo "=> Uninstalling <$package> ..."
+	echo "=> Uninstalling <$package> #$remainingPackages/$machingPackagesNumber remaining packages to process ..."
 	$adb uninstall $package | $dos2unix
+	let remainingPackages--
 done
