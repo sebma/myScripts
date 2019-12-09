67b2c46303edfb223ba40550ed3e1ab19781c452
sebma <sebma@users.noreply.github.com>
2019-12-09 15:59:05 +0100

    Updated adbClearAllApplicationsCache_AND_DATA.sh

---
+++ shl/adb/adbClearAllApplicationsCache_AND_DATA.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+
+[ $# -gt 1 ] && {
+	echo "=> Usage: $0 [applicationPattern]"
+	exit 1
+}
+
+typeset applicationPattern=$1
+typeset answer=no
+test -z "$applicationPattern" && read -p "Are you REALLY sure you want to delete all the DATA of ALL the applications ? (YES): " answer
+
+if [ -z "$applicationPattern" ]; then
+	if [ -z "$answer" ] || [ $answer != YES ];then
+		echo "=> INFO : Doing nothing, just exiting ..." >&2
+		exit
+	fi
+fi
+
+typeset adb=$(which adb)
+typeset dos2unix="$(which tr) -d '\r'"
+
+typeset packageList=$($adb shell pm list packages $applicationPattern | $dos2unix | cut -d: -f2)
+#echo "=> packageList = <$packageList>"
+for package in $packageList
+do
+	echo "=> Processing <$package> ..."
+	$adb shell pm clear $package | $dos2unix
+	sync
+done
