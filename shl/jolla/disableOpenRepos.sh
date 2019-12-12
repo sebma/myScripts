5a828bf36e475002e7bb28e1f8a931bf64a90d0e
sebma <sebma@users.noreply.github.com>
2019-12-12 02:40:40 +0100

    Updated disableOpenRepos.sh

--- shl/jolla/disableOpenRepos.sh
+++ shl/jolla/disableOpenRepos.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-ssu repos | awk '/openrepos/{print $2}' | xargs -rtl ssu disablerepo
+ssu repos | awk '/openrepos/{print $2}' | xargs -rti ssu disablerepo
 #ssu repos | awk '/openrepos/{print $2}' | while read openrepo; do echo "=> Disabling $openrepo ..."; ssu disablerepo $openrepo; done #Si le xargs de GNU n'est plus present
 echo
 ssu repos
