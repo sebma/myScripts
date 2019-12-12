18fa2a50ec12ba08e8964e3000ece69c3ba094b0
sebma <sebma@users.noreply.github.com>
2019-12-12 02:40:40 +0100

    Updated enableOpenReposNew.sh

--- shl/jolla/enableOpenReposNew.sh
+++ shl/jolla/enableOpenReposNew.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-ssu repos | awk '/openrepos/{print $2}' | xargs -rtl yum-config-manager --enable
+ssu repos | awk '/openrepos/{print $2}' | \xargs -rti yum-config-manager --enable
 #ssu repos | awk '/openrepos/{print $2}' | while read openrepo; do echo "=> Enabling $openrepo ..."; ssu enablerepo $openrepo; done #Si le xargs de GNU n'est plus present
 echo
 ssu repos
