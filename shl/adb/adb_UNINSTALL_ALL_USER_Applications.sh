c13cf590bd269d327f18c1f9837cc6b4a8113b10
sebma <sebma@users.noreply.github.com>
2019-12-18 13:39:48 +0100

    Updated adb_UNINSTALL_ALL_USER_Applications.sh

--- shl/adb/adb_UNINSTALL_ALL_USER_Applications.sh
+++ shl/adb/adb_UNINSTALL_ALL_USER_Applications.sh
@@ -1,33 +1,33 @@
 #!/usr/bin/env bash
 
-
-[ $# -gt 1 ] && {
-	echo "=> Usage: $0 [applicationPattern]"
-	exit 1
-}
+case $1 in
+	-h|-help|--h|--help|-*|"") echo "=> Usage: $0 [applicationPattern|-h|AlL]" >&2; exit 1 ;;
+esac
 
 declare applicationPattern=$1
-declare answer=no
-
-if [ -z "$applicationPattern" ];then
-	read -p "Are you REALLY sure you want to uninstall ALL USER applications ? (YES): " answer
+if [ "$applicationPattern" = AlL ];then
+	declare answer=no
+	read -p "Are you REALLY sure you want to uninstall ALL USER applications ? (YeS): " answer
+	if [ "$answer" = YeS ];then
+		applicationPattern=.
+	else
+		echo "=> INFO : Doing nothing, just exiting ..." >&2
+		exit
+	fi
 fi
 
-if [ -z "$applicationPattern" ] && ( [ -z "$answer" ] || [ $answer != YES ] );then
-	echo "=> INFO : Doing nothing, just exiting ..." >&2
-	exit
-fi
+adb get-state >/dev/null || exit
 
 declare adb=$(which adb)
 declare dos2unix="$(which tr) -d '\r'"
 
-declare machingPackagesNumber=$($adb shell pm list packages $applicationPattern | $dos2unix | wc -l)
-declare packageList=$($adb shell pm list packages -3 $applicationPattern | $dos2unix | cut -d: -f2)
+declare matchingPackagesNumber=$($adb shell pm list packages -3 | \egrep "$applicationPattern" | $dos2unix | wc -l)
+declare packageList=$($adb shell pm list packages -3 | \egrep "$applicationPattern" | $dos2unix | cut -d: -f2)
 #echo "=> packageList = <$packageList>"
-declare -i remainingPackages=$machingPackagesNumber
+declare -i remainingPackages=$matchingPackagesNumber
 time for package in $packageList
 do
-	echo "=> Uninstalling <$package> #$remainingPackages/$machingPackagesNumber remaining packages to process ..."
+	echo "=> Uninstalling <$package> #$remainingPackages/$matchingPackagesNumber remaining packages to process ..."
 	$adb uninstall $package | $dos2unix
 	let remainingPackages--
 done
