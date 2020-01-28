fe427119f370e494903c6724d7fdc91b10778b1d
sebma <sebma@users.noreply.github.com>
2020-01-28 15:57:15 +0100

    Updated service-script.sh

--- shl/jolla/service-script.sh
+++ shl/jolla/service-script.sh
@@ -2,23 +2,23 @@
 
 if [ -z $1 ]
 then
-  echo "*** No options given ***"
+	echo "*** No options given ***"
 elif [ -n $1 ]
 then
-  param1=$1
-  if [ -z $2 ]
-  then
-    echo "missing second parameter"
-  elif [ -n $2 ]
-  then
-    param2=$2
-    case $param2 in
-      "start") systemctl start $param1.service ;;
-      "stop") systemctl stop enable $param1.service ;;
-      "restart") systemctl restart $param1.service ;;
-      "reload") systemctl reload $param1.service ;;
-      "condrestart") systemctl condrestart $param1.service ;;
-   *) echo "unrecognized option $param2";;
-    esac
-  fi
+	param1=$1
+	if [ -z $2 ]
+	then
+		echo "missing second parameter"
+	elif [ -n $2 ]
+	then
+		param2=$2
+		case $param2 in
+			"start") systemctl start $param1.service ;;
+			"stop") systemctl stop enable $param1.service ;;
+			"restart") systemctl restart $param1.service ;;
+			"reload") systemctl reload $param1.service ;;
+			"condrestart") systemctl condrestart $param1.service ;;
+			*) echo "unrecognized option $param2";;
+		esac
+	fi
 fi
