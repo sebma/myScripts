a4076da19712c9abba3a88c76668b31a6f074e5c
sebma <sebma@users.noreply.github.com>
2020-01-31 13:54:43 +0100

    Updated 2MacOSX.sh

--- shl/2MacOSX.sh
+++ shl/2MacOSX.sh
@@ -16,9 +16,10 @@ time sudo apt update
 
 case $ubuntuRelease in
 	14.04|14.10)
-		appList="mbuntu-y-ithemes-v4 mbuntu-y-icons-v4 mbuntu-y-bscreen-v4 mbuntu-y-lightdm-v4 slingscold indicator-synapse libreoffice-style-sifr appmenu-qt appmenu-qt5 plasma-widget-menubar";;
+		appList="mbuntu-y-ithemes-v4 mbuntu-y-icons-v4 mbuntu-y-bscreen-v4 mbuntu-y-lightdm-v4 slingscold indicator-synapse libreoffice-style-sifr appmenu-qt appmenu-qt5 plasma-widget-menubar"
 		cd && wget -O config.sh http://drive.noobslab.com/data/Mac-14.10/config.sh
 		chmod +x config.sh;./config.sh
+		;;
 	15.04) appList="mbuntu-y-ithemes-v5 mbuntu-y-icons-v5 slingscold mutate libreoffice-style-sifr mbuntu-y-lightdm-v5";;
 	15.10) appList="macbuntu-ithemes-v6 macbuntu-icons-v6 slingscold mutate plank macbuntu-plank-theme-v6 libreoffice-style-sifr macbuntu-lightdm-v6 macbuntu-bscreen-v6";;
 	16.04) appList="macbuntu-os-icons-lts-v7 macbuntu-os-ithemes-lts-v7 slingscold albert libreoffice-style-sifr plank macbuntu-os-plank-theme-lts-v7 macbuntu-os-bscreen-lts-v7 macbuntu-os-lightdm-lts-v7";;
