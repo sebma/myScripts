958e38e8a4f39376cb468ecb0f00f87d78f06996
sebma <sebma@users.noreply.github.com>
2020-02-10 13:16:56 +0100

    Updated check_ALL_FS.sh

--- shl/system/check_ALL_FS.sh
+++ shl/system/check_ALL_FS.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-echo "=> Checking all filesystems " >&2
+mount -v -o remount,rw / 2>&1 | busybox tee -a fsck_$(date +%Y%m%d).log
+echo "=> Checking all filesystems " >&2 | busybox tee -a fsck_$(date +%Y%m%d).log
 busybox awk '/^\/dev/{print$1" "$3}' /etc/fstab | while read FS FSTYPE # car pour utiliser le "awk" standard, il serait necessaire de mounter "/usr"
 do
 	echo >&2
@@ -14,4 +15,6 @@ do
 	fi
 	set +x
 	echo >&2
-done 2>&1 | busybox tee fsck_$(date +%Y%m%d).log
+done 2>&1 | busybox tee -a fsck_$(date +%Y%m%d).log
+sync
+mount -v -o remount,ro / 2>&1 | busybox tee -a fsck_$(date +%Y%m%d).log
