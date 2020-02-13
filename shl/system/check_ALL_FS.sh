58b7245c8b10686f8b775eb0be44aa6de48f43b8
sebma <sebma@users.noreply.github.com>
2020-02-13 02:22:46 +0100

    Updated check_ALL_FS.sh

--- shl/system/check_ALL_FS.sh
+++ shl/system/check_ALL_FS.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
-mount -v -o remount,rw / 2>&1 | busybox tee -a fsck_$(date +%Y%m%d).log
-echo "=> Checking all filesystems " >&2 | busybox tee -a fsck_$(date +%Y%m%d).log
-busybox awk '/^\/dev/{print$1" "$3}' /etc/fstab | while read FS FSTYPE # car pour utiliser le "awk" standard, il serait necessaire de mounter "/usr"
+mount -o remount,rw / 2>&1
+echo "=> Checking all filesystems at $(date) ..." >&2 | busybox tee fsck_$(date +%Y%m%d).log
+grep -v "swap" /etc/fstab | busybox awk '/^\/dev/{print$1" "$3}' | while read FS FSTYPE # car pour utiliser le "awk" standard, il serait necessaire de mounter "/usr"
 do
 	echo >&2
 	echo "=> Checking $FS ..." >&2
-	if [ $FSTYPE = btrfs ];then
+	time if [ $FSTYPE = btrfs ];then
 		set -x
 		btrfsck -p --repair $FS
 	else
@@ -16,5 +16,6 @@ do
 	set +x
 	echo >&2
 done 2>&1 | busybox tee -a fsck_$(date +%Y%m%d).log
+date | busybox tee -a fsck_$(date +%Y%m%d).log
 sync
-mount -v -o remount,ro / 2>&1 | busybox tee -a fsck_$(date +%Y%m%d).log
+mount -v -o remount,ro / 2>&1
