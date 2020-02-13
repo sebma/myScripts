3a182de300629fc5634a4b8e2e3d66f44e1ba085
sebma <sebma@users.noreply.github.com>
2020-02-13 19:26:25 +0100

    Updated bluetoothDisconnectAllBlueZ5.sh

---
+++ shl/bluetoothDisconnectAllBlueZ5.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env sh
+
+bluetoothController=$(hciconfig 2>/dev/null | awk -F: '/^\w+:/{print$1;exit}')
+if [ -z "$bluetoothController" ]; then
+	echo "=> ERROR: Could not detect any bluetooth controller." >&2
+	exit 1
+else
+	hciconfig hci0 | grep -q DOWN && sudo hciconfig $bluetoothController up
+	echo "=> bluetoothController = $bluetoothController"
+fi
+
+if ! which bluetoothctl >/dev/null 2>&1; then {
+	echo "=> ERROR: You must install BlueZ v5." >&2
+	exit 2
+}
+fi
+
+bluetoothControllerMACAddress=$(printf "list\nquit\n" | bluetoothctl | awk /^Controller/'{print$2;exit}')
+
+deviceList=$(printf "power on\nscan on\ndevices\nquit\n" | bluetoothctl | grep ^Device)
+if [ -z "$deviceList" ]; then {
+	echo "=> ERROR: Could not find any device." >&2
+	exit 3
+}
+fi
+
+echo "$deviceList" | awk '/^Device/{print$2}' | while read device;
+do
+	set -x
+	echo disconnect $device | bluetoothctl
+	set +x
+done
+
