8f4893abc8af4cb6a4f77d9a4f9f8dd4db094e2f
sebma <sebma@users.noreply.github.com>
2020-02-15 16:23:04 +0100

    Updated recovery/

---
+++ shl/recovery/check_ALL_FS.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+tee="busybox tee"
+awk="busybox awk" # car pour utiliser le "awk" standard, il serait necessaire de mounter "/usr"
+
+mount -o remount,rw /
+{
+	echo "=> Checking all filesystems at $(date) ..." >&2
+
+	grep -v "swap" /etc/fstab | $awk '/^\/dev/{print$1" "$3}' | while read FS FSTYPE
+	time do
+		echo >&2
+		echo "=> Checking $FS ..." >&2
+		if [ $FSTYPE = btrfs ];then
+			set -x
+			btrfsck -p --repair $FS
+		else
+			set -x
+			fsck -p -v $FS
+		fi
+		set +x
+		echo >&2
+	done
+
+	date;sync
+} 2>&1 | $tee fsck_$(date +%Y%m%d).log
+mount -v -o remount,ro /
