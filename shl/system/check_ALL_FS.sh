2462b03ee9d49ae7d36bf647a54e9d00c0ee9e54
sebma <sebma@users.noreply.github.com>
2020-02-15 15:31:37 +0100

    Updated check_ALL_FS.sh

--- shl/system/check_ALL_FS.sh
+++ shl/system/check_ALL_FS.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
-mount -o remount,rw / 2>&1
-echo "=> Checking all filesystems at $(date) ..." >&2 | busybox tee fsck_$(date +%Y%m%d).log
-grep -v "swap" /etc/fstab | busybox awk '/^\/dev/{print$1" "$3}' | while read FS FSTYPE # car pour utiliser le "awk" standard, il serait necessaire de mounter "/usr"
-do
-	echo >&2
-	echo "=> Checking $FS ..." >&2
-	time if [ $FSTYPE = btrfs ];then
-		set -x
-		btrfsck -p --repair $FS
-	else
-		set -x
-		fsck -p -v $FS
-	fi
-	set +x
-	echo >&2
-done 2>&1 | busybox tee -a fsck_$(date +%Y%m%d).log
-date | busybox tee -a fsck_$(date +%Y%m%d).log
-sync
-mount -v -o remount,ro / 2>&1
+tee="busybox tee"
+awk="busybox awk" # car pour utiliser le "awk" standard, il serait necessaire de mounter "/usr"
+
+mount -o remount,rw /
+{
+	echo "=> Checking all filesystems at $(date) ..." >&2
+
+	grep -v "swap" /etc/fstab | $awk '/^\/dev/{print$1" "$3}' | while read FS FSTYPE
+	time do
+		echo >&2
+		echo "=> Checking $FS ..." >&2
+		if [ $FSTYPE = btrfs ];then
+			set -x
+			btrfsck -p --repair $FS
+		else
+			set -x
+			fsck -p -v $FS
+		fi
+		set +x
+		echo >&2
+	done
+
+	date;sync
+} 2>&1 | $tee fsck_$(date +%Y%m%d).log
+mount -v -o remount,ro /
