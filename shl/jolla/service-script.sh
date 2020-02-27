0212d06228b9e50c68072b89fa37f800286b0656
sebma <sebma@users.noreply.github.com>
2020-02-27 16:28:59 +0100

    Updated service-script.sh

--- shl/jolla/service-script.sh
+++ shl/jolla/service-script.sh
@@ -2,18 +2,14 @@
 
 if [ $# != 2 ]
 then
-	echo "Usage : $0 serviceName start|stop|status|restart|reload|condrestart" >&2
+	echo "Usage : $0 serviceName start|stop|status|restart|reload|cat|show" >&2
 	exit 1
 else
 	serviceName=$1
 	action=$2
 	case $action in
-		"start") systemctl start $serviceName.service ;;
-		"status") systemctl status $serviceName.service ;;
-		"stop") systemctl stop enable $serviceName.service ;;
-		"restart") systemctl restart $serviceName.service ;;
-		"reload") systemctl reload $serviceName.service ;;
-		"condrestart") systemctl condrestart $serviceName.service ;;
-		*) echo "unrecognized option $action";;
+		start|stop|status|restart|reload|cat|show) systemctl $action $serviceName;;
+		daemon-reload) systemctl $action;;
+		*) echo "Unknown operation $action" >&2;exit 1;;
 	esac
 fi
