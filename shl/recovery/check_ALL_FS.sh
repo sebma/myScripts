a679f27c7b6938f12c4e4102bc52469479ec478e
sebma <sebma@users.noreply.github.com>
2020-02-29 14:24:49 +0100

    Updated check_ALL_FS.sh

--- shl/recovery/check_ALL_FS.sh
+++ shl/recovery/check_ALL_FS.sh
@@ -5,7 +5,7 @@ type busybox >/dev/null || exit
 [ $USER != root ] && echo "=> ERROR [$0] You must run $0 as root." >&2 && exit 2
 
 currentTarget=$(systemctl -t target | egrep -o '^(emergency|rescue|graphical|multi-user|recovery|friendly-recovery).target')
-echo $currentTarget | egrep -q "(recovery|rescue).target" || echo "=> You must reboot in recovery|rescue mode to run $0." >&2 && exit 3
+echo $currentTarget | egrep -q "(recovery|rescue).target" || { echo "=> You must reboot in recovery|rescue mode to run $0." >&2 && exit 3; }
 
 tee="busybox tee"
 awk="busybox awk" # car pour utiliser le "awk" standard, il serait necessaire de mounter "/usr"
@@ -18,10 +18,10 @@ mount -o remount,rw /
 	echo "=> Checking all filesystems at $(date) ..." >&2
 
 	grep -v "swap" /etc/fstab | $awk '/^\/dev/{print$1" "$3}' | while read FS FSTYPE
-	time do
+	do
 		echo >&2
 		echo "=> Checking $FS ..." >&2
-		if [ $FSTYPE = btrfs ];then
+		time if [ $FSTYPE = btrfs ];then
 			set -x
 			btrfsck -p --repair $FS
 		else
