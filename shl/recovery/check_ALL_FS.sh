960f23a654103ba7a973452697b8a294903a9ab0
sebma <sebma@users.noreply.github.com>
2020-02-29 14:13:02 +0100

    Updated check_ALL_FS.sh

--- shl/recovery/check_ALL_FS.sh
+++ shl/recovery/check_ALL_FS.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 type busybox >/dev/null || exit
+
 [ $USER != root ] && echo "=> ERROR [$0] You must run $0 as root." >&2 && exit 2
+
+currentTarget=$(systemctl -t target | egrep -o '^(emergency|rescue|graphical|multi-user|recovery|friendly-recovery).target')
+echo $currentTarget | egrep -q "(recovery|rescue).target" || echo "=> You must reboot in recovery|rescue mode to run $0." >&2 && exit 3
 
 tee="busybox tee"
 awk="busybox awk" # car pour utiliser le "awk" standard, il serait necessaire de mounter "/usr"
-
 mount -o remount,rw /
 {
+	echo "=> hostname = $(hostname)" >&2
+	echo "=> The current target is <$currentTarget>."
 	echo "=> Unmounting all filesystems ..." >&2
 	umount -av
 	echo "=> Checking all filesystems at $(date) ..." >&2
