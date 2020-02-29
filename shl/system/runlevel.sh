37cd3e42223a233fb5deddeec48cd284103d873c
sebma <sebma@users.noreply.github.com>
2020-02-29 14:12:08 +0100

    Updated runlevel.sh

--- shl/system/runlevel.sh
+++ shl/system/runlevel.sh
@@ -2,8 +2,9 @@
 
 busybox="$(type -p busybox)"
 awk="$busybox awk"
+head="$busybox head"
 if $awk 2>&1 | grep -wq "Usage: awk";then
-	systemctl -t target | egrep '^(emergency|rescue|graphical|multi-user|recovery|friendly-recovery)' | $awk '{print$1;exit}'
+	systemctl -t target | egrep -o '^(emergency|rescue|graphical|multi-user|recovery|friendly-recovery).target' | $head -1
 else
-	systemctl -t target | egrep '^(emergency|rescue|graphical|multi-user|recovery|friendly-recovery)' | head -1
+	systemctl -t target | egrep -o '^(emergency|rescue|graphical|multi-user|recovery|friendly-recovery).target' | head -1 || systemctl -t target | egrep -o -m1 '^(emergency|rescue|graphical|multi-user|recovery|friendly-recovery).target'
 fi
