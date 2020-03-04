f71a6fa00329c1cb0d54df2567ae32173c6dcccc
sebma <sebma@users.noreply.github.com>
2020-03-04 23:00:12 +0100

    Updated check_ALL_FS.sh

--- shl/recovery/check_ALL_FS.sh
+++ shl/recovery/check_ALL_FS.sh
@@ -1,34 +1,25 @@
 #!/bin/bash
 
-logDir=log
-mkdir -p $logDir
-logFile=$logDir/fsck_$(date +%Y%m%d).log
 if mount | grep -q "/usr "; then
-	tee=tee
-	awk=awk
-	cut=cut
-	sed=sed
-	grep=grep
-	egrep=egrep
+	for tool in awk cut egrep grep sed tee;do declare $tool=$tool;done
 else # Si /usr n'est pas monte, on utilise les applets busybox
 	type busybox >/dev/null || exit
-	tee="busybox tee"
-	awk="busybox awk"
-	cut="busybox cut"
-	sed="busybox sed"
-	grep="busybox grep"
-	egrep="busybox egrep"
+	for tool in awk cut egrep grep sed tee;do declare $tool="busybox $tool";done
 fi
 
 [ $USER != root ] && echo "=> ERROR [$0] You must run $0 as root." >&2 && exit 2
 
-currentTarget=$(systemctl -t target | egrep -o '^(emergency|rescue|graphical|multi-user|recovery|friendly-recovery).target')
-echo $currentTarget | egrep -q "(recovery|rescue).target" || { echo "=> You must reboot in recovery|rescue mode to run $0." >&2 && exit 3; }
+currentTarget=$(systemctl -t target | $egrep -o '^(emergency|rescue|graphical|multi-user|recovery|friendly-recovery).target')
+echo $currentTarget | $egrep -q "(recovery|rescue).target" || { echo "=> You must reboot in recovery|rescue mode to run $0." >&2 && exit 3; }
 
 fsTypesList=$(\ls -1 /sbin/fsck.* | $cut -d. -f2)
 fsTypesERE=$(echo $fsTypesList | $sed "s/ /|/g")
 fsTypesCSV=$(echo $fsTypesList | $sed "s/ /,/g")
-storageMounted_FS_List=$(mount | awk "/\<$fsTypesERE\>/"'{print$1}')
+storageMounted_FS_List=$(mount | $awk "/\<$fsTypesERE\>/"'{print$1}')
+
+logDir=log
+mkdir -p $logDir
+logFile=$logDir/fsck_$(date +%Y%m%d).log
 mount -o remount,rw /
 {
 	echo "=> hostname = $(hostname)" >&2
