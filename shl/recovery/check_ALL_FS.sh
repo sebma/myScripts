90d820b5e1d1a418e7636475f096634316e84fc1
sebma <sebma@users.noreply.github.com>
2020-03-04 14:35:40 +0100

    Updated check_ALL_FS.sh

--- shl/recovery/check_ALL_FS.sh
+++ shl/recovery/check_ALL_FS.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
+logDir=log
+mkdir -p $logDir
+logFile=$logDir/fsck_$(date +%Y%m%d).log
 if mount | grep -q "/usr "; then
 	tee=tee
 	awk=awk
+	cut=cut
+	sed=sed
+	grep=grep
+	egrep=egrep
 else # Si /usr n'est pas monte, on utilise les applets busybox
 	type busybox >/dev/null || exit
 	tee="busybox tee"
 	awk="busybox awk"
+	cut="busybox cut"
+	sed="busybox sed"
+	grep="busybox grep"
+	egrep="busybox egrep"
 fi
 
 [ $USER != root ] && echo "=> ERROR [$0] You must run $0 as root." >&2 && exit 2
@@ -14,13 +25,18 @@ fi
 currentTarget=$(systemctl -t target | egrep -o '^(emergency|rescue|graphical|multi-user|recovery|friendly-recovery).target')
 echo $currentTarget | egrep -q "(recovery|rescue).target" || { echo "=> You must reboot in recovery|rescue mode to run $0." >&2 && exit 3; }
 
+fsTypesList=$(\ls -1 /sbin/fsck.* | $cut -d. -f2)
+fsTypesERE=$(echo $fsTypesList | $sed "s/ /|/g")
+fsTypesCSV=$(echo $fsTypesList | $sed "s/ /,/g")
+storageMounted_FS_List=$(mount | awk "/\<$fsTypesERE\>/"'{print$1}')
 mount -o remount,rw /
 {
 	echo "=> hostname = $(hostname)" >&2
 	date
 	echo "=> The current target is <$currentTarget>."
-	echo "=> Unmounting all filesystems ..." >&2
-	umount -av
+	echo "=> Unmounting all storage filesystems ..." >&2
+	echo "=> storageMounted_FS_List = $storageMounted_FS_List" >&2
+	umount -v -t $fsTypesCSV
 	echo "=> Checking all filesystems at $(date) ..." >&2
 
 	grep -v "swap" /etc/fstab | $awk '/^\/dev/{print$1" "$3}' | while read FS FSTYPE
@@ -39,5 +55,6 @@ mount -o remount,rw /
 	done
 
 	date;sync
-} 2>&1 | $tee fsck_$(date +%Y%m%d).log
+} 2>&1 | $tee $logFile
 mount -v -o remount,ro /
+echo "=> Check <$logFile>"
