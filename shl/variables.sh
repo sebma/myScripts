866452dfb3164aa08530095b11e9e81654f782f5
sebma <sebma@users.noreply.github.com>
2020-03-04 23:05:37 +0100

    Updated variables.sh

---
+++ shl/variables.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if mount | grep -q "/usr "; then
+	for tool in awk cut egrep grep sed tee;do typeset $tool=$tool;done
+else # Si /usr n'est pas monte, on utilise les applets busybox
+	type busybox >/dev/null || exit
+	for tool in awk cut egrep grep sed tee;do typeset $tool="busybox $tool";done
+fi
+
+echo "=> \$awk = $awk"
