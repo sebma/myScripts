513712d85135fdecf58087578e5f3efc55b9d08b
sebma <sebma@users.noreply.github.com>
2020-03-05 10:47:51 +0100

    Updated variables.sh

--- shl/variables.sh
+++ shl/variables.sh
@@ -1,14 +1,27 @@
 #!/bin/sh
 
-main() {
-if mount | grep -q "/usr "; then
-	for tool in awk cut egrep grep sed tee;do export $tool=$tool;done
-else # Si /usr n'est pas monte, on utilise les applets busybox
-	type busybox >/dev/null || exit
-	for tool in awk cut egrep grep sed tee;do export $tool="busybox $tool";done
-fi
-
-echo "=> \$awk = $awk"
+declareShellTools () {
+	tools="awk cut egrep grep sed tee"
+	if mount | grep -q "/usr "; then
+		for tool in $tools;do export $tool=$tool;done
+	else # Si /usr n'est pas monte, on utilise les applets busybox
+		type busybox >/dev/null || exit
+		for tool in $tools;do export $tool="busybox $tool";done
+	fi
 }
 
-main
+declareShellTools || exit
+echo "=> \$awk = $awk"
+
+main () {
+	tools="awk cut egrep grep sed tee sort"
+	if mount | grep -q "/usr "; then
+		for tool in $tools;do local $tool=$tool;done
+	else # Si /usr n'est pas monte, on utilise les applets busybox
+		type busybox >/dev/null || exit
+		for tool in $tools;do local $tool="busybox $tool";done
+	fi
+	echo "=> \$tee = $tee"
+}
+main || exit
+echo "=> \$sort = $sort"
