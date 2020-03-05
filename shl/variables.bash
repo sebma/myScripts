9679290673b9454a50d9e5f7173047949f3e2e1f
sebma <sebma@users.noreply.github.com>
2020-03-05 10:27:28 +0100

    Updated variables.bash

--- shl/variables.bash
+++ shl/variables.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if mount | grep -q "/usr "; then
-	for tool in awk cut egrep grep sed tee;do declare $tool="toto $tool";done
+	for tool in awk cut egrep grep sed tee;do declare $tool=$tool;done
 else # Si /usr n'est pas monte, on utilise les applets busybox
 	type busybox >/dev/null || exit
 	for tool in awk cut egrep grep sed tee;do declare $tool="busybox $tool";done
