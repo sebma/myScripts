f1c71a7ad64d6d32fdf3f255910f1bf659d28cd2
sebma <sebma@users.noreply.github.com>
2020-03-09 12:18:04 +0100

    Updated systemType.sh

--- shl/systemType.sh
+++ shl/systemType.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+set -o nounset
+
+tools="awk cut grep egrep sed head tail xargs tee sort strings"
+if mount | grep -q "/usr "; then
+	for tool in $tools;do declare $tool=$tool;done
+else # Si /usr n'est pas monte, on utilise les applets busybox
+	type busybox >/dev/null || exit
+	for tool in $tools;do declare $tool="busybox $tool";done
+fi
+
 function systemType {
-	local cut="busybox cut"
-	local head="busybox head"
-	local strings="busybox strings"
 	local initPath=$(\ps -p 1 o cmd= | $cut -d" " -f1)
-	$strings $initPath | egrep -o "upstart|sysvinit|systemd" | $head -1
+	$strings $initPath | $egrep -o "upstart|sysvinit|systemd" | $head -1
 }
 systemType
