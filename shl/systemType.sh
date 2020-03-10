9180b8a95ad453f948a9ad7010c2db0998a84c0c
sebma <sebma@users.noreply.github.com>
2020-03-10 12:01:47 +0100

    Updated systemType.sh

--- shl/systemType.sh
+++ shl/systemType.sh
@@ -3,9 +3,9 @@
 set -o nounset
 
 tools="awk cut grep egrep sed head tail xargs tee sort strings"
-if mount | grep -q "/usr "; then
+if [ -s /usr/bin/tee ]; then # Si "/usr" est accessible
 	for tool in $tools;do declare $tool=$tool;done
-else # Si /usr n'est pas monte, on utilise les applets busybox
+else # Si /usr n'est pas accessible, on utilise les applets busybox
 	type busybox >/dev/null || exit
 	for tool in $tools;do declare $tool="busybox $tool";done
 fi
