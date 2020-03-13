9c31a0226119d2295930addebed4da7a1293bdb0
sebma <sebma@users.noreply.github.com>
2020-03-13 01:28:26 +0100

    Updated bluetoothDeviceReconnect.sh

---
+++ shl/bluetoothDeviceReconnect.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+function bluetoothDeviceReconnect {
+	( [ $# != 1 ] || echo $1 | egrep -q -- "^--?(h|u)" ) && echo "=> Usage : $FUNCNAME bluetoothDeviceName|bluetoothDeviceMacAddr" 1>&2 && return 1
+	local firstArg=$1
+	local bluetoothDeviceName=null
+	local bluetoothDeviceMacAddr=0
+
+	if echo $firstArg | grep -q :; then
+		bluetoothDeviceMacAddr=$firstArg 
+	else
+		bluetoothDeviceName=$firstArg
+	fi
+
+	bluetoothDeviceMacAddr=$(echo | bluetoothctl 2>/dev/null | awk "/Device.*($bluetoothDeviceMacAddr|$bluetoothDeviceName)/"'{print$4;exit}')
+	if [ -n "$bluetoothDeviceMacAddr" ];then
+		set -x
+		echo disconnect $bluetoothDeviceMacAddr | bluetoothctl 2>/dev/null
+		sleep 2
+		echo connect $bluetoothDeviceMacAddr | bluetoothctl 2>/dev/null
+		set +x
+	fi
+}
+
+bluetoothDeviceReconnect "$@"
