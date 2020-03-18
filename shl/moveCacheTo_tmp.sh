976fee025f2345c16460ffffa1bb31096c8dc1da
sebma <sebma@users.noreply.github.com>
2020-03-18 16:58:57 +0100

    Updated moveCacheTo_tmp.sh

--- shl/moveCacheTo_tmp.sh
+++ shl/moveCacheTo_tmp.sh
@@ -1,17 +1,24 @@
 #!/usr/bin/env bash
 
-#Creation du .cache dans /tmp et pointage de ce dernier via un lien symbolique
-if [ $(uname -s) = Linux ] || [ $(uname -s) = Darwin ]
-then
-	myCacheRootDir=/tmp/$LOGNAME
-	if [ ! -L ~/.cache ]
+#Creation du .cache dans /tmp/$LOGNAME et pointage de ce dernier via le lien symbolique ~/.cache
+function moveCacheTo_tmp {
+	local osFamily=$(uname -s)
+	local cacheLinkName=.cache
+	local myCacheRootDir=/tmp/$LOGNAME
+	cd $HOME
+	if [ $osFamily = Linux ] || [ $osFamily = Darwin ]
 	then
-		mkdir -pv $myCacheRootDir/
-		test -d ~/.cache && mv ~/.cache $myCacheRootDir/ || mkdir -pv $myCacheRootDir/.cache/
-		cd $HOME && ln -sf $myCacheRootDir/.cache
-	else
-		#Si le lien symbolique existe mais qu'il ne pointe sur rien alors on cree la cible du lien symbolique
-		test -d $myCacheRootDir/.cache/ || mkdir -pv $myCacheRootDir/.cache/
+		if [ ! -L ~/.cache ]
+		then
+			mkdir -pv $myCacheRootDir/
+			test -d ~/.cache && mv ~/.cache $myCacheRootDir/ || mkdir -pv $myCacheRootDir/.cache/
+			cd $HOME && ln -sf $myCacheRootDir/.cache
+		else
+			#Si le lien symbolique existe mais qu'il ne pointe sur rien alors on cree la cible du lien symbolique
+			test -d $myCacheRootDir/.cache/ || mkdir -pv $myCacheRootDir/.cache/
+		fi
+		echo
 	fi
-	echo
-fi
+}
+
+moveCacheTo_tmp
