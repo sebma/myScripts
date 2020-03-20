cb67ab938e3fd3f4763997ca3a547c353f2f0f70
sebma <sebma@users.noreply.github.com>
2020-03-20 17:17:05 +0100

    Updated setDefaultMailer.sh

---
+++ shl/setDefaultMailer.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+[ $# = 0 ] && mailerList="thunderbird kmail claws-mail geary evolution mailspring" || mailerList="$@"
+
+for mailer in $mailerList
+do
+	which $mailer >/dev/null 2>&1 && defaultMailer=$mailer && break
+done
+
+echo "=> defaultMailer = $defaultMailer"
+desktopFile=$(basename $(dpkg -L $defaultMailer | grep desktop$))
+echo "=> desktopFile = $desktopFile"
+echo "=> Association du protocole mailto avec $desktopFile ..."
+xdg-mime default $desktopFile x-scheme-handler/mailto
+echo "=> Verification ..."
+xdg-mime query default x-scheme-handler/mailto
+#echo "=> xdg-settings set default-web-mailer $desktopFile ..."
+#xdg-settings set default-web-mailer $desktopFile
+#xdg-settings get default-web-mailer
+echo "=> Fait."
