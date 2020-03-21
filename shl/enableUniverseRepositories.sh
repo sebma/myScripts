a6a4595259eda3a71355b86259621160a6e3bcb6
sebma <sebma@users.noreply.github.com>
2020-03-21 20:03:11 +0100

    Updated enableUniverseRepositories.sh

--- shl/enableUniverseRepositories.sh
+++ shl/enableUniverseRepositories.sh
@@ -2,7 +2,8 @@
 
 for repo in universe multiverse
 do
-  sudo software-properties-gtk --enable-component=$repo
+#	sudo software-properties-gtk --enable-component=$repo
+	sudo add-apt-repository $repo
 done
 
 sudo apt-get update -qq
