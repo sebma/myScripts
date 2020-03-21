91895745709ae2cdf7cc771e300f854e23d12721
sebma <sebma@users.noreply.github.com>
2020-03-21 16:12:40 +0100

    Updated youtube_urlsFile2m3u.sh

--- shl/youtube_urlsFile2m3u.sh
+++ shl/youtube_urlsFile2m3u.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 if [ $# = 0 ];then
 	echo "=> Usage $(basename $0) urlsFile" >&2
@@ -9,7 +9,7 @@ urlsFile="$1"
 
 export LANG=fr_FR.UTF-8
 echo "#EXTM3U"
-awk '{print$1}' $urlsFile | uniq | while read url
+time awk '{print$1}' $urlsFile | uniq | while read url
 do
 	printf "#EXTINF:-1,"
 	\curl -Ls $url | awk -F'"' /og:title/'{print$4}'
