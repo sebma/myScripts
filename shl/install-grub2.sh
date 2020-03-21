2d6b95e7ebf570cbaf44375b30ee15c5d6eaf18a
sebma <sebma@users.noreply.github.com>
2020-03-21 18:46:48 +0100

    Updated install-grub2.sh

--- shl/install-grub2.sh
+++ shl/install-grub2.sh
@@ -12,8 +12,8 @@ echo "pendriveDevice = $pendriveDevice"
 
 pendriveMountPoint=$(df | grep $pendriveDevice | awk '{print $NF}')
 test "$pendriveMountPoint" || {
-  echo "=> Erreur: Veuillez d abord monter $pendriveDevice"
-  exit 1
+	echo "=> Erreur: Veuillez d abord monter $pendriveDevice"
+	exit 1
 }
 echo "pendriveMountPoint = $pendriveMountPoint"
 
@@ -22,10 +22,10 @@ echo "partitionDevice = $partitionDevice"
 
 fsType=$(df -T | grep $pendriveDevice | awk '{print $2}')
 case $fsType in
-  ext4) fsType=ext2 ;;
-  vfat) fsType=fat ;;
-  fuseblk) fsType=ntfs ;;
-  *) ;;
+	ext4) fsType=ext2 ;;
+	vfat) fsType=fat ;;
+	fuseblk) fsType=ntfs ;;
+	*) ;;
 esac
 
 echo "fsType = $fsType"
@@ -33,30 +33,30 @@ echo "fsType = $fsType"
 
 echo $partitionDevice | grep -q "${pendriveDevice}[^1]" && cd /media/boot/.. || cd "$pendriveMountPoint"
 grubDir=boot/grub
-  echo "=> currDir= `pwd`"
+	echo "=> currDir= `pwd`"
 	mkdir -vp $grubDir/
-  sudo grub-mkdevicemap -m $grubDir/device.map
-#  sudo grub-mkconfig -o $grubDir/grub.cfg
-  echo "Utilisation de grub-install version $(grub-install -v | awk '{print$NF}')"
-  date +"Il est %T, lancement de grub-install (cela peut durer jusqu'a 4 minutes suivant la version de grub)..."
-  sudo time -p grub-install --recheck --root-directory=. $pendriveDevice --modules=$fsType
-  locate unicode.pf2 | grep /usr | head -1 | xargs -ri sudo cp -v {} $grubDir/
+	sudo grub-mkdevicemap -m $grubDir/device.map
+#	sudo grub-mkconfig -o $grubDir/grub.cfg
+	echo "Utilisation de grub-install version $(grub-install -V | awk '{print$NF}')"
+	date +"Il est %T, lancement de grub-install (cela peut durer jusqu'a 4 minutes suivant la version de grub)..."
+	sudo time -p grub-install --recheck --root-directory=. $pendriveDevice --modules=$fsType
+	locate unicode.pf2 | grep /usr | head -1 | xargs -ri sudo cp -v {} $grubDir/
 	sudo cp -v $(locate -r syslinux/memdisk$) $grubDir/
 #	See /usr/share/X11/xkb/symbols/*
-  grub-kbdcomp -o fr.gkb fr || ckbcomp fr | sudo grub-mklayout -v -o $grubDir/fr.gkb
-  grub-kbdcomp -o en.gkb gb || ckbcomp gb | sudo grub-mklayout -v -o $grubDir/en.gkb
-  grub-kbdcomp -o us.gkb us || ckbcomp us | sudo grub-mklayout -v -o $grubDir/us.gkb
-  test -s /etc/grub.d/40_custom && {
-    grep -q at_keyboard /etc/grub.d/40_custom || echo terminal_input at_keyboard
-    grep -q keylayouts /etc/grub.d/40_custom || echo insmod keylayouts
-    grep -q keymap /etc/grub.d/40_custom || echo keymap fr
-  } | sudo tee -a /etc/grub.d/40_custom
-  test -s $grubDir/grub.cfg && {
-    grep -q at_keyboard $grubDir/grub.cfg || echo terminal_input at_keyboard
-    grep -q keylayouts $grubDir/grub.cfg || echo insmod keylayouts
-    grep -q keymap $grubDir/grub.cfg || echo keymap fr
-  } | sudo tee -a $grubDir/grub.cfg
-  sync
-  cd
+	grub-kbdcomp -o fr.gkb fr || ckbcomp fr | sudo grub-mklayout -v -o $grubDir/fr.gkb
+	grub-kbdcomp -o en.gkb gb || ckbcomp gb | sudo grub-mklayout -v -o $grubDir/en.gkb
+	grub-kbdcomp -o us.gkb us || ckbcomp us | sudo grub-mklayout -v -o $grubDir/us.gkb
+	test -s /etc/grub.d/40_custom && {
+		grep -q at_keyboard /etc/grub.d/40_custom || echo terminal_input at_keyboard
+		grep -q keylayouts /etc/grub.d/40_custom || echo insmod keylayouts
+		grep -q keymap /etc/grub.d/40_custom || echo keymap fr
+	} | sudo tee -a /etc/grub.d/40_custom
+	test -s $grubDir/grub.cfg && {
+		grep -q at_keyboard $grubDir/grub.cfg || echo terminal_input at_keyboard
+		grep -q keylayouts $grubDir/grub.cfg || echo insmod keylayouts
+		grep -q keymap $grubDir/grub.cfg || echo keymap fr
+	} | sudo tee -a $grubDir/grub.cfg
+	sync
+	cd
 	sudo parted $pendriveDevice set 1 boot on
-  sudo umount -v $pendriveMountPoint && test -d $pendriveMountPoint && sudo rmdir -v $pendriveMountPoint
+	sudo umount -v $pendriveMountPoint && test -d $pendriveMountPoint && sudo rmdir -v $pendriveMountPoint
