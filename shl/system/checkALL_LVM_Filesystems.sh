5989b246c3bc8831ba0d4de801b4deff14bbb881
sebma <sebma@users.noreply.github.com>
2020-03-26 09:09:31 +0100

    Updated checkALL_LVM_Filesystems*

--- shl/system/checkALL_LVM_Filesystems.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-#fsck -N /dev/mapper/* 2>/dev/null | egrep -v "/control|^fsck\>" | sort | awk '/btrfs/{print"btrfsck --repair -p "$NF}!/btrfs/{notFound+=1;if(notFound==1)printf"fsck -ps ";else printf$NF" ";}END{print""}'
-fsck -N /dev/mapper/* 2>/dev/null | egrep -v "/control|^fsck\>" | sort | awk '/btrfs/{print"btrfsck --repair -p "$NF}!/btrfs/{print"fsck -p "$NF}' | while read line;
-do
-  FS=$(echo $line | awk '{print$NF}')
-  mount | \grep -wq $FS || echo $line
-done | sh -x
