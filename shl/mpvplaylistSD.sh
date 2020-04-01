fcbdf3d0ae1206a58cbac1da6e65b8f4614c2dbd
sebma <sebma@users.noreply.github.com>
2020-04-01 12:39:16 +0200

    Updated mpvplaylistSD.sh

--- shl/mpvplaylistSD.sh
+++ shl/mpvplaylistSD.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-test $# != 0 && touch nohup.out && sleep 1 && LANG=en_US.utf8 nohup mpv --ytdl-format="best[height<=480]/mp4/best" --ytdl-raw-options=abort-on-error= --playlist $1 &
-test $# != 0 && tail -f nohup.out
+test $# != 0 && LANG=en_US.utf8 nohup mpv --ytdl-format="best[height<=480]/mp4/best" --ytdl-raw-options=abort-on-error= --playlist $1 &
+test $# != 0 && touch nohup.out && tail -f nohup.out
