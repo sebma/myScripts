f001090788692a68338d72e84de2c51eaebe7d2d
sebma <sebma@users.noreply.github.com>
2020-04-01 12:16:17 +0200

    Updated mpvplaylistSD.sh

--- shl/mpvplaylistSD.sh
+++ shl/mpvplaylistSD.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-test $# != 0 && nohup nohup mpv --ytdl-format='best[height<=480]/mp4/best' --ytdl-raw-options=abort-on-error= --playlist $1 &
+test $# != 0 && nohup mpv --ytdl-format='best[height<=480]/mp4/best' --ytdl-raw-options=abort-on-error= --playlist $1 &
 tail -f nohup.out
