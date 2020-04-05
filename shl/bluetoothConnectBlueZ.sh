73a8b523b5d64917755d64fe75d263e55a284c80
sebma <sebma@users.noreply.github.com>
2020-04-05 07:58:51 +0200

    Updated bluetoothConnectBlueZ.sh

--- shl/bluetoothConnectBlueZ.sh
+++ shl/bluetoothConnectBlueZ.sh
@@ -21,7 +21,7 @@ if which bluetoothctl >/dev/null 2>&1; then
 
 	echo "=> bluetoothControllerMACAddress = $bluetoothControllerMACAddress"
 
-	deviceList=$(printf "power on\nscan on\ndevices\nquit\n" | bluetoothctl | grep ^Device)
+	deviceList=$(printf "power on\nscan on\ndevices\nquit\n" | bluetoothctl | grep ^Device | sed 's/^.* Device/Device/')
 	if [ -z "$deviceList" ]; then {
 		echo "=> ERROR: Could not find any device to connect to." >&2
 		exit 3
@@ -44,7 +44,7 @@ if which bluetoothctl >/dev/null 2>&1; then
 	fi
 	
 	if echo "$deviceList" | awk '/^Device/{print$NF}' | grep -q "$deviceName"; then {
-		deviceHW=$(echo "$deviceList" | awk /^Device.*$deviceName/'{print$2}')
+		deviceHW=$(echo "$deviceList" | awk /^Device.*$deviceName/'{print$2}' | sort -u)
 		cat<<EOF | bluetoothctl -a
 	select $bluetoothControllerMACAddress
 	power on
