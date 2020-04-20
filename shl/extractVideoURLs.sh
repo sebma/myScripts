87c34436a28605e144032323648a8734cf172b12
sebma <sebma@users.noreply.github.com>
2020-04-20 21:34:18 +0200

    Updated extractVideoURLs.sh

--- shl/extractVideoURLs.sh
+++ shl/extractVideoURLs.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-for url
-do
-	fqdn=$(echo $url | cut -d/ -f1-3)
-	domain=$(echo $fqdn | awk -F '[.]|/' '{print $(NF-1)}')
-	\curl -qs $url | grep -oP 'video\b.*href="\K/[^/][^ &"]+' | uniq | sed "s|^|$fqdn|"
-done
+function extractVideoURLs {
+	for url
+	do
+		fqdn=$(echo $url | cut -d/ -f1-3)
+		domain=$(echo $fqdn | awk -F '[.]|/' '{print $(NF-1)}')
+		\curl -qs $url | grep -oP 'video\b.*href="\K/[^/][^ &"]+' | uniq | sed "s|^|$fqdn|"
+	done
+}
+
+extractVideoURLs "$@"
