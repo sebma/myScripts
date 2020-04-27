fdae908d175deb046a3ec662d245bf461128c00b
sebma <sebma@users.noreply.github.com>
2020-04-27 12:43:40 +0200

    Updated extract_urls_from_ok.ru_html.sh

--- shl/extract_urls_from_ok.ru_html.sh
+++ shl/extract_urls_from_ok.ru_html.sh
@@ -7,4 +7,4 @@ test $# != 1 && {
 }
 
 ok_ru_htmlFile=$1
-cat $ok_ru_htmlFile | pup 'json{}' | jq -r 'recurse | arrays[] | select(.class == "video-card_n ellip").title,select(.class == "video-card_lk").href' | awk '{videoRelativeURL=$0;url="https://ok.ru"gensub("?.*$","",videoRelativeURL); getline title; print url" # "title}'
+cat $ok_ru_htmlFile | pup 'json{}' | jq -r 'recurse | arrays[] | select(.class == "video-card_n ellip").title,select(.class == "video-card_lk").href' | awk '{videoRelativeURL=$0;url="https://ok.ru"gensub("?.*$",1,videoRelativeURL); getline title; print url" # "title}'
