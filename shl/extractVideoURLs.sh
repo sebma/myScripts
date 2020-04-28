2bbd5ae0ca6b1d4df041c38334447ac8e8f1d386
sebma <sebma@users.noreply.github.com>
2020-04-28 17:22:48 +0200

    Updated extractVideoURLs.sh

--- shl/extractVideoURLs.sh
+++ shl/extractVideoURLs.sh
@@ -4,8 +4,9 @@ function extractVideoURLs {
 	for url
 	do
 		urlBase=$(echo "$url" | cut -d/ -f1-3)
-		fqdn=$(echo "$url" | cut -d/ -f3 | awk -F. '{print$(NF-1)"."$NF}')
-		domain=$(echo $fqdn | awk -F. '{print $(NF-1)}')
+		fqdn=$(echo "$url" | cut -d/ -f3)
+		domain=$(echo $fqdn | awk -F. '{print$(NF-1)"."$NF}')
+		sld=$(echo $fqdn | awk -F. '{print $(NF-1)}') # Second level domain
 		\curl -qs $url | grep -w video | grep -oP 'href="\K/[^/][^ &"]+' | uniq | sed "s|^|$urlBase|"
 	done
 }
