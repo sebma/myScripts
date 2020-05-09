ab6506377db8fb4ef37f53585aaababbb40647f2
sebma <sebma@users.noreply.github.com>
2020-05-09 20:04:29 +0200

    Updated addThumbnail2media.sh

--- shl/addThumbnail2media.sh
+++ shl/addThumbnail2media.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
 addThumbnail2media() {
+	local scriptOptions=null
+	echo $1 | \grep -q -- "^-[a-z]" && scriptOptions=$1 && shift
+
 	if [ $# != 2 ];then
-		echo "=> [$FUNCNAME] $FUNCNAME mediaFile artworkFile" 1>&2
+		echo "=> Usage: $FUNCNAME [-v] mediaFile artworkFile" 1>&2
 		exit 1
 	fi
 
+	echo $scriptOptions | \grep -q -- "-v" && debug="set -x"
 	local fileName="$1"
 	local artworkFileName="$2"
 
@@ -13,18 +17,21 @@ addThumbnail2media() {
 	local ffprobe="$(which ffprobe)"
 	local jq="$(which jq)"
 
-	ffmpeg+="-hide_banner"
-	ffprobe+="-hide_banner"
+	ffmpeg+=" -hide_banner"
+	ffprobe+=" -hide_banner"
 
 	local ffmpegNormalLogLevel=repeat+error
 	local ffmpegInfoLogLevel=repeat+info
 	local ffmpegLogLevel=$ffmpegNormalLogLevel
 
 	local ffprobeJSON_File_Info=$($ffprobe -v error -show_format -show_streams -print_format json "$fileName")
+#	numberOfVideoStreams=$(echo $ffprobeJSON_File_Info | $jq -r '[ .streams[] | select(.codec_type=="video") ] | length'
 	local latestVideoStreamCodecName=$(echo $ffprobeJSON_File_Info | $jq -r '[ .streams[] | select(.codec_type=="video") ][-1].codec_name')
 
 	local videoContainer=$(echo $ffprobeJSON_File_Info | $jq -r .format.format_name | cut -d, -f1)
 	local major_brand=$(echo $ffprobeJSON_File_Info | $jq -r .format.tags.major_brand)
+
+	[ "$debug" ] && echo "=> videoContainer = <$videoContainer>  latestVideoStreamCodecName = <$latestVideoStreamCodecName> major_brand = <$major_brand>" && echo
 
 	if [ -s "$artworkFileName" ] && [ "$latestVideoStreamCodecName" != mjpeg ] && [ "$latestVideoStreamCodecName" != png ];then
 		timestampFileRef=$(mktemp) && touch -r "$fileName" $timestampFileRef
