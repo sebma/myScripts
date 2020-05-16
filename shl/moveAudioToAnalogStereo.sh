8ea6328336baa53e9efa1a277b57381b3429d898
sebma <sebma@users.noreply.github.com>
2020-05-16 09:33:45 +0200

    Updated moveAudioToAnalogStereo.sh

--- shl/moveAudioToAnalogStereo.sh
+++ shl/moveAudioToAnalogStereo.sh
@@ -6,7 +6,7 @@ function moveAudioToAnalogStereo {
 	pactl list short sink-inputs
 
 	analogStereoOutputPattern='\.analog-stereo\>'
-	sink_output=$(pactl list short sinks | awk "/$analogStereoOutputPattern/"'{printf$1}')
+	sink_output=$(pactl list short sources | awk "/$analogStereoOutputPattern/"'{printf$1}')
 	echo "=> sink_output = $sink_output"
 	pactl list short sink-inputs | awk '/protocol-native.c/{print$1}' | while read sink_input
 	do
