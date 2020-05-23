cf7c69bcfa9ac00fae7666b9b4129ce13b56ace9
sebma <sebma@users.noreply.github.com>
2020-05-23 18:00:15 +0200

    Updated moveAudio2AnalogStereo.sh

---
+++ shl/moveAudio2AnalogStereo.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+function moveAudioToAnalogStereo {
+	echo "=> BEFORE :"
+	! pactl list short sinks >/dev/null 2>&1 && test "$SSH_CONNECTION" && pax11publish -r
+	pactl list short sink-inputs
+
+	analogStereoOutputPattern='\.analog-stereo\>'
+	sink_output=$(pactl list short sources | awk "/$analogStereoOutputPattern/"'{printf$1}')
+	echo "=> sink_output = $sink_output"
+	pactl list short sink-inputs | awk '/protocol-native.c/{print$1}' | while read sink_input
+	do
+		pactl move-sink-input $sink_input $sink_output
+	done
+
+	echo "=> AFTER :"
+	pactl list short sink-inputs
+}
+
+moveAudioToAnalogStereo "$@"
