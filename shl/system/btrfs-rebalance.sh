dcc35b388efac518fb369fc984f0ed1a95b0714c
sebma <sebma@users.noreply.github.com>
2020-05-23 15:32:31 +0200

    Updated btrfs-rebalance.sh

--- shl/system/btrfs-rebalance.sh
+++ shl/system/btrfs-rebalance.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
+trap 'rc=127;set +x;echo "=> $scriptBaseName: CTRL+C Interruption trapped.">&2;exit $rc' INT
+scriptBaseName=${0##*/}
 if [ $# != 1 ];then
-	echo "=> Usage: $0 mountPoint"
+	echo "=> Usage: $scriptBaseName mountPoint"
 	exit 1
 fi
 
 mountPoint=$1
 time for p in $(seq 0 5 95);do
-	echo "$0: Running with $p%"
+	echo "[$scriptBaseName] Running with $p% ..."
 	time sudo btrfs balance start -dusage=$p -musage=$p $mountPoint
 done
+trap - INT
