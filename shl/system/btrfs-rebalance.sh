82766185a68152a3cbce4b7aa86028dc47a41fd6
sebma <sebma@users.noreply.github.com>
2020-05-23 15:47:10 +0200

    Updated btrfs-rebalance.sh

--- shl/system/btrfs-rebalance.sh
+++ shl/system/btrfs-rebalance.sh
@@ -12,6 +12,7 @@ mountPoint=$1
 time for p in $(seq 0 5 95);do
 	echo "[$scriptBaseName] Running with $p% ..."
 	time sudo btrfs balance start -dusage=$p -musage=$p $mountPoint
+	echo
 done
 sudo btrfs filesystem usage -T $mountPoint
 
