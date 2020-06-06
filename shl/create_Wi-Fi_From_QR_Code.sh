529fed8068ade28637c857fe29b1a5a4a1fce6ac
sebma <sebma@users.noreply.github.com>
2020-06-06 15:44:03 +0200

    Updated create_Wi-Fi_From_QR_Code.sh

--- shl/create_Wi-Fi_From_QR_Code.sh
+++ shl/create_Wi-Fi_From_QR_Code.sh
@@ -16,8 +16,20 @@ function create_Wi-Fi_From_QR_Code {
 	read ssid security pass hidden <<< $($qrdecode "$qrcodePictureFile" | awk -F":|;" '/WIFI:/{print$3" "$5" "$7" "$9}')
 	[ $hidden = true ] && hidden=yes || hidden=no
 	readonly ssid security pass hidden
-	echo nmcli device wifi connect $ssid password xxxxxxxxxxxx name ${ssid}_TEST hidden $hidden
-	nmcli device wifi connect $ssid password "$pass" name ${ssid}_TEST hidden $hidden
+#	local nmcliVersion=$(nmcli -v | awk -F"[. ]" '/version/{printf"%d.%d%02d\n", $4, $5, $6}')
+	local nmcliVersion=$(nmcli -v | awk '/version/{print$NF}')
+	if versionSmallerEqual $nmcliVersion 0.9.10;then
+		echo "=>" nmcli device wifi connect $ssid password xxxxxxxxxxxx name ${ssid}_TEST
+		nmcli device wifi connect $ssid password "$pass" name ${ssid}_TEST
+	else
+		echo "=>" nmcli device wifi connect $ssid password xxxxxxxxxxxx name ${ssid}_TEST hidden $hidden
+		nmcli device wifi connect $ssid password "$pass" name ${ssid}_TEST hidden $hidden
+	fi
+}
+function versionSmallerEqual {
+	version1=$1
+	version2=$2
+	return $( perl -Mversion -e "exit ! ( version->parse( $version1 ) <= version->parse( $version2 ) )" )
 }
 
 create_Wi-Fi_From_QR_Code "$@"
