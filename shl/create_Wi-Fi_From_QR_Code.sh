fe6b21f6eae45720723c5eb7d1dbf067477e5b0b
sebma <sebma@users.noreply.github.com>
2020-06-06 16:26:04 +0200

    Updated create_Wi-Fi_From_QR_Code.sh

--- shl/create_Wi-Fi_From_QR_Code.sh
+++ shl/create_Wi-Fi_From_QR_Code.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 scriptBaseName=${0/*\/}
 function create_Wi-Fi_From_QR_Code {
+#	local wifiInterface="$(iwconfig 2>/dev/null | awk '/^[^ \t]/ { if ($1 ~ /^[0-9]+:/) { lastInterface=$2 } else { lastInterface=$1 } }END{print lastInterface}')"
+	local wifiInterface=$(iw dev 2>/dev/null | awk '/Interface/{lastInterface=$NF}END{print lastInterface}')
+
+	if ! iwlist $wifiInterface frequency | grep -q '5\..* GHz' 2>/dev/null || ! iw phy | egrep -q "[*] 5[0-9]{3} MHz";then
+		echo "=> WARNING: Your Wi-Fi cannot connect to 5GHz networks." >&2
+	fi
+
 	if [ $# != 1 ];then
 		echo "=> Usage: $scriptBaseName qrcodePictureFile" >&2
 		return 1
@@ -11,8 +18,14 @@ function create_Wi-Fi_From_QR_Code {
 		return 2
 	fi
 
+	local qrdecode=$(which zbarimg)
+	if [ -z "$qrdecode" ];then
+		echo "=> ERROR: You must install the <zbarimg> utility." >&2
+		return 3
+	fi
+
+	qrdecode+=" -q --raw"
 	local nmcliVersion=$(nmcli -v | awk '/version/{print$NF}')
-	local qrdecode="zbarimg -q --raw"
 	local ssid security pass hidden
 
 	read ssid security pass hidden <<< $($qrdecode "$qrcodePictureFile" | awk -F":|;" '/WIFI:/{print$3" "$5" "$7" "$9}')
