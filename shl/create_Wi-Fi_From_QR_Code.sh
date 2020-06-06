30b25740bf47f7f26bbe84c55624cd669e910644
sebma <sebma@users.noreply.github.com>
2020-06-06 15:10:22 +0200

    Updated create_Wi-Fi_From_QR_Code.sh

--- shl/create_Wi-Fi_From_QR_Code.sh
+++ shl/create_Wi-Fi_From_QR_Code.sh
@@ -9,7 +9,11 @@ function create_Wi-Fi_From_QR_Code {
 	local qrdecode="zbarimg -q --raw"
 	local ssid security pass hidden
 	read ssid security pass hidden <<< $($qrdecode "$qrcodePictureFile" | awk -F":|;" '/WIFI:/{print$3" "$5" "$7" "$9}')
+	[ $hidden = true ] && hidden=yes || hidden=no
 	readonly ssid security pass hidden
+	set -x
+	nmcli device wifi connect $ssid password "$pass" name ${ssid}_TEST hidden $hidden
+	set +x
 }
 
 create_Wi-Fi_From_QR_Code "$@"
