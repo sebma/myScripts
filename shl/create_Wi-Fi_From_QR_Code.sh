d3b7fd53f75394dfa65c06c91d9efd4866db1cbc
sebma <sebma@users.noreply.github.com>
2020-06-06 14:41:25 +0200

    Updated create_Wi-Fi_From_QR_Code.sh

---
+++ shl/create_Wi-Fi_From_QR_Code.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+scriptBaseName=${0/*\/}
+function create_Wi-Fi_From_QR_Code {
+	if [ $# != 1 ];then
+		echo "=> Usage: $scriptBaseName qrcodePictureFile" >&2
+		return 1
+	fi
+	local qrcodePictureFile="$1"
+	local qrdecode="zbarimg -q --raw"
+	local ssid security pass hidden
+	read ssid security pass hidden <<< $($qrdecode "$qrcodePictureFile" | awk -F":|;" '/WIFI:/{print$3" "$5" "$7" "$9}')
+	readonly ssid security pass hidden
+}
+
+create_Wi-Fi_From_QR_Code "$@"
