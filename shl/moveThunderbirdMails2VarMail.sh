fe82dbdfd4ba2d7164f9dc34b0115c6fe99e3997
sebma <sebma@users.noreply.github.com>
2020-07-23 18:31:23 +0200

    Updated moveThunderbirdMails2VarMail.sh

--- shl/moveThunderbirdMails2VarMail.sh
+++ shl/moveThunderbirdMails2VarMail.sh
@@ -2,15 +2,22 @@
 
 varMailPartition=/var/mail
 
-if ! df $varMailPartition | grep $varMailPartition;then
+if ! df $varMailPartition | grep -q $varMailPartition;then
 	echo "=> You must have a separate $varMailPartition partition." >&2
 	exit 1
 fi
 
 rsync=$(which rsync)
 move="$rsync -uth -P -z --skip-compress=$RSYNC_SKIP_COMPRESS_LIST $RSYNC_EXCLUSION -ogpuv -lH --remove-source-files"
-sudo mkdir -p $varMailPartition/thunderbird/$USER
-sudo chown $USER:$USER $varMailPartition/thunderbird/$USER
-$move $HOME/.thunderbird/* $varMailPartition/thunderbird/$USER || exit $?
-ln -svf $varMailPartition/thunderbird/$USER $HOME/.thunderbird
-sync
+
+test -d $varMailPartition/thunderbird/$USER || sudo mkdir -v -p $varMailPartition/thunderbird/$USER
+ls -ld $varMailPartition/thunderbird/$USER | grep -q $USER || sudo chown -v $USER:$USER $varMailPartition/thunderbird/$USER
+
+if [ -e $HOME/.thunderbird ];then
+	if ! [ -L $HOME/.thunderbird ];then
+		$move $HOME/.thunderbird/* $varMailPartition/thunderbird/$USER || exit
+		rmdir -v $HOME/.thunderbird/
+		ln -svf $varMailPartition/thunderbird/$USER $HOME/.thunderbird
+		sync
+	fi
+fi
