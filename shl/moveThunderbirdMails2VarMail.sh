f7918fbab61cbc7517d727c4ae606792a509b605
sebma <sebma@users.noreply.github.com>
2020-07-23 18:49:13 +0200

    Updated moveThunderbirdMails2VarMail.sh

--- shl/moveThunderbirdMails2VarMail.sh
+++ shl/moveThunderbirdMails2VarMail.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
+LANGUAGE=C
 varMailPartition=/var/mail
 
 if ! df $varMailPartition | grep -q $varMailPartition;then
@@ -11,11 +12,12 @@ rsync=$(which rsync)
 move="$rsync -uth -P -z --skip-compress=$RSYNC_SKIP_COMPRESS_LIST $RSYNC_EXCLUSION -ogpuv -lH --remove-source-files"
 
 test -d $varMailPartition/thunderbird/$USER || sudo mkdir -v -p $varMailPartition/thunderbird/$USER
-ls -ld $varMailPartition/thunderbird/$USER | grep -q $USER || sudo chown -v $USER:$USER $varMailPartition/thunderbird/$USER
+ls -ld $varMailPartition/thunderbird/$USER | grep -q "$USER " || sudo chown -v $USER:$USER $varMailPartition/thunderbird/$USER
 
 if [ -e $HOME/.thunderbird ];then
 	if ! [ -L $HOME/.thunderbird ];then
-		$move $HOME/.thunderbird/* $varMailPartition/thunderbird/$USER || exit
+		set -x
+		$move -r $HOME/.thunderbird/* $varMailPartition/thunderbird/$USER || exit
 		rmdir -v $HOME/.thunderbird/
 		ln -svf $varMailPartition/thunderbird/$USER $HOME/.thunderbird
 		sync
