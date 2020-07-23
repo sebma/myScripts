064c7d878736124e9302c460acaaef8fdd33ac57
sebma <sebma@users.noreply.github.com>
2020-07-23 17:54:12 +0200

    Updated moveThunderbirdMails2VarMail.sh

---
+++ shl/moveThunderbirdMails2VarMail.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+varMailPartition=/var/mail
+
+if ! df $varMailPartition | grep $varMailPartition;then
+	echo "=> You must have a separate $varMailPartition partition." >&2
+	exit 1
+fi
+
+sudo mkdir -p $varMailPartition/thunderbird/$USER
+sudo chown $USER:$USER $varMailPartition/thunderbird/$USER
+mv $HOME/.thunderbird/* $varMailPartition/thunderbird/$USER
+ln -svf 
