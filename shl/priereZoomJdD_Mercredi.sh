94639a61bb8e05242d6a16fb1725ea5a1778c6bc
sebma <sebma@users.noreply.github.com>
2020-07-30 13:37:57 +0200

    Updated priereZoomJdD_Mercredi.sh

--- shl/priereZoomJdD_Mercredi.sh
+++ shl/priereZoomJdD_Mercredi.sh
@@ -3,5 +3,9 @@
 priereZoomHttps_URL="https://us04web.zoom.us/j/6378505229?pwd=d3RUR0hxQStIZGRSZEZMeEdveG5mdz09"
 
 zoommtgURL="$(echo "$priereZoomHttps_URL" | sed 's/^https:/zoommtg:/;s|/j/|/join?action=join\&confno=|;s/?pwd=/\&pwd=/')"
-echo "zoom $zoommtgURL & ..."
-zoom $zoommtgURL &
+xdg-mime default Zoom.desktop x-scheme-handler/zoommtg
+#xdg-mime query default x-scheme-handler/zoommtg
+xdg-open $zoommtgURL
+
+#echo "zoom $zoommtgURL & ..."
+#zoom $zoommtgURL &
