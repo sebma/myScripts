44c76481bd798976e4625c0afe37d1d563beb277
sebma <sebma@users.noreply.github.com>
2020-08-08 18:42:05 +0200

    Updated pingMyLAN.sh

--- shl/pingMyLAN.sh
+++ shl/pingMyLAN.sh
@@ -3,10 +3,21 @@
 pingMyLAN ()
 {
 	local myLAN=$(\ip addr show | awk '/inet /{print$2}' | egrep -v '127.0.0.[0-9]|192.168.122.[0-9]')
-	if which fping > /dev/null 2>&1; then
-		time fping -r 0 -aAg $myLAN 2> /dev/null | sort -u
+	if [ $# = 0 ]; then
+		if which fping > /dev/null 2>&1; then
+			time fping -r 0 -aAg $myLAN 2> /dev/null | sort -u
+		else
+			time \nmap -T5 -sP $myLAN | sed -n '/Nmap scan report for /s/Nmap scan report for //p'
+		fi
 	else
-		time \nmap -T5 -sP $myLAN | sed -n '/Nmap scan report for /s/Nmap scan report for //p'
+		time if [ $# = 1 ]; then
+			port=$1
+			if which fping > /dev/null 2>&1; then
+				\nmap -T5 -sP $myLAN | sed -n '/Nmap scan report for /s/Nmap scan report for //p' | while read ip; do
+					tcpConnetTest $ip $port
+				done
+			fi
+		fi
 	fi
 }
 
