5bcc69fed9163ec23c8bf37517348f91723f0ebd
sebma <sebma@users.noreply.github.com>
2020-08-24 15:27:13 +0200

    Updated 2MacOSX.sh

--- shl/2MacOSX.sh
+++ shl/2MacOSX.sh
@@ -16,19 +16,35 @@ time sudo apt update
 
 case $ubuntuRelease in
 	14.04|14.10)
+		version=v4
 		appList="mbuntu-y-ithemes-v4 mbuntu-y-icons-v4 mbuntu-y-bscreen-v4 mbuntu-y-lightdm-v4 slingscold indicator-synapse libreoffice-style-sifr appmenu-qt appmenu-qt5 plasma-widget-menubar"
 		cd && wget -O config.sh http://drive.noobslab.com/data/Mac-14.10/config.sh
 		chmod +x config.sh;./config.sh
 		;;
-	15.04) appList="mbuntu-y-ithemes-v5 mbuntu-y-icons-v5 slingscold mutate libreoffice-style-sifr mbuntu-y-lightdm-v5";;
-	15.10) appList="macbuntu-ithemes-v6 macbuntu-icons-v6 slingscold mutate plank macbuntu-plank-theme-v6 libreoffice-style-sifr macbuntu-lightdm-v6 macbuntu-bscreen-v6";;
-	16.04) appList="macbuntu-os-ithemes-lts-v7 slingscold albert libreoffice-style-sifr plank macbuntu-os-plank-theme-lts-v7 macbuntu-os-bscreen-lts-v7 macbuntu-os-lightdm-lts-v7";;
-	16.10) appList="macbuntu-os-icons-lts-v8 macbuntu-os-ithemes-lts-v8 slingscold albert libreoffice-style-sifr plank macbuntu-os-plank-theme-lts-v8 macbuntu-os-bscreen-lts-v8 macbuntu-os-lightdm-lts-v8";;
-	17.04) appList="macbuntu-os-icons-lts-v9 macbuntu-os-ithemes-lts-v9 slingscold albert libreoffice-style-sifr plank macbuntu-os-plank-theme-lts-v9 macbuntu-os-bscreen-lts-v9 macbuntu-os-lightdm-lts-v9";;
-	17.10) appList="macbuntu-os-icons-lts-v10 macbuntu-os-ithemes-lts-v10 slingscold albert libreoffice-style-sifr plank macbuntu-os-plank-theme-lts-v10 macbuntu-os-bscreen-lts-v10 macbuntu-os-lightdm-lts-v10";;
-	18.04) appList="macbuntu-os-icons-lts-v1804 macbuntu-os-ithemes-lts-v1804 slingscold albert libreoffice-style-sifr plank macbuntu-os-plank-theme-lts-v1804 macbuntu-os-bscreen-lts-v1804 macbuntu-os-lightdm-lts-v1804";;
+	15.04)
+		version=v5
+		appList="mbuntu-y-ithemes-v5 mbuntu-y-icons-v5 slingscold mutate libreoffice-style-sifr mbuntu-y-lightdm-v5";;
+	15.10)
+		version=v6
+		appList="macbuntu-ithemes-v6 macbuntu-icons-v6 slingscold mutate plank macbuntu-plank-theme-v6 libreoffice-style-sifr macbuntu-lightdm-v6 macbuntu-bscreen-v6";;
+	16.04)
+		version=v7
+		appList="macbuntu-os-ithemes-lts-v7 slingscold albert libreoffice-style-sifr plank macbuntu-os-plank-theme-lts-v7 macbuntu-os-bscreen-lts-v7 macbuntu-os-lightdm-lts-v7";;
+	16.10)
+		version=v8
+		appList="macbuntu-os-icons-lts-v8 macbuntu-os-ithemes-lts-v8 slingscold albert libreoffice-style-sifr plank macbuntu-os-plank-theme-lts-v8 macbuntu-os-bscreen-lts-v8 macbuntu-os-lightdm-lts-v8";;
+	17.04)
+		version=v9
+		appList="macbuntu-os-icons-lts-v9 macbuntu-os-ithemes-lts-v9 slingscold albert libreoffice-style-sifr plank macbuntu-os-plank-theme-lts-v9 macbuntu-os-bscreen-lts-v9 macbuntu-os-lightdm-lts-v9";;
+	17.10)
+		version=v10
+		appList="macbuntu-os-icons-lts-v10 macbuntu-os-ithemes-lts-v10 slingscold albert libreoffice-style-sifr plank macbuntu-os-plank-theme-lts-v10 macbuntu-os-bscreen-lts-v10 macbuntu-os-lightdm-lts-v10";;
+	18.04)
+		version=v1804
+		appList="macbuntu-os-icons-lts-v1804 macbuntu-os-ithemes-lts-v1804 slingscold albert libreoffice-style-sifr plank macbuntu-os-plank-theme-lts-v1804 macbuntu-os-bscreen-lts-v1804 macbuntu-os-lightdm-lts-v1804";;
 esac
 
 set -x
 sudo apt install -V $appList $@
+sudo dpkg-reconfigure macbuntu-os-bscreen-lts-$version
 set +x
