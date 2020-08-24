3a564a9d45a5dfb426a998df25a2158f6d3698aa
sebma <sebma@users.noreply.github.com>
2020-08-24 15:19:12 +0200

    Updated installLinux4.4.sh

--- shl/installLinux4.4.sh
+++ shl/installLinux4.4.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env sh
 
-#sudo apt install -V linux-headers-virtual-lts-xenial linux-image-virtual-lts-xenial linux-image-extra-virtual-lts-xenial
-sudo apt install -V linux-generic-lts-xenial linux-headers-generic-lts-xenial linux-image-generic-lts-xenial linux-image-extra-virtual-lts-xenial
+distribName=$(lsb_release -si)
+currentKernelVersion=$(uname -r | cut -d. -f1,2 | tr -d .)
+if [ $distribName = Ubuntu ] && [ $currentKernelVersion -lt 44 ];then
+	#sudo apt install -V linux-headers-virtual-lts-xenial linux-image-virtual-lts-xenial linux-image-extra-virtual-lts-xenial
+	sudo apt install -V linux-generic-lts-xenial linux-headers-generic-lts-xenial linux-image-generic-lts-xenial linux-image-extra-virtual-lts-xenial
+fi
