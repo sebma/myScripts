cab8c28abde4bded046c222feadc6cddbfcdc07d
sebma <sebma@users.noreply.github.com>
2020-09-09 00:52:36 +0200

    Updated mountAllBeforeChroot_LVM.sh

--- shl/system/mountAllBeforeChroot_LVM.sh
+++ shl/system/mountAllBeforeChroot_LVM.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-type sudo >/dev/null 2>&1 && sudo=$(which sudo) || sudo=""
+type sudo >/dev/null 2>&1 && [ $UID != 0 ] && sudo=$(which sudo) || sudo=""
 #set -o nounset
 set -o errexit
+
+for path in /sbin /bin /usr/sbin /usr/bin
+do
+	echo $PATH | grep -q $path || export PATH=$path:$PATH
+done
 
 $sudo lvs | grep -q root || {
 	$sudo pvscan
@@ -15,4 +20,3 @@ $sudo mkdir -pv /mnt/{dev/pts,proc,sys}
 mount | grep -q $rootFSDevice || $sudo mount /dev/mapper/$rootFSDevice /mnt          # montage de celle-ci en remplacant le X par le bon numero de partition
 for i in dev dev/pts proc sys ; do $sudo mount --bind /$i /mnt/$i ; done
 $sudo chroot /mnt /bin/bash
-$sudo umount -v /mnt/{sys,proc,dev/pts,dev,}
