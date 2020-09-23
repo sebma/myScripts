3e118f2d64293f0adaf023e4e6f8f787bf157d98
sebma <sebma@users.noreply.github.com>
2020-09-23 10:57:36 +0200

    Added hw-probeInstall_N_Run.sh

--- shl/hw-probeInstall_N_Run.sh
+++ shl/hw-probeInstall_N_Run.sh
@@ -3,8 +3,8 @@
 ubuntuSources=/etc/apt/sources.list
 grep -q universe $ubuntuSources   || sudo add-apt-repository universe -y
 grep -q multiverse $ubuntuSources || sudo add-apt-repository multiverse -y
-sudo add-apt-repository ppa:unit193/inxi -y
-sudo add-apt-repository ppa:mikhailnov/hw-probe -y
+grep -q "^deb .*unit193/inxi" /etc/apt/sources.list.d/*.list || sudo add-apt-repository ppa:unit193/inxi -y
+grep -q "^deb .*mikhailnov/hw-probe" /etc/apt/sources.list.d/*.list || sudo add-apt-repository ppa:mikhailnov/hw-probe -y
 sudo apt-get update
-sudo apt-get install hw-probe --no-install-recommends -y
-sudo -E hw-probe -all -upload
+sudo apt-get install inxi hw-probe --no-install-recommends -y
+#sudo -E hw-probe -all -upload
