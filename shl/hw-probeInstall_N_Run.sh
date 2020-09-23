e6ce4a6d29a0136104462762dc9dc12472d64aff
sebma <sebma@users.noreply.github.com>
2020-09-23 10:51:33 +0200

    Added hw-probeInstall_N_Run.sh

--- shl/hw-probeInstall_N_Run.sh
+++ shl/hw-probeInstall_N_Run.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 ubuntuSources=/etc/apt/sources.list
-grep -q universe $ubuntuSources   || sudo add-apt-repository universe
-grep -q multiverse $ubuntuSources || sudo add-apt-repository multiverse
-sudo add-apt-repository ppa:unit193/inxi
-sudo add-apt-repository ppa:mikhailnov/hw-probe
+grep -q universe $ubuntuSources   || sudo add-apt-repository universe -y
+grep -q multiverse $ubuntuSources || sudo add-apt-repository multiverse -y
+sudo add-apt-repository ppa:unit193/inxi -y
+sudo add-apt-repository ppa:mikhailnov/hw-probe -y
 sudo apt-get update
-sudo apt-get install hw-probe --no-install-recommends
+sudo apt-get install hw-probe --no-install-recommends -y
