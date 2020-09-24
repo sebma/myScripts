5ede5158dd8c39e5d4a8c60041b7ec68f4ebab9a
sebma <sebma@users.noreply.github.com>
2020-09-24 17:47:32 +0200

    Updated findLoopsInAllLinuxFS.sh

--- shl/findLoopsInAllLinuxFS.sh
+++ shl/findLoopsInAllLinuxFS.sh
@@ -2,9 +2,9 @@
 
 findLoopsInAllLinuxFS() {
 	type sudo >/dev/null 2>&1 && [ $UID != 0 ] && sudo=$(which sudo) || sudo=""
-	fsRegExp="ext[234]|btrfs|f2fs|xfs"
+	fsRegExp="ext[234]|btrfs|f2fs|xfs|jfs|reiserfs|nilfs"
 	if $sudo true; then
-		time for dir in $(df -T | awk "/$fsRegExp/"'{print$NF}' | egrep -vw "/home|/tmp" | sort -u)
+		time for dir in $(df -T | egrep -v "/media/" | awk "/$fsRegExp/"'{print$NF}' | egrep -vw "/home|/tmp" | sort -u)
 		do
 			echo "=> Scanning $dir ..." >&2
 			sudo $(which find) $dir -xdev -follow -printf ""
