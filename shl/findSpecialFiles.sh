92a2ac693f97e07fe6b8782462b55e0e93168b0b
sebma <sebma@users.noreply.github.com>
2020-09-24 18:11:16 +0200

    Updated findSpecialFiles.sh

--- shl/findSpecialFiles.sh
+++ shl/findSpecialFiles.sh
@@ -3,12 +3,12 @@
 type sudo >/dev/null 2>&1 && sudo=$(which sudo) || sudo=""
 $sudo -v
 
-test $# = 0 && fileSystemList=$(df -T | egrep -iv "home|data" | awk "/btrfs|ext(2|3|4)/"'{print$NF}') || fileSystemList=$@
+test $# = 0 && fileSystemList=$(df -T | egrep -iv "home|data" | awk "/btrfs|ext[234]/"'{print$NF}') || fileSystemList=$@
 
 for fileSystem in $fileSystemList
 do
 	echo "=> fileSystem = $fileSystem"
-	$sudo find $fileSystem -xdev -ls | awk '{print substr($3,1,1)}' | sort -u
+	$sudo find $fileSystem -xdev -printf "%M\n" 2>/dev/null | cut -c1  | sort -u
 	echo "==> Inode (hardlinks) :"
 	$sudo find $fileSystem -xdev -ls | awk '{print $1}' | sort | uniq -c | egrep -v " +1 "
 done
