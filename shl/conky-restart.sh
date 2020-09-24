e8d641a31cc4632bc79d4b5a165a1f7d79c5f899
sebma <sebma@users.noreply.github.com>
2020-09-24 17:36:22 +0200

    Updated conky-restart.sh

---
+++ shl/conky-restart.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+restart_conky() {
+	while ! \pgrep plasma-desktop >/dev/null
+	do
+		sleep 1
+	done
+	\pgrep conky && \killall -SIGUSR1 conky || conky -d
+}
+
+restart_conky "$@"
