4ecadc3c41164235b92450b84dfe009b6c9e8d34
sebma <sebma@users.noreply.github.com>
2020-09-24 22:52:42 +0200

    Updated findSparseFiles.sh

--- shl/findSparseFiles.sh
+++ shl/findSparseFiles.sh
@@ -2,5 +2,5 @@
 
 find=$(which find)
 if sudo true;then
-	sudo find . -type f -printf "%S\t%p\n" 2>/dev/null | gawk '$1 < 1.0 {print}'
+	sudo $find . -type f -printf "%S\t%p\n" 2>/dev/null | gawk '$1 > 0 && $1 < 1.0 {print}'
 fi
