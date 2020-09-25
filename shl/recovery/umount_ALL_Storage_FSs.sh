d4331482ed652280b434bd939f8305fcda7853c0
sebma <sebma@users.noreply.github.com>
2020-09-25 17:28:55 +0200

    Updated recovery/umount_ALL_Storage_FSs.sh

--- shl/recovery/umount_ALL_Storage_FSs.sh
+++ shl/recovery/umount_ALL_Storage_FSs.sh
@@ -10,9 +10,16 @@ fi
 
 [ $USER != root ] && echo "=> ERROR [$0] You must run $0 as root." >&2 && exit 2
 
-initPath=$(\ps -p 1 -o cmd= | $cut -d" " -f1)
-set -o pipefail
-systemType=$($strings $initPath | egrep -o "upstart|sysvinit|systemd" | $head -1 || echo unknown)
+ps --help 2>&1 | $grep -q "error while loading shared libraries" && ps="busybox ps" || ps=$(which ps)
+initPath=$($ps -e -o comm= -o pid= | $grep "  *1$" | $cut -d" " -f1)
+if [ -n "$initPath" ];then
+	initPath=$(which $initPath) #Needed for gentoo and maybe others
+	set -o pipefail
+	systemType=$($strings $initPath | egrep -o "upstart|sysvinit|systemd" | $head -1 || echo unknown)
+else
+	exit 1
+fi
+
 set +o pipefail
 if [ $systemType = systemd ];then
 	currentTarget=$(systemctl -t target | $egrep -o '^(emergency|rescue|graphical|multi-user|recovery|friendly-recovery).target')
