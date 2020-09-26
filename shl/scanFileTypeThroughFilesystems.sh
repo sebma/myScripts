67ba37dde2b6fc44a8490b516fea6810e3994f93
sebma <sebma@users.noreply.github.com>
2020-09-26 19:16:04 +0200

    Updated scanFileTypeThroughFilesystems.sh

--- shl/scanFileTypeThroughFilesystems.sh
+++ shl/scanFileTypeThroughFilesystems.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
-fsRegExp="\<(ext[234]|btrfs|f2fs|xfs|jfs|reiserfs|nilfs|hfs|vfat|fuseblk|devtmpfs)\>"
+fsRegExp="\<(ext[234]|btrfs|f2fs|xfs|jfs|reiserfs|nilfs|hfs|vfat|fuseblk)\>"
 RSYNC_SKIP_COMPRESS_LIST=7z/aac/avi/bz2/deb/flv/gz/iso/jpeg/jpg/mkv/mov/m4a/mp2/mp3/mp4/mpeg/mpg/oga/ogg/ogm/ogv/webm/rpm/tbz/tgz/z/zip
 
 #sudo -k
 find=$(which find)
 rsync=$(which rsync)
 rsyncOptions="-h -P -z --skip-compress=$RSYNC_SKIP_COMPRESS_LIST -ut -pgo -r"
+filesystemsList=$(df -T | egrep -v "/media/|/dev/sd[b-z]" | awk "/$fsRegExp/"'{print$NF}' | egrep -vw "/home|/tmp" | sort -u)
+echo "=> filesystemsList = $filesystemsList"
 if sudo true;then
-	time for dir in $(df -T | egrep -v "/media/|/dev/sd[b-z]" | awk "/$fsRegExp/"'{print$NF}' | egrep -vw "/home|/tmp" | sort -u)
+	time for dir in $filesystemsList
 	do
 		printf "=> dir = $dir "
 		findOutput=$(sudo $find $dir -xdev -printf "%y %n %S %p\n" 2>/dev/null)
