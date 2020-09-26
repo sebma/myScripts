a573eb08e3c0c1c16b440201e8b8318b8affa3d1
sebma <sebma@users.noreply.github.com>
2020-09-26 19:54:31 +0200

    Updated osSize.sh

--- shl/osSize.sh
+++ shl/osSize.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 df=$(which df)
-$df -T | sort | egrep -vw "tmpfs|efi|vfat|fuse|fuseblk|squashfs|devtmpfs|^Filesystem.* on" | awk 'BEGIN{printf "sudo du -cxsk / "}/boot|opt|tmp|usr|var/{printf $NF" "}' | sh | awk '/\<total\>/{print$1/1024^2" GiB"}'
+time $df -T | sort | egrep -vw "tmpfs|efi|vfat|fuse|fuseblk|squashfs|devtmpfs|^Filesystem.* on" | awk 'BEGIN{printf "sudo du -cxsk / "}/boot|opt|tmp|usr|var/{printf $NF" "}' | sh -x | awk '/\<total\>/{print$1/1024^2" GiB"}'
