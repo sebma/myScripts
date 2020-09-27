cee0f907a24c1bb5500f788b95bf6979b1104cb0
sebma <sebma@users.noreply.github.com>
2020-09-27 19:17:59 +0200

    Updated backUp_All_Linux_FS_To_New_LVM_Disk.sh

--- shl/system/backUp_All_Linux_FS_To_New_LVM_Disk.sh
+++ shl/system/backUp_All_Linux_FS_To_New_LVM_Disk.sh
@@ -2,7 +2,7 @@
 
 scriptBaseName=${0##*/}
 if [ $# != 1 ];then
-	echo "=> $scriptBaseName" >&2
+	echo "=> $scriptBaseName destinationDisk" >&2
 	exit 1
 fi
 
@@ -10,3 +10,5 @@ destinationDisk=$1
 destinationPVPartition=$(sudo fdisk $destinationDisk -l | awk '/Linux LVM/{print$1}')
 #destinationVG=$(sudo pvdisplay -C $destinationPVPartition -o vg_name | awk 'END{print$1}')
 destinationVG=$(sudo pvs $destinationPVPartition -o vg_name | awk 'END{print$1}')
+destinationLVs=$(sudo lvs $destinationVG -o lv_name | awk 'FNR>1{printf$1" "}')
+echo "=> destinationLVs = $destinationLVs"
