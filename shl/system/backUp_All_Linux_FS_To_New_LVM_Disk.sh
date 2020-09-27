2417fcd943c518752c712cb33574a550370b8b39
sebma <sebma@users.noreply.github.com>
2020-09-27 23:48:43 +0200

    Updated backUp_All_Linux_FS_To_New_LVM_Disk.sh

--- shl/system/backUp_All_Linux_FS_To_New_LVM_Disk.sh
+++ shl/system/backUp_All_Linux_FS_To_New_LVM_Disk.sh
@@ -32,13 +32,33 @@ destinationEFI_UUID=$(sudo blkid $destinationEFI_FS -o value -s UUID)
 RSYNC_SKIP_COMPRESS_LIST=7z/aac/avi/bz2/deb/flv/gz/iso/jpeg/jpg/mkv/mov/m4a/mp2/mp3/mp4/mpeg/mpg/oga/ogg/ogm/ogv/webm/rpm/tbz/tgz/z/zip
 RSYNC_EXCLUSION=$(printf -- "--exclude %s/ " /dev /sys /run /proc /mnt /media)
 echo "=> sourceFilesystemsList = $sourceFilesystemsList"
+echo
 rsync="time $(which rsync) -x -uth -P -z --skip-compress=$RSYNC_SKIP_COMPRESS_LIST $RSYNC_EXCLUSION"
 cp2ext234="$rsync -ogpuv -lSH"
 cp2FAT32="$rsync --modify-window=1"
-destinationDir=/mnt/destinationVGDir
-test -d $destinationDir/ || sudo mkdir -v $destinationDir/
-#sudo mount -v /dev/$destinationVG/$(echo $destinationLVList | grep root) $destinationDir/
-#sudo $cp2ext234 -r -x / $destinationDir/
-test -d $destinationDir/etc/ || sudo mkdir -v $destinationDir/etc/
+destinationRootDir=/mnt/destinationVGDir
+test -d $destinationRootDir/ || sudo mkdir -v $destinationRootDir/
+#sudo mount -v /dev/$destinationVG/$(echo $destinationLVList | grep root) $destinationRootDir/
+#sudo $cp2ext234 -r -x / $destinationRootDir/
+test -d $destinationRootDir/etc/ || sudo mkdir -v $destinationRootDir/etc/
 
-grep -q $destinationVG $destinationDir/etc/fstab 2>/dev/null || sed "s/$sourceEFI_UUID/$destinationEFI_UUID/" /etc/fstab | sed "s,$sourceVG_Or_Disk,$destinationVG," | sudo tee $destinationDir/etc/fstab
+grep -q $destinationVG $destinationRootDir/etc/fstab 2>/dev/null || sed "s/$sourceEFI_UUID/$destinationEFI_UUID/" /etc/fstab | sed "s,$sourceVG_Or_Disk,$destinationVG," | sudo tee $destinationRootDir/etc/fstab
+
+for sourceDir in $sourceFilesystemsList
+do
+	echo "=> sourceDir = $sourceDir"
+	sourceFSType=$(mount | grep "$sourceDir " | awk '{print$5}')
+	echo "=> sourceFSType = $sourceFSType"
+	destinationDir=${destinationRootDir}$sourceDir
+	echo "=> destinationDir = $destinationDir"
+	case $sourceFSType in
+		vfat) copyCommand="$cp2FAT32 -x";;
+		ext2|ext3|ext4) copyCommand="$cp2ext234 -x";;
+	esac
+#	echo "=> copyCommand = $copyCommand"
+#	echo $copyCommand -r $sourceDir $destinationDir/
+done
+#for i in dev dev/pts proc sys ; do sudo mount --bind /$i $destinationRootDir/$i ; done
+#sudo chroot $destinationRootDir/ "$(which grub-install) $destinationDisk"
+#sudo chroot $destinationRootDir/ "$(which update-grub)"
+#sudo umount -v $destinationRootDir/{sys,proc,dev/pts,dev,}
