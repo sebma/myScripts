3cd9dd7c25f6787f4a6b483ab2b75cb4c361e86b
sebma <sebma@users.noreply.github.com>
2020-09-27 19:26:42 +0200

    Updated backUp_All_Linux_FS_To_New_LVM_Disk.sh

--- shl/system/backUp_All_Linux_FS_To_New_LVM_Disk.sh
+++ shl/system/backUp_All_Linux_FS_To_New_LVM_Disk.sh
@@ -10,6 +10,8 @@ destinationDisk=$1
 destinationPVPartition=$(sudo fdisk $destinationDisk -l | awk '/Linux LVM/{print$1}')
 #destinationVG=$(sudo pvdisplay -C $destinationPVPartition -o vg_name | awk 'END{print$1}')
 destinationVG=$(sudo pvs $destinationPVPartition -o vg_name | awk 'END{print$1}')
-destinationLVList=$(sudo lvs $destinationVG -o lv_name | awk 'FNR>1{printf$1" "}')
+destinationLVList=$(sudo lvs $destinationVG -o lv_name | awk 'FNR>1{print$1}' | sort -u | paste -sd' ')
 echo "=> destinationLVList = $destinationLVList"
-sourcePartitionList=
+fsRegExp="\<(ext[234]|btrfs|f2fs|xfs|jfs|reiserfs|nilfs|hfs|vfat|fuseblk)\>"
+sourceFilesystemsList=$(df -T | egrep -vw "/media|/mnt|/tmp" | awk "/$fsRegExp/"'{print$NF}' | sort -u | paste -sd' ')
+echo "=> sourceFilesystemsList = $sourceFilesystemsList"
