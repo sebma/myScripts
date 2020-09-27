a6145093b3e524740a591285d62fddea46bafc4b
sebma <sebma@users.noreply.github.com>
2020-09-27 03:59:36 +0200

    Updated scanFileTypeThroughFilesystems.sh

--- shl/scanFileTypeThroughFilesystems.sh
+++ shl/scanFileTypeThroughFilesystems.sh
@@ -7,32 +7,39 @@ RSYNC_SKIP_COMPRESS_LIST=7z/aac/avi/bz2/deb/flv/gz/iso/jpeg/jpg/mkv/mov/m4a/mp2/
 find=$(which find)
 rsync=$(which rsync)
 rsyncOptions="-h -P -z --skip-compress=$RSYNC_SKIP_COMPRESS_LIST -ut -r"
-filesystemsList=$(df -T | egrep -v "/media/|/dev/sd[b-z]" | awk "/$fsRegExp/"'{print$NF}' | egrep -vw "/home|/tmp" | sort -u)
+filesystemsList=$(df -T | egrep -vw "/media|/tmp" | awk "/$fsRegExp/"'{print$NF}' | sort -u)
 echo "=> filesystemsList = $filesystemsList"
 if sudo true;then
 	time for dir in $filesystemsList
 	do
 		printf "=> dir = $dir "
-		fsType=$(mount | grep $dir | awk '{print$5}')
-		[ "$fsType" != vfat ] && rsyncAdditionalOptions="-ogp"
+		fsType=$(mount | grep "$dir " | awk '{print$5}')
+		printf "=> fsType = <$fsType> "
 		findOutput=$(sudo $find $dir -xdev -printf "%y %n %S %p\n" 2>/dev/null)
-
-		mount | grep $dir | grep -q acl && rsyncAdditionalOptions+=" -A"
-		rsyncAdditionalOptions+=$(echo "$findOutput" | awk '$3 > 0 && $3 < 1.0 {print" -S";exit}') # File's sparseness. normally sparse files will have values less than 1.0
-		rsyncAdditionalOptions+=$(echo "$findOutput" | awk '($1 == "f" && $2 > 1) {print" -H";exit}') # Number of hardlink to a file
-
 		fileTypes=$(echo "$findOutput" | cut -c1  | sort -u | tr "\n" " ")
 		printf "fileTypes = $fileTypes "
-		for fileType in $fileTypes
-		do
-			case $fileType in
-#				b|c|p|s) echo $rsyncAdditionalOptions | grep -q "\-D" || rsyncAdditionalOptions+=" -D" ;;
-				b|c) echo $rsyncAdditionalOptions | grep -q "\--devices" || rsyncAdditionalOptions+=" --devices" ;; # Cold backup : do not backup pipes'n'sockets
-				d) rsyncAdditionalOptions+=" -r" ;;
-				l) rsyncAdditionalOptions+=" -l" ;;
-				*) ;;
-			esac
-		done
+		rsyncAdditionalOptions=""
+		if [ "$fsType" != vfat ];then
+			rsyncAdditionalOptions="-ogp"
+
+			mount | grep $dir | grep -q acl && rsyncAdditionalOptions+=" -A"
+			rsyncAdditionalOptions+=$(echo "$findOutput" | awk '$3 > 0 && $3 < 1.0 {print" -S";exit}') # File's sparseness. normally sparse files will have values less than 1.0
+			rsyncAdditionalOptions+=$(echo "$findOutput" | awk '($1 == "f" && $2 > 1) {print" -H";exit}') # Number of hardlink to a file
+
+			for fileType in $fileTypes
+			do
+				case $fileType in
+#					b|c|p|s) echo $rsyncAdditionalOptions | grep -q "\-D" || rsyncAdditionalOptions+=" -D" ;;
+					b|c) echo $rsyncAdditionalOptions | grep -q "\--devices" || rsyncAdditionalOptions+=" --devices" ;; # Cold backup : do not backup pipes'n'sockets
+					d) rsyncAdditionalOptions+=" -r" ;;
+					l) rsyncAdditionalOptions+=" -l" ;;
+					*) ;;
+				esac
+			done
+		else
+#			rsyncAdditionalOptions+=" --size-only"
+			rsyncAdditionalOptions+=" --modify-window=1" # i.e https://unix.stackexchange.com/a/461881/135038
+		fi
 		echo "rsyncAdditionalOptions = $rsyncAdditionalOptions"
 		echo
 	done
