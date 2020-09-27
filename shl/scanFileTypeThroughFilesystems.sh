d3ed6873b5fb9022a1a1af31fb17db00f4222950
sebma <sebma@users.noreply.github.com>
2020-09-27 03:28:35 +0200

    Updated scanFileTypeThroughFilesystems.sh

--- shl/scanFileTypeThroughFilesystems.sh
+++ shl/scanFileTypeThroughFilesystems.sh
@@ -6,25 +6,26 @@ RSYNC_SKIP_COMPRESS_LIST=7z/aac/avi/bz2/deb/flv/gz/iso/jpeg/jpg/mkv/mov/m4a/mp2/
 #sudo -k
 find=$(which find)
 rsync=$(which rsync)
-rsyncOptions="-h -P -z --skip-compress=$RSYNC_SKIP_COMPRESS_LIST -ut -pgo -r"
+rsyncOptions="-h -P -z --skip-compress=$RSYNC_SKIP_COMPRESS_LIST -ut -r"
 filesystemsList=$(df -T | egrep -v "/media/|/dev/sd[b-z]" | awk "/$fsRegExp/"'{print$NF}' | egrep -vw "/home|/tmp" | sort -u)
 echo "=> filesystemsList = $filesystemsList"
 if sudo true;then
 	time for dir in $filesystemsList
 	do
 		printf "=> dir = $dir "
+		fsType=$(mount | grep $dir | awk '{print$5}')
+		[ "$fsType" != vfat ] && rsyncAdditionalOptions="-ogp"
 		findOutput=$(sudo $find $dir -xdev -printf "%y %n %S %p\n" 2>/dev/null)
 
-		rsyncAdditionalOptions=""
 		mount | grep $dir | grep -q acl && rsyncAdditionalOptions+=" -A"
 		rsyncAdditionalOptions+=$(echo "$findOutput" | awk '$3 > 0 && $3 < 1.0 {print" -S";exit}') # File's sparseness. normally sparse files will have values less than 1.0
 		rsyncAdditionalOptions+=$(echo "$findOutput" | awk '($1 == "f" && $2 > 1) {print" -H";exit}') # Number of hardlink to a file
 
 		fileTypes=$(echo "$findOutput" | cut -c1  | sort -u | tr "\n" " ")
 		printf "fileTypes = $fileTypes "
-		for type in $fileTypes
+		for fileType in $fileTypes
 		do
-			case $type in
+			case $fileType in
 #				b|c|p|s) echo $rsyncAdditionalOptions | grep -q "\-D" || rsyncAdditionalOptions+=" -D" ;;
 				b|c) echo $rsyncAdditionalOptions | grep -q "\--devices" || rsyncAdditionalOptions+=" --devices" ;; # Cold backup : do not backup pipes'n'sockets
 				d) rsyncAdditionalOptions+=" -r" ;;
