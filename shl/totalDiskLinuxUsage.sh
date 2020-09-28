0445b2c2af31071ae62bb708c8bcf81a03f8873a
sebma <sebma@users.noreply.github.com>
2020-09-28 13:50:25 +0200

    Updated totalDiskLinuxUsage.sh

--- shl/totalDiskLinuxUsage.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-df=$(which df)
-time $df -T | sort | egrep -vw "/mnt|/media|tmpfs|efi|vfat|fuse|fuseblk|squashfs|devtmpfs|^Filesystem.* on" | awk 'BEGIN{printf "sudo du -cxsk / "}{printf $NF" "}' | sh -x | awk '/\<total\>/{print$1/1024^2" GiB"}'
