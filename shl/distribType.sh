fdb221bc9d20cbf616ee3adfb3737f083b7928cf
sebma <sebma@users.noreply.github.com>
2020-09-30 16:58:45 +0200

    Updated distribType.sh

--- shl/distribType.sh
+++ shl/distribType.sh
@@ -6,36 +6,22 @@ distribType ()
 	local distribType=unknown
 	echo $OSTYPE | grep -q android && local osFamily=Android || local osFamily=$(uname -s)
 
-	distribName=$(distribName.sh)
+	distribName=$(distribName.sh | awk '{print tolower($0)}')
 
-	if which lsb_release > /dev/null 2>&1; then
-		distribName=$(\lsb_release -si)
+	if [ $osFamily = Linux ]; then
 		case $distribName in
-			Ubuntu|Debian) distribType=debian ;;
-			Mer|Redhat|Fedora) distribType=redhat ;;
-			*) distribType=unknown ;;
+			sailfishos|rhel|fedora|centos) distribType=redhat ;;
+			ubuntu) distribType=debian;;
+			*) distribType=$distribName ;;
 		esac
-	elif [ $distribType = unknown ]; then
-		if [ $osFamily = Linux ]; then
-			distribType=$(source /etc/os-release && echo $ID_LIKE | cut -d'"' -f2 | cut -d" " -f1)
-			if [ -z "$distribType" ]; then
-				distribName=$(source /etc/os-release;echo $ID)
-				case $distribName in
-					sailfishos|rhel|fedora|centos) distribType=redhat ;;
-					arch|gentoo) distribType=arch ;;
-					*) distribType=unknown ;;
-				esac
-			fi
-		else
-			if [ $osFamily = Darwin ]; then
-				distribName="$(sw_vers -productName)"
-				distribType=Darwin
-			else
-				distribName=unknown
-				distribType=$osFamily
-			fi
-		fi
+	elif [ $osFamily = Darwin ]; then
+			distribType=Darwin
+	elif [ $osFamily = Android ]; then
+			distribType=Android
+	else
+		which bash >/dev/null 2>&1 && distribType=$(bash -c 'echo $OSTYPE') || distribType=$osFamily
 	fi
+
 	echo $distribType
 }
 
