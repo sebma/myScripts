dab4b429b83da4eb0ed1dacf2a7719054093e1e5
sebma <sebma@users.noreply.github.com>
2020-09-30 16:38:57 +0200

    Updated distribType.sh

---
+++ shl/distribType.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+distribType ()
+{
+	local distribName=unknown
+	local distribType=unknown
+	echo $OSTYPE | grep -q android && local osFamily=Android || local osFamily=$(uname -s)
+
+	distribName=$(distribType.sh)
+
+	if which lsb_release > /dev/null 2>&1; then
+		distribName=$(\lsb_release -si)
+		case $distribName in
+			Ubuntu|Debian) distribType=debian ;;
+			Mer|Redhat|Fedora) distribType=redhat ;;
+			*) distribType=unknown ;;
+		esac
+	elif [ $distribType = unknown ]; then
+		if [ $osFamily = Linux ]; then
+			distribType=$(source /etc/os-release && echo $ID_LIKE | cut -d'"' -f2 | cut -d" " -f1)
+			if [ -z "$distribType" ]; then
+				distribName=$(source /etc/os-release;echo $ID)
+				case $distribName in
+					sailfishos|rhel|fedora|centos) distribType=redhat ;;
+					arch|gentoo) distribType=arch ;;
+					*) distribType=unknown ;;
+				esac
+			fi
+		else
+			if [ $osFamily = Darwin ]; then
+				distribName="$(sw_vers -productName)"
+				distribType=Darwin
+			else
+				distribName=unknown
+				distribType=$osFamily
+			fi
+		fi
+	fi
+	echo $distribType
+}
+
+distribType
