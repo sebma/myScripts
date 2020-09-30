75036787a8c3c2e98e1e874cf9ef0ba5a974561b
sebma <sebma@users.noreply.github.com>
2020-09-30 17:27:03 +0200

    Updated distribType.sh

--- shl/distribType.sh
+++ shl/distribType.sh
@@ -1,11 +1,33 @@
 #!/usr/bin/env bash
 
+distribName () {
+	local osName=unknown
+	echo $OSTYPE | grep -q android && local osFamily=Android || local osFamily=$(uname -s)
+
+	if [ $osFamily = Linux ]; then
+		if which lsb_release >/dev/null 2>&1; then
+			osName=$(lsb_release -si)
+			[ $osName = "n/a" ] && osName=$(\sed -n "s/[\"']//g;s/^ID=//p;" /etc/os-release)
+		elif [ -s /etc/os-release ]; then
+			osName=$(\sed -n "s/[\"']//g;s/^ID=//p;" /etc/os-release)
+		fi
+	elif [ $osFamily = Darwin ]; then
+		osName="$(sw_vers -productName)"
+	elif [ $osFamily = Android ]; then
+		osName=Android
+	else
+		osName=$OSTYPE
+	fi
+
+	echo $osName | awk '{print tolower($0)}'
+}
+
 distribType () {
 	local distribName=unknown
 	local distribType=unknown
 	echo $OSTYPE | grep -q android && local osFamily=Android || local osFamily=$(uname -s)
 
-	distribName=$(distribName.sh)
+	distribName=$(distribName)
 
 	if [ $osFamily = Linux ]; then
 		case $distribName in
