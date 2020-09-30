b982f7daf74e7006e67344497f0afffb0bb22719
sebma <sebma@users.noreply.github.com>
2020-09-30 17:10:59 +0200

    Updated hw-probeInstall_N_Run.sh

--- shl/hw-probeInstall_N_Run.sh
+++ shl/hw-probeInstall_N_Run.sh
@@ -1,12 +1,41 @@
 #!/usr/bin/env bash
 
-ubuntuSources=/etc/apt/sources.list
-grep -q universe $ubuntuSources   || sudo add-apt-repository universe -y
-grep -q multiverse $ubuntuSources || sudo add-apt-repository multiverse -y
-grep -q "^deb .*unit193/inxi" /etc/apt/sources.list.d/*.list || sudo add-apt-repository ppa:unit193/inxi -y
-grep -q "^deb .*mikhailnov/hw-probe" /etc/apt/sources.list.d/*.list || sudo add-apt-repository ppa:mikhailnov/hw-probe -y
-sudo apt update
-sudo apt install -V inxi hw-probe
+distribName ()
+{
+	local osName=unknown
+	echo $OSTYPE | grep -q android && local osFamily=Android || local osFamily=$(uname -s)
+
+	if [ $osFamily = Linux ]; then
+		if which lsb_release > /dev/null; then
+			osName=$(lsb_release -si)
+			[ $osName = "n/a" ] && osName=$(\sed -n "s/[\"']//g;s/^ID=//p;" /etc/os-release)
+		elif [ -s /etc/os-release ]; then
+			osName=$(\sed -n "s/[\"']//g;s/^ID=//p;" /etc/os-release)
+		fi
+	elif [ $osFamily = Darwin ]; then
+		osName="$(sw_vers -productName)"
+	elif [ $osFamily = Android ]; then
+		osName=Android
+	else
+		osName=$OSTYPE
+	fi
+
+	echo $osName | awk '{print tolower($0)}'
+}
+
+distribName=$(distribName)
+if [ $distribName = ubuntu ]; then
+	ubuntuSources=/etc/apt/sources.list
+	grep -q universe $ubuntuSources   || sudo add-apt-repository universe -y
+	grep -q multiverse $ubuntuSources || sudo add-apt-repository multiverse -y
+	grep -q "^deb .*unit193/inxi" /etc/apt/sources.list.d/*.list || sudo add-apt-repository ppa:unit193/inxi -y
+	grep -q "^deb .*mikhailnov/hw-probe" /etc/apt/sources.list.d/*.list || sudo add-apt-repository ppa:mikhailnov/hw-probe -y
+	apt-cache policy inxi | grep -q unit193/inxi || sudo apt update
+	apt-cache policy hw-probe | grep -q mikhailnov/hw-probe || sudo apt update
+	dpkg -l inxi | grep -q ^.i || sudo apt install -V inxi
+	dpkg -l hw-probe | grep -q ^.i || sudo apt install -V hw-probe
+fi
+
 #sudo -E hw-probe -all -upload
 #Request inventory ID:
 #hw-probe -generate-inventory -email YOUR@EMAIL
