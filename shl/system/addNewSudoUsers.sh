878c965c3c4bf7d76ce6dbf32ae4f5ca8ca97e0f
sebma <sebma@users.noreply.github.com>
2020-09-30 15:55:11 +0200

    Updated addNewSudoUsers.sh

---
+++ shl/system/addNewSudoUsers.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+scriptBaseName=$(basename $0)
+if [ $USER != root ];then
+	echo "=> ERROR : You must run $scriptBaseName as root." >&2
+	exit 1
+fi
+
+for newSudoUser
+do
+	id -un $newSudoUser || useradd -m $newSudoUser
+	for group in admin sudo wheel
+	do
+		grep -qw $group /etc/group && usermod -aG $group $newSudoUser
+		\sed -i "/$group ALL=(ALL) ALL/s/#[ 	]*//" /etc/sudoers
+	done
+done
