8510fe2cd0b8184fcea0e20e29d404c4e66f0e84
sebma <sebma@users.noreply.github.com>
2020-10-01 14:33:19 +0200

    Updated system/rebuild_LVM_grub.sh

--- shl/system/rebuild_LVM_grub.sh
+++ shl/system/rebuild_LVM_grub.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-type sudo >/dev/null 2>&1 && sudo=$(which sudo) || sudo=""
+type sudo >/dev/null 2>&1 && [ $(id -u) != 0 ] && groups | egrep -wq "sudo|adm|admin|root|wheel" && sudo=$(which sudo) || sudo=""
 #set -o nounset
 set -o errexit
 
@@ -13,15 +13,16 @@ $sudo lvs | grep -q root || {
 rootFSLogicalVolume=$($sudo lvs | awk '/root/{print$2"-"$1}')
 osVGName=$($sudo lvs | awk '/root/{print$2}')
 diskDevice=$(pvs | awk "/$osVGName/"'{print substr($1,1,8)}')
-$sudo mkdir -p /mnt/{dev/pts,proc,sys}
+
 mount | grep -q $rootFSLogicalVolume || $sudo mount /dev/mapper/$rootFSLogicalVolume /mnt          # montage de celle-ci en remplacant le X par le bon numero de partition
-for i in dev dev/pts proc sys ; do $sudo mount --bind /$i /mnt/$i ; done
+for special in dev dev/pts proc sys ; do $sudo mkdir -pv /mnt/$special;$sudo mount -v --bind /$special /mnt/$special ; done
+
 set +o errexit
 $sudo chroot /mnt /bin/bash <<-EOF # mise a la racine du disque monte
-	mount -av                      # montage des partitions dans le chroot
+	findmnt >/dev/null && mount -av || exit                      # montage des partitions dans le chroot
 	update-grub                   # creation d'un nouveau fichier de configuration : grub.cfg
 	grub-install $diskDevice || grub-install --force $diskDevice        # installation de grub sur le MBR
 	sync
 	umount -av
 EOF
-$sudo umount -v /mnt/{sys,proc,dev/pts,dev,}
+$sudo umount -v /mnt/{usr,sys,proc,dev/pts,dev,}
