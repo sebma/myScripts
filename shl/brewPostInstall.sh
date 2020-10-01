d67f44806d04661ee0c27b09cba9ea5ce5a830c9
sebma <sebma@users.noreply.github.com>
2020-10-01 11:29:05 +0200

    Updated brewPostInstall.sh

---
+++ shl/brewPostInstall.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+brewPostInstall ()
+{
+	local brew=$(which brew)
+	echo "=> Updating homebrew ..." 1>&2
+	echo 1>&2
+	time $brew update -v
+	echo 1>&2
+	echo "=> Adding missing taps ..." 1>&2
+	echo 1>&2
+	time for tap in homebrew/core homebrew/services homebrew/cask homebrew/cask-versions homebrew/cask-drivers homebrew/cask-fonts buo/cask-upgrade
+	do
+		$brew tap | \grep -q $tap || {
+			set -x
+			time $brew tap $tap
+			set +x
+		}
+	done
+}
+
+brewPostInstall
