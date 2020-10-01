36fcf5da2c14ac63859e6f82ac6d8fb1ec7942b0
sebma <sebma@users.noreply.github.com>
2020-10-01 14:33:18 +0200

    Updated recovery/restore_LVM_Grub_btrfs_normal_module.sh

--- shl/recovery/restore_LVM_Grub_btrfs_normal_module.sh
+++ shl/recovery/restore_LVM_Grub_btrfs_normal_module.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-type sudo >/dev/null 2>&1 && sudo=$(which sudo) || sudo=""
+type sudo >/dev/null 2>&1 && [ $(id -u) != 0 ] && groups | egrep -wq "sudo|adm|admin|root|wheel" && sudo=$(which sudo) || sudo=""
 #set -o nounset
 set -o errexit
 
@@ -11,14 +11,14 @@ $sudo lvs | grep -q root || {
 }
 
 rootFSDevice=$($sudo lvs | awk '/root/{print$2"-"$1}')
-$sudo mkdir -p /mnt/{dev/pts,proc,sys}
 mount | grep -q $rootFSDevice || $sudo mount -o subvol=@ /dev/mapper/$rootFSDevice /mnt          # montage de celle-ci en remplacant le X par le bon numero de partition
-for i in dev dev/pts proc sys ; do $sudo mount --bind /$i /mnt/$i ; done
+for special in dev dev/pts proc sys ; do $sudo mkdir -pv /mnt/$special;$sudo mount -v --bind /$special /mnt/$special ; done
+
 set +o errexit
 $sudo chroot /mnt /bin/bash <<-EOF
-	mount -av
+	findmnt >/dev/null && mount -av || exit
 	cp -puv /usr/lib/grub/x86_64-efi/*.mod /boot/grub/x86_64-efi/
 	sync
 	umount -av
 EOF
-$sudo umount -v /mnt/{sys,proc,dev/pts,dev,}
+$sudo umount -v /mnt/{usr,sys,proc,dev/pts,dev,}
