c406e812d5e738fe5c690f3015dab9d554c8e7cf
sebma <sebma@users.noreply.github.com>
2020-10-01 14:33:18 +0200

    Updated recovery/restore_LVM_GrubBootLoader.sh

--- shl/recovery/restore_LVM_GrubBootLoader.sh
+++ shl/recovery/restore_LVM_GrubBootLoader.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-type sudo >/dev/null 2>&1 && sudo=$(which sudo) || sudo=""
+type sudo >/dev/null 2>&1 && [ $(id -u) != 0 ] && groups | egrep -wq "sudo|adm|admin|root|wheel" && sudo=$(which sudo) || sudo=""
 #set -o nounset
 set -o errexit
 
@@ -11,15 +11,15 @@ $sudo lvs | grep -q root || {
 }
 
 rootFSDevice=$($sudo lvs | awk '/root/{print$2"-"$1}')
-$sudo mkdir -p /mnt/{dev/pts,proc,sys}
 mount | grep -q $rootFSDevice || $sudo mount /dev/mapper/$rootFSDevice /mnt          # montage de celle-ci en remplacant le X par le bon numero de partition
-for i in dev dev/pts proc sys ; do $sudo mount --bind /$i /mnt/$i ; done
+for special in dev dev/pts proc sys ; do $sudo mkdir -pv /mnt/$special;$sudo mount -v --bind /$special /mnt/$special ; done
+
 set +o errexit
 $sudo chroot /mnt /bin/bash <<-EOF # mise a la racine du disque monte
-	mount -av                      # montage des partitions dans le chroot
+	findmnt >/dev/null && mount -av || exit                      # montage des partitions dans le chroot
 	test -s /boot/grub/grub.cfg || update-grub # creation d'un nouveau fichier de configuration : grub.cfg
 	grub-install /dev/sda || grub-install --force /dev/sda        # installation de grub sur le MBR
 	sync
 	umount -av
 EOF
-$sudo umount -v /mnt/{sys,proc,dev/pts,dev,}
+$sudo umount -v /mnt/{usr,sys,proc,dev/pts,dev,}
