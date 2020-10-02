cd685f352474f5245a716d97eb0f39bc96d03d92
sebma <sebma@users.noreply.github.com>
2020-10-02 16:00:10 +0200

    Updated Ventoy2DiskUpdate.sh

--- shl/Ventoy2DiskUpdate.sh
+++ shl/Ventoy2DiskUpdate.sh
@@ -13,10 +13,12 @@ Ventoy2DiskLatestGitHubReleaseURL=$(curl -s $gitHubAPIRepoURL/releases/latest | 
 ventoyLatestRelease=$(git ls-remote --tags --refs https://github.com/$gitHubUser/$gitHubRepo | awk -F/ 'END{print gensub("^v","",1,$NF)}')
 
 curl -s "$Ventoy2DiskLatestGitHubReleaseURL" | tar -C /tmp -xz
+chmod +x /tmp/ventoy-$ventoyLatestRelease/tool/xzcat
 sudo mkdir -pv /opt/ventoy
 rsync="$(which rsync) -uth -P -z --skip-compress=$RSYNC_SKIP_COMPRESS_LIST"
 cp2ext234="$rsync -ogpuv -lSH"
-mv2ext234="$cp2ext234 --remove-source-files"
-time sudo $mv2ext234 -r /tmp/ventoy-$ventoyLatestRelease/* /opt/ventoy
+time sudo $cp2ext234 -r /tmp/ventoy-$ventoyLatestRelease/* /opt/ventoy
+sudo chown root -R /opt/ventoy/*
+sudo rm -fr /tmp/ventoy-$ventoyLatestRelease/
 
 cd /opt/ventoy/ && sudo ./Ventoy2Disk.sh -u "$@"
