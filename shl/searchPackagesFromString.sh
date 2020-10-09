828b77bfe6601c218a98834d63582eff882af977
sebma <sebma@users.noreply.github.com>
2020-10-09 15:01:22 +0200

    Updated searchPackagesFromString.sh

---
+++ shl/searchPackagesFromString.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+type sudo >/dev/null 2>&1 && [ $(id -u) != 0 ] && groups | egrep -wq "sudo|adm|admin|root|wheel" && sudo=$(which sudo) || sudo=""
+distribName () {
+	local osName=unknown
+	echo $OSTYPE | grep -q android && local osFamily=Android || local osFamily=$(uname -s)
+
+	if [ $osFamily = Linux ]; then
+		if which lsb_release >/dev/null 2>&1; then
+			osName=$(lsb_release -si)
+			[ $osName = "n/a" ] && osName=$(\sed -n "s/[\"']//g;s/^ID=//p;" /etc/os-release)
+		elif [ -s /etc/os-release ]; then
+			osName=$(\sed -n "s/[\"']//g;s/^ID=//p;" /etc/os-release)
+		fi
+	elif [ $osFamily = Darwin ]; then
+		osName="$(sw_vers -productName)"
+	elif [ $osFamily = Android ]; then
+		osName=Android
+	else
+		osName=$OSTYPE
+	fi
+
+	echo $osName | awk '{print tolower($0)}'
+}
+
+searchPackagesFromString() {
+	packagesList=""
+	distribName=$(distribName)
+	if [ $distribName = ubuntu ] || [ $distribName = debian ]; then
+		for string
+		do
+			packagesList+="$(apt-cache search $string | cut -d' ' -f1) "
+		done
+	elif [ $distribName = arch ]; then
+		for string
+		do
+			packagesList+="$(pacman -Ss $string | awk '/\//{printf$1}') "
+		done
+	fi
+
+	echo "$packagesList"
+}
+
+searchPackagesFromString "$@"
