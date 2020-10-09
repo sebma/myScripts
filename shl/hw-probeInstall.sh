41a45871f383bd02f4af3b3c4938b73deba0c3ed
sebma <sebma@users.noreply.github.com>
2020-10-09 14:19:24 +0200

    Updated hw-probeInstall.sh

--- shl/hw-probeInstall.sh
+++ shl/hw-probeInstall.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+type sudo >/dev/null 2>&1 && [ $(id -u) != 0 ] && groups | egrep -wq "sudo|adm|admin|root|wheel" && sudo=$(which sudo) || sudo=""
 distribName ()
 {
 	local osName=unknown
@@ -26,21 +27,21 @@ distribName ()
 distribName=$(distribName)
 if [ $distribName = ubuntu ]; then
 	ubuntuSources=/etc/apt/sources.list
-	grep -q universe $ubuntuSources   || sudo add-apt-repository universe -y
-	grep -q multiverse $ubuntuSources || sudo add-apt-repository multiverse -y
-	grep -q "^deb .*unit193/inxi" /etc/apt/sources.list.d/*.list || sudo add-apt-repository ppa:unit193/inxi -y
-	grep -q "^deb .*mikhailnov/hw-probe" /etc/apt/sources.list.d/*.list || sudo add-apt-repository ppa:mikhailnov/hw-probe -y
-	apt-cache policy inxi | grep -q unit193/inxi || sudo apt update
-	apt-cache policy hw-probe | grep -q mikhailnov/hw-probe || sudo apt update
-	dpkg -l inxi | grep -q ^.i || sudo apt install -V inxi
-	dpkg -l hw-probe | grep -q ^.i || sudo apt install -V hw-probe
+	grep -q universe $ubuntuSources   || $sudo add-apt-repository universe -y
+	grep -q multiverse $ubuntuSources || $sudo add-apt-repository multiverse -y
+	grep -q "^deb .*unit193/inxi" /etc/apt/sources.list.d/*.list || $sudo add-apt-repository ppa:unit193/inxi -y
+	grep -q "^deb .*mikhailnov/hw-probe" /etc/apt/sources.list.d/*.list || $sudo add-apt-repository ppa:mikhailnov/hw-probe -y
+	apt-cache policy inxi | grep -q unit193/inxi || $sudo apt update
+	apt-cache policy hw-probe | grep -q mikhailnov/hw-probe || $sudo apt update
+	dpkg -l inxi | grep -q ^.i || $sudo apt install -V inxi
+	dpkg -l hw-probe | grep -q ^.i || $sudo apt install -V hw-probe
 elif [ $distribName = arch ]; then
-	if sudo echo "";then
-		sudo pacman -Sy
-		sudo pacman -Fy
+	if $sudo echo "";then
+		$sudo pacman -Sy
+		$sudo pacman -Fy
 
-		which fakeroot >/dev/null 2>&1 || sudo pacman -S fakeroot
-		which strip >/dev/null 2>&1 || sudo pacman -S binutils
+		which fakeroot >/dev/null 2>&1 || $sudo pacman -S fakeroot
+		which strip >/dev/null 2>&1 || $sudo pacman -S binutils
 
 		if ! which inxi >/dev/null 2>&1;then
 			cd inxi >/dev/null 2>&1 || { git clone https://aur.archlinux.org/inxi.git;cd inxi; }
@@ -52,7 +53,7 @@ elif [ $distribName = arch ]; then
 			fi
 		fi
 
-		which make >/dev/null 2>&1 || sudo pacman -S make
+		which make >/dev/null 2>&1 || $sudo pacman -S make
 
 		if ! which hw-probe >/dev/null 2>&1;then
 			cd hw-probe >/dev/null 2>&1 || { git clone https://aur.archlinux.org/hw-probe.git;cd hw-probe; }
