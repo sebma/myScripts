06f2b1e776747ba467ae575208b0b5db02391478
sebma <sebma@users.noreply.github.com>
2020-10-10 14:06:17 +0200

    Updated shl/kpcliRequirementsInstall.sh

--- shl/kpcliRequirementsInstall.sh
+++ shl/kpcliRequirementsInstall.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
-printf "o conf prerequisites_policy follow\no conf build_requires_install_policy yes\no conf commit" | $(which cpan)
-$(which cpan) Sort::Naturally YAML Term::ShellUI File::KeePass Log::Log4perl
-$(which cpan) -f Term::ReadLine::Gnu Clone Crypt::Rijndael Term::ReadKey
+cpan=$(which cpan)
+printf "o conf prerequisites_policy follow\no conf build_requires_install_policy yes\no conf commit" | $cpan
+$cpan Sort::Naturally YAML Term::ShellUI File::KeePass Log::Log4perl
+$cpan -f Term::ReadLine::Gnu Clone Crypt::Rijndael Term::ReadKey
