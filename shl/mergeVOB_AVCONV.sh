8957ab009e7979101491866bcb5f61d76728219d
sebma <sebma@users.noreply.github.com>
2020-10-11 00:33:29 +0200

    Updated mergeVOB_AVCONV.sh

---
+++ shl/mergeVOB_AVCONV.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+set -x
+#time -p avconv -probesize 400M -analyzeduration 400M -i concat:$(printf "%s|" $(ls -v $@)) -map 0 -c copy output.vob
+time -p avconv -probesize 400M -analyzeduration 400M -i concat:$(printf "%s|" $(ls -v $@)) -map 0 -c copy -f mpeg output.vob
+#time -p avconv -probesize 400M -analyzeduration 400M -i concat:$(printf "%s|" $(ls -v $@)) -map 0 -scodec copy -c:a copy -c:v copy -f vob output.vob
