54d1d86730f64d614cfa59bcf6952f7b19dabff8
sebma <sebma@users.noreply.github.com>
2020-10-10 12:36:06 +0200

    Updated wifi5GHz.sh

--- shl/wifi5GHz.sh
+++ shl/wifi5GHz.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 
 wifi5GHz() {
+	wifiInterface=$(iw dev 2>/dev/null | awk '/Interface/{lastInterface=$NF}END{print lastInterface}')
+	test "$wifiInterface" || wifiInterface="$(iwconfig 2>/dev/null | awk '/^[^ \t]/ { if ($1 ~ /^[0-9]+:/) { lastInterface=$2 } else { lastInterface=$1 } }END{print lastInterface}')"
 	if which iw >/dev/null 2>&1 ;then
 		iw phy | egrep -q '5[0-9]{3} MHz' && echo true || echo false
 	elif which iwlist >/dev/null 2>&1;then
