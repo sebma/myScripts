8f8124b51f11cd3774ed5a3f9e6aa0157eed886a
sebma <sebma@users.noreply.github.com>
2020-10-12 23:34:02 +0200

    Updated usbFlashDriveInfo.sh

--- shl/usbFlashDriveInfo.sh
+++ shl/usbFlashDriveInfo.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env sh
 
-usbFlashDriveDevice=$1
-sudo smartctl -i -d scsi -T permissive $usbFlashDriveDevice
-sudo parted $usbFlashDriveDevice print
-sudo gdisk -l $usbFlashDriveDevice
+scriptName=$(basename $0)
+usbFlashDriveInfo() {
+	usbFlashDriveDevice=$1
+	test $# != 1 && echo "=> Usage: $scriptName usbFlashDriveDevice" >&2 && return 1
+	sudo smartctl -i -d scsi -T permissive $usbFlashDriveDevice
+	sudo parted $usbFlashDriveDevice print
+	sudo gdisk -l $usbFlashDriveDevice
+}
+
+usbFlashDriveInfo $1
