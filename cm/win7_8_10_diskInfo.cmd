62371ae57f49700dded1e8286575e22cb9c7e2a7
sebma <sebma@users.noreply.github.com>
2020-10-12 15:36:43 +0200

    Updated win7_8_10_diskInfo.cmd

--- cm/win7_8_10_diskInfo.cmd
+++ cm/win7_8_10_diskInfo.cmd
@@ -3,7 +3,9 @@ REM show Windows disk boot information
 
 prompt $$$s
 echo on
-( echo list disk & echo sel disk 0 & echo list part & echo list volume ) | diskpart
+( echo list disk & echo sel disk 0 & echo list part & echo list volume ) > diskInfo.dpt
+cat diskInfo.dpt
+diskpart -s diskInfo.dpt
 echo.
 bcdedit
 bootrec /ScanOs
