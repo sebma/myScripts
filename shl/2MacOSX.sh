d2f1c79a8a16d1a9e786beb1b772a111e5969bef
sebma <sebma@users.noreply.github.com>
2020-10-25 19:37:22 +0100

    Updated 2MacOSX.sh

--- shl/2MacOSX.sh
+++ shl/2MacOSX.sh
@@ -18,6 +18,8 @@ case $ubuntuRelease in
 	14.04|14.10)
 		cd && wget -O config.sh http://drive.noobslab.com/data/Mac-14.10/config.sh
 		chmod +x config.sh;./config.sh
+		wget -O launcher_bfb.png http://drive.noobslab.com/data/Mac-14.10/launcher-logo/apple/launcher_bfb.png
+		sudo mv -v launcher_bfb.png /usr/share/unity/icons/
 		version=v4;;
 	15.04) version=v5;;
 	15.10) version=v6;;
@@ -29,7 +31,7 @@ case $ubuntuRelease in
 esac
 
 case $ubuntuRelease in
-	14.04|14.10) appList="mbuntu-y-ithemes-$version mbuntu-y-icons-$version mbuntu-y-bscreen-$version mbuntu-y-lightdm-$version slingscold indicator-synapse libreoffice-style-sifr appmenu-qt appmenu-qt5 plasma-widget-menubar"; macBUNTU_BScreenPackage=mbuntu-y-bscreen-$version;;
+	14.04|14.10) appList="mbuntu-y-ithemes-$version mbuntu-y-icons-$version mbuntu-y-bscreen-$version mbuntu-y-lightdm-$version slingscold indicator-synapse libreoffice-style-sifr appmenu-qt appmenu-qt5 plasma-widget-menubar docky mbuntu-y-docky-skins-v4"; macBUNTU_BScreenPackage=mbuntu-y-bscreen-$version;;
 	15.04) appList="mbuntu-y-ithemes-$version mbuntu-y-icons-$version slingscold mutate libreoffice-style-sifr mbuntu-y-lightdm-$version";;
 	15.10) appList="macbuntu-ithemes-$version macbuntu-icons-$version slingscold mutate plank macbuntu-plank-theme-$version libreoffice-style-sifr macbuntu-lightdm-$version macbuntu-bscreen-$version";macBUNTU_BScreenPackage=macbuntu-bscreen-$version;;
 	16.04|16.10|17.04|17.10) appList="macbuntu-os-icons-$version macbuntu-os-ithemes-$version slingscold albert libreoffice-style-sifr plank macbuntu-os-plank-theme-$version macbuntu-os-bscreen-$version macbuntu-os-lightdm-$version";macBUNTU_BScreenPackage=macbuntu-os-bscreen-$version;;
