376779c2f44cb9c302a9fde2d89f3b6f35dc215a
sebma <sebma@users.noreply.github.com>
2020-10-26 19:39:24 +0100

    Updated grub-reinstall-ForRecoveryMode.sh

---
+++ shl/system/grub-reinstall-ForRecoveryMode.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+tools="awk cut egrep grep head runlevel sed strings tee"
+if [ -s /usr/bin/tee ]; then # Si "/usr" est accessible
+	for tool in $tools;do declare $tool=$tool;done
+else # Si /usr n'est pas accessible, on utilise les applets busybox
+	type busybox >/dev/null || exit
+	for tool in $tools;do declare $tool="busybox $tool";done
+fi
+
+[ $USER != root ] && echo "=> ERROR [$0] You must run $0 as root." >&2 && exit 2
+
+initPath=$(\ps -p 1 -o cmd= | $cut -d" " -f1)
+set -o pipefail
+systemType=$($strings $initPath | $egrep -o "upstart|sysvinit|systemd" | $head -1 || echo unknown)
+set +o pipefail
+
+if [ $systemType = systemd ];then
+	currentTarget=$(systemctl -t target | $egrep -o '^(emergency|rescue|graphical|multi-user|recovery|friendly-recovery).target')
+	echo $currentTarget | $egrep -q "(recovery|rescue).target" || { echo "=> You must reboot in recovery|rescue mode to run $0." >&2 && exit 3; }
+elif [ $systemType = upstart ];then
+	:
+fi
+
+mount | grep -q "/usr " || mount -v -r /usr
+mount -v /boot
+mount -v /boot/efi
+[ -d /sys/firmware/efi ] && efiDirectory=$(mount | awk '/\/efi /{print$3}') && grub-install --removable --efi-directory=$efiDirectory || grub-install /dev/sda
+sync
+umount -v /boot/efi /boot /usr
