af5611508bed2447fbbd090f030760695805e412
sebma <sebma@users.noreply.github.com>
2020-10-30 19:20:18 +0100

    Updated isSSD.sh

---
+++ shl/system/isSSD.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+scriptBaseName=${0##*/}
+#set -o errexit
+set -o nounset
+type sudo >/dev/null 2>&1 && [ $(id -u) != 0 ] && groups | egrep -wq "sudo|adm|admin|root|wheel" && sudo=$(which sudo) || sudo=""
+diskDevice=""
+os=$(uname -s)
+
+if [ $# = 0 ]
+then
+    [ $os = Linux  ] && diskDevice=sda
+    [ $os = Darwin ] && diskDevice=disk0
+else
+    [ "$1" = "-h" ] && {
+        echo "=> Usage: $scriptBaseName [disk device name]" >&2
+        exit 1
+    } || diskDevice=$1
+fi
+
+if ! echo $diskDevice | grep -q /dev/; then
+    diskDevice=/dev/$diskDevice
+fi
+
+deviceType=$(test $(</sys/block/${diskDevice/*\//}/queue/rotational) = 0 && echo SSD || echo HDD)
+
+echo "[$scriptBaseName] => INFO: $diskDevice is a $deviceType."
