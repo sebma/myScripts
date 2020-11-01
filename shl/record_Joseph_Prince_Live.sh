14ecaeab7ca9e65983c07e2d53128efaf4e5eb00
sebma <sebma@users.noreply.github.com>
2020-11-01 07:53:23 +0100

    Updated record_Joseph_Prince_Live.sh

--- shl/record_Joseph_Prince_Live.sh
+++ shl/record_Joseph_Prince_Live.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+url=https://www.youtube.com/c/JosephPrinceOnline/live
+url="$(ytdlGetLiveURL.sh "$url")"
 getRestrictedFilenamesFORMAT.sh 94 https://www.youtube.com/c/JosephPrinceOnline/live
