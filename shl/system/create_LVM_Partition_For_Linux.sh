61dfd1c9c619e2cf1bd1778da023f8faaefdcb8c
sebma <sebma@users.noreply.github.com>
2020-11-01 22:06:17 +0100

    Updated create_LVM_Partition_For_Linux.sh

--- shl/system/create_LVM_Partition_For_Linux.sh
+++ shl/system/create_LVM_Partition_For_Linux.sh
@@ -36,13 +36,6 @@ diskModelName=$(sudo smartctl -i $disk | awk '/Device Model:/{$1=$2="";gsub("  "
 diskSerialNumber=$(sudo smartctl -i $disk | awk '/Serial Number:/{$1=$2="";gsub("  ","");gsub(" ","_");print}')
 
 if ! $sudo gdisk -l $disk | grep -qw 8E00;then
-#	cat <<-EOF | $sudo parted $disk
-#	print
-#	mkpart $diskModelName-$diskSerialNumber ext2 256MB -1
-#	set $lvmPartitionNumber lvm on
-#	print
-#	EOF
-
 	$sudo sgdisk -n $lvmPartitionNumber:0:0 -t $lvmPartitionNumber:8E00 -c $lvmPartitionNumber:$diskModelName-$diskSerialNumber $disk
 	$sudo gdisk -l $disk
 
