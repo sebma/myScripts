a3b22f32e25824a7dc4feda6bb0a39db4889601a
sebma <sebma@users.noreply.github.com>
2020-11-01 07:53:23 +0100

    Updated record_NCC_Live.sh

--- shl/record_NCC_Live.sh
+++ shl/record_NCC_Live.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+url=https://www.youtube.com/user/NewCreationChurch/live
+url="$(ytdlGetLiveURL.sh "$url")"
 getRestrictedFilenamesFORMAT.sh 94 https://www.youtube.com/user/NewCreationChurch/live
