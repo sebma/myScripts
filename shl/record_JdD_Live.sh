3476da65f14b3244beb4877d83ffb472aae69eda
sebma <sebma@users.noreply.github.com>
2020-11-01 07:53:23 +0100

    Updated record_JdD_Live.sh

--- shl/record_JdD_Live.sh
+++ shl/record_JdD_Live.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+url=https://www.youtube.com/c/JusticedeDieuMinistry/live
+url="$(ytdlGetLiveURL.sh "$url")"
 getRestrictedFilenamesFORMAT.sh 94 https://www.youtube.com/c/JusticedeDieuMinistry/live
