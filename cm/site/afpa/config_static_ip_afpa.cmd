a5511733344a06cca4bc4ba2e43b7d297fabe2ab
sebma <sebma@users.noreply.github.com>
2020-11-05 15:09:54 +0100

    Updated site/afpa/config_static_ip_afpa.cmd

--- cm/site/afpa/config_static_ip_afpa.cmd
+++ cm/site/afpa/config_static_ip_afpa.cmd
@@ -2,4 +2,4 @@ netsh int ipv4 add addr lan 192.168.22.141
 netsh int ipv4 add addr lan gateway=192.168.22.200 gwmetric=1
 netsh int ipv4 add dns lan 192.168.28.247
 netsh int ipv4 add dns lan 9.9.9.9 index=2
-netsh int ipv4 sh addr lan
+netsh int ip sh addr lan
