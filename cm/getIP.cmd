5b3ee5506fd7bc3e1eae6def39aa5ab315ad1cd7
sebma <sebma@users.noreply.github.com>
2020-11-05 13:17:39 +0100

    Updated getIP.cmd

--- cm/getIP.cmd
+++ cm/getIP.cmd
@@ -2,7 +2,7 @@
 sc config remoteaccess start= demand
 sc qc remoteaccess | findstr START_TYPE
 net start remoteaccess
-netsh interface ip sh ipa
+netsh int ip sh ipa
 net stop remoteaccess
 sc config remoteaccess start= disabled
 sc qc remoteaccess | findstr START_TYPE
