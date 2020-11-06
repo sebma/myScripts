02793ef90de91d76ec318593e66a675feadc3277
sebma <sebma@users.noreply.github.com>
2020-11-06 13:47:38 +0100

    Updated gitUpdateAllMyLocalRepos.sh

--- shl/gitUpdateAllMyLocalRepos.sh
+++ shl/gitUpdateAllMyLocalRepos.sh
@@ -7,7 +7,7 @@ gitUpdateAllMyLocalRepos ()
 	echo $OSTYPE | grep -q android && local osFamily=Android || local osFamily=$(uname -s) # The bash interpreter is needed for it sets the OSTYPE variable
 	[ $osFamily = Darwin ] && find=gfind
 	local dir=""
-	$find ~ -maxdepth 2 -type d -name .git | sort | while read dir; do
+	$find ~ -maxdepth 2 -type d -name .git 2>/dev/null | \egrep -v "/.linuxbrew/" | sort | while read dir; do
 		cd $dir/..
 		remoteRepoUrl=$(git config --local remote.origin.url)
 		test -n "$remoteRepoUrl" && echo "=> Updating <$dir> repo. from <$remoteRepoUrl> repo. ..." 1>&2 && git pull && sync
