db4026936df8b3f8711a180301385e32fd2e061a
sebma <sebma@users.noreply.github.com>
2020-11-06 14:54:51 +0100

    Updated brewInstall.sh

--- shl/brewInstall.sh
+++ shl/brewInstall.sh
@@ -3,16 +3,6 @@
 scriptDir=$(dirname $0)
 scriptDir=$(cd $scriptDir;pwd)
 
-addpaths ()
-{
-	for path in "$@"
-	do
-		test ! -d "$path" && continue
-		echo $PATH | \grep -wq "$path" || PATH=$path:$PATH
-	done
-	export PATH
-}
-
 brewInstall ()
 {
 	brew=undefined
@@ -22,29 +12,22 @@ brewInstall ()
 	if ! which brew > /dev/null 2>&1; then
 		if [ $osFamily = Linux ]; then
 			if groups | \egrep -wq "adm|admin|sudo|wheel"; then
-				$SHELL -c "$(\curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" || return
 				brewPrefix=/home/linuxbrew/.linuxbrew
 			else
-				git --help >/dev/null || return
-				brewPrefix=$HOME/brew
-				cd $brewPrefix
-				git clone https://github.com/homebrew/brew
-#				time git clone https://github.com/homebrew/homebrew-core ./Library/Taps/homebrew/homebrew-core
-				time $brewPrefix/bin/brew tab homebrew/core
-				cd - >/dev/null
+				brewPrefix=$HOME/.linuxbrew
 			fi
 		elif [ $osFamily = Darwin ]; then
-			$SHELL -c "$(\curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" || return
 			brewPrefix=/usr/local
 		fi
 
-		addpaths $brewPrefix
+		$SHELL -c "$(\curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" || return
+
+		echo $PATH | grep -q $brewPrefix || export PATH=$brewPrefix/bin:$PATH
 		brew=$brewPrefix/bin/brew
 		$brew -v
 	fi
 
 	$scriptDir/brewPostInstall.sh
-	sync
 }
 
 brewInstall "$@"
