c2896d7eb1f236bb1ed27bd2b0c0c2e1ad15ac4e
sebma <sebma@users.noreply.github.com>
2020-11-06 22:17:45 +0100

    Updated umountAllAfterChroot_LVM.sh

--- shl/system/umountAllAfterChroot_LVM.sh
+++ shl/system/umountAllAfterChroot_LVM.sh
@@ -8,7 +8,6 @@ fi
 
 type sudo >/dev/null 2>&1 && [ $(id -u) != 0 ] && groups | egrep -wq "sudo|adm|admin|root|wheel" && sudo=$(which sudo) || sudo=""
 set -o nounset
-set -o errexit
 
 for path in /sbin /bin /usr/sbin /usr/bin
 do
@@ -25,7 +24,7 @@ $sudo lvs | grep -q root || {
 #rootFSDevice=$($sudo lvs | awk '/root/{print$2"-"$1}')
 #rootFSDevice=/dev/mapper/$rootFSDevice
 
-fsTypesList="$(printf "ext%d " $(seq 2 4))btrfs f2fs xfs jfs reiserfs nilfs hfs vfat fuseblk"
+#fsTypesList="$(printf "ext%d " $(seq 2 4))btrfs f2fs xfs jfs reiserfs nilfs hfs vfat fuseblk"
 rootFSDevice=$1
 isLVM=$(lsblk -n -o TYPE $rootFSDevice | grep -wq lvm && echo true || echo false)
 umount="umount -v"
@@ -37,8 +36,7 @@ if [ -n "$chrootMntPoint" ];then
 	set -x
 	test -d $chrootMntPoint/boot/efi && mount | grep -q "$chrootMntPoint/.*/efi " && $sudo $umount $chrootMntPoint/boot/efi
 #	[ $isLVM = true ] && rootFS_VG=$(sudo lvs --noheadings  -o vg_name $rootFSDevice) && $sudo $umount /dev/$rootFS_VG/*
-	$df | grep $chrootMntPoint/  | awk '{print$1}' | xargs -rt $sudo $umount
+	$df | grep $chrootMntPoint/  | awk '{print$1}' | xargs -r $sudo $umount
 	$df | grep $chrootMntPoint/ || $sudo $umount $chrootMntPoint/{usr,sys/firmware/efi/efivars,sys,proc,dev/pts,dev,run,}
-	$sudo $umount $chrootMntPoint
 	$df -ah | grep $chrootMntPoint
 fi
