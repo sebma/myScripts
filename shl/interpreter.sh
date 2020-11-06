96d064591ff8e5cef84dbadc85f07a1913a7d9c9
sebma <sebma@users.noreply.github.com>
2020-11-06 16:33:56 +0100

    Updated interpreter.sh

--- shl/interpreter.sh
+++ shl/interpreter.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 get_interpreter() {
-	interpreter=$(ps -o args= $$ | awk '{gsub("^/.*/","",$1);print $1}')
+	interpreter=$(ps -o args= $$ | awk '{print gensub("^/.*/","",1,$1)}')
 	echo $interpreter  
 }
 
