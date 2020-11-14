c8b559a592b99f12fc1fe53227a49cf8316b40bd
sebma <sebma@users.noreply.github.com>
2020-11-14 10:52:55 +0100

    Updated brewPostInstall.sh

--- shl/brewPostInstall.sh
+++ shl/brewPostInstall.sh
@@ -19,6 +19,7 @@ brewPostInstall ()
 	echo 1>&2
 	echo "=> Adding missing taps ..." 1>&2
 	echo 1>&2
+
 	for tap in homebrew/core homebrew/services homebrew/cask homebrew/cask-versions homebrew/cask-drivers homebrew/cask-fonts buo/cask-upgrade
 	do
 		$brew tap | \grep -q $tap || {
@@ -28,6 +29,10 @@ brewPostInstall ()
 			set +x
 		}
 	done
+
+	local macOS_EssentialPackages="iproute2mac"
+	local common_EssentialPackages="awk grep gsed coreutils findutils"
+	[ $osFamily = Darwin ] && $brew install $common_EssentialPackages $macOS_EssentialPackages
 }
 
 brewPostInstall
