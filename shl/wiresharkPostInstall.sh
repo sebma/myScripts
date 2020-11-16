ee704b8cf1fcaaedcbbfe1e5d7c000da7c7deb7b
sebma <sebma@users.noreply.github.com>
2020-11-16 12:48:26 +0100

    Updated wiresharkPostInstall.sh

---
+++ shl/wiresharkPostInstall.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+wiresharkGroup=wireshark
+grep -wq $wiresharkGroup /etc/group || sudo groupadd  --system $wiresharkGroup
+ls -l $(which dumpcap) | grep -q $wiresharkGroup || sudo chown -v root:$wiresharkGroup $(which dumpcap)
+setcap -q -v cap_net_raw,cap_net_admin=eip $(which dumpcap) || sudo setcap cap_net_raw,cap_net_admin=eip $(which dumpcap)
+groups | grep -qw $wiresharkGroup || sudo usermod -a -G $wiresharkGroup $USER
