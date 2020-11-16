1f3f618421bfeb76eb05003d36ae6f0270a4dab9
sebma <sebma@users.noreply.github.com>
2020-11-16 01:43:20 +0100

    Updated downloadOpenSSH.ps1

--- ps1/downloadOpenSSH.ps1
+++ ps1/downloadOpenSSH.ps1
@@ -1,5 +1,7 @@
+# Whatever
 $arch = (gwmi win32_processor).AddressWidth
-$url = "https://github.com/PowerShell/Win32-OpenSSH/releases/download/v8.1.0.0p1-Beta/OpenSSH-Win"+$arch".zip"
+openSSHLatestVersion = "v8.1.0.0p1-Beta"
+$url = "https://github.com/PowerShell/Win32-OpenSSH/releases/download/"+$openSSHLatestVersion+"/OpenSSH-Win"+$arch".zip"
 $OutFile = basename "$url"
 $sslTlsProtocols = [Net.ServicePointManager]::SecurityProtocol
 [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, " + $sslTlsProtocols
@@ -9,4 +11,4 @@ new-alias -name unzip -value expand-archive
 unzip "$OutFile"
 cd $(basename $OutFile .zip)
 cd $(basename $OutFile .zip)
-echo 
+echo "Please run ./install-sshd.ps1"
