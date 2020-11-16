9df72aa0ccf47f58a7d919f522de14c5015fd365
sebma <sebma@users.noreply.github.com>
2020-11-16 01:25:14 +0100

    Updated downloadOpenSSH.ps1

--- ps1/downloadOpenSSH.ps1
+++ ps1/downloadOpenSSH.ps1
@@ -3,7 +3,7 @@ $url = "https://github.com/PowerShell/Win32-OpenSSH/releases/download/v8.1.0.0p1
 $OutFile = basename "$url"
 $sslTlsProtocols = [Net.ServicePointManager]::SecurityProtocol
 [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, " + $sslTlsProtocols
-wget -O "$OutFile" "$url"
+iwr -O "$OutFile" "$url"
 [Net.ServicePointManager]::SecurityProtocol = $sslTlsProtocols
 new-alias -name unzip -value expand-archive
 unzip "$OutFile"
