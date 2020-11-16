b34753ceb54f8a280b16aa487509c0a88438761d
sebma <sebma@users.noreply.github.com>
2020-11-16 01:25:14 +0100

    Updated downloadNUnzip.ps1

--- ps1/downloadNUnzip.ps1
+++ ps1/downloadNUnzip.ps1
@@ -2,7 +2,7 @@ $url="$1"
 $OutFile=basename "$url"
 $sslTlsProtocols = [Net.ServicePointManager]::SecurityProtocol
 [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, " + $sslTlsProtocols
-wget -O "$OutFile" "$url"
+iwr -O "$OutFile" "$url"
 [Net.ServicePointManager]::SecurityProtocol = $sslTlsProtocols
 new-alias -name unzip -value expand-archive
 unzip "$OutFile"
