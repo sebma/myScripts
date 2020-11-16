67674f0ae5af80f6388343417d8e5ce734629b62
sebma <sebma@users.noreply.github.com>
2020-11-16 01:25:14 +0100

    Updated getFile.ps1

--- ps1/getFile.ps1
+++ ps1/getFile.ps1
@@ -2,5 +2,5 @@ $url="$1"
 $OutFile=basename "$url"
 $sslTlsProtocols = [Net.ServicePointManager]::SecurityProtocol
 [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, " + $sslTlsProtocols
-wget -O "$OutFile" "$url"
+iwr -O "$OutFile" "$url"
 [Net.ServicePointManager]::SecurityProtocol = $sslTlsProtocols
