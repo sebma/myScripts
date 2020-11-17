451154a3925221badac111bacb0b4b91863e1ed7
sebma <sebma@users.noreply.github.com>
2020-11-17 19:46:28 +0100

    Updated seb_aliases.ps1

---
+++ psh/seb_aliases.ps1
@@ -0,0 +1,5 @@
+"env","Get-Variable","","ReadOnly, AllScope"
+"np","notepad","","ReadOnly, AllScope"
+"unzip","expand-archive","","ReadOnly, AllScope"
+"grep","findstr","","ReadOnly, AllScope"
+"","ex","","ReadOnly, AllScope"
