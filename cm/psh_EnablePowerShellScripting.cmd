2a98eadab5fead4323f01de1d8cb582e29cbeb7a
sebma <sebma@users.noreply.github.com>
2020-11-17 21:06:35 +0100

    Updated psh_EnablePowerShellScripting.cmd

--- cm/psh_EnablePowerShellScripting.cmd
+++ cm/psh_EnablePowerShellScripting.cmd
@@ -1,3 +1,3 @@
 @echo on
-powershell Get-ExecutionPolicy | findstr -i RemoteSigned >NUL || powershell "Set-ExecutionPolicy RemoteSigned -scope CurrentUser"
+powershell Get-ExecutionPolicy | findstr -i RemoteSigned >NUL || powershell "Set-ExecutionPolicy RemoteSigned -scope CurrentUser -f"
 powershell Get-ExecutionPolicy
