3c4decf1e25a767ed1e7aa8e92f73b2514e1a028
sebma <sebma@users.noreply.github.com>
2020-11-17 15:42:56 +0100

    Updated psh_FirstRun.cmd

--- cm/psh_FirstRun.cmd
+++ cm/psh_FirstRun.cmd
@@ -1,5 +1,5 @@
 @echo on
-powershell Get-ExecutionPolicy | findstr -i RemoteSigned >NUL || powershell "Set-ExecutionPolicy RemoteSigned"
+powershell Get-ExecutionPolicy | findstr -i RemoteSigned >NUL || powershell "Set-ExecutionPolicy RemoteSigned -scope CurrentUser"
 powershell Get-ExecutionPolicy
 powershell -Set-ExecutionPolicy RemoteSigned -File "../psh/psh_CreateNewProfile_N_Initialize.ps1"
 
