0c77cf72ebec3a33b5fdcdd9e95e2e0ce53f28db
sebma <sebma@users.noreply.github.com>
2020-11-27 16:56:50 +0100

    Updated restore_LVM_GrubBootLoader-EFI.sh

--- shl/recovery/restore_LVM_GrubBootLoader-EFI.sh
+++ shl/recovery/restore_LVM_GrubBootLoader-EFI.sh
@@ -15,7 +15,7 @@ mount | grep -q $rootFSDevice || $sudo mount /dev/mapper/$rootFSDevice /mnt     
 for special in dev dev/pts proc sys ; do $sudo mkdir -pv /mnt/$special;$sudo mount -v --bind /$special /mnt/$special ; done
 
 set +o errexit
-$sudo chroot /mnt /bin/bash <<-EOF # mise a la racine du disque monte
+$sudo chroot /mnt $SHELL <<-EOF # mise a la racine du disque monte
 	findmnt >/dev/null && mount -av || exit                      # montage des partitions dans le chroot
 	mkdir -p /media/boot/efi
 	mount /dev/sda1 /media/boot/efi
