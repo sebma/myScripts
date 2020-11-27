03906347a145c81b6d4084bae415424f3700b4dc
sebma <sebma@users.noreply.github.com>
2020-11-27 16:56:15 +0100

    Updated rebuild_LVM_grub.sh

--- shl/system/rebuild_LVM_grub.sh
+++ shl/system/rebuild_LVM_grub.sh
@@ -18,7 +18,7 @@ mount | grep -q $rootFSLogicalVolume || $sudo mount /dev/mapper/$rootFSLogicalVo
 for special in dev dev/pts proc sys ; do $sudo mkdir -pv /mnt/$special;$sudo mount -v --bind /$special /mnt/$special ; done
 
 set +o errexit
-$sudo chroot /mnt /bin/bash <<-EOF # mise a la racine du disque monte
+$sudo chroot /mnt $SHELL <<-EOF # mise a la racine du disque monte
 	findmnt >/dev/null && mount -av || exit                      # montage des partitions dans le chroot
 	update-grub                   # creation d'un nouveau fichier de configuration : grub.cfg
 	grub-install $diskDevice || grub-install --force $diskDevice        # installation de grub sur le MBR
