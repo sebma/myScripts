92b0ae28ef7203fc3156833deabe81cd8b9cadcb
sebma <sebma@users.noreply.github.com>
2020-11-27 16:56:50 +0100

    Updated restore_Debian_LVM_GrubBootLoader.sh

--- shl/recovery/restore_Debian_LVM_GrubBootLoader.sh
+++ shl/recovery/restore_Debian_LVM_GrubBootLoader.sh
@@ -15,7 +15,7 @@ mount | grep -q $rootFSDevice || $sudo mount /dev/mapper/$rootFSDevice /mnt     
 for special in dev dev/pts proc sys ; do $sudo mkdir -pv /mnt/$special;$sudo mount -v --bind /$special /mnt/$special ; done
 
 set +o errexit
-$sudo chroot /mnt /bin/bash <<-EOF # mise a la racine du disque monte
+$sudo chroot /mnt $SHELL <<-EOF # mise a la racine du disque monte
 	findmnt >/dev/null && mount -av || exit                      # montage des partitions dans le chroot
 	test -s /boot/grub/grub.cfg || update-grub # creation d'un nouveau fichier de configuration : grub.cfg
 	grub-install /dev/sda || grub-install --force /dev/sda        # installation de grub sur le MBR
