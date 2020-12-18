cafe4ce07e97fa9c58bc557b5299ab121e818838
sebma <sebma@users.noreply.github.com>
2020-12-18 08:10:51 +0100

    Updated dockerBuild.sh

---
+++ shl/dockerBuild.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+dockerBuild () {
+	local imageName="$1"
+	local dockerFile="$2"
+	local dir="$3"
+
+	if [ $# = 2 ];then
+		dir=.
+		shift 2
+	elif [ $# = 3 ];then
+		shift 3
+	elif [ $# -lt 2 ];then
+		echo "=> $FUNCNAME imageName dockerFile [dir = .]" >&2
+		return 1
+	fi
+
+	time docker build -t "$imageName" -f "$dockerFile" "$dir" "$@"
+	return $?
+}
+
+dockerBuild "$@"
