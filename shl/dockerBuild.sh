dc0e7bdf9ad44cec5517d7d1b240f0209d00d745
sebma <sebma@users.noreply.github.com>
2020-12-18 08:24:11 +0100

    Updated dockerBuild.sh

--- shl/dockerBuild.sh
+++ shl/dockerBuild.sh
@@ -15,7 +15,7 @@ dockerBuild () {
 		return 1
 	fi
 
-	time docker build -t "$imageName" -f "$dockerFile" "$dir" "$@" && docker images "$imageName"
+	time docker build -t "$imageName" -f "$dockerFile" "$dir" "$@" && docker images "$imageName" && echo "=> docker run -it --rm $imageName"
 	return $?
 }
 
