3f92f6af2e1ea315d337a35d79b94416f89869e2
sebma <sebma@users.noreply.github.com>
2021-01-16 16:18:06 +0100

    Updated btrestart.sh

--- shl/system/btrestart.sh
+++ shl/system/btrestart.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 [ $(id -u) != 0 ] && sudo=sudo || sudo=""
-$sudo service bluetooth stop;sleep 1;$sudo service bluetooth start;sleep 1;service bluetooth status
+$sudo killall pulseaudio;sleep 1;$sudo service bluetooth start;sleep 1;service bluetooth status
