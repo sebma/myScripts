0724494c81b394bccf97f1e3ab579be8ae06241b
sebma <sebma@users.noreply.github.com>
2021-04-08 21:24:16 +0200

    Updated restart_wmi.cmd

--- cm/restart_wmi.cmd
+++
@@ -1,6 +0,0 @@
-@rundll32 wbemupgd, UpgradeRepository
-@net stop sharedaccess
-@net stop winmgmt
-@net start winmgmt
-@net start sharedaccess
-@net use p: /del && net use p: \\NASTIG003\DEF007_users$\X064304 /persistent:yes
