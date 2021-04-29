76e4dd70eec7f48504352e6eb153ff3c7779f900
sebma <sebma@users.noreply.github.com>
2021-04-29 15:59:28 +0200

    Updated KWinStart.sh

---
+++ shl/KWinStart.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+nohup $(which kwin) --replace &
+echo "=> Sleeping 10 seconds to wait for KWin to load ..."
+sleep 10
+pgrep conky && {
+	killall conky
+	conky -d
+	sleep 1
+}
+echo "=> DONE."
