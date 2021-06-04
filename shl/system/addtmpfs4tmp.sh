7ccfde99f8e501801a214944fccaa8fd31ab51d0
sebma <sebma@users.noreply.github.com>
2021-06-04 15:03:33 +0200

    Updated addtmpfs4tmp.sh

--- shl/system/addtmpfs4tmp.sh
+++ shl/system/addtmpfs4tmp.sh
@@ -2,6 +2,7 @@
 
 if ! grep -v "^#" /etc/fstab | grep -q "tmpfs.*/tmp"; 
 then
-	size=$(awk '/^MemTotal/{size=0.2*$2/1024;printf int(size)"M"}' /proc/meminfo)
-	echo "tmpfs /tmp tmpfs rw,nosuid,nodev,size=$size" | sudo tee -a /etc/fstab
+#	tmpfsSize=$(awk '/^MemTotal/{size=0.2*$2/1024;printf int(size)"M"}' /proc/meminfo)
+	tmpfsSize=20%
+	echo "tmpfs /tmp tmpfs rw,nosuid,nodev,size=$tmpfsSize" | sudo tee -a /etc/fstab
 fi
