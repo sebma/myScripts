1d2fdfca55df9d00b7ad0db1273861bb037f9783
sebma <sebma@users.noreply.github.com>
2021-06-23 22:11:15 +0200

    Updated mpvFORMAT.sh

--- shl/mpvFORMAT.sh
+++ shl/mpvFORMAT.sh
@@ -16,9 +16,7 @@ mpvFORMAT() {
 	shift
 
 	# Si on lance mpv via un ssh sur le PC b206, alors on ne forward pas l'affichage
-	if [ "$SSH_CONNECTION" ];then
-		hostname | grep -qi b206$ && DISPLAY=:0 
-	fi
+	test "$SSH_CONNECTION" && hostname | grep -qi b206$ && DISPLAY=:0
 
 	if grep -q "\[$format\]" "$mpvConfigFile";then
 		LANG=en_US.utf8 nohup $mpv --profile="$format" "$@" &
