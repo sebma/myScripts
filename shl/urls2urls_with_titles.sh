a0ad00d709a34c48cc9344b2f359bc901e20cb68
sebma <sebma@users.noreply.github.com>
2021-07-14 20:42:49 +0200

    Updated urls2urls_with_titles.sh

--- shl/urls2urls_with_titles.sh
+++ shl/urls2urls_with_titles.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
 function getURLTitle() {
+	[ $# != 0 ] && [ "$1" = "-h" ] && {
+		echo "=> Usage: $FUNCNAME url1 url2 url3 ..." >&2
+		return 1
+	}
+
 	for URL
 	do
 		printf "$URL # ";\curl -qLs $URL | pup --charset utf8 'title text{}' | \recode html..latin9
 	done
 }
 
-getURLTitle $@
+getURLTitle "$@"
