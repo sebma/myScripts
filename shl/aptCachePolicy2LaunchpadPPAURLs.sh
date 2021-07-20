9c1a4b34b20dc1ce850ca245a9b0a728317e29b0
sebma <sebma@users.noreply.github.com>
2021-07-20 22:03:20 +0200

    Updated aptCachePolicy2LaunchpadPPAURLs.sh

--- shl/aptCachePolicy2LaunchpadPPAURLs.sh
+++ shl/aptCachePolicy2LaunchpadPPAURLs.sh
@@ -1,11 +1,24 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 aptCachePolicy2LaunchpadPPAURLs ()
 {
+	local platform=$(uname -m)
+	local arch=unset
+
+	case $platform in
+		x86_64)arch=amd64;;
+		i686) arch=i386;;
+		*) arch=UNKNOWN;;
+	esac
+
 	for pkg
 	do
-		\apt-cache -q0 policy $pkg | awk '/launchpad.net/{print$2}' | awk -v pkg=$pkg -F/ 'BEGIN{OFS=FS}{printf pkg" ppa:"$4"/"$5" ";sub("ppa.","",$3);$4="~"$4"/+archive";tmp=$5;$5=$6;$6=tmp;print}'
-	done | uniq | column -t
+		aptCachePolicyOutPut=$(apt-cache policy $pkg)
+		if echo $aptCachePolicyOutPut | grep -q launchpad.net;then
+			echo "$aptCachePolicyOutPut" | awk -F: '/^[^ ]+:/{printf$1}/Installed:/{printf$2}'
+			echo "$aptCachePolicyOutPut" | awk "/launchpad.net.*$arch Packages/"'{print$2}' | awk -F/ 'BEGIN{OFS=FS}{printf " ppa:"$4"/"$5" ";sub("ppa.","",$3);$4="~"$4"/+archive";tmp=$5;$5=$6;$6=tmp;print}'
+		fi
+	done | column -t
 }
 
 aptCachePolicy2LaunchpadPPAURLs "$@"
