98595da75401eb91aab7bf4f263ffdc66efc66cd
sebma <sebma@users.noreply.github.com>
2021-07-21 12:28:10 +0200

    Updated setDefaultBrowser.sh

--- shl/setDefaultBrowser.sh
+++ shl/setDefaultBrowser.sh
@@ -2,24 +2,22 @@
 
 [ $# = 0 ] && browserList="google-chrome brave-browser palemoon firefox-esr firefox midori qupzilla chromium-browser konqueror" || browserList="$@"
 
-#Association du protocole apt:, ssh: avec les applications adequoites
-echo "=> Association du protocole apt:, ssh: avec les applications adequoites ..."
-xdg-mime default apturl.desktop x-scheme-handler/apt
-xdg-mime default putty.desktop x-scheme-handler/ssh
-
 for browser in $browserList
 do
 	which $browser >/dev/null 2>&1 && defaultBrowser=$browser && break
 done
 
-echo "=> defaultBrowser = $defaultBrowser"
+echo "=> Chosen defaultBrowser = $defaultBrowser"
 desktopFile=$(basename $(locate /usr/share/*/$defaultBrowser.desktop) | head -1)
-echo "=> desktopFile = $desktopFile"
+echo "=> Corresponding desktopFile = $desktopFile"
 echo "=> Association des protocoles http et https avec $desktopFile ..."
+echo "=> xdg-mime default $desktopFile x-scheme-handler/http x-scheme-handler/https ..."
 xdg-mime default $desktopFile x-scheme-handler/http x-scheme-handler/https
-echo "=> Verification ..."
+echo "=> Verification de l'ouverture des protocoles http et https par <$defaultBrowser> ..."
+xdg-mime query default x-scheme-handler/http
 xdg-mime query default x-scheme-handler/https
 echo "=> xdg-settings set default-web-browser $desktopFile ..."
 xdg-settings set default-web-browser $desktopFile
+echo "=> xdg-settings get default-web-browser ..."
 xdg-settings get default-web-browser
 echo "=> Fait."
