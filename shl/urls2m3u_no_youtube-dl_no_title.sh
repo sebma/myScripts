5d8b52c3577c37cc5a3e15f89ab1a521316d17e4
sebma <sebma@users.noreply.github.com>
2021-07-25 16:28:37 +0200

    Updated urls2m3u_no_youtube-dl_no_title.sh

--- shl/urls2m3u_no_youtube-dl_no_title.sh
+++ shl/urls2m3u_no_youtube-dl_no_title.sh
@@ -6,11 +6,11 @@ test $# = 0 && {
 	exit 1
 }
 
-echo "#EXTM3U"
+echo "#EXTM3U" | \recode ..utf-8
 egrep -v "^(#|$)" $urlsFile | uniq | while read url
 do
 	extension=.mp4
 	title=$(basename "$url" $extension | sed "s/%20/ /g")
-	echo "#EXTINF:-1,$title"
-	echo $url
+	echo "#EXTINF:-1,$title" | \recode ..utf-8
+	echo $url | \recode ..utf-8
 done
