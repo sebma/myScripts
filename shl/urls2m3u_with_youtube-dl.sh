59d02b67463a9b5acf50363086002733c983b47b
sebma <sebma@users.noreply.github.com>
2021-07-25 16:28:37 +0200

    Updated urls2m3u_with_youtube-dl.sh

--- shl/urls2m3u_with_youtube-dl.sh
+++ shl/urls2m3u_with_youtube-dl.sh
@@ -8,10 +8,9 @@ test $# = 0 && {
 
 alias youtube-dl='LANG=C.UTF-8 command youtube-dl'
 
-echo "#EXTM3U"
+echo "#EXTM3U" | \recode ..utf-8
 time awk '!/^($|#)/{print$1}' $urlsFile | uniq | while read url
 do
-	printf "#EXTINF:-1,"
-	youtube-dl -e $url
-	echo $url
+	printf "#EXTINF:-1," | \recode ..utf-8
+	youtube-dl -e $url | \recode ..utf-8
 done
