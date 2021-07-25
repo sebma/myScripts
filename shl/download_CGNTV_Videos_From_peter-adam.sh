62a7fddaa369a2df0eeb808ac8edd6410bddda57
sebma <sebma@users.noreply.github.com>
2021-07-26 00:15:27 +0200

    Updated download_CGNTV_Videos_From_peter-adam.sh

--- shl/download_CGNTV_Videos_From_peter-adam.sh
+++ shl/download_CGNTV_Videos_From_peter-adam.sh
@@ -12,9 +12,8 @@ do
 		videoTitle=$(\curl -qLs "$url" | pup 'meta[property=og:title] attr{content}')
 		videoFileName=$(echo $videoTitle | sed 's/ /_/g').mp4
 		videoID=$(\curl -qLs "$url" | pup 'meta[property=og:description] attr{content}' | awk -F'[[ ]' '{print$2}')
-		videoDirectURL=$(youtube-dl --ignore-config --no-warnings -g $peterAdamJPV_BaseURL/pop/popJW.php?nc=$videoID)
 		set -x
-		wget --no-config -cO "$videoFileName" "$videoDirectURL"
+		youtube-dl --ignore-config -co "$videoFileName" $peterAdamJPV_BaseURL/pop/popJW.php?nc=$videoID
 		set +x
 	else
 		echo "=> Only <$peterAdamJPV_BaseURL/viewitem.php> url types are supported." >&2
