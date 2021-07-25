44370930b00cac2bf6c74672f360b4423e1be00e
sebma <sebma@users.noreply.github.com>
2021-07-25 16:48:06 +0200

    Updated getURLTitle.sh

--- shl/getURLTitle.sh
+++ shl/getURLTitle.sh
@@ -4,17 +4,20 @@ function getURLTitle() {
 	if which pup > /dev/null; then
 		for URL
 		do
+			printf "$URL # "
 			\curl -qLs $URL | pup --charset utf8 'head title text{}'
 		done
 	elif which xidel > /dev/null; then
 		for URL
 		do
+			printf "$URL # "
 			\curl -qLs $URL | xidel -s --css 'head title'
 		done
 	elif which hxselect > /dev/null; then
 		for URL
 		do
-			\curl -qLs $URL | hxnormalize -x | hxselect -s '\n' 'head title'
+			printf "$URL # "
+			\curl -qLs $URL | hxnormalize -x | hxselect -s '\n' 'head title' -c
 		done
 	fi
 }
