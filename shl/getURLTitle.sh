1b1cb256d3b0423bab389e753c4b8fbf6e4b7f00
sebma <sebma@users.noreply.github.com>
2021-07-25 16:43:20 +0200

    Updated getURLTitle.sh

--- shl/getURLTitle.sh
+++ shl/getURLTitle.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
 
 function getURLTitle() {
-	for URL
-	do
-		\curl -qLs $URL | pup --charset utf8 'title text{}' | \recode html..latin9
-	done
+	if which pup > /dev/null; then
+		for URL
+		do
+			\curl -qLs $URL | pup --charset utf8 'head title text{}'
+		done
+	elif which xidel > /dev/null; then
+		for URL
+		do
+			\curl -qLs $URL | xidel -s --css 'head title'
+		done
+	elif which hxselect > /dev/null; then
+		for URL
+		do
+			\curl -qLs $URL | hxnormalize -x | hxselect -s '\n' 'head title'
+		done
+	fi
 }
 
 getURLTitle $@
