dcfb4c8f968d0af750aa8746511ca4856c3fb85a
sebma <sebma@users.noreply.github.com>
2021-07-26 00:00:26 +0200

    Updated download_CGNTV_Videos_From_peter-adam.sh

--- shl/download_CGNTV_Videos_From_peter-adam.sh
+++ shl/download_CGNTV_Videos_From_peter-adam.sh
@@ -8,7 +8,7 @@
 for url
 do
 	if echo "$url" | grep -q www.peter-adam.com/jpv/viewitem.php;then
-		videoTitle=$(\curl -qLs "$url" | pup 'head title text{}' | cut -d- -f2- | sed 's/^ *//;s/#//')
+		videoTitle=$(\curl -qLs "$url" | pup 'meta[property=og:title] attr{content}')
 		videoFileName=$(echo $videoTitle | sed 's/ /_/g').mp4
 		videoID=$(\curl -qLs "$url" | pup 'meta[property=og:description] attr{content}' | awk -F'[[ ]' '{print$2}')
 		videoDirectURL=$(youtube-dl --ignore-config --no-warnings -g http://www.peter-adam.com/jpv/pop/popJW.php?nc=$videoID)
