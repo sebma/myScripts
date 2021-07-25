e4f65a81aa5b714163f749fd362b99ed3f276daf
sebma <sebma@users.noreply.github.com>
2021-07-25 19:10:37 +0200

    Updated m3u2urls.sh

--- shl/m3u2urls.sh
+++ shl/m3u2urls.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-#grep -v EXTM3U $@ | awk -F"," '/EXTINF/{$1="";title=$0}/^(https?|s?ftps?|ssh):/{url=$0;print url" #"title}'
-tail -n +2 $@ | tac | \sed -E 'N;s/\n#EXTINF:-1,/ # /' | tac
+#grep -v EXTM3U "$@" | awk -F"," '/EXTINF/{$1="";title=$0}/^(https?|s?ftps?|ssh):/{url=$0;print url" #"title}'
+tail -n +2 "$@" | tac | \sed -E 'N;s/\n#EXTINF:-1,/ # /' | tac
