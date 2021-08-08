1546886e047553fe94ff7b54f4df9a0e995cd482
sebma <sebma@users.noreply.github.com>
2021-08-08 17:17:23 +0200

    Updated hw-probeAddNodeToInventory.sh

--- shl/hw-probeAddNodeToInventory.sh
+++ shl/hw-probeAddNodeToInventory.sh
@@ -3,6 +3,10 @@
 hw-probeAddNodeToInventory ()
 {
 	local inventoryID=null
+	if ! which hw-probe >/dev/null 2>&1;then
+		echo "=> ERROR [$FUNCNAME] : You must first install <hw-probe>." >&2
+		return 1
+	fi
 	if [ $# = 0 ]; then
 		inventoryID="LHW-8028-0102-D496-15BF"
 	else
@@ -10,7 +14,7 @@ hw-probeAddNodeToInventory ()
 			inventoryID=$1
 		else
 			echo "=> $FUNCNAME [inventoryID]" 1>&2
-			return 1
+			return 2
 		fi
 	fi
 	\sudo -E \hw-probe -all -upload -i $inventoryID && echo "=> INFO: Check your email to add confirm adding the new node to your inventory : https://linux-hardware.org/index.php?view=computers&inventory=$inventoryID" 1>&2
