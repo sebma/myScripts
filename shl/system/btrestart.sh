0cfc5a07d37d982901b4fbd22e54f740e99418cd
sebma <sebma@users.noreply.github.com>
2021-09-03 15:26:43 +0200

    Updated btrestart.sh

--- shl/system/btrestart.sh
+++ shl/system/btrestart.sh
@@ -5,10 +5,18 @@ set -o pipefail
 systemType=$(strings $initPath | egrep -o "upstart|sysvinit|systemd" | head -1 || echo unknown)
 set +o pipefail
 [ $(id -u) != 0 ] && sudo=sudo || sudo=""
+
 echo "=> Restarting the pulseaudio server ..."
 pulseaudio --kill;sleep 1;pidof pulseaudio >/dev/null || pulseaudio --start --log-target=syslog # Restart pulseaudio
 sleep 1
 ps -fC pulseaudio
+
+echo "=> Starting bluetooth controllers ..."
+hciconfig | awk -F'[\t :]+' '/^\w+:/{controller=$1}/\tDOWN/{controller2Start=controller;controller="";print controller2Start}' | while read controller
+do
+	sudo hciconfig $controller up
+done
+
 echo "=> Restarting the bluetooth service ..."
 $sudo service bluetooth stop;sleep 1;$sudo service bluetooth start;sleep 1
 if [ $systemType = systemd ];then
