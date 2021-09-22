2828f6a0d3845c50bb9d79b752b74a8c53ad3248
sebma <sebma@users.noreply.github.com>
2021-09-22 19:38:09 +0200

    Updated greplibreoffice.sh

---
+++ shl/greplibreoffice.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+[ "$1" ] || { echo "You forgot search string!" ; exit 1 ; }
+find . -type f -name "*.od?" | while read file ; do
+	 unzip -ca "$file" 2>/dev/null | grep -P --label="$file" "$@"
+done
