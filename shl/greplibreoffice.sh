db88ec3a9b3f60f7996d6d36b5d5e3fcff237f29
sebma <sebma@users.noreply.github.com>
2021-09-22 20:22:06 +0200

    Updated greplibreoffice.sh

--- shl/greplibreoffice.sh
+++ shl/greplibreoffice.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 [ "$1" ] || { echo "You forgot search string!" ; exit 1 ; }
-find . -type f -name "*.od?" | while read file ; do
+find .  -type f -and '(' -name "*.od?" -or -name "*.docx" -or -name "*.xlsx" ')' | while read file ; do
 	 unzip -ca "$file" 2>/dev/null | grep -P --label="$file" "$@"
 done
