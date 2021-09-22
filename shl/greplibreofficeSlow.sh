cbdc1cd7edee778b5d9aa6a1df350994ef7334ce
sebma <sebma@users.noreply.github.com>
2021-09-22 20:22:06 +0200

    Updated greplibreofficeSlow.sh

--- shl/greplibreofficeSlow.sh
+++ shl/greplibreofficeSlow.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 [ "$1" ] || { echo "You forgot search string!" ; exit 1 ; }
-find . -type f -name "*.od?" | while read file ; do
+find .  -type f -and '(' -name "*.od?" -or -name "*.docx" -or -name "*.xlsx" ')' | while read file ; do
 	printf "$file:"
 	unoconv --stdout -f text "$file" 2>/dev/null | grep -P --label="$file" "$@" || printf "\r"
 done
