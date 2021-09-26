164a11d6a69944fbc320cc09637de3e9f26d078a
sebma <sebma@users.noreply.github.com>
2021-09-27 01:08:54 +0200

    Updated record_JP_USA_Live.sh

---
+++ shl/record_JP_USA_Live.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+url=https://www.youtube.com/c/JosephPrinceMinistriesUSA/live
+#url="$(ytdlGetLiveURL.sh "$url")"
+cd /multimedia/Videos/CHRIST/Joseph_Prince/Live_sermons_JP_USA && getRestrictedFilenamesFORMAT.sh 93 "$url"
