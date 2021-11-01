b74f4b319a58dc0a2dd7139e50d4b7a1c11a41c6
sebma <sebma@users.noreply.github.com>
2021-11-01 19:25:00 +0100

    Updated not_mine/Ventoy2Disk.sh

--- shl/not_mine/Ventoy2Disk.sh
+++ shl/not_mine/Ventoy2Disk.sh
@@ -4,4 +4,8 @@ Ventoy2DiskScript=/opt/ventoy/Ventoy2Disk.sh
 ls -l $Ventoy2DiskScript | grep -q "root $USER" || sudo chgrp $USER $Ventoy2DiskScript
 test ! -x $Ventoy2DiskScript && sudo chmod g+x $Ventoy2DiskScript
 
-cd /opt/ventoy && ./$(basename $Ventoy2DiskScript) "$@"
+if cd /opt/ventoy;then
+	if [ $(id -u) != 0 ];then
+		sudo ./$(basename $Ventoy2DiskScript) "$@"
+	fi
+fi
