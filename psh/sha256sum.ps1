fbcb01c74cf3cd53fbf5a152938a9d39047eac69
sebma <sebma@users.noreply.github.com>
2021-11-04 23:21:17 +0100

    Updated sha256sum.ps1

--- psh/sha256sum.ps1
+++ psh/sha256sum.ps1
@@ -1,3 +1,15 @@
 #!/usr/bin/env pwsh
 param($filename)
-(Get-FileHash -algo SHA256 $filename).Hash + "  " + $filename
+function calcSignature() {
+	$scriptName = Split-Path -Leaf $PSCommandPath
+	switch( $scriptName ) {
+		"md5sum.ps1" { $algo = "MD5"; Break }
+		"sha1sum.ps1" { $algo = "SHA1"; Break }
+		"sha256sum.ps1" { $algo = "SHA256"; Break }
+		"sha384sum.ps1" { $algo = "SHA384"; Break }
+		"sha512sum.ps1" { $algo = "SHA512"; Break }
+	}
+	(Get-FileHash -algo $algo $filename).Hash + "  " + $filename
+}
+
+calcSignature
