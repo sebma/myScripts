1b68069db3de446216b9255c3083ec617fbf74e5
sebma <sebma@users.noreply.github.com>
2021-11-12 11:52:40 +0100

    Updated Ventoy2DiskUpdate.sh

--- shl/Ventoy2DiskUpdate.sh
+++ shl/Ventoy2DiskUpdate.sh
@@ -11,17 +11,18 @@ gitHubUser=ventoy
 gitHubRepo=Ventoy
 gitHubAPIRepoURL=$gitHubAPIURL/repos/$gitHubUser/$gitHubRepo
 ventoyLatestRelease=$(git ls-remote --tags --refs --sort=-version:refname https://github.com/$gitHubUser/$gitHubRepo | awk -F/ '{print gensub("^v","",1,$NF);exit}')
-Ventoy2DiskLatestGitHubReleaseURL=$(curl -s $gitHubAPIRepoURL/releases |  jq -r '.[0].assets[] | select( .content_type == "application/x-gzip" ).browser_download_url')
+Ventoy2DiskLatestGitHubReleaseURL=$(curl -s $gitHubAPIRepoURL/releases |  jq -r '.[1].assets[] | select( .content_type == "application/x-gzip" ).browser_download_url')
 
 if [ -n "$Ventoy2DiskLatestGitHubReleaseURL" ];then
-curl -s "$Ventoy2DiskLatestGitHubReleaseURL" | tar -C /tmp -xz
-chmod +x /tmp/ventoy-$ventoyLatestRelease/tool/xzcat
-sudo mkdir -pv $ventoyROOT/
-rsync="$(which rsync) -uth -P -z --skip-compress=$RSYNC_SKIP_COMPRESS_LIST"
-cp2ext234="$rsync -ogpuv -lSH"
-time sudo $cp2ext234 -r /tmp/ventoy-$ventoyLatestRelease/* /opt/ventoy
-sudo chown root -R $ventoyROOT/*
-sudo rm -fr /tmp/ventoy-$ventoyLatestRelease/
+	curl -s "$Ventoy2DiskLatestGitHubReleaseURL" | tar -C /tmp -xz
+	chmod +x /tmp/ventoy-$ventoyLatestRelease/tool/xzcat
+	sudo mkdir -pv $ventoyROOT/
+	rsync="$(which rsync) -uth -P -z --skip-compress=$RSYNC_SKIP_COMPRESS_LIST"
+	cp2ext234="$rsync -ogpuv -lSH"
+	time sudo $cp2ext234 -r /tmp/ventoy-$ventoyLatestRelease/* /opt/ventoy
+	sudo chown root -R $ventoyROOT/*
+	sudo rm -fr /tmp/ventoy-$ventoyLatestRelease/
+fi
 
 if cd $ventoyROOT;then
 	sudo ./Ventoy2Disk.sh -u "$@"
