b7752dd4a515e2f0e406c615064a36b556a579c0
sebma <sebma@users.noreply.github.com>
2021-11-12 12:08:06 +0100

    Updated Ventoy2DiskUpdate.sh

--- shl/Ventoy2DiskUpdate.sh
+++ shl/Ventoy2DiskUpdate.sh
@@ -10,21 +10,29 @@ gitHubAPIURL=https://api.github.com
 gitHubUser=ventoy
 gitHubRepo=Ventoy
 gitHubAPIRepoURL=$gitHubAPIURL/repos/$gitHubUser/$gitHubRepo
-ventoyLatestRelease=$(git ls-remote --tags --refs --sort=-version:refname https://github.com/$gitHubUser/$gitHubRepo | awk -F/ '{print gensub("^v","",1,$NF);exit}')
-Ventoy2DiskLatestGitHubReleaseURL=$(curl -s $gitHubAPIRepoURL/releases |  jq -r '.[1].assets[] | select( .content_type == "application/x-gzip" ).browser_download_url')
+#ventoyLatestRelease=$(git ls-remote --tags --refs --sort=-version:refname https://github.com/$gitHubUser/$gitHubRepo | awk -F/ '{print gensub("^v","",1,$NF);exit}')
+ventoyLatestRelease=$(curl -s $gitHubAPIRepoURL/tags | jq -r '.[0].name' | sed 's/v//;s/-beta//')
+Ventoy2DiskLatestGitHubReleaseURL=$(curl -s $gitHubAPIRepoURL/releases |  jq -r '.[0].assets[] | select( .content_type == "application/x-gzip" ).browser_download_url')
+
+architecture=$(uname -m)
+case $architecture in
+	x86_64) ;;
+	i686) architecture=i386;;
+	*) echo "=> Unkown architecture.";exit;;
+esac
 
 if [ -n "$Ventoy2DiskLatestGitHubReleaseURL" ];then
-	curl -s "$Ventoy2DiskLatestGitHubReleaseURL" | tar -C /tmp -xz
-	chmod +x /tmp/ventoy-$ventoyLatestRelease/tool/xzcat
-	sudo mkdir -pv $ventoyROOT/
+	\curl -Ls "$Ventoy2DiskLatestGitHubReleaseURL" | tar -C /tmp -xz || exit
+#	chmod +x /tmp/ventoy-$ventoyLatestRelease/tool/$architecture/xzcat
+	test -d $ventoyROOT/ || sudo mkdir -pv $ventoyROOT/
 	rsync="$(which rsync) -uth -P -z --skip-compress=$RSYNC_SKIP_COMPRESS_LIST"
 	cp2ext234="$rsync -ogpuv -lSH"
 	time sudo $cp2ext234 -r /tmp/ventoy-$ventoyLatestRelease/* /opt/ventoy
 	sudo chown root -R $ventoyROOT/*
-	sudo rm -fr /tmp/ventoy-$ventoyLatestRelease/
+	rm -fr /tmp/ventoy-$ventoyLatestRelease/
 fi
 
-if cd $ventoyROOT;then
+if cd $ventoyROOT && [ $# != 0 ];then
 	sudo ./Ventoy2Disk.sh -u "$@"
 fi
 
