ddeadc519dfa886d88627a9f69f5e0daea28ac7f
sebma <sebma@users.noreply.github.com>
2021-11-12 01:18:22 +0100

    Updated freeTubeUpdate.sh

--- shl/freeTubeUpdate.sh
+++ shl/freeTubeUpdate.sh
@@ -8,7 +8,8 @@ gitHubAPIURL=https://api.github.com
 gitHubUser=FreeTubeApp
 gitHubRepo=FreeTube
 gitHubAPIRepoURL=$gitHubAPIURL/repos/$gitHubUser/$gitHubRepo
-freeTubeLatestGitHubReleaseURL=$(curl -s $gitHubAPIRepoURL/releases |  jq -r '.[0].assets[] | select(.name | contains("amd64.deb")) | .browser_download_url')
+freeTubeLatestGitHubReleaseURL=$(curl -s $gitHubAPIRepoURL/releases | jq -r '.[0].assets[] | select(.name | contains("amd64.deb")) | .browser_download_url')
 freeTubeLatestGitHubReleaseName=$(basename $freeTubeLatestGitHubReleaseURL)
+freeTubeLatestGitHubReleaseTag=$(curl -s $gitHubAPIRepoURL/tags | jq -r '.[0].name')
 wget -O $freeTubeLatestGitHubReleaseName "$freeTubeLatestGitHubReleaseURL"
 sudo gdebi -n $freeTubeLatestGitHubReleaseName && rm -v $freeTubeLatestGitHubReleaseName
