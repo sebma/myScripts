7bf6d280651f9b3f1427e62659e84eee8f39af28
sebma <sebma@users.noreply.github.com>
2021-11-12 12:25:17 +0100

    Updated freeTubeUpdate.sh

--- shl/freeTubeUpdate.sh
+++ shl/freeTubeUpdate.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+architecture=$(uname -m)
+case $architecture in
+	x86_64) arch=amd64;;
+	i686)   arch=i386;;
+	*) echo "=> Unsupported architecture.";exit;;
+esac
+
 scriptDir=$(dirname $0)
 scriptDir=$(cd $scriptDir;pwd)
 app=freeTube
@@ -8,7 +15,7 @@ gitHubAPIURL=https://api.github.com
 gitHubUser=FreeTubeApp
 gitHubRepo=FreeTube
 gitHubAPIRepoURL=$gitHubAPIURL/repos/$gitHubUser/$gitHubRepo
-freeTubeLatestGitHubReleaseURL=$(curl -s $gitHubAPIRepoURL/releases | jq -r '.[0].assets[] | select(.name | contains("amd64.deb")) | .browser_download_url')
+freeTubeLatestGitHubReleaseURL=$(curl -s $gitHubAPIRepoURL/releases | jq -r ".[0].assets[] | select( .name | contains( \"$arch.deb\") ) | .browser_download_url")
 freeTubeLatestGitHubReleaseTag=$(curl -s $gitHubAPIRepoURL/releases | jq -r '.[0].tag_name' | sed 's/v//;s/-beta//')
 freeTubeInstalledVersion=$(dpkg-query --showformat='${Version}' -W freetube)
 if [ "$freeTubeLatestGitHubReleaseTag" != "$freeTubeInstalledVersion" ];then
