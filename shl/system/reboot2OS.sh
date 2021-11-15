48a8eb19f4b2fa9e4f11839aa01519bace79cd85
sebma <sebma@users.noreply.github.com>
2021-11-15 18:35:21 +0100

    Updated reboot2OS.sh

--- shl/system/reboot2OS.sh
+++ shl/system/reboot2OS.sh
@@ -6,11 +6,11 @@ elif [ $# = 1 ];then
 	OSName="$1"
 fi
 
-entryNumber=$(awk -F: -vOS=$OSName '/^menuentry/{i+=1}/OS/{printf i-1;exit}' /boot/grub/grub.cfg)
-entryName=$(awk -F"'" '/OS/{printf $2;exit}' /boot/grub/grub.cfg)
+#entryNumber=$(awk -F: -v OS=$OSName '/^menuentry/{i+=1}$0 ~ OS{printf i-1;exit}' /boot/grub/grub.cfg)
+entryName=$(awk -F"'" -v OS=$OSName '$0 ~ OS{printf $2;exit}' /boot/grub/grub.cfg)
 
-if [ -n "$entryNumber" ];then
-	echo "=> Rebooting to Windows in 5 seconds ..."
+if [ -n "$entryName" ];then
+	echo "=> Rebooting to <$entryName> in 5 seconds ..."
 	sleep 5s
 	sudo grub-reboot "$entryName"
 	sync
