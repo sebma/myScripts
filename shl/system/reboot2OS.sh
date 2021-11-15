4b63a21400c7ac27ea1651d77aad01880e3a4c60
sebma <sebma@users.noreply.github.com>
2021-11-15 18:41:58 +0100

    Updated reboot2OS.sh

--- shl/system/reboot2OS.sh
+++ shl/system/reboot2OS.sh
@@ -6,6 +6,8 @@ elif [ $# = 1 ];then
 	OSName="$1"
 fi
 
+grep GRUB_DEFAULT=.*saved /etc/default/grub -q || sudo sed -i "s/GRUB_DEFAULT=.*/GRUB_DEFAULT=saved/" /etc/default/grub
+
 #entryNumber=$(awk -F: -v OS=$OSName '/^menuentry/{i+=1}$0 ~ OS{printf i-1;exit}' /boot/grub/grub.cfg)
 entryName=$(awk -F"'" -v OS=$OSName '$0 ~ OS{printf $2;exit}' /boot/grub/grub.cfg)
 
