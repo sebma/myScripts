b1daa51d5c2ef988d1c16828fbbe74ca8dd7a873
sebma <sebma@users.noreply.github.com>
2021-11-15 18:13:48 +0100

    Updated reboot2Windows.sh

--- shl/system/reboot2Windows.sh
+++ shl/system/reboot2Windows.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env sh
 
 entryNumber=$(awk -F: '/^menuentry/{i+=1}/Windows/{printf i-1;exit}' /boot/grub/grub.cfg)
+entryName=$(awk -F"'" '/Windows/{printf $2;exit}' /boot/grub/grub.cfg)
 
 if [ -n "$entryNumber" ];then
 	echo "=> Rebooting to Windows in 5 seconds ..."
 	sleep 5s
-	sudo grub-reboot $entryNumber
+	sudo grub-reboot "$entryName"
 	sync
 	sudo reboot
 	exit
