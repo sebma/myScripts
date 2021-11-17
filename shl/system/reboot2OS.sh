6b086ddc87e886e2938861693407b60189c2f191
sebma <sebma@users.noreply.github.com>
2021-11-17 19:44:35 +0100

    Updated reboot2OS.sh

--- shl/system/reboot2OS.sh
+++ shl/system/reboot2OS.sh
@@ -7,17 +7,18 @@ elif [ $# = 1 ];then
 fi
 
 if ! grep GRUB_DEFAULT=.*saved /etc/default/grub -q;then
-	sudo sed -i "s/GRUB_DEFAULT=.*/GRUB_DEFAULT=saved/" /etc/default/grub
-	sudo update-grub
+#	sudo sed -i "s/GRUB_DEFAULT=.*/GRUB_DEFAULT=saved/" /etc/default/grub
+#	sudo update-grub
+	:
 fi
 
 #entryNumber=$(awk -F: -v OS=$OSName '/^menuentry/{i+=1}$0 ~ OS{printf i-1;exit}' /boot/grub/grub.cfg)
 entryName=$(awk -F"'" -v OS=$OSName '$0 ~ OS{printf $2;exit}' /boot/grub/grub.cfg)
 
 if [ -n "$entryName" ];then
-	printf "=> At the being, the system is configured to boot to <%s> by default.\n" "$(grub-editenv - list | cut -d= -f2-)"
+	printf "=> At the being, the system is configured to boot to <%s>.\n" "$(grub-editenv - list | cut -d= -f2-)"
 	grub-editenv - list | grep "next_entry=$entryName" -q || sudo grub-reboot "$entryName"
-	printf "=> Will now boot to <%s> by default.\n" "$(grub-editenv - list | cut -d= -f2-)"
+	printf "=> Will now boot to <%s>.\n" "$(grub-editenv - list | cut -d= -f2-)"
 	echo "=> Rebooting to <$entryName> in 5 seconds ..."
 	sleep 5s
 	sync
