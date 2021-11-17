8b51290d0fd8cca45a5e4488f84624254a261150
sebma <sebma@users.noreply.github.com>
2021-11-17 19:52:22 +0100

    Updated reboot2OS.sh

--- shl/system/reboot2OS.sh
+++ shl/system/reboot2OS.sh
@@ -6,15 +6,9 @@ elif [ $# = 1 ];then
 	OSName="$1"
 fi
 
-if ! grep GRUB_DEFAULT=.*saved /etc/default/grub -q;then
-#	sudo sed -i "s/GRUB_DEFAULT=.*/GRUB_DEFAULT=saved/" /etc/default/grub
-#	sudo update-grub
-	:
-fi
+# ! grep GRUB_DEFAULT=.*saved /etc/default/grub -q && sudo sed -i "s/GRUB_DEFAULT=.*/GRUB_DEFAULT=saved/" /etc/default/grub && sudo update-grub
 
-#entryNumber=$(awk -F: -v OS=$OSName '/^menuentry/{i+=1}$0 ~ OS{printf i-1;exit}' /boot/grub/grub.cfg)
 entryName=$(awk -F"'" -v OS=$OSName '$0 ~ OS{printf $2;exit}' /boot/grub/grub.cfg)
-
 if [ -n "$entryName" ];then
 	printf "=> At the being, the system is configured to boot to <%s>.\n" "$(grub-editenv - list | cut -d= -f2-)"
 	grub-editenv - list | grep "next_entry=$entryName" -q || sudo grub-reboot "$entryName"
