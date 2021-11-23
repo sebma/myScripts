12e03c5891c0c2decd3467799580a74642586ad6
sebma <sebma@users.noreply.github.com>
2021-11-23 21:47:11 +0100

    Updated moveAudio2AnalogStereo.sh

--- shl/moveAudio2AnalogStereo.sh
+++ shl/moveAudio2AnalogStereo.sh
@@ -3,21 +3,24 @@
 function moveAudioToAnalogStereo {
 	! pidof pulseaudio >/dev/null && echo "=> Pulseaudio is down, restarting Pulseaudio ..." && pulseaudio --start --log-target=syslog
 
-	echo "=> BEFORE :"
 	! pactl list sinks short >/dev/null 2>&1 && test "$SSH_CONNECTION" && pax11publish -r
-	pactl list sink-inputs short
 
 	analogStereoOutputPattern='\.analog-stereo\>'
+
 	sink_output=$(pactl list sinks short | awk "/$analogStereoOutputPattern/"'{printf$1}')
-
-	echo "=> sink_output = $sink_output"
-	pactl list sink-inputs short | awk '/protocol-native.c/{print$1}' | while read sink_input
-	do
-		pactl move-sink-input $sink_input $sink_output
-	done
-
-	echo "=> AFTER :"
-	pactl list sink-inputs short
+	if ! pactl list sink-inputs short | egrep "^[0-9]+\s$sink_output\s" -q;then
+		echo "=> BEFORE :"
+		pactl list sink-inputs short
+	
+		echo "=> sink_output = $sink_output"
+		pactl list sink-inputs short | awk '/protocol-native.c/{print$1}' | while read sink_input
+		do
+			pactl move-sink-input $sink_input $sink_output
+		done
+	
+		echo "=> AFTER :"
+		pactl list sink-inputs short
+	fi
 }
 
 moveAudioToAnalogStereo "$@"
