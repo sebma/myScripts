852c475f236132d4053ce3d7d2f68cc5b79c3326
sebma <sebma@users.noreply.github.com>
2021-11-23 21:29:49 +0100

    Updated moveAudio2HDMI.sh

--- shl/moveAudio2HDMI.sh
+++ shl/moveAudio2HDMI.sh
@@ -3,23 +3,23 @@
 function moveAudioToHDMI {
 	! pidof pulseaudio >/dev/null && echo "=> Pulseaudio is down, restarting Pulseaudio ..." && pulseaudio --start --log-target=syslog
 
-	echo "=> BEFORE :"
 	! pactl list sinks short >/dev/null 2>&1 && test "$SSH_CONNECTION" && pax11publish -r
-	pactl list sink-inputs short
-
 	hdmiOutputPattern='\.hdmi-stereo\>'
 	sink_output=$(pactl list sinks short | awk "/$hdmiOutputPattern/"'{printf$1}')
 
 	if pactl list sink-inputs short | grep "^$sink_input\s$sink_output\s" -q;then
+		echo "=> BEFORE :"
+		pactl list sink-inputs short
+
 		echo "=> sink_output = $sink_output"
 		pactl list sink-inputs short | awk '/protocol-native.c/{print$1}' | while read sink_input
 		do
 			pactl move-sink-input $sink_input $sink_output
 		done
-	fi
 
-	echo "=> AFTER :"
-	pactl list sink-inputs short
+		echo "=> AFTER :"
+		pactl list sink-inputs short
+	fi
 }
 
 moveAudioToHDMI "$@"
