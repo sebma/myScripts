1f48b054ee0fe77b492f8a39bec6d228d1473e5a
sebma <sebma@users.noreply.github.com>
2021-11-23 22:01:24 +0100

    Updated moveAudio2AnalogStereo.sh

--- shl/moveAudio2AnalogStereo.sh
+++ shl/moveAudio2AnalogStereo.sh
@@ -10,6 +10,7 @@ function moveAudioToAnalogStereo {
 	sink_output=$(pactl list sinks short | awk "/$analogStereoOutputPattern/"'{printf$1}')
 	if [ -n "$sink_output" ] && ! pactl list sink-inputs short | egrep "^[0-9]+\s$sink_output\s" -q;then
 		echo "=> ${0/*\/} ..."
+
 		echo "=> BEFORE :"
 		pactl list sink-inputs short
 	
@@ -21,6 +22,7 @@ function moveAudioToAnalogStereo {
 	
 		echo "=> AFTER :"
 		pactl list sink-inputs short
+
 		echo "=> DONE."
 	fi
 }
