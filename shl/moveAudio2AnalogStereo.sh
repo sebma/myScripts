e24ba4b152a4801c0de6ee9fb42eac774d532bc5
sebma <sebma@users.noreply.github.com>
2021-11-24 09:37:01 +0100

    Updated moveAudio2AnalogStereo.sh

--- shl/moveAudio2AnalogStereo.sh
+++ shl/moveAudio2AnalogStereo.sh
@@ -14,13 +14,13 @@ function moveAudioToAnalogStereo {
 
 		echo "=> BEFORE :"
 		pactl list sink-inputs short
-	
+
 		echo "=> sink_output = <$sink_output>"
 		pactl list sink-inputs short | awk '/protocol-native.c/{print$1}' | while read sink_input
 		do
 			pactl move-sink-input $sink_input $sink_output
 		done
-	
+
 		echo "=> AFTER :"
 		pactl list sink-inputs short
 
