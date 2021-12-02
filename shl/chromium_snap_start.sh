719074bb908567811c0d9d4d8b95b9c1de51966f
sebma <sebma@users.noreply.github.com>
2021-12-02 19:42:42 +0100

    Updated chromium_snap_start.sh

--- shl/chromium_snap_start.sh
+++ shl/chromium_snap_start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-export SNAP=/snap/chromium/$(snap list chromium | awk '/chromium/{printf$3}')
-$SNAP/usr/lib/chromium-browser/chrome &
+export SNAP=/snap/chromium/current
+bash $SNAP/bin/chromium.launcher &
