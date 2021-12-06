ab2f94352d8045fe1bf820a4e39b13b6328c9fb4
sebma <sebma@users.noreply.github.com>
2021-12-06 22:45:34 +0100

    Updated mp42Mono.sh

---
+++ shl/mp42Mono.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo
+ffprobe="command ffprobe -hide_banner -probesize 400M -analyzeduration 400M"
+movflags="+frag_keyframe"
+for file
+do
+	extension=${file/*./}
+#	audioStreamSpecifier=$($ffprobe "$file" -show_entries stream=codec_type -of flat -v 0 | awk -F "." '/="audio"/{print "0."$3".0"}')
+#	time ffmpeg -hide_banner -i "$file" -map 0 -c copy -map_channel $audioStreamSpecifier -acodec aac -aq 1.5 -movflags $movflags "${file/.$extension/_MONO.$extension}" -y
+	time ffmpeg -hide_banner -i "$file" -map 0 -c copy -ac 1 -acodec aac -aq 1.5 -movflags $movflags "${file/.$extension/_MONO.$extension}" -y
+	sync
+	echo "=> Output File = <${file/.$extension/_MONO.$extension}>"
+	echo
+done
