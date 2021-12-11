64d3ad17faa316d0624e51622cfb4eb2c9b1c7d5
sebma <sebma@users.noreply.github.com>
2021-12-12 00:19:56 +0100

    Updated kpcliRequirementsInstall.sh

--- shl/kpcliRequirementsInstall.sh
+++ shl/kpcliRequirementsInstall.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cpan=$(which cpan)
+cpan="command cpan"
 printf "o conf prerequisites_policy follow\no conf build_requires_install_policy yes\no conf commit" | $cpan
 $cpan Sort::Naturally YAML Term::ShellUI File::KeePass Log::Log4perl
 $cpan -f Term::ReadLine::Gnu Clone Crypt::Rijndael Term::ReadKey
