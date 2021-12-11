51e13aa34013b8b7f2f9c6b2cddd6b9202e5ee59
sebma <sebma@users.noreply.github.com>
2021-12-11 13:23:33 +0100

    Updated download_CGNTV_Videos_From_peter-adam.sh

--- shl/download_CGNTV_Videos_From_peter-adam.sh
+++ shl/download_CGNTV_Videos_From_peter-adam.sh
@@ -9,11 +9,14 @@ peterAdamJPV_BaseURL=http://www.peter-adam.com/jpv
 for url
 do
 	if echo "$url" | grep -q $peterAdamJPV_BaseURL/viewitem.php;then
-		videoTitle=$(\curl -qLs "$url" | pup 'meta[property=og:title] attr{content}') # Gives here different title than "pup 'head title'"
+		videoTitlePrefix="$(\curl -qLs "$url" | pup 'meta[property=og:title] attr{content}' | awk '{print$1" "$2" - "$3}')"
+		videoTitle="$(\curl -qLs "$url" | pup 'span span[style="font-weight:bold; "] text{}' | sed "s/&#39;/'/g")"
+		videoTitle="$videoTitlePrefix - $videoTitle"
 		videoID=$(\curl -qLs "$url" | pup 'meta[property=og:description] attr{content}' | awk -F'[[ ]' '{print$2}')
-		videoFileName=$(echo $videoTitle | sed 's/ /_/g').mp4
+		videoFileName="$(echo $videoTitle | sed 's/ /_/g')__peter-adam_com.mp4"
+echo "=> videoFileName = $videoFileName"
 		set -x
-		youtube-dl --ignore-config -co "$videoFileName" $peterAdamJPV_BaseURL/pop/popJW.php?nc=$videoID && chmod -w "$videoFileName"
+#		youtube-dl --ignore-config -co "$videoFileName" $peterAdamJPV_BaseURL/pop/popJW.php?nc=$videoID && chmod -w "$videoFileName"
 		set +x
 	else
 		echo "=> Only <$peterAdamJPV_BaseURL/viewitem.php> url types are supported." >&2
