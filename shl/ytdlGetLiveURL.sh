aed27e41523e135716667cff6cbb43ca102098f0
sebma <sebma@users.noreply.github.com>
2021-12-12 00:19:56 +0100

    Updated ytdlGetLiveURL.sh

--- shl/ytdlGetLiveURL.sh
+++ shl/ytdlGetLiveURL.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 function ytdlGetLiveURL {
-	local youtube_dl=$(which youtube-dl)
+	local youtube_dl="command youtube-dl"
 	local url="$1"
 	set -o pipefail
 	url="$($youtube_dl -j --playlist-items 1 "$url" | jq -r .webpage_url || echo "$url")"
