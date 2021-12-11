11354f2dd8d96e8d17ea297426af490dd2bcc0e6
sebma <sebma@users.noreply.github.com>
2021-12-11 13:35:26 +0100

    Updated checkTatoue.sh

--- shl/checkTatoue.sh
+++ shl/checkTatoue.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 sudo dd if=/dev/sda count=63 >/tmp/mbr_partiel 2>/dev/null
 hexdump -C /tmp/mbr_partiel | egrep -q "OEM|WIN[A-Z0-0]+" && echo Ce PC est tatoue :-\( ! || echo Ce PC ne semble pas tatoue :-\)
 rm /tmp/mbr_partiel
