3a6988276d12ef6bb611362365e26d9f7d739b66
sebma <sebma@users.noreply.github.com>
2021-12-12 00:19:56 +0100

    Updated wifiList.sh

--- shl/wifiList.sh
+++ shl/wifiList.sh
@@ -2,7 +2,7 @@
 
 wifiList () {
 	local wifiInterface=$(iw dev 2>/dev/null | awk '/Interface/{lastInterface=$NF}END{print lastInterface}')
-	type sudo >/dev/null 2>&1 && [ $(id -u) != 0 ] && groups | egrep -wq "sudo|adm|admin|root|wheel" && sudo=$(which sudo) || sudo=""
+	type sudo >/dev/null 2>&1 && [ $(id -u) != 0 ] && groups | egrep -wq "sudo|adm|admin|root|wheel" && sudo="command sudo" || sudo=""
 	test "$wifiInterface" || wifiInterface="$(iwconfig 2>/dev/null | awk '/^[^ \t]/ { if ($1 ~ /^[0-9]+:/) { lastInterface=$2 } else { lastInterface=$1 } }END{print lastInterface}')"
 	if [ -n "$wifiInterface" ]; then
 		if type -P nmcli > /dev/null; then
