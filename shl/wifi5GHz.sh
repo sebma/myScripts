036c7755eaac2904528318d97d5e41d0ce564721
sebma <sebma@users.noreply.github.com>
2021-12-11 13:39:25 +0100

    Updated wifi5GHz.sh

--- shl/wifi5GHz.sh
+++ shl/wifi5GHz.sh
@@ -3,9 +3,9 @@
 wifi5GHz() {
 	wifiInterface=$(iw dev 2>/dev/null | awk '/Interface/{lastInterface=$NF}END{print lastInterface}')
 	test "$wifiInterface" || wifiInterface="$(iwconfig 2>/dev/null | awk '/^[^ \t]/ { if ($1 ~ /^[0-9]+:/) { lastInterface=$2 } else { lastInterface=$1 } }END{print lastInterface}')"
-	if which iw >/dev/null 2>&1 ;then
+	if type -P iw >/dev/null 2>&1 ;then
 		iw phy | egrep -q '5[0-9]{3} MHz' && echo true || echo false
-	elif which iwlist >/dev/null 2>&1;then
+	elif type -P iwlist >/dev/null 2>&1;then
 		iwlist $wifiInterface frequency 2>&1 | grep "no frequency information" || iwlist $wifiInterface frequency | grep -q '5\..* GHz' && echo true || echo false
 	fi
 }
