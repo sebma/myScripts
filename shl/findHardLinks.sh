bf41660e5b77ea79b7348cc6115f173112200dbc
sebma <sebma@users.noreply.github.com>
2021-12-12 00:19:55 +0100

    Updated findHardLinks.sh

--- shl/findHardLinks.sh
+++ shl/findHardLinks.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-find=$(which find)
+find="command find"
 if sudo true;then
 #	sudo $find . ! -type l -printf "%y\t%n\t%p\n" 2>/dev/null | awk '($1 == "f" && $2 > 1) || ($1 == "d" && $2 > 2) {print}'
 	sudo $find . -type f -printf "%y\t%n\t%p\n" 2>/dev/null | awk '($1 == "f" && $2 > 1) {print}'
