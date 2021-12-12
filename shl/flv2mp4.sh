775236b90ce2b2d30b5b23e4f1c0039f34f1dc59
sebma <sebma@users.noreply.github.com>
2021-12-12 17:29:37 +0100

    Updated flv2mp4.sh

--- shl/flv2mp4.sh
+++ shl/flv2mp4.sh
@@ -2,7 +2,7 @@
 
 #file="$1"
 #shift
-ffmpeg="$(which ffmpeg) -hide_banner -probesize 400M -analyzeduration 400M"
+ffmpeg="command ffmpeg -hide_banner -probesize 400M -analyzeduration 400M"
 for file
 do
 #	time avconv  -i "$file" -acodec copy -copyts -qscale:v 0 -movflags frag_keyframe $@ "${file/.flv/.mp4}"
