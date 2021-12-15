fafaaa13c4dab3279b72462ad2931194ee493e9a
sebma <sebma@users.noreply.github.com>
2021-12-15 12:35:25 +0100

    Updated wayDroidInstall.sh

---
+++ shl/wayDroidInstall.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+export DISTRO=$(lsb_release -sc)
+curl -# -Sf https://repo.waydro.id/waydroid.gpg | sudo apt-key add -
+echo "deb https://repo.waydro.id/ $DISTRO main" | sudo tee /etc/apt/sources.list.d/waydroid.list
+sudo apt update
+sudo apt install ca-certificates waydroid -V
+sudo waydroid init
+echo "=> Check the /var/lib/waydroid filesystem ."
