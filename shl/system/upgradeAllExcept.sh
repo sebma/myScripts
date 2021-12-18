4fae0e00f047b4092acb5ca05fbc2a708a876a01
sebma <sebma@users.noreply.github.com>
2021-12-18 13:14:31 +0100

    Updated system/upgradeAllExcept.sh

--- shl/system/upgradeAllExcept.sh
+++ shl/system/upgradeAllExcept.sh
@@ -2,20 +2,21 @@
 
 function upgradeAllBut {
 	local packagesNotUpgraded="$@"
-	local apt="command apt"
+	local apt=$(type -P apt)
+	local apt_get=$(type -P apt-get)
 	local retCode=-1
 	if [ -z "$packagesNotUpgraded" ]
 	then
-		packagesToBeUpgraded=$($(which apt-get) dist-upgrade --dry-run | awk '/^Inst/{print$2}' | grep -v "Listing..." | xargs)
+		packagesToBeUpgraded=$($apt_get dist-upgrade --dry-run | awk '/^Inst/{print$2}' | grep -v "Listing..." | xargs)
 	else
 		packagesNotUpgraded=$(echo $packagesNotUpgraded | tr ' ' '|')
-		packagesToBeUpgraded=$($(which apt-get) dist-upgrade --dry-run | awk '/^Inst/{print$2}' | egrep -v "$packagesNotUpgraded" | grep -v "Listing..." | xargs)
+		packagesToBeUpgraded=$($apt_get dist-upgrade --dry-run | awk '/^Inst/{print$2}' | egrep -v "$packagesNotUpgraded" | grep -v "Listing..." | xargs)
 	fi
 
 	echo "=> packagesToBeUpgraded = <$packagesToBeUpgraded>"
 	echo
 	if test -n "$packagesToBeUpgraded";then
-		if [ -n "$SSH_CONNECTION" ] && which screen >/dev/null 2>&1;then
+		if [ -n "$SSH_CONNECTION" ] && type -P screen >/dev/null 2>&1;then
 			sudo screen -L $apt install -V $packagesToBeUpgraded
 		else
 			sudo $apt install -V $packagesToBeUpgraded
