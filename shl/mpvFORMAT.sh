651b378d08b9271e3f67d5f455df28176e45fbd7
sebma <sebma@users.noreply.github.com>
2021-12-27 20:54:43 +0100

    Updated mpvFORMAT.sh

--- shl/mpvFORMAT.sh
+++ shl/mpvFORMAT.sh
@@ -17,7 +17,7 @@ mpvFORMAT() {
 		return 1
 	fi
 
-	local mpv="command mpv"
+	local mpv="$(type -P mpv)"
 	echo $OSTYPE | grep -q android && local osFamily=Android || local osFamily=$(uname -s)
 	[ $osFamily = Linux ] && [ -c /dev/fb0 ] && tty | egrep -q "/dev/tty[0-9]+" && local mpvDefaultOptions="--vo=drm" && local mplayerDefaultOptions="--vo=fbdev2"
 	mpv="$mpv $mpvDefaultOptions"
