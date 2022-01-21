d0eaf7f4f8d699643ce22c64f9f5262551b9ae97
sebma <sebma@users.noreply.github.com>
2022-01-21 20:02:09 +0100

    Updated gitUpdateAllMyLocalRepos.sh

--- shl/gitUpdateAllMyLocalRepos.sh
+++ shl/gitUpdateAllMyLocalRepos.sh
@@ -4,6 +4,12 @@ gitUpdateAllMyLocalRepos ()
 {
 	local find="command find"
 	local remoteRepoUrl=""
+
+	if ! type -P git >/dev/null;then
+		echo "=> $FUNCNAME: ERROR : You must first install git." >&2
+		return 1
+	fi
+
 	echo $OSTYPE | grep -q android && local osFamily=Android || local osFamily=$(uname -s) # The bash interpreter is needed for it sets the OSTYPE variable
 	[ $osFamily = Darwin ] && find=gfind
 	local dir=""
