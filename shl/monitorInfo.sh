7aa912feeab8a3eb3452ade60b785e3c233af5f4
sebma <sebma@users.noreply.github.com>
2022-02-17 09:21:31 +0100

    Updated monitorInfo.sh

--- shl/monitorInfo.sh
+++ shl/monitorInfo.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
-echo "=> xrandr | grep connected ..."
-xrandr | grep connected
+if [ -z "$SSH_CONNECTION" ];then
+	echo "=> xrandr | grep connected ..."
+	xrandr | grep connected
 
-for EDID in $(ls /sys/class/drm/*/edid)
-do
-	text=$(tr -d '\0' <"$EDID")
-	[ -n "$text" ] && echo "=> edid-decode < $EDID | egrep 'Manufacturer:|Product|Alphanumeric' ..." && edid-decode < $EDID | egrep 'Manufacturer:|Product|Alphanumeric'
-	sleep 0.0001s
-done | more
+	for EDID in $(ls /sys/class/drm/*/edid)
+	do
+		text=$(tr -d '\0' <"$EDID")
+		[ -n "$text" ] && echo "=> edid-decode < $EDID | egrep 'Manufacturer:|Product|Alphanumeric' ..." && edid-decode < $EDID | egrep 'Manufacturer:|Product|Alphanumeric'
+		sleep 0.0001s
+	done | more
 
-#xrandr --verbose | edid-decode | egrep 'Manufacturer:|Product|Alphanumeric'
-echo "=> xrandr --props | edid-decode | egrep 'Manufacturer:|Product|Alphanumeric' ..."
-xrandr --props | edid-decode | egrep 'Manufacturer:|Product|Alphanumeric'
+	#xrandr --verbose | edid-decode | egrep 'Manufacturer:|Product|Alphanumeric'
+	echo "=> xrandr --props | edid-decode | egrep 'Manufacturer:|Product|Alphanumeric' ..."
+	xrandr --props | edid-decode | egrep 'Manufacturer:|Product|Alphanumeric'
+fi
