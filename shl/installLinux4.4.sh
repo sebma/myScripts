bf7c0dab27f52203fc1bf677ae59d0124686fa3b
sebma <sebma@users.noreply.github.com>
2022-02-20 11:18:26 +0100

    Updated installLinux4.4.sh

--- shl/installLinux4.4.sh
+++ shl/installLinux4.4.sh
@@ -3,5 +3,5 @@
 distribName=$(lsb_release -si)
 currentKernelVersion=$(uname -r | cut -d. -f1,2 | tr -d .)
 if [ $distribName = Ubuntu ] && [ $currentKernelVersion -lt 44 ];then
-	sudo apt install -V linux-generic-lts-xenial linux-headers-generic-lts-xenial linux-image-generic-lts-xenial linux-image-extra-virtual-lts-xenial
+	sudo apt install -V linux-generic-lts-xenial linux-headers-generic-lts-xenial linux-image-extra-virtual-lts-xenial linux-image-generic-lts-xenial
 fi
