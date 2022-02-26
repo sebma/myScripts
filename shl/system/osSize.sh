ef6cc6adfd9efac2cc99edf2ad6fdc324e52f829
sebma <sebma@users.noreply.github.com>
2022-02-26 16:06:41 +0100

    Updated osSize.sh

--- shl/system/osSize.sh
+++ shl/system/osSize.sh
@@ -5,9 +5,9 @@ LC_NUMERIC=C
 df="command df"
 echo $OSTYPE | grep -q android && export osFamily=Android || export osFamily=$(uname -s)
 if [ $osFamily = Linux ];then
-	time $df -T | awk 'BEGIN{printf "sudo du -cxsk "} !/tmpfs/ && /\/$|boot$|opt|tmp$|usr|var/{printf $NF" "}' | sh -x | awk '/[\t ]+total$/{print$1/1024^2" GiB"}'
-	echo
 	time $df -T | awk 'BEGIN{printf "df -T "} !/tmpfs/ && /\/$|boot$|opt|tmp$|usr|var/{printf $NF" "}' | sh -x | awk '{total+=$4}END{print total/1024^2" GiB"}'
+	echo
+	time $df -T | awk 'BEGIN{printf "sudo du -cxsk "} !/tmpfs/ && /\/$|boot$|opt|tmp$|usr|var/{printf $NF" "}' | sh -x | awk '/[\t ]+total$/{print$1/1024^2" GiB"}'
 else
 	echo "=> [${0/*\//}] ERROR : $osFamily is not supported yet." >&2
 fi
