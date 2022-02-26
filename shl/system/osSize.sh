fff6267aeb76dd8a7e4822f96a5147ef82806fa9
sebma <sebma@users.noreply.github.com>
2022-02-26 15:57:05 +0100

    Updated osSize.sh

--- shl/system/osSize.sh
+++ shl/system/osSize.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 #La partition swap n'est pas prise en compte pour le moment
-df="LC_NUMERIC=C command df"
+LC_NUMERIC=C
+df="command df"
 echo $OSTYPE | grep -q android && export osFamily=Android || export osFamily=$(uname -s)
 if [ $osFamily = Linux ];then
-	time $df -T | awk 'BEGIN{printf "sudo du -cxsk "} !/tmpfs/ && /\/$|boot$|opt|tmp$|usr|var/{printf $NF" "}' | sh -x | awk '/\<total\>/{print$1/1024^2" GiB"}'
+#	time $df -T | awk 'BEGIN{printf "sudo du -cxsk "} !/tmpfs/ && /\/$|boot$|opt|tmp$|usr|var/{printf $NF" "}' | sh -x | awk '/\<total\>/{print$1/1024^2" GiB"}'
 	echo
 	time $df -T | awk 'BEGIN{printf "df -T "} !/tmpfs/ && /\/$|boot$|opt|tmp$|usr|var/{printf $NF" "}' | sh -x | awk '{total+=$4}END{print total/1024^2" GiB"}'
 else
