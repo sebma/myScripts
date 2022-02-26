a087298cd94a8c5f8d257a18d6176c0553bffbd6
sebma <sebma@users.noreply.github.com>
2022-02-26 15:44:19 +0100

    Updated osSize.sh

--- shl/system/osSize.sh
+++ shl/system/osSize.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
+#La partition swap n'est pas prise en compte pour le moment
 df="command df"
-time $df -T | sort | egrep -vw "/mnt|/media|tmpfs|efi|vfat|fuse|fuseblk|squashfs|devtmpfs|^Filesystem.* on" | awk 'BEGIN{printf "sudo du -cxsk / "}/boot|opt|tmp|usr|var/{printf $NF" "}' | sh -x | awk '/\<total\>/{print$1/1024^2" GiB"}'
+echo $OSTYPE | grep -q android && export osFamily=Android || export osFamily=$(uname -s)
+if [ $osFamily = Linux ];then
+	time $df -T | awk 'BEGIN{printf "sudo du -cxsk "} !/tmpfs/ && /\/$|boot$|opt|tmp$|usr|var/{printf $NF" "}' | sh -x | awk '/\<total\>/{print$1/1024^2" GiB"}'
+	echo
+	time $df -T | awk 'BEGIN{printf "df -T "} !/tmpfs/ && /\/$|boot$|opt|tmp$|usr|var/{printf $NF" "}' | sh -x | awk '{total+=$4}END{print total/1024^2" GiB"}'
+else
+	echo "=> [${0/*\//}] ERROR : $osFamily is not supported yet." >&2
+fi
