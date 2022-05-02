8431d7b9657ee297e5e39aefc79946183cdfa429
sebma <sebma@users.noreply.github.com>
2022-05-02 21:06:40 +0200

    Updated system/pulseaudioRestart.sh

---
+++ shl/system/pulseaudioRestart.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+initPath=$(ps -p 1 -o cmd= | cut -d" " -f1)
+set -o pipefail
+systemType=$(strings $initPath | egrep -o "upstart|sysvinit|systemd" | head -1 || echo unknown)
+set +o pipefail
+[ $(id -u) != 0 ] && sudo=sudo || sudo=""
+
+echo "=> Restarting the pulseaudio server ..."
+pulseaudio --kill;sleep 1;pidof pulseaudio >/dev/null || pulseaudio --start --log-target=syslog # Restart pulseaudio
+sleep 1s
+ps -fC pulseaudio
