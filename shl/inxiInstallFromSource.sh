f98a3174a81a480b49966827bb4bb8dc5f48391f
sebma <sebma@users.noreply.github.com>
2022-05-03 15:19:20 +0200

    Updated inxiInstallFromSource.sh

--- shl/inxiInstallFromSource.sh
+++ shl/inxiInstallFromSource.sh
@@ -61,7 +61,7 @@ inxiInstallFromSource () {
 
 	echo
 	sync
-	type -P inxi && inxi -V | head && rm /tmp/myTmp/inxi /tmp/myTmp/inxi.1
+	type -P inxi >/dev/null 2>&1 && inxi -V | head && rm /tmp/myTmp/inxi /tmp/myTmp/inxi.1
 	return $retCode
 }
 
