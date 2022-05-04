33c4c2cc630d53f0c93a76571383716f1d7058fa
sebma <sebma@users.noreply.github.com>
2022-05-04 17:17:04 +0200

    Updated isbn2Barcode_with_barcode.sh

--- shl/isbn2Barcode_with_barcode.sh
+++ shl/isbn2Barcode_with_barcode.sh
@@ -2,10 +2,16 @@
 
 ISBN2Barcode_with_barcode ()
 {
-	local string=""
+	local string="" stringLength=0
 	if [ $# = 1 ]; then
 		string="$1"
-		barcode -e ean-13 -S -b "$string" | mogrify -format png -resize 200% - | feh -
+		stringLength="${#string}"
+		echo "=> stringLength = $stringLength"
+		if [ $stringLength = 10 ];then
+			barcode -e isbn -S -b "$string" | mogrify -format png -resize 200% - | feh -
+		elif [ $stringLength = 13 ];then
+			barcode -e ean-13 -S -b "$string" | mogrify -format png -resize 200% - | feh -
+		fi
 	fi
 }
 
