5c1f7a260be42e69c50beeb13e3f29ea966f0f3a
sebma <sebma@users.noreply.github.com>
2022-05-04 18:40:07 +0200

    Updated string2qrcode_with_zint.sh

--- shl/string2qrcode_with_zint.sh
+++ shl/string2qrcode_with_zint.sh
@@ -6,7 +6,7 @@ string2qrcode_with_zint ()
 	local dotSize=8 string=""
 	if [ $# = 1 ]; then
 		string="$1"
-		zint -b 58 --scale 4 --secure 4 --direct -d "$string" | feh -
+		zint -b QRCODE --scale 4 --secure 4 --direct -d "$string" | feh -
 	fi
 }
 
