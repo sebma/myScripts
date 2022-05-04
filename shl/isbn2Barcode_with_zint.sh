c4b3a9828165971b111ac0b50df736352a58110d
sebma <sebma@users.noreply.github.com>
2022-05-04 18:40:07 +0200

    Updated isbn2Barcode_with_zint.sh

--- shl/isbn2Barcode_with_zint.sh
+++ shl/isbn2Barcode_with_zint.sh
@@ -5,7 +5,7 @@ ISBN2Barcode_with_zint ()
 	local string=""
 	if [ $# = 1 ]; then
 		string="$1"
-		zint -b 69 --scale 2 --direct -d "$string" | feh -
+		zint -b ISBNX --scale 2 --direct -d "$string" | feh -
 	fi
 }
 
