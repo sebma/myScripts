eda856bb50a267dbaea63035acefbeeb40e58872
sebma <sebma@users.noreply.github.com>
2022-05-04 09:09:16 +0200

    Updated chromium_snap_start.sh

--- shl/chromium_snap_start.sh
+++ shl/chromium_snap_start.sh
@@ -1,4 +1,8 @@
-#!/usr/bin/env bash
-SNAP=$(snap run --shell chromium -c 'echo $SNAP')
-SNAP_USER_COMMON=$(snap run --shell chromium -c 'echo $SNAP_USER_COMMON')
-SNAP=$SNAP SNAP_USER_COMMON=$SNAP_USER_COMMON $SNAP/bin/chromium.launcher "$@" &
+#!/usr/bin/env sh
+
+chromium_snap_start() {
+	local SNAP=$(snap run --shell chromium -c 'echo $SNAP')
+	local SNAP_USER_COMMON=$(snap run --shell chromium -c 'echo $SNAP_USER_COMMON')
+	SNAP=$SNAP SNAP_USER_COMMON=$SNAP_USER_COMMON $SNAP/bin/chromium.launcher "$@" &
+}
+chromium_snap_start "$@"
