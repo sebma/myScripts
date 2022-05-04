a44045b41f59584341f24170e00fdda0576a32cd
sebma <sebma@users.noreply.github.com>
2022-05-04 18:18:54 +0200

    Updated string2qrcode_with_zint.sh

--- shl/string2qrcode_with_zint.sh
+++ shl/string2qrcode_with_zint.sh
@@ -2,10 +2,11 @@
 
 string2qrcode_with_zint ()
 {
+	# https://www.zint.org.uk/manual
 	local dotSize=8 string=""
 	if [ $# = 1 ]; then
 		string="$1"
-		zint -b 58 --scale 4 --secure 8 --direct -d "$string" | feh -
+		zint -b 58 --scale 4 --secure 4 --direct -d "$string" | feh -
 	fi
 }
 
