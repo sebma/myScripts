8fa878f189ac2f97df26fc62b6f07321da2cf37e
sebma <sebma@users.noreply.github.com>
2022-05-19 09:03:38 +0200

    Updated extract_urls_from_ok.ru_html_file.sh

--- shl/extract_urls_from_ok.ru_html_file.sh
+++ shl/extract_urls_from_ok.ru_html_file.sh
@@ -8,4 +8,5 @@ test $# != 1 && {
 
 ok_ru_htmlFile=$1
 urlBase="https://ok.ru"
-cat $ok_ru_htmlFile | pup 'json{}' | jq -r 'recurse | arrays[] | ( select(.class == "video-card_n ellip").title | gsub("\n";" ") ),( select(.class == "video-card_lk").href | sub("[?].*$";"") )' | sed 's/\xC2\xA0/ /g' | awk '{videoRelativeURL=$0;url=videoRelativeURL; getline title; print url" # "title}'
+nbsp="\xC2\xA0" # https://en.wikipedia.org/wiki/Non-breaking_space
+cat $ok_ru_htmlFile | pup 'json{}' | jq -r 'recurse | arrays[] | ( select(.class == "video-card_n ellip").title | gsub("\n";" ") ),( select(.class == "video-card_lk").href | sub("[?].*$";"") )' | sed "s/$nbsp/ /g" | awk '{videoRelativeURL=$0;url=videoRelativeURL; getline title; print url" # "title}'
