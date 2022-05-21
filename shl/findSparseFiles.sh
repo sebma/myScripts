df670b29afb38641ba2ed5b8ddcb3c55ef89ee9e
sebma <sebma@users.noreply.github.com>
2022-05-21 12:57:00 +0200

    Updated findSparseFiles.sh

--- shl/findSparseFiles.sh
+++ shl/findSparseFiles.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 find="$(type -P find)"
-if sudo true;then
-	sudo $find . -type f -printf "%S\t%p\n" 2>/dev/null | awk '$1 > 0 && $1 < 1.0'
-fi
+pwd -P | grep $HOME -q && sudo="" || sudo=sudo
+$sudo $find . -type f -printf "%S\t%p\n" 2>/dev/null | awk '$1 > 0 && $1 < 1.0'
