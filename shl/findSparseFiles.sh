76501ac5330a5c4c771a6f7e486490354072f78d
sebma <sebma@users.noreply.github.com>
2022-05-21 12:52:58 +0200

    Updated findSparseFiles.sh

--- shl/findSparseFiles.sh
+++ shl/findSparseFiles.sh
@@ -2,5 +2,5 @@
 
 find="$(type -P find)"
 if sudo true;then
-	sudo $find . -type f -printf "%S\t%p\n" 2>/dev/null | awk '$1 > 0 && $1 < 1.0 {print}'
+	sudo $find . -type f -printf "%S\t%p\n" 2>/dev/null | awk '$1 > 0 && $1 < 1.0'
 fi
