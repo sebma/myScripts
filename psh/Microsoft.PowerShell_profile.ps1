6e04bf668a14df0c809e898c6ef4e9dfcdb1b08e
sebma <sebma@users.noreply.github.com>
2022-08-07 18:27:24 +0200

    Updated Microsoft.PowerShell_profile.ps1

--- psh/Microsoft.PowerShell_profile.ps1
+++ psh/Microsoft.PowerShell_profile.ps1
@@ -263,16 +263,6 @@ if( $IsWindows ) {
 	function types { $argc=$args.Count;for($i=0;$i -lt $argc;$i++) { gcm $($args[$i]) } }
 	function which { (types @args).definition }
 	function winVersionCaption { (gwmi -class Win32_OperatingSystem).Caption }
-	if ( $osVersion -eq 10 ) {
-		function install_RSAT_AD_Tools {
-			New-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Servicing -Name RepairContentServerSource -Type dword -Value 2
-			$getWindowsCapabilityCommand = "& " + (Get-Command -Noun WindowsCapability | sls get).ToString() + " -Name RSAT.ActiveDirectory* -Online"
-			iex $getWindowsCapabilityCommand | Select-Object -Property name , displayname , state
-			iex $getWindowsCapabilityCommand | sudo Add-WindowsCapability -Online
-			iex $getWindowsCapabilityCommand | Select-Object -Property name , displayname , state
-			sudo Set-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Servicing -Name RepairContentServerSource -Type dword -Value 0
-		}
-	}
 } elseif( $IsLinux ) {
 	# TO BE DONE
 } elseif( $IsMacOS ) {
