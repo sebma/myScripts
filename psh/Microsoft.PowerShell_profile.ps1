9c4490b40beed90190764659603fa6081a2b1038
sebma <sebma@users.noreply.github.com>
2022-08-07 17:19:18 +0200

    Updated Microsoft.PowerShell_profile.ps1

--- psh/Microsoft.PowerShell_profile.ps1
+++ psh/Microsoft.PowerShell_profile.ps1
@@ -120,25 +120,6 @@ if( $IsWindows ) {
 		}
 	}
 
-	function InstallScoop {
-		if( $isAdmin ) {
-			if( ! (isInstalled("scoop.ps1")) ) {
-				if( (Get-ExecutionPolicy) -ne "Unrestricted" -and (Get-ExecutionPolicy) -ne "RemoteSigned" -and (Get-ExecutionPolicy) -ne "Bypass" ) { Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force }
-				iex "& {$(irm get.scoop.sh)} -RunAsAdmin -ScoopDir $env:ProgramData\scoop"
-			}
-			if( ! (isInstalled("git.exe")) ) {
-				scoop install -g git
-			} else {
-				if( ( git config --global credential.helper ) -ne "manager-core" ) {
-					git config --global credential.helper manager-core
-				}
-			}
-
-			if( ! (scoop bucket list | sls extras) ) { scoop bucket add extras }
-			scoop bucket list
-		}
-	}
-
 	function changeLanguage2English {
 		[Threading.Thread]::CurrentThread.CurrentUICulture = 'en-UK'
 		if( (Get-WinSystemLocale).Name -ne "en-UK" ) { Set-WinSystemLocale en-UK }
