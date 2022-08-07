b049053c5238490ab6a6466dff939c3b6ae0f887
sebma <sebma@users.noreply.github.com>
2022-08-07 17:16:40 +0200

    Updated Microsoft.PowerShell_profile.ps1

--- psh/Microsoft.PowerShell_profile.ps1
+++ psh/Microsoft.PowerShell_profile.ps1
@@ -122,21 +122,6 @@ if( $IsWindows ) {
 		}
 	}
 
-	function InstallChocolatey {
-		$tls12 = [Enum]::ToObject([System.Net.SecurityProtocolType], 3072)
-		[System.Net.ServicePointManager]::SecurityProtocol = $tls12
-
-		if( ! [System.Net.ServicePointManager]::SecurityProtocol.HasFlag([Net.SecurityProtocolType]::Tls12) ) {
-			[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
-		}
-
-		[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
-		if( $isAdmin ) {
-			if( (Get-ExecutionPolicy) -ne "Unrestricted" -and (Get-ExecutionPolicy) -ne "Bypass" ) { Set-ExecutionPolicy Bypass -Scope Process -Force }
-			iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-		}
-	}
-
 	function InstallScoop {
 		if( $isAdmin ) {
 			if( ! (isInstalled("scoop.ps1")) ) {
