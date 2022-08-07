b1e4856d01bbfab2aa7e90154f0226f88abe6ca9
sebma <sebma@users.noreply.github.com>
2022-08-07 18:20:14 +0200

    Updated installScoop.ps1

--- psh/installScoop.ps1
+++ psh/installScoop.ps1
@@ -3,6 +3,7 @@ function isInstalled($cmd) { return gcm "$cmd" 2>$null }
 if( $IsWindows ) {
 	function InstallScoop {
 		if( $isAdmin ) {
+			sudo cache on # i.e https://github.com/gerardog/gsudo#credentials-cache
 			if( ! (isInstalled("scoop.ps1")) ) {
 				if( (Get-ExecutionPolicy) -ne "Unrestricted" -and (Get-ExecutionPolicy) -ne "RemoteSigned" -and (Get-ExecutionPolicy) -ne "Bypass" ) {
 					sudo Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
