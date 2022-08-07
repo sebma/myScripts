1e54d75803f047744e8d33040e7aadbca843d6f9
sebma <sebma@users.noreply.github.com>
2022-08-07 18:30:24 +0200

    Updated install_RSAT_AD_Tools.ps1

--- psh/install_RSAT_AD_Tools.ps1
+++ psh/install_RSAT_AD_Tools.ps1
@@ -1,11 +1,13 @@
 $osVersion = $PSVersionTable.BuildVersion.Major
 if ( $osVersion -eq 10 ) {
 	function install_RSAT_AD_Tools {
-		New-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Servicing -Name RepairContentServerSource -Type dword -Value 2
+		sudo cache on
+		sudo New-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Servicing -Name RepairContentServerSource -Type dword -Value 2
 		$getWindowsCapabilityCommand = "& " + (Get-Command -Noun WindowsCapability | sls get).ToString() + " -Name RSAT.ActiveDirectory* -Online"
 		iex $getWindowsCapabilityCommand | Select-Object -Property name , displayname , state
 		iex $getWindowsCapabilityCommand | sudo Add-WindowsCapability -Online
 		iex $getWindowsCapabilityCommand | Select-Object -Property name , displayname , state
 		sudo Set-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Servicing -Name RepairContentServerSource -Type dword -Value 0
+		sudo cache off
 	}
 }
