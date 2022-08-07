2f5aa1d982a9436be61937411666bbc6d7db6841
sebma <sebma@users.noreply.github.com>
2022-08-07 17:17:19 +0200

    Updated installChocolatey.ps1

---
+++ psh/installChocolatey.ps1
@@ -0,0 +1,21 @@
+
+function isInstalled($cmd) { return gcm "$cmd" 2>$null }
+
+if( $IsWindows ) {
+	function InstallChocolatey {
+		if( ! (isInstalled("choco.exe")) ) {
+			$tls12 = [Enum]::ToObject([System.Net.SecurityProtocolType], 3072)
+			[System.Net.ServicePointManager]::SecurityProtocol = $tls12
+
+			if( ! [System.Net.ServicePointManager]::SecurityProtocol.HasFlag([Net.SecurityProtocolType]::Tls12) ) {
+				[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+			}
+
+			[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+			if( $isAdmin ) {
+				if( (Get-ExecutionPolicy) -ne "Unrestricted" -and (Get-ExecutionPolicy) -ne "Bypass" ) { Set-ExecutionPolicy Bypass -Scope Process -Force }
+				iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+			}
+		}
+	}
+}
