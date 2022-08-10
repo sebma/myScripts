c2b777997f8e9e66669ee61c7f0f5e52d1dd837d
sebma <sebma@users.noreply.github.com>
2022-08-10 22:36:48 +0200

    Updated scoopPostInstall.ps1

--- psh/scoopPostInstall.ps1
+++ psh/scoopPostInstall.ps1
@@ -6,7 +6,7 @@ if( $IsWindows ) {
 			sudo scoop install -g git
 		}
 		if( ( git config --global credential.helper ) -ne "manager-core" ) {
-			sudo git config --global credential.helper manager-core
+			sudo git config --global credential.helper manager-core # i.e https://github.com/ScoopInstaller/Main/blob/master/bucket/git.json
 		}
 
 		if( ! (sudo scoop bucket list | sls extras) ) { sudo scoop bucket add extras }
