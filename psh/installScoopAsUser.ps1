88551e1311ca109688981dd9840ad0a508572f33
sebma <sebma@users.noreply.github.com>
2022-08-10 21:25:38 +0200

    Updated installScoopAsUser.ps1

---
+++ psh/installScoopAsUser.ps1
@@ -0,0 +1,25 @@
+function isInstalled($cmd) { return gcm "$cmd" 2>$null }
+
+if( $IsWindows ) {
+	function InstallScoopAsUser {
+		sudo config CacheMode auto # i.e https://github.com/gerardog/gsudo#credentials-cache
+		if( ! (isInstalled("scoop.ps1")) ) {
+			if( (Get-ExecutionPolicy) -ne "Unrestricted" -and (Get-ExecutionPolicy) -ne "RemoteSigned" -and (Get-ExecutionPolicy) -ne "Bypass" ) {
+				Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
+			}
+			iwr -useb get.scoop.sh | iex
+		}
+		if( ! (isInstalled("git.exe")) ) {
+			sudo scoop install -g git gsudo
+		} else {
+			if( ( git config --global credential.helper ) -ne "manager-core" ) {
+				sudo git config --global credential.helper manager-core
+			}
+		}
+
+		if( ! (scoop bucket list | sls extras) ) { scoop bucket add extras }
+		scoop bucket list
+		sudo cache off
+	}
+	InstallScoopAsUser
+}
