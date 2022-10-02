09dc50dd3ea52b7c9f7d1cf15f44ef2b5170a4e0
sebma <sebma@users.noreply.github.com>
2022-10-02 21:10:01 +0200

    Updated record_NCC_Live.sh

--- shl/record_NCC_Live.sh
+++ shl/record_NCC_Live.sh
@@ -2,4 +2,4 @@
 
 url=https://www.youtube.com/user/NewCreationChurch/live
 #url="$(ytdlGetLiveURL.sh "$url")"
-cd /multimedia/Videos/CHRIST/Joseph_Prince/Live_sermons_NCC/ && getRestrictedFilenamesFORMAT.sh 94 "$url"
+cd /multimedia/Videos/CHRIST/Joseph_Prince/Live_sermons/Live_NCC/ && getRestrictedFilenamesFORMAT.sh 94 "$url"
