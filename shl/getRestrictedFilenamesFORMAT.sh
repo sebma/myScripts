32bd81b005165483c878eb26ec2f775fce614eab
sebma <sebma@users.noreply.github.com>
2023-01-26 22:14:06 +0100

    Updated getRestrictedFilenamesFORMAT.sh

--- shl/getRestrictedFilenamesFORMAT.sh
+++ shl/getRestrictedFilenamesFORMAT.sh
@@ -165,7 +165,7 @@ getRestrictedFilenamesFORMAT () {
 		do
 			let j++
 			let numberOfFilesToDownload=$numberOfURLsToDownload*${#formatsIDs[@]}
-#			$undebug
+			$undebug
 
 			videoFormatID=${formatID/+*/}
 
@@ -290,7 +290,7 @@ getRestrictedFilenamesFORMAT () {
 				ytdlExtraOptions+=( --hls-prefer-native )
 			fi
 
-#			$undebug
+			$undebug
 			[ "$debugLevel" = 1 ] && echo "=> ytdlExtraOptions = ${ytdlExtraOptions[@]}" && echo
 
 			if [ -f "$newFileName" ] && [ $isLIVE != true ]; then
@@ -318,7 +318,7 @@ getRestrictedFilenamesFORMAT () {
 			$debug
 			time videoDownloader -v --ignore-config -o "$fileName" -f "$chosenFormatID" "${ytdlExtraOptions[@]}" "$url" $embedThumbnail 2>$errorLogFile
 			downloadOK=$?
-#			$undebug
+			$undebug
 			sync
 			echo
 
@@ -339,7 +339,7 @@ getRestrictedFilenamesFORMAT () {
 
 				$grepColor -A1 'ERROR:.*' $errorLogFile >&2 && echo "=> \$? = $downloadOK" >&2 && echo >&2 && return $downloadOK || \rm $errorLogFile
 			fi
-#			$undebug
+			$undebug
 
 			if [ $downloadOK = 0 ]; then
 				ffprobeJSON_File_Info=$($ffprobe -v error -show_format -show_streams -print_format json "$fileName")
