4a2cb6e8e39598ba2a932a2dbfc351e92e93991e
sebma <sebma@users.noreply.github.com>
2023-01-30 17:03:24 +0100

    Update disable_IPv6.sh

--- shl/system/disable_IPv6.sh
+++ shl/system/disable_IPv6.sh
@@ -11,7 +11,7 @@ elif echo $distribID | egrep "debian|ubuntu" -q;then
         isDebianLike=true
 fi
 
-for iface in $(ip -o a | awk '/inet6/{gsub("\\.","/",$2);print$2}');do
+for iface in $(ip -o a | awk '/inet6/{gsub("\\.","/",$2);print$2}');do #cf. https://superuser.com/q/1765288/528454
         $sudo sysctl -w net.ipv6.conf.$iface.disable_ipv6=1
         grep $iface.disable_ipv6=1 /etc/sysctl.conf -q || echo net.ipv6.conf.$iface.disable_ipv6=1 | $sudo tee -a /etc/sysctl.conf
 done
