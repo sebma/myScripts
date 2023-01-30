071d3a5d3cc5dc08fe8064eb4c76b82ef7dda9f2
sebma <sebma@users.noreply.github.com>
2023-01-30 17:02:31 +0100

    Added "disable_IPv6.sh"

---
+++ shl/system/disable_IPv6.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+declare {isDebian,isRedHat}Like=false
+
+distribID=$(source /etc/os-release;echo $ID)
+if echo $distribID | egrep "centos|rhel|fedora" -q;then
+    isRedHatLike=true
+        sudo=""
+elif echo $distribID | egrep "debian|ubuntu" -q;then
+        sudo=sudo
+        isDebianLike=true
+fi
+
+for iface in $(ip -o a | awk '/inet6/{gsub("\\.","/",$2);print$2}');do
+        $sudo sysctl -w net.ipv6.conf.$iface.disable_ipv6=1
+        grep $iface.disable_ipv6=1 /etc/sysctl.conf -q || echo net.ipv6.conf.$iface.disable_ipv6=1 | $sudo tee -a /etc/sysctl.conf
+done
+
+ip -o a
