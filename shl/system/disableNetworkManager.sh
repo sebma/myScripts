dedd5be597af05fea31e48c51b21badbc99aea42
sebma <sebma@users.noreply.github.com>
2023-02-07 23:57:54 +0100

    Updated disableNetworkManager.sh

--- shl/system/disableNetworkManager.sh
+++ shl/system/disableNetworkManager.sh
@@ -11,36 +11,36 @@ elif echo $distribID | egrep "debian|ubuntu" -q;then
 	sudo=sudo
 fi
 
+test $(id -g) == 0 && sudo=""
+
 if which systemctl >/dev/null 2>&1;then
 	if systemctl is-enabled NetworkManager >/dev/null;then
-		if test -z "$sudo" && test $(id -g) == 0;then
-#			isVirtual=$(egrep virtual /sys/class/dmi/id/product_name -q && echo true || echo false)
-			ifList=$(nmcli dev | awk '!/wlan/&&/connected/{print$1}')
-			if $isRedHatLike;then
-				$sudo mkdir -v /etc/sysconfig/network-scripts/backups/
-				for iface in $ifList;do
-					grep -q "ONBOOT=no" /etc/sysconfig/network-scripts/ifcfg-$iface || $sudo sed -i".$(date +%Y%m%d_%HH%MM%S)" "s/ONBOOT=no/ONBOOT=yes/" /etc/sysconfig/network-scripts/ifcfg-$iface
-					sleep 1s
-					grep -q "^DNS[12]=" /etc/sysconfig/network-scripts/ifcfg-$iface || $sudo sed -i".$(date +%Y%m%d_%HH%MM%S)" "s/\(DNS[12]=.*\)/#\1/" /etc/sysconfig/network-scripts/ifcfg-$iface
-				done
-			fi
-
-			grep -q dns /etc/NetworkManager/NetworkManager.conf || $sudo sed -i".$(date +%Y%m%d_%HH%MM%S)" "s/\(plugins=.*ifcfg-rh.*\)/\1\ndns=none/" /etc/NetworkManager/NetworkManager.conf
-
-			if ! env | grep SSH_CONNECTION -q;then #Se lance ssi on le lance a partir de la console
-				systemctl is-active NetworkManager >/dev/null || $sudo systemctl start NetworkManager
-				for iface in $ifList;do
-					$sudo nmcli dev set $iface managed no
-					$sudo ifup $iface
-				done
-
-				$sudo systemctl stop NetworkManager.service
-			fi
-
-			$sudo systemctl disable NetworkManager.service
-			$sudo systemctl mask NetworkManager.service
-			systemctl -at service | grep NetworkManager.service
+		if $isRedHatLike;then
+			$sudo mkdir -v /etc/sysconfig/network-scripts/backups/
+			ifList=$(ip -o link | awk -F '[ :]' '/state.UP/{print$3}')
+			for iface in $ifList;do
+				grep -q "ONBOOT=no" /etc/sysconfig/network-scripts/ifcfg-$iface || $sudo sed -i".$(date +%Y%m%d_%HH%MM%S)" "s/ONBOOT=no/ONBOOT=yes/" /etc/sysconfig/network-scripts/ifcfg-$iface
+				sleep 1s
+				grep -q "^DNS[12]=" /etc/sysconfig/network-scripts/ifcfg-$iface || $sudo sed -i".$(date +%Y%m%d_%HH%MM%S)" "s/\(DNS[12]=.*\)/#\1/" /etc/sysconfig/network-scripts/ifcfg-$iface
+			done
 		fi
+
+		grep -q dns /etc/NetworkManager/NetworkManager.conf || $sudo sed -i".$(date +%Y%m%d_%HH%MM%S)" "s/\(plugins=.*ifcfg-rh.*\)/\1\ndns=none/" /etc/NetworkManager/NetworkManager.conf
+
+		if ! env | grep SSH_CONNECTION -q;then #Se lance ssi on le lance a partir de la console
+			systemctl is-active NetworkManager >/dev/null || $sudo systemctl start NetworkManager
+			ifList=$(nmcli dev | awk '!/wlan/&&/connected/{print$1}')
+			for iface in $ifList;do
+				$sudo nmcli dev set $iface managed no
+				$sudo ifup $iface
+			done
+
+			$sudo systemctl stop NetworkManager.service
+		fi
+
+		$sudo systemctl disable NetworkManager.service
+		$sudo systemctl mask NetworkManager.service
+		systemctl -at service | grep NetworkManager.service
 	else
 		echo "=> NetworkManager.service is already disabled."
 		$sudo systemctl mask NetworkManager.service
