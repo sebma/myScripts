94c68317b7946b475cde5f9c4f0d07d3b00cd328
sebma <sebma@users.noreply.github.com>
2023-02-07 20:21:58 +0100

    Updated disable_IPv6.sh

--- shl/system/disable_IPv6.sh
+++ shl/system/disable_IPv6.sh
@@ -4,16 +4,16 @@ declare {isDebian,isRedHat}Like=false
 
 distribID=$(source /etc/os-release;echo $ID)
 if echo $distribID | egrep "centos|rhel|fedora" -q;then
-    isRedHatLike=true
-        sudo=""
+	isRedHatLike=true
+	sudo=""
 elif echo $distribID | egrep "debian|ubuntu" -q;then
-        sudo=sudo
-        isDebianLike=true
+	sudo=sudo
+	isDebianLike=true
 fi
 
 for iface in $(ip -o a | awk '/inet6/{gsub("\\.","/",$2);print$2}');do #cf. https://superuser.com/q/1765288/528454
-        $sudo sysctl -w net.ipv6.conf.$iface.disable_ipv6=1
-        grep $iface.disable_ipv6=1 /etc/sysctl.conf -q || echo net.ipv6.conf.$iface.disable_ipv6=1 | $sudo tee -a /etc/sysctl.conf
+	$sudo sysctl -w net.ipv6.conf.$iface.disable_ipv6=1
+	grep $iface.disable_ipv6=1 /etc/sysctl.conf -q || echo net.ipv6.conf.$iface.disable_ipv6=1 | $sudo tee -a /etc/sysctl.conf
 done
 
 if $isDebianLike;then
