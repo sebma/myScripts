2da1d4f2b3dd22eb71878005959dd14bb5b81740
sebma <sebma@users.noreply.github.com>
2023-02-16 20:58:19 +0100

    Updated disableNetworkManager.sh

--- shl/system/disableNetworkManager.sh
+++ shl/system/disableNetworkManager.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 declare {isDebian,isRedHat}Like=false
-
 distribID=$(source /etc/os-release;echo $ID)
 if   echo $distribID | egrep "centos|rhel|fedora" -q;then
 	isRedHatLike=true
@@ -10,7 +9,6 @@ elif echo $distribID | egrep "debian|ubuntu" -q;then
 	isDebianLike=true
 	sudo=sudo
 fi
-
 test $(id -u) == 0 && sudo=""
 
 if which systemctl >/dev/null 2>&1;then
