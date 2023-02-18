8472dee80b9bbf97adacc93d6e3d3df21f6e681a
sebma <sebma@users.noreply.github.com>
2023-02-18 15:46:48 +0100

    Updated create_firstboot_service.sh

---
+++ shl/system/create_firstboot_service.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+test $(id -u) == 0 && sudo="" || sudo=$(which sudo)
+
+cat << EOF | $sudo tee /etc/systemd/system/firstboot.service
+[Unit]
+Description=One time boot script
+[Service]
+Type=simple
+ExecStart=/firstboot.sh
+[Install]
+WantedBy=multi-user.target
+EOF
+
+cat << EOF | $sudo tee /firstboot.sh
+#!/bin/bash 
+#SOME COMMANDS YOU WANT TO EXECUTE
+systemctl disable firstboot.service 
+rm -f /etc/systemd/system/firstboot.service
+rm -f /firstboot.sh
+EOF
+
+$sudo systemctl enable firstboot.service
+$sudo chmod +x /firstboot.sh
