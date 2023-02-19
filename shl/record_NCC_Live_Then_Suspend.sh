c668e2ac6271abc7f0ac84263ef152b95bf085e2
sebma <sebma@users.noreply.github.com>
2023-02-19 22:28:38 +0100

    Updated record_NCC_Live_Then_Suspend.sh

--- shl/record_NCC_Live_Then_Suspend.sh
+++ shl/record_NCC_Live_Then_Suspend.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+scriptDir=$(dirname $0)
+scriptDir=$(cd $scriptDir;pwd)
 url=https://www.youtube.com/user/NewCreationChurch/live
 #url="$(ytdlGetLiveURL.sh "$url")"
-cd /multimedia/Videos/CHRIST/Joseph_Prince/Live_sermons/Live_NCC/ && systemd-inhibit getRestrictedFilenamesFORMAT.sh 94 "$url"
+cd /multimedia/Videos/CHRIST/Joseph_Prince/Live_sermons/Live_NCC/ && systemd-inhibit $scriptDir/getRestrictedFilenamesFORMAT.sh 94 "$url"
 systemctl -i suspend
