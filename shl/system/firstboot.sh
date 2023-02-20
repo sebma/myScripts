d0bbcb3a9b0876e0009fae715b3fed2a13499e8f
sebma <sebma@users.noreply.github.com>
2023-02-20 17:44:37 +0100

    Update firstboot.sh

--- shl/system/firstboot.sh
+++ shl/system/firstboot.sh
@@ -5,6 +5,7 @@ iface=$(netplan get network.ethernets | awk -F: '/^[^ ]*:$/{print$1;exit}')
 echo "=> Removing the first interface ($iface) IP ..."
 #yq -i ".network.ethernets.$iface.addresses=[]" /etc/netplan/00-installer-config.yaml
 netplan set "network.ethernets.$iface.addresses=null"
+#netplan get "network.ethernets.$iface.addresses"
 netplan apply
 rm -f /etc/ssh/ssh_host_*
 dpkg-reconfigure openssh-server
