1cac6e08806c1b73eabb0d6152398fe9bffa9c1b
sebma <sebma@users.noreply.github.com>
2023-02-22 10:20:18 +0100

    Update firstboot.sh

--- shl/system/firstboot.sh
+++ shl/system/firstboot.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-mkdir -pv /etc/netplan/BACKUP/
-cp -pv /etc/netplan/00-installer-config.yaml /etc/netplan/BACKUP/00-installer-config-ORIG.yaml
-if netplan get network | grep ethernets -q;then
-	echo "=> Removing the interface ($iface) IP ..."
-	netplan get network.ethernets | awk -F: '/^[^ ]*:$/{print$1}' | while read iface;do
-		netplan set "network.ethernets.$iface.addresses=null"
-	done
-	netplan apply
+if egrep -i "vmware|virtal" /sys/class/dmi/id/sys_vendor -q;then
+	if netplan get network | grep ethernets -q;then
+		mkdir -pv /etc/netplan/BACKUP/
+		cp -pv /etc/netplan/00-installer-config.yaml /etc/netplan/BACKUP/00-installer-config-ORIG.yaml
+		netplan get network.ethernets | awk -F: '/^[^ ]*:$/{print$1}' | while read iface;do
+			echo "=> Removing the interface ($iface) IP ..."
+			netplan set "network.ethernets.$iface.addresses=null"
+		done
+		netplan apply
+	fi
 fi
 rm -f /etc/ssh/ssh_host_*
 ssh-keygen -A
