134fc4e7bf91b99207109321bc42a37c82c9d323
sebma <sebma@users.noreply.github.com>
2023-02-22 07:36:55 +0100

    Updated firstboot.sh

--- shl/system/firstboot.sh
+++ shl/system/firstboot.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 mkdir -pv /etc/netplan/BACKUP/
 cp -pv /etc/netplan/00-installer-config.yaml /etc/netplan/BACKUP/00-installer-config-ORIG.yaml
-iface=$(netplan get network.ethernets | awk -F: '/^[^ ]*:$/{print$1;exit}')
-echo "=> Removing the first interface ($iface) IP ..."
-#yq -i ".network.ethernets.$iface.addresses=[]" /etc/netplan/00-installer-config.yaml
-netplan set "network.ethernets.$iface.addresses=null"
-#netplan get "network.ethernets.$iface.addresses"
-netplan apply
+if netplan get network | grep ethernets -q;then
+	echo "=> Removing the interface ($iface) IP ..."
+	netplan get network.ethernets | awk -F: '/^[^ ]*:$/{print$1}' | while read iface;do
+		netplan set "network.ethernets.$iface.addresses=null"
+		#netplan get "network.ethernets.$iface.addresses"
+	done
+	netplan apply
+fi
 rm -f /etc/ssh/ssh_host_*
-dpkg-reconfigure openssh-server
+ssh-keygen -A
 systemctl disable firstboot.service
 #rm -f /etc/systemd/system/firstboot.service
 #rm -f /firstboot.sh
