4396113b007614e82b182ed6dd7fbf34f0be63c5
sebma <sebma@users.noreply.github.com>
2023-03-03 19:50:36 +0100

    Updated create_firstboot_service.sh

--- shl/system/create_firstboot_service.sh
+++ shl/system/create_firstboot_service.sh
@@ -26,16 +26,16 @@ if egrep -i "vmware|virtal" /sys/class/dmi/id/sys_vendor -q;then
 		done
 		netplan apply
 	fi
-	echo "=> Re-generating ssh host keys ..."
-	for type in dsa ecdsa ed25519 rsa;do
-		ssh-keygen -q -f /etc/ssh/ssh_host_${type}_key -N '' -t $type <<< y | grep Generating
-	done
-	echo "=> done."
 	echo "=> Re-generating /etc/machine-id and /var/lib/dbus/machine-id ..."
 	rm -f /etc/machine-id
 	dbus-uuidgen --ensure=/etc/machine-id
 	rm -f /var/lib/dbus/machine-id
 	dbus-uuidgen --ensure
+	echo "=> done."
+	echo "=> Re-generating ssh host keys ..."
+	for type in dsa ecdsa ed25519 rsa;do
+		ssh-keygen -q -f /etc/ssh/ssh_host_${type}_key -N '' -t $type <<< y | grep Generating
+	done
 	echo "=> done."
 fi
 systemctl disable firstboot.service
