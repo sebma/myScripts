35511f0ca805fe954d59bf3adf60a6434ff5c1ad
sebma <sebma@users.noreply.github.com>
2023-03-08 13:04:01 +0100

    Updated CA.sh

--- pl/CA.sh
+++ pl/CA.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
-CAToolPath=$(locate -r /CA.pl$ | grep -v /snap/ | grep ^/usr/)
-if [ ! -s $CAToolPath ];then
-	scriptDir=$(dirname $0)
-	scriptDir=$(cd $scriptDir;pwd)
-	CAToolPath=$scriptDir/not_mine/CA.pl
-fi
+function CA {
+	local CAToolPath=$(locate -r /CA.pl$ | grep -v /snap/ | grep ^/usr/)
+	if [ ! -s $CAToolPath ];then
+		scriptDir=$(dirname $0)
+		scriptDir=$(cd $scriptDir;pwd)
+		CAToolPath=$scriptDir/not_mine/CA.pl
+	fi
 
-exec $CAToolPath "$@"
+	exec $CAToolPath "$@"
+}
+
+CA "$@"
