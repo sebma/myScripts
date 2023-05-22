9c2bee97dbdd1b4e839f95ddb916e0982ca739ef
sebma <sebma@users.noreply.github.com>
2023-05-22 12:15:39 +0200

    Updated videoInfo.sh

--- shl/videoInfo.sh
+++ shl/videoInfo.sh
@@ -10,8 +10,7 @@ function videoInfo {
 		local youtube_dl='command youtube-dl'
 		local ffprobe='command ffprobe -hide_banner'
 		local userAgent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3513.1 Safari/537.36"
-		for urlOrFile
-		do
+		for urlOrFile;do
 			echo "=> urlOrFile = $urlOrFile"
 			if echo "$urlOrFile" | egrep -q "(https?|s?ftps?|ssh|rtmp|rtsp|mms)://"
 			then
@@ -19,19 +18,19 @@ function videoInfo {
 				streamMimeType=$(\curl -qLs --user-agent "$userAgent" "$urlOrFile" | file -bi -)
 				if echo $streamMimeType | \grep "^text/" -q;then
 					echo "=> This stream needs first to be resolved by youtube-dl ..."
-					possibleFormats=best[ext=mp4]/best[ext=webm]/best[ext=flv]/18/webm/sd/http-480
+					possibleFormats="best[ext=mp4]/best[ext=webm]/best[ext=flv]/18/webm/sd/http-480"
 					infos="$($youtube_dl -gef $possibleFormats -- "$urlOrFile")"
 					test -z "$infos" && continue
 					title="$(echo "$infos" | sed -n 1p )"
 					directURLOfStream="$(echo "$infos" | sed -n 2p)"
-					size=$(\curl -qLsI --user-agent "$userAgent" "$directURLOfStream" | awk 'BEGIN{IGNORECASE=1;size=0}/Content-Length:/{if($2>0)size=$2}END{printf "%8.3f MiB\n",size/2^20}')
+					size="$(\curl -qLsI --user-agent "$userAgent" "$directURLOfStream" | awk 'BEGIN{IGNORECASE=1;size=0}/Content-Length:/{if($2>0)size=$2}END{printf "%8.3f MiB\n",size/2^20}')"
 					echo "Title: $title"
 					echo "Size: $size"
 					time $ffprobe -user_agent "$userAgent" $ffprobeOptions "$directURLOfStream" 2>&1
 				elif echo $streamMimeType | \grep charset=binary -q;then
 					#direct stream
 					echo "=> This stream is direct stream"
-					size=$(\curl -qLsI --user-agent "$userAgent" "$urlOrFile" | awk 'BEGIN{IGNORECASE=1;size=0}/Content-Length:/{if($2>0)size=$2}END{printf "%8.3f MiB\n",size/2^20}')
+					size="$(\curl -qLsI --user-agent "$userAgent" "$urlOrFile" | awk 'BEGIN{IGNORECASE=1;size=0}/Content-Length:/{if($2>0)size=$2}END{printf "%8.3f MiB\n",size/2^20}')"
 					echo "Size: $size"
 					time $ffprobe -user_agent "$userAgent" "$urlOrFile"
 				fi
@@ -39,12 +38,12 @@ function videoInfo {
 				#Local file
 				[ ! -s "$urlOrFile" ] && echo "=> ERROR: The file <$urlOrFile> is empty or does not exist." 1>&2 && continue
 				echo "=> This file is local to this machine."
-				size=$(\ls -l "$urlOrFile" | awk '/[0-9]+/{printf "%8.3f MiB\n",$5/1024^2}')
+				size="$(\ls -l "$urlOrFile" | awk '/[0-9]+/{printf "%8.3f MiB\n",$5/1024^2}')"
 				echo "Size: $size"
 				$ffprobe "$urlOrFile" || $ffprobe $ffprobeOptions "$urlOrFile"
 			fi
 			echo
-		done 2>&1 | \egrep -vi "^ +(:\s+$|comment|description +: [^/]+$)" | uniq | egrep --color -iw "^$|description.*:/|PURL.*:/|stream|local|urlOrFile|kb/s|Input|Size:|Title:|Duration:|Channel.*:|Stream|Chapter|Invalid|error|bad| no such file|^\[.* not"
+		done 2>&1 | \egrep -vi "^ +(:\s+$|comment|description +: [^/]+$)" | uniq | egrep --color -iw "^$|description.*:/|PURL.*:/|stream|local|urlOrFile|kb/s|Input|Size:|Title\s*:|Duration:|Channel.*:|Stream|Chapter|Invalid|error|bad| no such file|^\[.* not"
 	}
 }
 
