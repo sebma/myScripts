19b4f3e6db235402c1d4d234aea9abeee7c68607
sebma <sebma@users.noreply.github.com>
2023-05-25 22:07:48 +0200

    Updated myUFWRules.sh

--- shl/system/myUFWRules.sh
+++ shl/system/myUFWRules.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 
-grep Kodi /etc/ufw/applications.d/* -q || cat <<EOF | sudo tee /etc/ufw/applications.d/kodi
+type sudo >/dev/null 2>&1 && [ $(id -u) != 0 ] && groups | egrep -wq "sudo|adm|admin|root|wheel" && sudo="command sudo" || sudo=""
+
+grep Kodi /etc/ufw/applications.d/* -q || cat <<EOF | $sudo tee /etc/ufw/applications.d/kodi
 [Kodi]
 title=Kodi Media Center
 description=Kodi, formerly known as XBMC Media Center, is a software media-player and entertainment hub for all your digital media.
@@ -10,9 +12,9 @@ EOF
 
 for lan in 192.168.0.0/24 192.168.1.0/24;do
 	for app in OpenSSH Kodi;do
-		sudo ufw allow from $lan to any app $app
+		$sudo ufw allow from $lan to any app $app
 	done
 done
 
-sudo ufw enable
-sudo ufw status
+$sudo ufw enable
+$sudo ufw status
