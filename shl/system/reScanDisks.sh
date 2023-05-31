4a73ec342b16eadd1d3289efa1192ea846a206f7
sebma <sebma@users.noreply.github.com>
2023-05-31 17:04:12 +0200

    Update reScanDisks.sh

--- shl/system/reScanDisks.sh
+++ shl/system/reScanDisks.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-echo "=> Re-scanning SCSI hosts for new disks devices, just in case ..."
-time echo "- - -" | sudo tee /sys/class/scsi_host/host*/scan /sys/class/block/sd?/device/rescan >/dev/null
+echo "=> Re-scanning existing disks for changes (such as size for virtual disks), just in case ..."
+time echo "- - -" | sudo tee /sys/class/block/sd?/device/rescan >/dev/null
