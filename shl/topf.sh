0471d4eca8fba5d09d2df1661df15c0fdb52e2e1
sebma <sebma@users.noreply.github.com>
2023-06-10 19:37:25 +0200

    Updated topf.sh

--- shl/topf.sh
+++ shl/topf.sh
@@ -6,7 +6,7 @@ topf () {
 	local findPrunePathsExpression="( -type d -a ( -name .git $(printf -- " -o -path %s" $findExcludedPathList) ) ) -prune -o"
 	[ $# = 0 ] && local lines=10 || local lines=$1
 
-    $find . -xdev $findPrunePathsExpression -type f -size +10M -exec ls -l --block-size=M --time-style=+"%Y-%m-%d %T" {} \; 2> /dev/null | sort -nrk5 | head -n $lines | numfmt --field 5 --from=iec --to=iec-i --suffix=B | column -t
+    $find . -xdev $findPrunePathsExpression -type f -size +10M -exec ls -l --block-size=M --time-style=+"%Y-%m-%d %T" {} \; 2> /dev/null | sort -nrk5 | head -n $lines | numfmt --field 5 --from=iec --to=iec-i --suffix=B
 }
 
 topf "$@"
