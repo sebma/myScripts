ed79ba510ee8cf0fc16cacf6a158a2bfe8d40ec4
sebma <sebma@users.noreply.github.com>
2023-08-25 18:24:32 +0200

    Update create_vdisk.sh

--- shl/system/create_vdisk.sh
+++ shl/system/create_vdisk.sh
@@ -1,10 +1,34 @@
 #!/usr/bin/env bash
 
+scriptName=$(basename $0)
+declare -i virtualDiskNumber=-1
+if [ $# = 0 ];then
+    lastest=true
+elif [ $# = 1 ];then
+    if [ $1 = -h ];then
+        echo "=> Usage : $scriptName virtualDiskNumber|[latest]|-l" >&2
+        exit 1
+    elif [ $1 = -l ];then
+        listReadyPhysicalDisks=true
+        id=$(omreport storage controller -fmt ssv | awk -F';' '/^[0-9];/{printf$1;exit}')
+        omreport storage pdisk controller=$id | egrep '^(ID|Status|Capacity|Sector Size|Bus|Power|Media|State|Vendor|Product|Serial|Part.Number|^$)' | awk -v myPATTERN=Ready -v RS= '$0 ~ myPATTERN'
+        exit $?
+    else
+        lastest=false
+        virtualDiskNumber=$1
+        if [ $virtualDiskNumber = 0 ];then
+            echo "=> ERROR [$scriptName] : virtualDiskNumber must be an integer." >&2
+            exit 2
+        fi
+    fi
+else
+    echo "=> Usage : $scriptName virtualDiskNumber|[latest]|-l" >&2
+    exit 1
+fi
+
 declare -i last=-1
 if which omreport >/dev/null;then
     id=$(omreport storage controller -fmt ssv | awk -F';' '/^[0-9];/{printf$1;exit}')
-    last=$(omreport storage vdisk controller=$id -fmt ssv | awk -F';' 'BEGIN{IGNORECASE=1;last=1}/virtual\s*disk\s*[0-9]+;/{last+=1}END{printf last}')
-    last+=1
     raid=$(omreport storage vdisk controller=$id -fmt ssv | awk -F';' 'BEGIN{IGNORECASE=1}/virtual\s*disk\s*[0-9]+;/{value=$7}END{printf tolower(gensub("RAID-","r",1,value))}')
     pdisk=$(omreport storage pdisk controller=$id -fmt ssv | awk -F';' 'BEGIN{IGNORECASE=1}/Ready;/{value=$1;print value;exit}')
     if [ -z "$pdisk" ];then
@@ -35,7 +59,13 @@ if which omreport >/dev/null;then
         *) echo "=> Unsupported write policy = <$writepolicy>." >&2;exit 3;;
     esac
 
-    name=VirtualDisk$last
+    if $lastest;then
+        last=$(omreport storage vdisk controller=$id -fmt ssv | awk -F';' 'BEGIN{IGNORECASE=1;last=1}/virtual\s*disk\s*[0-9]+;/{last+=1}END{printf last}')
+        last+=1
+        name=VirtualDisk$last
+    else
+        name=VirtualDisk$virtualDiskNumber
+    fi
     echo "=> Creating $name for Physical Disk $pdisk ..."
     echo "=> omconfig storage controller action=createvdisk controller=$id raid=$raid size=max pdisk=$pdisk stripesize=$stripesize diskcachepolicy=$diskcachepolicy readpolicy=$readpolicy writepolicy=$writepolicy name=$name ..."
     time omconfig storage controller action=createvdisk controller=$id raid=$raid size=max pdisk=$pdisk stripesize=$stripesize diskcachepolicy=$diskcachepolicy readpolicy=$readpolicy writepolicy=$writepolicy name=$name
@@ -44,4 +74,5 @@ if which omreport >/dev/null;then
     echo "=> The new created Virtual Disk is :"
     omreport storage vdisk controller=$id -fmt ssv | grep "$name"
 fi
+
 exit $retCode
