5c0d4d56d98aff5548291ffbac192692381f5997
sebma <sebma@users.noreply.github.com>
2023-09-01 18:00:08 +0200

    Create dir2iso.ps1

---
+++ psh/dir2iso.ps1
@@ -0,0 +1,14 @@
+$scriptName = Split-Path -Leaf $PSCommandPath
+
+function dis2iso {
+	$argc=$args.Count
+	if ( $argc -ne 2 ) {
+		echo "=> Usage $scriptName isoFileName directoryPath"
+		exit 1
+	}
+	$isoFileName = $args[0]
+	$directoryPath = $args[1]
+	mkisofs -o $isoFileName $directoryPath
+}
+
+dis2iso @args
