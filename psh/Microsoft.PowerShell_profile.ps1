f7de99741f20534e888ad5b25b6143e019969061
sebma <sebma@users.noreply.github.com>
2023-09-06 11:20:18 +0200

    Update Microsoft.PowerShell_profile.ps1

--- psh/Microsoft.PowerShell_profile.ps1
+++ psh/Microsoft.PowerShell_profile.ps1
@@ -282,11 +282,11 @@ function initVars {
 }
 
 function defaultRoute {
-	Get-NetRoute -DestinationPrefix 0.0.0.0/0 | select DestinationPrefix,NextHop,InterfaceAlias,InterfaceMetric
+	Get-NetRoute -DestinationPrefix 0.0.0.0/0 | select DestinationPrefix,NextHop,InterfaceAlias,InterfaceMetric,RouteMetric | Format-Table
 }
 
 function defaultRouteV6 {
-	Get-NetRoute -DestinationPrefix ::0/0 | select DestinationPrefix,NextHop,InterfaceAlias,InterfaceMetric
+	Get-NetRoute -DestinationPrefix ::0/0 | select DestinationPrefix,NextHop,InterfaceAlias,InterfaceMetric,RouteMetric | Format-Table
 }
 
 function interfaceInfo {
