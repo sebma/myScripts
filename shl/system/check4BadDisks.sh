169dabb56f0857c18d98a015685b269689078f45
sebma <sebma@users.noreply.github.com>
2023-09-07 18:57:13 +0200

    Update check4BadDisks.sh

--- shl/system/check4BadDisks.sh
+++ shl/system/check4BadDisks.sh
@@ -8,6 +8,6 @@ test $(id -u) == 0 && sudo="" || sudo=sudo
 for disk in $(lsdisks);do
         smartBadsectors=$($sudo smartctl -A $disk | awk '/Reallocated_Sector_Ct|Current_Pending_Sector|Offline_Uncorrectable/{bad+=$NF}END{printf bad}')
         if [ $smartBadsectors != 0 ];then
-                echo "=> WARNING : $disk has $smartBadsectors." | mail -s "WARNING : $disk has $smartBadsectors." g.makridis@ellipseanimation.com -c s.mansfeld@pluriad.fr
+                echo "=> WARNING : $disk has $smartBadsectors." | mail -s "WARNING : $disk has $smartBadsectors." sebmansfeld@yahoo.fr -c sebmansfeld@yahoo.fr
         fi
 done
