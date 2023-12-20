786f74d32387dfe74c848725eac5dc8602309599
sebma <sebma@users.noreply.github.com>
2023-12-20 15:29:11 +0100

    Create groups.ps1

---
+++ psh/groups.ps1
@@ -0,0 +1,13 @@
+function groups {
+	$argc=$args.Count
+	if ( $argc -eq 0) {
+		( (Get-ADUser -Identity $env:username -Properties MemberOf).memberof | Get-ADGroup ).name | sort
+	} else {
+		for($i=0;$i -lt $argc;$i++) {
+			echo "=> Memberships of $($args[$i]) :"
+			( ( Get-ADUser -Identity $($args[$i]) -Properties MemberOf ).memberof | Get-ADGroup ).name | sort
+		}
+	}
+}
+
+groups @args
