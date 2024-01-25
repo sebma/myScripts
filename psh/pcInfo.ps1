3a03934b93c5d6d39f8e6709eddbe0fa3426e012
sebma <sebma@users.noreply.github.com>
2024-01-25 17:47:11 +0100

    Update pcInfo.ps1

--- psh/pcInfo.ps1
+++ psh/pcInfo.ps1
@@ -1,1 +1,2 @@
 gin WindowsProductName,BiosFirmwareType,CsManufacturer,CsModel,CsChassisSKUNumber,CsProcessors,CsTotalPhysicalMemory,OsName,OsVersion,CsNetworkAdapters,CsName,CsUserName,OsPagingFiles,TimeZone
+echo "UserProfile`t      : $ENV:USERPROFILE"
