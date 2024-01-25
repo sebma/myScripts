1b66098eb59d3331c6dfff54d1f63e596fc1b6e8
sebma <sebma@users.noreply.github.com>
2024-01-25 15:49:38 +0100

    Update service_info.ps1

--- psh/service_info.ps1
+++ psh/service_info.ps1
@@ -1,7 +1,18 @@
 function service_info {
-	$serviceName = $args[0]
-	"=> serviceName = $serviceName :"
-	Get-CimInstance win32_service | ? Name -eq $serviceName | Select Name , InstallDate , State , ServiceType , StartMode , ErrorControl , PathName , TagId , Caption , StartName , DelayedAutoStart
+	$ErrorActionPreference = 'Stop'
+	$argc=$args.Count
+	if ( $argc -gt 0) {
+		for($i=0;$i -lt $argc;$i++) {
+			$serviceName = $($args[$i])
+			"=> serviceName = <$serviceName> :"
+			try {
+				Get-Service $serviceName 2>$null
+				Get-CimInstance win32_service | ? Name -eq $serviceName | Select Name , InstallDate , State , ServiceType , StartMode , ErrorControl , PathName , TagId , Caption , StartName , DelayedAutoStart
+			} catch {
+				echo "=> WARNING: The service <$serviceName> does not exist."
+			}
+		}
+	}
 }
 
 service_info @args
