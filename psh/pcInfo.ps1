8ad156b02b5580b86e79b219f7c26f056c7d9743
sebma <sebma@users.noreply.github.com>
2024-02-02 14:21:13 +0100

    Update pcInfo.ps1

--- psh/pcInfo.ps1
+++ psh/pcInfo.ps1
@@ -1,2 +1,3 @@
 gin WindowsProductName,BiosFirmwareType,CsManufacturer,CsModel,CsChassisSKUNumber,CsProcessors,CsTotalPhysicalMemory,OsName,OsVersion,CsNetworkAdapters,CsName,CsUserName,OsPagingFiles,TimeZone
 echo "UserProfile`t      : $ENV:USERPROFILE"
+Get-PSDrive -PSProvider FileSystem | Format-Table
