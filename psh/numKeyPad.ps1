26df442f21b69f59ad841309947f920a8fbc3195
sebma <sebma@users.noreply.github.com>
2024-02-20 19:49:21 +0100

    Updated numKeyPad.ps1

--- psh/numKeyPad.ps1
+++ psh/numKeyPad.ps1
@@ -1,5 +1,5 @@
 mount HKU Registry HKEY_USERS
 sp "HKU:\.DEFAULT\Control Panel\Keyboard" -n InitialKeyboardIndicators -v 2
 gpv "HKU:\.DEFAULT\Control Panel\Keyboard" -n InitialKeyboardIndicators
-gpv "HKCU:\Control Panel\Keyboard" -n InitialKeyboardIndicators
 Remove-PSDrive HKU
+gpv "HKCU:\Control Panel\Keyboard" -n InitialKeyboardIndicators
