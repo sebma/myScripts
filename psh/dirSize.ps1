837a943f1e270df8a2254f5b3f5a90b6950bd9ee
sebma <sebma@users.noreply.github.com>
2024-02-26 16:57:31 +0100

    Update dirSize.ps1

--- psh/dirSize.ps1
+++ psh/dirSize.ps1
@@ -1,7 +1,9 @@
 $dirName = $args[0]
 $dirSize = ( dir "$dirName" -force -recurse | measure -property length -sum ).Sum
-if ( $dirSize -gt 1tb ) { "{0:n2} TiB" -f ( $dirSize / 1tb ) }
-elseif ( $dirSize -gt 1gb ) { "{0:n2} GiB" -f ( $dirSize / 1gb ) }
-elseif ( $dirSize -gt 1mb ) { "{0:n2} MiB" -f ( $dirSize / 1mb ) }
-elseif ( $dirSize -gt 1kb ) { "{0:n2} KiB" -f ( $dirSize / 1kb ) }
-else { "{0:n2} B" -f $dirSize }
+switch( $dirSize ) {
+	{ $_ -ge 1tb -and $_ -le 1pb } { "{0:n2} TiB" -f ( $dirSize / 1tb ); break }
+	{ $_ -ge 1gb -and $_ -le 1tb } { "{0:n2} GiB" -f ( $dirSize / 1gb ); break }
+	{ $_ -ge 1mb -and $_ -le 1gb } { "{0:n2} MiB" -f ( $dirSize / 1mb ); break }
+	{ $_ -ge 1kb -and $_ -le 1mb } { "{0:n2} KiB" -f ( $dirSize / 1kb ); break }
+	{ $_ -ge 0   -and $_ -le 1kb } { "{0:n2} B" -f $dirSize; break }
+}
