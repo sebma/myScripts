94909ae9e4141e4cb1c99fd668267fac83b54b73
sebma <sebma@users.noreply.github.com>
2024-02-26 15:17:21 +0100

    Update dirSize.ps1

--- psh/dirSize.ps1
+++ psh/dirSize.ps1
@@ -4,4 +4,4 @@ if ( $dirSize -gt 1tb ) { "{0:n2} TiB" -f ( $dirSize / 1tb ) }
 elseif ( $dirSize -gt 1gb ) { "{0:n2} GiB" -f ( $dirSize / 1gb ) }
 elseif ( $dirSize -gt 1mb ) { "{0:n2} MiB" -f ( $dirSize / 1mb ) }
 elseif ( $dirSize -gt 1kb ) { "{0:n2} KiB" -f ( $dirSize / 1kb ) }
-else { "{0:n2} B" -f ( $dirSize / 1 ) }
+else { "{0:n2} B" -f $dirSize }
