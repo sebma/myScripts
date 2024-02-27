440de4b1fc64123b08dd5815b3c9e2e111751123
sebma <sebma@users.noreply.github.com>
2024-02-27 14:18:36 +0100

    Update Microsoft.PowerShell_profile.ps1

--- psh/Microsoft.PowerShell_profile.ps1
+++ psh/Microsoft.PowerShell_profile.ps1
@@ -216,12 +216,12 @@ if( $IsWindows ) {
 			}
 		}
 	}
-	function lsgroups {
+	function lsgroup {
 		$argc=$args.Count
 		if ( $argc -gt 0) {
 			for($i=0;$i -lt $argc;$i++) {
 				echo "=> Members of group $($args[$i]) :"
-				(Get-ADGroupMember $($args[$i])).SamAccountName | sort
+				(Get-ADGroupMember $args[$i]).SamAccountName | sort
 			}
 		}
 	}
