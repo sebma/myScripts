8531961bc77f7be648da0fe266f70a135390f5fc
sebma <sebma@users.noreply.github.com>
2024-02-27 14:32:49 +0100

    Update lsgroup.ps1

--- psh/lsgroup.ps1
+++ psh/lsgroup.ps1
@@ -2,8 +2,9 @@ function lsgroup {
 	$argc=$args.Count
 	if ( $argc -gt 0) {
 		for($i=0;$i -lt $argc;$i++) {
-			echo "=> Members of group $($args[$i]) :"
-			(Get-ADGroupMember $args[$i]).SamAccountName | sort
+  			$group = $args[$i]
+			echo "=> Members of group $group :"
+			(Get-ADGroupMember $group).SamAccountName | sort
 		}
 	}
 }
