09da3bb2822052655209fb22449b2480c24afd37
sebma <sebma@users.noreply.github.com>
2024-02-28 22:56:50 +0100

    Updated flvCopy2mp4.sh

--- shl/flvCopy2mp4.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-#file="$1"
-#shift
-#	time avconv -i "$file" -codec copy -copyts -movflags frag_keyframe $@ "${file/.flv/.mp4}"
-for file
-do
-	time ffmpeg -i "$file" -codec copy "${file/.flv/.mp4}"
-done
