8f6e00bf66e5fe7ae4a4a10091c254bc34573119
sebma <sebma@users.noreply.github.com>
2024-02-28 22:56:49 +0100

    Updated VOB_merge_AVCONV.sh

--- shl/VOB_merge_AVCONV.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -x
-#time -p avconv -probesize 400M -analyzeduration 400M -i concat:$(printf "%s|" $(ls -v $@)) -map 0 -scodec copy -c:a copy -c:v copy -f vob output.vob
-time -p  avconv -probesize 400M -analyzeduration 400M -i concat:$(printf "%s|" $(ls -v $@)) -map 0 -c copy -f mpeg output.vob
-sync
