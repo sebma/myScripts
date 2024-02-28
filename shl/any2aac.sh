3318fc59e9c733a842f949efaaa6b13f6b358ddd
sebma <sebma@users.noreply.github.com>
2024-02-28 22:56:49 +0100

    Updated any2aac.sh

--- shl/any2aac.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-#file="$1"
-#shift
-for file
-do
-#	time ffmpeg -hide_banner -i "$file" -acodec aac -aprofile aac_low -q:a 1 -f adts $@ "${file/.???/.aac}"
-	time ffmpeg -hide_banner -i "$file" -acodec aac -aprofile aac_low -q:a 1 -f adts "${file/.???/.aac}"
-done
