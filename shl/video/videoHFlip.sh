29c224675bb249c9c3e3c94fae481774b37dfb8f
sebma <sebma@users.noreply.github.com>
2024-03-01 10:34:44 +0100

    Update videoHFlip.sh

--- shl/video/videoHFlip.sh
+++ shl/video/videoHFlip.sh
@@ -14,7 +14,7 @@ function videoHFlip {
  	test $# -ge 2 && local outputFilePath=$2 && shift 2 || local outputFilePath=.
   	local remainingArgs=("${@}")
    	suffix=("${remainingArgs[@]/ /_}")
-	outputFile="$outputFilePath/$fileBaseName-FLIPPED-$suffix.$extension"
+	outputFile="$outputFilePath/$fileBaseName-${suffix}-FLIPPED.$extension"
 	outputExtension=${outputFile/*./}
 	local options
 	case $outputExtension in
