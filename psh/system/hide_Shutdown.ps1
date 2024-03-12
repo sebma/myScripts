96763e6cb31fb168ba798667d2d664c51b0f0970
sebma <sebma@users.noreply.github.com>
2024-03-12 17:40:25 +0100

    Create hide_Shutdown.ps1

---
+++ psh/system/hide_Shutdown.ps1
@@ -0,0 +1,5 @@
+gpv HKLM:\SOFTWARE\Microsoft\PolicyManager\default\Start\HideShutDown -n value
+
+sp HKLM:\SOFTWARE\Microsoft\PolicyManager\default\Start\HideShutDown -n value -v 1
+
+gpv HKLM:\SOFTWARE\Microsoft\PolicyManager\default\Start\HideShutDown -n value
