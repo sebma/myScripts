76a1a7517f0bdccbfacfaacdaf405da9c9a10b4c
sebma <sebma@users.noreply.github.com>
2024-03-12 17:41:47 +0100

    Create show_File_Extensions.ps1

---
+++ psh/show_File_Extensions.ps1
@@ -0,0 +1,5 @@
+gpv HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -n HideFileExt
+
+sp HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -n HideFileExt -v 0
+
+gpv HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -n HideFileExt
