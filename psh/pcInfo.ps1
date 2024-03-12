c8cc759849d6b57533470d26a982146869070a15
sebma <sebma@users.noreply.github.com>
2024-03-12 16:36:13 +0100

    Update pcInfo.ps1

--- psh/pcInfo.ps1
+++ psh/pcInfo.ps1
@@ -2,5 +2,5 @@ gin WindowsProductName , BiosFirmwareType , CsManufacturer , CsModel , CsChassis
 echo "UserProfile`t      : $ENV:USERPROFILE"
 Get-PSDrive -PSProvider FileSystem | Format-Table
 Get-PSDrive -PSProvider FileSystem | Format-Table -Property Name,Used,Free
-Get-NetIPAddress -AddressFamily IPv4  | select InterfaceAlias , IPAddress | Sort-Object InterfaceAlias
+Get-NetIPAddress -AddressFamily IPv4  | select InterfaceAlias , IPAddress | Sort-Object InterfaceAlias | Format-Table
 Get-NetRoute -AddressFamily IPv4 | select DestinationPrefix,NextHop,ifIndex,InterfaceAlias,InterfaceMetric,RouteMetric | Format-Table | Out-String -stream | sls -n "224.0.0.0/4|/32"
