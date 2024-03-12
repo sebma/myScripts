262334f90d6a782f12e8c64b5bd1fc393db72d33
sebma <sebma@users.noreply.github.com>
2024-03-12 17:42:32 +0100

    Create verboseLogin.ps1

---
+++ psh/system/verboseLogin.ps1
@@ -0,0 +1,5 @@
+gpv HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -n VerboseStatus
+
+sp HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -n VerboseStatus -v 1
+
+gpv HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -n VerboseStatus
