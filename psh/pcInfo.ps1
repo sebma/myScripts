e1f9fdbd5c3147b4557e540b5452b64499f90d0d
sebma <sebma@users.noreply.github.com>
2024-03-12 16:33:07 +0100

    Update pcInfo.ps1

--- psh/pcInfo.ps1
+++ psh/pcInfo.ps1
@@ -3,3 +3,4 @@ echo "UserProfile`t      : $ENV:USERPROFILE"
 Get-PSDrive -PSProvider FileSystem | Format-Table
 Get-PSDrive -PSProvider FileSystem | Format-Table -Property Name,Used,Free
 Get-NetIPAddress -AddressFamily IPv4  | select InterfaceAlias , IPAddress | Sort-Object InterfaceAlias
+Get-NetRoute -AddressFamily IPv4 | select DestinationPrefix,NextHop,ifIndex,InterfaceAlias,InterfaceMetric,RouteMetric | Format-Table | Out-String -stream | sls -n "224.0.0.0/4|/32"
