a598a800b25a4e40bf45038e4b50b37eabd121f9
sebma <sebma@users.noreply.github.com>
2024-03-12 17:38:16 +0100

    Create disable_Autorun_All_users.ps1

---
+++ psh/system/disable_Autorun_All_users.ps1
@@ -0,0 +1,2 @@
+sp HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\policies\Explorer -n NoDriveTypeAutorun -v 0xFF
+gpv HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\policies\Explorer -n NoDriveTypeAutorun
