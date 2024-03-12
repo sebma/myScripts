86bae0de0feecfc7fa02af9b7a5888102115f771
sebma <sebma@users.noreply.github.com>
2024-03-12 17:36:03 +0100

    Create disable_APIPA.ps1

---
+++ psh/system/disable_APIPA.ps1
@@ -0,0 +1,2 @@
+sp HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces -n IPAutoconfigurationEnabled -v 0
+gpv HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces -n IPAutoconfigurationEnabled
