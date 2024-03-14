d0687e456a260087822c94f6e812c59da501dcf2
sebma <sebma@users.noreply.github.com>
2024-03-14 14:53:01 +0100

    Update scanNewDisks.sh

--- shl/system/scanNewDisks.sh
+++ shl/system/scanNewDisks.sh
@@ -2,4 +2,3 @@
 
 echo "=> Re-scanning SCSI hosts for new disks, just in case ..."
 time echo "- - -" | sudo tee /sys/class/scsi_host/host*/scan >/dev/null
-time echo 1 | sudo tee /sys/class/scsi_device/*/device/block/sd?/device/*scan
