ec56fac2450bee44bc5c307ad0be156703d13b96
sebma <sebma@users.noreply.github.com>
2024-03-18 07:42:25 +0100

    Update host.ps1

--- psh/host.ps1
+++ psh/host.ps1
@@ -1,1 +1,15 @@
-Resolve-DnsName @args
+# Resolve-DnsName @args
+function main {
+	$argc = $args.Count
+	if ( $argc -eq 0 ) {
+		echo USAGE
+	} elseif ( $argc -eq 1 ) {
+		Resolve-DnsName $args[0]
+	} elseif ( $argc -eq 2 ) {
+		Resolve-DnsName -name $args[0] -server $args[1]
+	} else {
+		echo WHATEVER
+	}
+}
+
+main @args
