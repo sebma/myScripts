b054f71982cae508e3a4925c5208ec5a755516e5
sebma <sebma@users.noreply.github.com>
2024-03-18 14:02:42 +0100

    Update IPv4.ps1

--- psh/IPv4.ps1
+++ psh/IPv4.ps1
@@ -1,1 +1,2 @@
+Get-NetIPInterface -ConnectionState Connected -AddressFamily IPv4 | % { Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias $_.InterfaceAlias } | select InterfaceAlias , IPAddress , PrefixLength
 Get-NetIPInterface -ConnectionState Connected -AddressFamily IPv4 | % { Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias $_.InterfaceAlias } | select InterfaceAlias , IPAddress , PrefixLength , @{ Name='IPv4DefaultGateway'; Expression={ ( $_ | Get-NetIPConfiguration ).IPv4DefaultGateway.Nexthop } }
