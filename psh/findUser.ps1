825633454ddb0e48a71b50d4639fabf4c0d74806
sebma <sebma@users.noreply.github.com>
2024-03-19 16:43:09 +0100

    Update findUser.ps1

--- psh/findUser.ps1
+++ psh/findUser.ps1
@@ -1,2 +1,2 @@
 $myPattern = '*'+$args[0]+'*'
-Get-ADUser -Properties CanonicalName , Created , LastLogonDate , Enabled -Filter { name -like $myPattern }
+Get-ADUser -Properties CanonicalName , Created , Description , LastLogonDate , Enabled -Filter { name -like $myPattern }
