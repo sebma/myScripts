eba7237d0c42e1567624b84b0aa3c759024fc32e
sebma <sebma@users.noreply.github.com>
2024-04-03 17:13:32 +0200

    Update lastFiles.ps1

--- psh/lastFiles.ps1
+++ psh/lastFiles.ps1
@@ -1,1 +1,1 @@
-ls -File -R | ? { $_.LastWriteTime -gt (Get-Date).AddMinutes(-5) } | % FullName
+ls -Force -Dir | ? { ( $_.Name -notLike 'AppData' ) -and ( $_.Name -notLike 'Application Data' ) } | ls -Force -File -R | ? { $_.LastWriteTime -gt (Get-Date).AddMinutes(-5) } | % FullName
