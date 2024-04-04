4555985a4b704434ca2d0db8c408f73b03b37774
sebma <sebma@users.noreply.github.com>
2024-04-04 20:51:37 +0200

    Updated mpvlog.sh

---
+++ shl/mpvlog.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+mpv () 
+{ 
+	local today=$(date +%Y%m%d)
+	local mpv="command mpv";
+	local mpvOptions="$mpvDefaultOptions --log-file=$HOME/log/mpv-$today.log";
+	local DISPLAY=$DISPLAY;
+	test "$SSH_CONNECTION" && hostname | \egrep -qi "b206$|eb1501p$" && export DISPLAY=:0;
+	LANG=en_US.utf8 $mpv $mpvOptions "$@"
+}
+
+mpv "$@"
