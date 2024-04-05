b9de48eefbf8776e601eb17614539c804cfb1b3c
sebma <sebma@users.noreply.github.com>
2024-04-05 14:54:00 +0200

    Update findBigFiles.ps1

--- psh/findBigFiles.ps1
+++ psh/findBigFiles.ps1
@@ -1,7 +1,7 @@
 $nbLines = 48
 $dirName = $args[0]
 if( $args.Count -eq 0 ) { $dirName = "." }
-$dirName
+# " => $dirName = " + $dirName
 
 #dir -Force -r -file $dirName | sort Length -desc | select @{ n= "RelativePath"; e={ Resolve-Path -Relative $_.fullname } } , @{ n="Size"; e={ "{0,5:n3} MiB" -f ($_.length / 1mb) } } | select -f $nbLines
 #dir -Force -r -file $dirName | sort Length -desc | select -f $nbLines | select @{ n= "RelativePath"; e={ Resolve-Path -Relative $_.fullname } } , @{ n="Size"; e={ "{0,5:n3} MiB" -f ($_.length / 1mb) } }
