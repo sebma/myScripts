8e6c5037053638a31459c2f2664a796d3d5e3abe
sebma <sebma@users.noreply.github.com>
2024-04-09 18:24:06 +0200

    Update findComputer.ps1

--- psh/findComputer.ps1
+++ psh/findComputer.ps1
@@ -1,2 +1,2 @@
 $myPattern = '*'+$args[0]+'*'
-Get-ADComputer -Properties CanonicalName , Description , IPv4Address -Filter { name -like $myPattern }
+Get-ADComputer -Properties CanonicalName , Description , IPv4Address , ServicePrincipalNames -Filter { name -like $myPattern }
