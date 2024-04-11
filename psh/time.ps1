d387e533efee20e2ee1785dfca8af8471613793c
sebma <sebma@users.noreply.github.com>
2024-04-11 17:44:47 +0200

    Update time.ps1

--- psh/time.ps1
+++ psh/time.ps1
@@ -1,7 +1,7 @@
 function time {
-	$duration = ( "$args" | Measure-Command { Invoke-Expression $_ | Out-Default } ).toString()
+	$duration = ( "$args" | Measure-Command { Invoke-Expression $_ | Out-Default } ).toString("hh\:mm\:ss\.ff")
 	echo ""
- 	echo $duration
+ 	"=> The processing took "+$duration+"."
 }
 
 time @args
