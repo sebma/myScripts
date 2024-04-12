736cf393978f41c1b472e598f1406041776737f7
sebma <sebma@users.noreply.github.com>
2024-04-12 11:42:20 +0200

    Create upload-smallFiles.ps1

---
+++ psh/upload-smallFiles.ps1
@@ -0,0 +1,26 @@
+$dirSep = [io.path]::DirectorySeparatorChar
+$dir2copy = "myDIR"
+$srcDIR = $HOME+"\Downloads\" + $dir2copy
+$dstDIR = $HOME"+"\Downloads\"+$ENV:USERNAME + $dirSep + $dir2copy
+
+#$robocopyOPTIONS = "/E /IM /NFL"
+$robocopyOPTIONS = "/E /R:0 /W:0 /IM /IS /IT /NFL /NP"
+$robocopyCOMMAND = "robocopy.exe"
+#$robocopyARGS = "$srcDIR", "$dstDIR" , "/IM" , "/NP"
+
+"=> $robocopyARGS = "+$robocopyARGS
+
+"=> Copying $srcDIR $dstDIR  ..."
+$cmd = "$robocopyCOMMAND $srcDIR $dstDIR $robocopyOPTIONS"
+$duration = Measure-Command { iex $cmd | tee -var result | Out-Default }
+
+"=> The command launched was : "
+$cmd
+"=> The copy took " + $duration.toString("hh\:mm\:ss\.ff") + " ."
+
+#$speed = cat result.out 2>$null | sls Speed | select -f 1 | % { "{0:n3} MiB" -f ( ($_ -split '\s+')[3]/1MB ) }
+$speed = $result | sls Speed | select -f 1 | % { "{0:n3}MiB/s" -f ( ($_ -split '\s+')[3]/1MB ) }
+
+#$result
+if( $speed ) { "=> Download speed was $speed ." }
+if( Test-Path result.out 2>$null ) { rm result.out }
