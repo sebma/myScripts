f6e58f3ec6ec38416a52242b0a77612bbd411ad3
sebma <sebma@users.noreply.github.com>
2024-04-15 14:38:32 +0200

    Update ip-route-get.ps1

--- psh/ip-route-get.ps1
+++ psh/ip-route-get.ps1
@@ -1,2 +1,2 @@
 $dest = $args[0]
-Find-NetRoute -RemoteIPAddress $dest | Select-Object ifIndex , InterfaceAlias , DestinationPrefix , NextHop , RouteMetric -Last 1
+Find-NetRoute -RemoteIPAddress $dest | Select-Object ifIndex , DestinationPrefix , NextHop , InterfaceAlias , RouteMetric -Last 1 | Format-Table
