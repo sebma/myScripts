5180ca4c27a63ef5fa7ec6c3b8b546d5b22278f6
sebma <sebma@users.noreply.github.com>
2024-04-15 17:00:13 +0200

    Update ip-route.cmd

--- cm/ip-route.cmd
+++ cm/ip-route.cmd
@@ -1,3 +1,3 @@
 @REM route -4 print | findstr -r "[0-9].[0-9].[0-9].[0-9].*[0-9]$" | findstr -v "224.0.0.0 255.255.255.255"
 route -4 print | findstr "On-link 0.0.0.0 Gateway Persistent" | findstr -v "224.0.0.0 255.255.255.255"
-netsh int ipv4 sh route | findstr -v "224.0.0.0/4 /32"
+netsh int ipv4 show route | findstr -v "224.0.0.0/4 /32"
