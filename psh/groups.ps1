26b3f159e169488c75dc983c1c2c3bd653ee7c83
sebma <sebma@users.noreply.github.com>
2024-04-16 17:23:42 +0200

    Update groups.ps1

--- psh/groups.ps1
+++ psh/groups.ps1
@@ -1,11 +1,13 @@
 function groups {
+	$DC = (Get-ADDomainController).Name
+	$DC = $env:LOGONSERVER.Substring(2)
 	$argc=$args.Count
 	if ( $argc -eq 0) {
 		( (Get-ADUser -Identity $env:username -Properties MemberOf).memberof | Get-ADGroup ).name | sort
 	} else {
 		for($i=0;$i -lt $argc;$i++) {
 			echo "=> Memberships of $($args[$i]) :"
-			( ( Get-ADUser -Identity $args[$i] -Properties MemberOf ).memberof | Get-ADGroup ).name | sort
+			( ( Get-ADUser -Identity $args[$i] -Properties MemberOf -Server $DC ).memberof | Get-ADGroup -Server $DC).name | sort
 		}
 	}
 }
