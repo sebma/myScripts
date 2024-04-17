3db332c65090f4818f9735cf21c447257bca4630
sebma <sebma@users.noreply.github.com>
2024-04-17 12:33:10 +0200

    Update findUser.ps1

--- psh/findUser.ps1
+++ psh/findUser.ps1
@@ -1,3 +1,4 @@
 $myPattern = '*'+$args[0]+'*'
+$DC = $env:LOGONSERVER.Substring(2)
 Get-ADUser -Properties CanonicalName , Created , Description, EmailAddress , Enabled , LastLogonDate, LockedOut, msDS-UserPasswordExpiryTimeComputed , PasswordExpired , SamAccountName -Filter { name -like $myPattern } `
 | select CanonicalName , Created , Description, EmailAddress , Enabled , LastLogonDate, LockedOut, @{name="PasswordExpiryDate";expression={ [datetime]::fromfiletime($_."msDS-UserPasswordExpiryTimeComputed") } } , PasswordExpired , SamAccountName
