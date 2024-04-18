7da9fbe6ea54a89e24ea2b16d95c5ff54799a3b5
sebma <sebma@users.noreply.github.com>
2024-04-18 19:04:54 +0200

    Update dcList.ps1

--- psh/dcList.ps1
+++ psh/dcList.ps1
@@ -1,1 +1,1 @@
-Get-ADDomainController -Filter '*' | Select Hostname , IPv4Address
+Get-ADDomainController -Filter '*' | Select Hostname , IPv4Address , OperatingSystem , Site , IsReadOnly | sort Hostname | Format-Table
