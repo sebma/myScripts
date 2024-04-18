dbabf8487656fd9cb26009268cd2078c5a78360f
sebma <sebma@users.noreply.github.com>
2024-04-18 13:57:15 +0200

    Update pcInfo.ps1

--- psh/pcInfo.ps1
+++ psh/pcInfo.ps1
@@ -1,10 +1,23 @@
+$today = $( get-date -f "yyyyMMdd" )
+& {
+echo "=> PC Summary info :"
 gin WindowsProductName , BiosFirmwareType , CsManufacturer , CsModel , CsChassisSKUNumber , CsProcessors , CsTotalPhysicalMemory , OsName , OsVersion, OsLanguage, OsMuiLanguages , CsNetworkAdapters , CsName , CsUserName , OsPagingFiles , TimeZone
 echo "UserProfile`t      : $ENV:USERPROFILE"
+echo ""
+echo "=> Network Maps :"
 Get-SMBMapping | Format-Table
+echo "=> Network Drives Usage :"
 Get-PSDrive -PSProvider FileSystem | Format-Table
 #Get-PSDrive -PSProvider FileSystem | Format-Table Name , @{ N="Used";E={ "{0:n3} GiB" -f ($_.Used/1GB) };A="right" } , @{ N="Free";E={ "{0:n3} GiB" -f ($_.Free/1GB) };A="right" } , @{ N="Total";E={ "{0:n3} GiB" -f (($_.Used+$_.Free)/1GB) };A="right" }
+echo "=> Volumes Usage :"
 Get-Volume
 #Get-NetIPAddress -AddressFamily IPv4  | select InterfaceAlias , IPAddress | Sort-Object InterfaceAlias | Format-Table
+echo ""
+echo "=> IP information :"
 Get-NetIPInterface -ConnectionState Connected -AddressFamily IPv4 | % { Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias $_.InterfaceAlias } | select InterfaceAlias , IPAddress , PrefixLength , @{ Name='IPv4DefaultGateway'; Expression={ ( $_ | Get-NetIPConfiguration ).IPv4DefaultGateway.Nexthop } } | Format-Table
+echo "=> Routing table :"
 Get-NetRoute -AddressFamily IPv4 | select DestinationPrefix,NextHop,InterfaceAlias,ifIndex,InterfaceMetric,RouteMetric | Format-Table | Out-String -stream | sls -n "224.0.0.0/4|/32"
+echo "=> Software List :"
+#wmic product get name,version,installDate | sls -n "^\s*$" | Out-String -stream | Sort { ($_.trim() -split '\s+')[1] }
 Get-WmiObject -Class Win32_Product | select Name , Version , InstallDate | sort Name | Format-Table
+} | Tee "$env:COMPUTERNAME-$today.txt"
