7d06f6707511d0afbdd28d0892c65779ad5de272
sebma <sebma@users.noreply.github.com>
2024-04-18 14:28:00 +0200

    Update pcInfo.ps1

--- psh/pcInfo.ps1
+++ psh/pcInfo.ps1
@@ -11,15 +11,15 @@ Get-PSDrive -PSProvider FileSystem | Format-Table
 #Get-PSDrive -PSProvider FileSystem | Format-Table Name , @{ N="Used";E={ "{0:n3} GiB" -f ($_.Used/1GB) };A="right" } , @{ N="Free";E={ "{0:n3} GiB" -f ($_.Free/1GB) };A="right" } , @{ N="Total";E={ "{0:n3} GiB" -f (($_.Used+$_.Free)/1GB) };A="right" }
 echo "=> Volumes Usage :"
 Get-Volume
-#Get-NetIPAddress -AddressFamily IPv4  | select InterfaceAlias , IPAddress | Sort-Object InterfaceAlias | Format-Table
+#Get-NetIPAddress -AddressFamily IPv4  | Select InterfaceAlias , IPAddress | Sort InterfaceAlias | Format-Table
 echo ""
 echo "=> IP information :"
-Get-NetIPInterface -ConnectionState Connected -AddressFamily IPv4 | % { Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias $_.InterfaceAlias } | select InterfaceAlias , IPAddress , PrefixLength , @{ Name='IPv4DefaultGateway'; Expression={ ( $_ | Get-NetIPConfiguration ).IPv4DefaultGateway.Nexthop } } | Format-Table
+Get-NetIPInterface -ConnectionState Connected -AddressFamily IPv4 | % { Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias $_.InterfaceAlias } | Select InterfaceAlias , IPAddress , PrefixLength , @{ Name='IPv4DefaultGateway'; Expression={ ( $_ | Get-NetIPConfiguration ).IPv4DefaultGateway.Nexthop } } | Format-Table
 echo "=> Routing table :"
-Get-NetRoute -AddressFamily IPv4 | select DestinationPrefix,NextHop,InterfaceAlias,ifIndex,InterfaceMetric,RouteMetric | Format-Table | Out-String -stream | sls -n "224.0.0.0/4|/32"
+Get-NetRoute -AddressFamily IPv4 | Select DestinationPrefix,NextHop,InterfaceAlias,ifIndex,InterfaceMetric,RouteMetric | Format-Table | Out-String -stream | sls -n "224.0.0.0/4|/32"
 echo "=> Software List :"
 #wmic product get name,version,installDate | sls -n "^\s*$" | Out-String -stream | Sort { ($_.trim() -split '\s+')[1] }
-#Get-WmiObject -Class Win32_Product | select Name , Version , InstallDate | sort Name | Format-Table
+#Get-WmiObject -Class Win32_Product | Select Name , Version , InstallDate | Sort Name | Format-Table
 #Get-Package | ? Name -notMatch "update|microsoft" | Select Name , Version , InstallDate | Sort Name | Format-Table -AutoSize
-ls HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall | % { gp $_.PsPath } | select Displayname , DisplayVersion , InstallDate | sort Displayname | Format-Table | Out-String -Stream | % { $_.Trim() }
+ls HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall | % { gp $_.PsPath } | Select Displayname , DisplayVersion , InstallDate | Sort -u Displayname | Format-Table | Out-String -Stream | % { $_.Trim() }
 } | Tee "$env:COMPUTERNAME-$today.txt"
