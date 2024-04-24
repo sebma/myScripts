95d01b0a9867c979577a6dccf022d95b9323226a
sebma <sebma@users.noreply.github.com>
2024-04-24 11:22:28 +0200

    Update re-encode.ps1

--- psh/re-encode.ps1
+++ psh/re-encode.ps1
@@ -1,5 +1,5 @@
 $inVideo = $($args[0])
-$ext = gi $inVideo | % Extension
+$ext = ls $inVideo | % Extension
 $outVideo = $inVideo.replace( $ext , "-SMALLER$ext" )
 echo "=> ffmpeg -i $inVideo $outVideo ..."
 ffmpeg -i $inVideo $outVideo
