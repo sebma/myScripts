4266070ea1236e65b81926e1efd99c3f8da95b97
sebma <sebma@users.noreply.github.com>
2024-04-26 12:04:48 +0200

    Update firstboot.sh

--- shl/system/firstboot.sh
+++ shl/system/firstboot.sh
@@ -47,7 +47,7 @@ if egrep -i "vmware|virtal" /sys/class/dmi/id/sys_vendor -q;then
 	done
 	echo "=> done."
 	systemctl start syslog.socket
-	syslogSERVER=172.16.5.231
+	syslogSERVER=x.y.z.t1
 	grep $syslogSERVER /etc/rsyslog.conf /etc/rsyslog.d/* -q || echo "*.* @$syslogSERVER" | tee -a /etc/rsyslog.d/$companyNAME-rsyslog.conf
 	systemctl restart rsyslog
 fi
