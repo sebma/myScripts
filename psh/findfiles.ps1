1ef28297bfd43f4d74ab1c1a2dda023bdee3ff2c
sebma <sebma@users.noreply.github.com>
2024-05-13 15:41:56 +0200

    Create findfiles.ps1

---
+++ psh/findfiles.ps1
@@ -0,0 +1,17 @@
+function findfiles {
+	$argc=$args.Count
+	if ( $argc -eq 1 ) {
+		$dirName = "."
+		$regexp = $args[0]
+	} elseif ( $argc -eq 2 ) {
+		$dirName = $args[0]
+		$regexp = $args[1]
+	} else {
+		write-warning "Usage : [dirName] regexp"
+		exit 1
+	}
+
+	dir -r -fo $dirName 2>$null | % FullName | sls $regexp
+}
+
+findfiles @args
