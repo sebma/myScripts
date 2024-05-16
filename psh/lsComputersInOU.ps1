295537dd3315ff5c9f2a0b90367df41a95608913
sebma <sebma@users.noreply.github.com>
2024-05-16 15:56:29 +0200

    Update lsComputersInOU.ps1

--- psh/lsComputersInOU.ps1
+++ psh/lsComputersInOU.ps1
@@ -1,2 +1,2 @@
 $DC = $env:LOGONSERVER.Substring(2)
-Get-ADComputer -Properties CanonicalName , Description , IPv4Address -Filter * -SearchBase $args[0]
+Get-ADComputer -Properties CN , CanonicalName , Description , IPv4Address -Filter * -SearchBase $args[0]
