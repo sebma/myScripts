c64cd33681678d8045587a3768613e761e64e518
sebma <sebma@users.noreply.github.com>
2024-05-16 15:53:16 +0200

    Update findComputer.ps1

--- psh/findComputer.ps1
+++ psh/findComputer.ps1
@@ -10,6 +10,6 @@ if( ! $firstArg.Contains("*") ) {
 
 "=> myPattern = $myPattern"
 
-Get-ADComputer -Server $DC -Properties CanonicalName , Description , IPv4Address , OperatingSystem , ServicePrincipalNames -Filter { Name -like $myPattern }
+Get-ADComputer -Server $DC -Properties CN , CanonicalName , Description , IPv4Address , OperatingSystem , ServicePrincipalNames -Filter { Name -like $myPattern }
 
-# Get-ADComputer -Server $DC -Properties CanonicalName , Description , IPv4Address , OperatingSystem , ServicePrincipalNames -Filter { Name -like $myPattern } | ? Name -match $myRegExp
+# Get-ADComputer -Server $DC -Properties CN , CanonicalName , Description , IPv4Address , OperatingSystem , ServicePrincipalNames -Filter { Name -like $myPattern } | ? Name -match $myRegExp
