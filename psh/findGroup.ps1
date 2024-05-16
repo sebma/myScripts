aadc128ae4658cf252c3008da11441de96229297
sebma <sebma@users.noreply.github.com>
2024-05-16 15:52:27 +0200

    Update findGroup.ps1

--- psh/findGroup.ps1
+++ psh/findGroup.ps1
@@ -1,3 +1,3 @@
 $myPattern = '*'+$args[0]+'*'
 $DC = $env:LOGONSERVER.Substring(2)
-Get-ADGroup -Properties CanonicalName , Created , Description -Filter { name -like $myPattern }
+Get-ADGroup -Properties CN , CanonicalName , Created , Description -Filter { name -like $myPattern }
