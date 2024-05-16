aa4bd84ebdd825a85ed4d4072951edc6a2e24d6a
sebma <sebma@users.noreply.github.com>
2024-05-16 15:56:17 +0200

    Update lsGroupsInOU.ps1

--- psh/lsGroupsInOU.ps1
+++ psh/lsGroupsInOU.ps1
@@ -1,2 +1,2 @@
 $DC = $env:LOGONSERVER.Substring(2)
-Get-ADGroup -Properties CanonicalName , Created , Description -Filter * -SearchBase $args[0]
+Get-ADGroup -Properties CN , CanonicalName , Created , Description -Filter * -SearchBase $args[0]
