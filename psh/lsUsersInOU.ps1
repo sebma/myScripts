5b561b2f5e079c08a30f3e91e3126971d96fc18a
sebma <sebma@users.noreply.github.com>
2024-05-22 17:57:48 +0200

    Update lsUsersInOU.ps1

--- psh/lsUsersInOU.ps1
+++ psh/lsUsersInOU.ps1
@@ -1,2 +1,2 @@
 $DC = $env:LOGONSERVER.Substring(2)
-Get-ADUser -Properties CN , CanonicalName , Created , Description , LastLogonDate , Enabled -Filter * -SearchBase $args[0]
+Get-ADUser -Properties CN , CanonicalName , Created , Description , EmailAddress , LastLogonDate , Enabled -Filter * -SearchBase $args[0]
