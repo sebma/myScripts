1e12384c65572a62140733666101ac333cbaaf82
sebma <sebma@users.noreply.github.com>
2024-07-28 12:47:16 +0200

    Updated record_NCC_Live_Then_Suspend.sh

--- shl/record_NCC_Live_Then_Suspend.sh
+++ shl/record_NCC_Live_Then_Suspend.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-scriptDir=$(dirname $0)
+scriptDir=$(dirname $(readlink -f $0))
 scriptDir=$(cd $scriptDir;pwd)
 echo $PATH | grep /usr/local/bin -q || export PATH+=:/usr/local/bin
 echo $PATH | grep $scriptDir -q || export PATH+=:$scriptDir
