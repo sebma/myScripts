f976bc67b85d72a35c3729593dacfb5c895f27fc
sebma <sebma@users.noreply.github.com>
2024-08-02 22:58:46 +0200

    Updated updateFreeBox-Playlists.sh

--- shl/updateFreeBox-Playlists.sh
+++ shl/updateFreeBox-Playlists.sh
@@ -13,7 +13,7 @@ wget -c http://mafreebox.freebox.fr/freeboxtv/playlist.m3u
 $egrep -w -B1 --no-group-separator "EXTM3U|hd" playlist.m3u > FreeBoxTV-HD.m3u
 $egrep -w -B1 --no-group-separator "EXTM3U|sd" playlist.m3u > FreeBoxTV-SD.m3u
 $egrep -w -B1 --no-group-separator "EXTM3U|ld" playlist.m3u > FreeBoxTV-LD.m3u
-$egrep -w -B1 --no-group-separator 'EXTM3U|service=[0-9]+$' playlist.m3u > FreeBoxTV-AutoDefinition.m3u
+$egrep -w -B1 --no-group-separator 'EXTM3U|service=[0-9]+$' playlist.m3u > FreeBoxTV-AUTO.m3u
 for file in playlist.m3u FreeBoxTV-*.m3u;do
 	git diff $file | grep -w diff -q && git ci $file
 done
