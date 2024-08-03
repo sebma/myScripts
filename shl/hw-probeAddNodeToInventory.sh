414ea93fc06d8a407c9a1bda0089751809ce7f98
sebma <sebma@users.noreply.github.com>
2024-08-03 11:48:48 +0200

    Updated hw-probeAddNodeToInventory.sh

--- shl/hw-probeAddNodeToInventory.sh
+++ shl/hw-probeAddNodeToInventory.sh
@@ -20,10 +20,12 @@ hw_probeAddNodeToInventory ()
 		fi
 	fi
 
-	[ -n "$sudo" ] && local sudo="sudo -E"
+	type sudo >/dev/null 2>&1 && [ $(id -u) != 0 ] && groups | egrep -wq "sudo|adm|admin|root|wheel" && local sudo="command sudo" || local sudo=""
+	[ -n "$sudo" ] && sudo="sudo -E"
 	local architecture=$(uname)
 	test $architecture = Darwin && architecture=bsd
 	hwprobe=$(which hw-probe)
+
 	$sudo $hwprobe -all -upload -i $inventoryID && echo "=> INFO: Check your email to add confirm adding the new node to your inventory : https://$architecture-hardware.org/index.php?view=computers&inventory=$inventoryID" 1>&2
 }
 
