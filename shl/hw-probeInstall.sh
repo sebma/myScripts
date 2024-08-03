cccdc9175af111972adc15d5954904d7ab6db482
sebma <sebma@users.noreply.github.com>
2024-08-03 10:47:39 +0200

    Updated hw-probeInstall.sh

--- shl/hw-probeInstall.sh
+++ shl/hw-probeInstall.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 distribName () {
-	local osName=unknown
 	echo $OSTYPE | grep -q android && local osFamily=Android || local osFamily=$(uname -s)
 
+	local osName=unknown
 	if [ $osFamily = Linux ]; then
 		if type -P lsb_release >/dev/null 2>&1; then
 			osName=$(lsb_release -si)
@@ -21,20 +21,52 @@ distribName () {
 
 	echo $osName | awk '{print tolower($0)}'
 }
-hw_probeInstall () {
+inxiInstall() {
 	type sudo >/dev/null 2>&1 && [ $(id -u) != 0 ] && groups | egrep -wq "sudo|adm|admin|root|wheel" && local sudo="command sudo" || local sudo=""
 	local distribName=$(distribName)
 	local retCode=0
+	if [ $distribName = ubuntu ]; then
+		grep -q "^deb .*unit193/inxi" /etc/apt/sources.list.d/*.list || $sudo add-apt-repository ppa:unit193/inxi -y
+		apt-cache policy inxi | grep -q unit193/inxi || $sudo apt update
+		dpkg -l inxi | grep -q ^.i || $sudo apt install -V inxi
+	fi
+}
+toolsInstall () {
+	echo "=> Checking the required tools ..."
+	requiredTools="acpidump dmidecode hdparm hwinfo sensors lsb_release glxinfo lspci smartctl usb-devices xdpyinfo"
+	for tool in $requiredTools;do
+		if ! type -P $tool;then
+			echo "=> WARNING : You must install the $tool tool."
+		fi
+	done
+	echo
+	echo "=> Checking the optional tools ..."
+	optionalTools="alsa-info cpuid edid-decode ethtool fdisk i2cdetect iw memtester sar upower vainfo vdpauinfo vulkaninfo iwlist xrandr xinput"
+	for tool in $optionalTools;do
+		if ! type -P $tool;then
+			echo "=> WARNING : You may install the $tool tool."
+		fi
+	done
+	echo
+}
+hw_probeInstall () {
+	echo $OSTYPE | grep -q android && local osFamily=Android || local osFamily=$(uname -s)
+	type sudo >/dev/null 2>&1 && [ $(id -u) != 0 ] && groups | egrep -wq "sudo|adm|admin|root|wheel" && local sudo="command sudo" || local sudo=""
+	local distribName=$(distribName)
+	local retCode=0
+
+	if [ $osFamily != Linux ];then
+		echo "=> $osFamily is not supported !"
+		exit 1
+	fi
+
 	if [ $distribName = ubuntu ]; then
 		local ubuntuSources=/etc/apt/sources.list
 		local distribMajorNumber=$(source /etc/os-release;echo $VERSION_ID | cut -d. -f1)
 		grep -q universe $ubuntuSources   || $sudo add-apt-repository universe -y
 		grep -q multiverse $ubuntuSources || $sudo add-apt-repository multiverse -y
-		grep -q "^deb .*unit193/inxi" /etc/apt/sources.list.d/*.list || $sudo add-apt-repository ppa:unit193/inxi -y
 		[ $distribMajorNumber -lt 20 ] && ! grep -q "^deb .*mikhailnov/hw-probe" /etc/apt/sources.list.d/*.list && $sudo add-apt-repository ppa:mikhailnov/hw-probe -y
-		apt-cache policy inxi | grep -q unit193/inxi || $sudo apt update
 		apt-cache policy hw-probe | grep -q mikhailnov/hw-probe || $sudo apt update
-		dpkg -l inxi | grep -q ^.i || $sudo apt install -V inxi
 		dpkg -l hw-probe | grep -q ^.i || $sudo apt install -V hw-probe
 		retCode=$?
 	elif [ $distribName = arch ]; then
@@ -79,4 +111,6 @@ hw_probeInstall () {
 	return $retCode
 }
 
+toolsInstall
+inxiInstall
 hw_probeInstall
