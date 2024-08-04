74e64b34d6caa9e3dac996130fd09ad9cb369c53
sebma <sebma@users.noreply.github.com>
2024-08-04 17:06:56 +0200

    Updated record_Live.sh

--- shl/record_Live.sh
+++ shl/record_Live.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 estimatedDuration=0m # last for ever when the "estimatedDuration" is unknown
-format=231+139
+format=231+233
 case $# in
 	1) url=$1;;
 	2) estimatedDuration=$1; url=$2;;
 	3) estimatedDuration=$1; format=$2; url=$3;;
-	*) echo "=> Usage: ${0##*/} [ estimatedDuration=0m ] [ format=231+139 ] URL" >&2;exit 1;;
+	*) echo "=> Usage: ${0##*/} [ estimatedDuration=0m ] [ format=231+233 ] URL" >&2;exit 1;;
 esac
 url="$(ytdlGetLiveURL.sh "$url")"
 set -x
