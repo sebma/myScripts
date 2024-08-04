342bdbf902a3c8a7e663d69c247217bd4d9c36d3
sebma <sebma@users.noreply.github.com>
2024-08-04 16:59:04 +0200

    Updated record_NCC_Live_Then_Suspend.sh

--- shl/record_NCC_Live_Then_Suspend.sh
+++ shl/record_NCC_Live_Then_Suspend.sh
@@ -7,8 +7,8 @@ echo $PATH | grep /usr/local/bin -q || export PATH+=:/usr/local/bin
 echo $PATH | grep $scriptDir -q || export PATH+=:$scriptDir
 url=https://www.youtube.com/user/NewCreationChurch/live
 #url="$(ytdlGetLiveURL.sh "$url")"
-#cd ~/Videos/ENGLISH/CHRIST/Joseph_Prince/Live_sermons/Live_NCC/ && systemd-inhibit $scriptDir/getRestrictedFilenamesFORMAT.sh 94/231 "$url"
-cd ~/Videos/ENGLISH/CHRIST/Joseph_Prince/Live_sermons/Live_NCC/ && timeout -s SIGINT $estimatedDuration getRestrictedFilenamesFORMAT.sh 94/231 "$url"
+#cd ~/Videos/ENGLISH/CHRIST/Joseph_Prince/Live_sermons/Live_NCC/ && systemd-inhibit $scriptDir/getRestrictedFilenamesFORMAT.sh 231+139 "$url"
+cd ~/Videos/ENGLISH/CHRIST/Joseph_Prince/Live_sermons/Live_NCC/ && timeout -s SIGINT $estimatedDuration getRestrictedFilenamesFORMAT.sh 231+139 "$url"
 initPath=$(ps -p 1 -o comm= | cut -d" " -f1)
 systemType=$(strings $initPath | grep -o -E "upstart|sysvinit|systemd|launchd" | head -1 || echo unknown)
 test $systemType = systemd && systemctl suspend
