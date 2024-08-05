58ee843cf0e28d4bb7722838a4cb0e8cd7ac9928
sebma <sebma@users.noreply.github.com>
2024-08-05 15:07:11 +0200

    Updated record_JdD_Live.sh

--- shl/record_JdD_Live.sh
+++ shl/record_JdD_Live.sh
@@ -3,4 +3,4 @@
 estimatedDuration=150m
 url=https://www.youtube.com/c/JusticedeDieuMinistry/live
 #url="$(ytdlGetLiveURL.sh "$url")"
-timeout -s SIGINT $estimatedDuration getRestrictedFilenamesFORMAT.sh 231+233 https://www.youtube.com/c/JusticedeDieuMinistry/live
+getRestrictedFilenamesFORMAT.sh --timeout $estimatedDuration 231+233 https://www.youtube.com/c/JusticedeDieuMinistry/live
