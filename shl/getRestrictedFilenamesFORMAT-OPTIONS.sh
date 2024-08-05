e49368a3e0fa827ce3f89c37d0ebf4c459755fd2
sebma <sebma@users.noreply.github.com>
2024-08-05 14:58:18 +0200

    Updated getRestrictedFilenamesFORMAT-OPTIONS.sh

--- shl/getRestrictedFilenamesFORMAT-OPTIONS.sh
+++ shl/getRestrictedFilenamesFORMAT-OPTIONS.sh
@@ -10,3 +10,49 @@ if [ $osType = Linux ];then
 elif [ $osType = Darwin ];then
 	getopt=/usr/local/opt/gnu-getopt/bin/getopt
 fi
+
+TEMP=$($getopt -o 'dp:t:v' --long 'create-playlist:,debug,timeout:,verbose,yt-dlp,ytdl-k,ytdl-x,ytdl-v' -- "$@")
+
+if [ $? -ne 0 ]; then
+	echo 'Terminating...' >&2
+	exit 1
+fi
+
+# Note the quotes around "$TEMP": they are essential!
+eval set -- "$TEMP"
+unset TEMP
+
+debugLevel=0
+while true; do
+	case "$1" in
+		-d|--debug) shift
+			debugLevel=2
+			debug="set -x" && ytdlInitialOptions+=( -v )
+			;;
+		-v|--verbose) shift
+			debugLevel=1
+			;;
+		-p|--create-playlist) shift
+			playlistFileName=$1
+			shift
+			;;
+		-t|--timeout) shift
+			estimatedDuration=$1
+			shift
+			;;
+		--yt-dl) shift
+			downloader=yt-dl
+			;;
+		--ytdl-k) shift
+			ytdlInitialOptions+=( -k )
+			;;
+		--ytdl-x) shift
+			ytdlInitialOptions+=( -x )
+			;;
+		--ytdl-v) shift
+			ytdlInitialOptions+=( -v )
+			;;
+		-- ) shift; break ;;
+		* ) break ;;
+	esac
+done
