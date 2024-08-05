e08dc13eda54bedede04c1d26209932d4b2b6cff
sebma <sebma@users.noreply.github.com>
2024-08-05 21:19:12 +0200

    Updated getRestrictedFilenamesFORMAT-OPTIONS.sh

--- shl/getRestrictedFilenamesFORMAT-OPTIONS.sh
+++ shl/getRestrictedFilenamesFORMAT-OPTIONS.sh
@@ -3,20 +3,6 @@
 set -o nounset
 
 scriptName=$(basename $0)
-
-osType=$(uname -s)
-if [ $osType = Linux ];then
-	#getopt=$(getopt -V | grep getopt.*enhanced -q && getopt || getopts)
-	if getopt -V | grep getopt.*util-linux -q;then
-		export getopt=getopt
-	else
-		echo "=> ERROR : You must use getopt from util-linux." >&2
-		exit 2
-	fi
-elif [ $osType = Darwin ];then
-	export getopt=/usr/local/opt/gnu-getopt/bin/getopt
-fi
-
 function usage {
 	cat <<-EOF >&2
 Usage: $scriptName [STRING]...
@@ -37,63 +23,80 @@ EOF
 	exit 1
 }
 
-#echo "=> getopt = $(which $getopt)"
-TEMP=$($getopt -o 'dhp:t:vy' --long 'playlist:,debug,help,overwrite,timeout:,verbose,yt-dl,ytdl-k,ytdl-x,ytdl-v' -- "$@")
+function parseArgs {
+	osType=$(uname -s)
+	if [ $osType = Linux ];then
+		#getopt=$(getopt -V | grep getopt.*enhanced -q && getopt || getopts)
+		if getopt -V | grep getopt.*util-linux -q;then
+			export getopt=getopt
+		else
+			echo "=> ERROR : You must use getopt from util-linux." >&2
+			exit 2
+		fi
+	elif [ $osType = Darwin ];then
+		export getopt=/usr/local/opt/gnu-getopt/bin/getopt
+	fi
+	
+	#echo "=> getopt = $(which $getopt)"
+	TEMP=$($getopt -o 'dhp:t:vy' --long 'playlist:,debug,help,overwrite,timeout:,verbose,yt-dl,ytdl-k,ytdl-x,ytdl-v' -- "$@")
+	
+	if [ $? -ne 0 ]; then
+		echo 'Terminating...' >&2
+		exit 1
+	fi
+	
+	# Note the quotes around "$TEMP": they are essential!
+	eval set -- "$TEMP"
+	unset TEMP
+	
+	while true; do
+		case "$1" in
+			-d|--debug) shift
+				debug="set -x"
+				ytdlInitialOptions+=( -v )
+				;;
+			-h|--help) shift
+				usage=true
+				;;
+			-v|--verbose) shift
+				verboseLevel+=1
+				;;
+			-p|--playlist) shift
+				playlistFileName=$1
+				shift
+				;;
+			-t|--timeout) shift
+				estimatedDuration=$1
+				shift
+				;;
+			--yt-dl) shift
+				downloader=youtube-dl
+				;;
+			--ytdl-k) shift
+				ytdlInitialOptions+=( -k )
+				;;
+			--ytdl-x) shift
+				ytdlInitialOptions+=( -x )
+				;;
+			--ytdl-v) shift
+				ytdlInitialOptions+=( -v )
+				;;
+			-y|--overwrite) shift
+				overwrite=true
+				;;
+			-- ) shift; break ;;
+			* ) break ;;
+		esac
+	done
+	[ $usage = true ] && usage
+}
 
-if [ $? -ne 0 ]; then
-	echo 'Terminating...' >&2
-	exit 1
-fi
-
-# Note the quotes around "$TEMP": they are essential!
-eval set -- "$TEMP"
-unset TEMP
-
+usage=false
 declare -i verboseLevel=0
 #debug="set +x"
-usage=false
 #playlistFileName=unset
 #estimatedDuration=150m
 #downloader=yt-dlp
 #overwrite=false
-while true; do
-	case "$1" in
-		-d|--debug) shift
-			debug="set -x"
-			ytdlInitialOptions+=( -v )
-			;;
-		-h|--help) shift
-			usage=true
-			;;
-		-v|--verbose) shift
-			verboseLevel+=1
-			;;
-		-p|--playlist) shift
-			playlistFileName=$1
-			shift
-			;;
-		-t|--timeout) shift
-			estimatedDuration=$1
-			shift
-			;;
-		--yt-dl) shift
-			downloader=youtube-dl
-			;;
-		--ytdl-k) shift
-			ytdlInitialOptions+=( -k )
-			;;
-		--ytdl-x) shift
-			ytdlInitialOptions+=( -x )
-			;;
-		--ytdl-v) shift
-			ytdlInitialOptions+=( -v )
-			;;
-		-y|--overwrite) shift
-			overwrite=true
-			;;
-		-- ) shift; break ;;
-		* ) break ;;
-	esac
-done
-[ $usage = true ] && usage
-set | egrep "(verboseLevel|debug|playlistFileName|estimatedDuration|downloader|overwrite|ytdlInitialOptions)=" | sort
+parseArgs "$@"
+set | egrep "^(verboseLevel|debug|playlistFileName|estimatedDuration|downloader|overwrite|ytdlInitialOptions)=" | sort
