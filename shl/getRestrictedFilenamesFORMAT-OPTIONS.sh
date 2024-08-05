86dc5c964a7b8521a8c401937a5d209c35cfc39d
sebma <sebma@users.noreply.github.com>
2024-08-05 15:30:13 +0200

    Updated getRestrictedFilenamesFORMAT-OPTIONS.sh

--- shl/getRestrictedFilenamesFORMAT-OPTIONS.sh
+++ shl/getRestrictedFilenamesFORMAT-OPTIONS.sh
@@ -1,17 +1,41 @@
 #!/usr/bin/env bash
 
+set -o nounset
+
+scriptName=$(basename $0)
+
 osType=$(uname -s)
 if [ $osType = Linux ];then
 	#getopt=$(getopt -V | grep getopt.*enhanced -q && getopt || getopts)
-	if ! getopt -V | grep getopt.*util-linux -q && getopt=getopt;then
+	if ! getopt -V | grep getopt.*util-linux -q && export getopt=getopt;then
 		echo "=> ERROR : You must use getopt from util-linux." >&2
 		exit 2
 	fi
 elif [ $osType = Darwin ];then
-	getopt=/usr/local/opt/gnu-getopt/bin/getopt
+	export getopt=/usr/local/opt/gnu-getopt/bin/getopt
 fi
 
-TEMP=$($getopt -o 'dp:t:v' --long 'create-playlist:,debug,timeout:,verbose,yt-dlp,ytdl-k,ytdl-x,ytdl-v' -- "$@")
+function usage {
+	cat <<-EOF >&2
+Usage: $scriptName [STRING]...
+  or:  $scriptName OPTION
+
+	-h|--help 		display this help and exit
+	-v|--verbose	output version information and exit
+	-y|--overwite	overwrite all downloaded/generated files
+	-d|--debug		be even more verbose
+	-p|--playlist	create M3U playlist
+	-t|--timeout	timeout the recording by speficied value (150m by default)
+	--ytdl			change downloader to "ytdl" (default is "yt-dlp")
+	--ytdl-k		keep downloaded intermediate files
+	--ytdl-x		extract audio
+	--ytdl-v		set downloader in verbose mode
+
+EOF
+	exit 1
+}
+
+TEMP=$($getopt -o 'dhp:t:vy' --long 'playlist:,debug,help,overwrite,timeout:,verbose,yt-dlp,ytdl-k,ytdl-x,ytdl-v' -- "$@")
 
 if [ $? -ne 0 ]; then
 	echo 'Terminating...' >&2
@@ -23,16 +47,21 @@ eval set -- "$TEMP"
 unset TEMP
 
 debugLevel=0
+usage=false
 while true; do
 	case "$1" in
 		-d|--debug) shift
 			debugLevel=2
-			debug="set -x" && ytdlInitialOptions+=( -v )
+			debug="set -x"
+			ytdlInitialOptions+=( -v )
+			;;
+		-h|--help) shift
+			usage=true
 			;;
 		-v|--verbose) shift
 			debugLevel=1
 			;;
-		-p|--create-playlist) shift
+		-p|--playlist) shift
 			playlistFileName=$1
 			shift
 			;;
@@ -52,7 +81,12 @@ while true; do
 		--ytdl-v) shift
 			ytdlInitialOptions+=( -v )
 			;;
+		-y|--overwrite) shift
+			overwrite=true
+			;;
 		-- ) shift; break ;;
 		* ) break ;;
 	esac
 done
+[ $usage = true ] && usage
+set | egrep -w "(debugLevel|debug|playlistFileName|estimatedDuration|downloader|overwrite)="
