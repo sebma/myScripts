7ca1b4819243b14f60c490e03178e788362ee355
sebma <sebma@users.noreply.github.com>
2024-08-06 12:29:02 +0200

    Updated getRestrictedFilenamesFORMAT.sh

--- shl/getRestrictedFilenamesFORMAT.sh
+++ shl/getRestrictedFilenamesFORMAT.sh
@@ -104,17 +104,115 @@ getRestrictedFilenamesFORMAT () {
 	grepColor=$grep
 	grep --help 2>&1 | grep -q -- --color && grepColor+=" --color"
 
-	echo $1 | $grep -q -- "^-[a-z]" && scriptOptions=$1 && shift
-	echo $scriptOptions | $grep -q -- "-k" && ytdlInitialOptions+=( -k )
-	echo $scriptOptions | $grep -q -- "-x" && ytdlInitialOptions+=( -x )
-	echo $scriptOptions | $grep -q -- "-y" && downloader=yt-dl
-	echo $scriptOptions | $grep -q -- "-p" && playlistFileName=$2 && shift 2
-	echo $scriptOptions | $grep -q -- "-v" && debugLevel=1
-	echo $scriptOptions | $grep -q -- "-vv" && debugLevel=2 && debug="set -x" && ytdlInitialOptions+=( -v )
-	echo $scriptOptions | $grep -q -- "-vvv" && debugLevel=3 && ffmpegLogLevel=$ffmpegInfoLogLevel
+#	echo $1 | $grep -q -- "^-[a-z]" && scriptOptions=$1 && shift
+#	echo $scriptOptions | $grep -q -- "-k" && ytdlInitialOptions+=( -k )
+#	echo $scriptOptions | $grep -q -- "-x" && ytdlInitialOptions+=( -x )
+#	echo $scriptOptions | $grep -q -- "-y" && downloader=yt-dl
+#	echo $scriptOptions | $grep -q -- "-p" && playlistFileName=$2 && shift 2
+#	echo $scriptOptions | $grep -q -- "-v" && debugLevel=1
+#	echo $scriptOptions | $grep -q -- "-vv" && debugLevel=2 && debug="set -x" && ytdlInitialOptions+=( -v )
+#	echo $scriptOptions | $grep -q -- "-vvv" && debugLevel=3 && ffmpegLogLevel=$ffmpegInfoLogLevel
 
-	initialSiteVideoFormat="$1"
-	shift
+	function parseArgs {
+		osType=$(uname -s)
+		if [ $osType = Linux ];then
+			if getopt -V | grep getopt.*util-linux -q;then
+				export getopt=getopt
+			else
+				echo "=> ERROR : You must use getopt from util-linux." >&2
+				exit 2
+			fi
+		elif [ $osType = Darwin ];then
+			export getopt=/usr/local/opt/gnu-getopt/bin/getopt
+		fi
+
+		TEMP=$($getopt -o 'df:hp:t:vy' --long 'debug,downloader:,ffmpeg-e,ffmpeg-i,ffmpeg-w,formats:,help,playlist:,overwrite,timeout:,verbose,yt-dl,ytdl-k,ytdl-x,ytdl-v' -- "$@")
+
+		if [ $? -ne 0 ]; then
+			echo 'Terminating...' >&2
+			exit 1
+		fi
+
+		# Note the quotes around "$TEMP": they are essential!
+		eval set -- "$TEMP"
+
+		while true; do
+			case "$1" in
+				-d|--debug) shift
+					debug="set -x"
+					ytdlInitialOptions+=( -v )
+					let nbOptions++
+					;;
+				--downloader) shift
+					downloader=$1
+					shift
+					let nbOptions+=2
+					;;
+				--ffmpeg-e) shift
+					ffmpegLogLevel=$ffmpegErrorLogLevel
+					let nbOptions++
+					;;
+				--ffmpeg-i) shift
+					ffmpegLogLevel=$ffmpegInfoLogLevel
+					let nbOptions++
+					;;
+				--ffmpeg-w) shift
+					ffmpegLogLevel=$ffmpegWarningLogLevel
+					let nbOptions++
+					;;
+				-f|--formats) shift
+					initialSiteVideoFormat="$1"
+					shift
+					let nbOptions+=2
+					;;
+				-h|--help) shift
+					usage=true
+					let nbOptions++
+					;;
+				-p|--playlist) shift
+					playlistFileName=$1
+					shift
+					let nbOptions+=2
+					;;
+				-t|--timeout) shift
+					timeout=$1
+					shift
+					let nbOptions+=2
+					;;
+				-v|--verbose) shift
+					let verboseLevel++
+					let nbOptions++
+					;;
+				--yt-dl) shift
+					downloader=youtube-dl
+					let nbOptions++
+					;;
+				--ytdl-k) shift
+					ytdlInitialOptions+=( -k )
+					let nbOptions++
+					;;
+				--ytdl-x) shift
+					ytdlInitialOptions+=( -x )
+					let nbOptions++
+					;;
+				--ytdl-v) shift
+					ytdlInitialOptions+=( -v )
+					let nbOptions++
+					;;
+				-y|--overwrite) shift
+					overwrite=true
+					let nbOptions++
+					;;
+				-- ) shift; let nbOptions++; break ;;
+				* ) break ;;
+			esac
+		done
+	}
+
+	parseArgs "$@"
+	eval set -- "$TEMP"
+	unset TEMP
+	shift $nbOptions
 
 	echo "=> Started <$scriptBaseName> on $@ at : $startTime ..."
 	echo
@@ -582,49 +680,49 @@ addThumbnail2media() {
 
 bestFormats="bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best[ext=webm]/best[ext=avi]/best[ext=mov]/best[ext=flv]"
 function getRestrictedFilenamesBEST {
-	getRestrictedFilenamesFORMAT "($bestFormats)" $@ # because of the "eval" statement in the "youtube_dl" bash variable
+	getRestrictedFilenamesFORMAT -f "($bestFormats)" $@ # because of the "eval" statement in the "youtube_dl" bash variable
 }
 function getRestrictedFilenamesFHD {
 	local height=1080
 	local other_Formats=fhd
 	local possibleFormats="bestvideo[ext=mp4][height<=?$height]+bestaudio[ext=m4a]/$other_Formats/best[ext=mp4][height<=?$height]"
-	getRestrictedFilenamesFORMAT "($possibleFormats/$bestFormats)" $@ # because of the "eval" statement in the "youtube_dl" bash variable
+	getRestrictedFilenamesFORMAT -f "($possibleFormats/$bestFormats)" $@ # because of the "eval" statement in the "youtube_dl" bash variable
 }
 function getRestrictedFilenamesHD {
 	local height=720
 	local other_Formats=hd/high
 	local possibleFormats="22/bestvideo[ext=mp4][height<=?$height]+bestaudio[ext=m4a]/$other_Formats/best[ext=mp4][height<=?$height]"
-	getRestrictedFilenamesFORMAT "($possibleFormats/$bestFormats)" $@ # because of the "eval" statement in the "youtube_dl" bash variable
+	getRestrictedFilenamesFORMAT -f "($possibleFormats/$bestFormats)" $@ # because of the "eval" statement in the "youtube_dl" bash variable
 }
 function getRestrictedFilenamesHQ {
 	local height=576
 	local other_Formats=hq/fsd/std/sd
 	local possibleFormats="bestvideo[ext=mp4][height<=?$height]+bestaudio[ext=m4a]/$other_Formats/best[ext=mp4][height<=?$height]"
-	getRestrictedFilenamesFORMAT "($possibleFormats/$bestFormats)" $@ # because of the "eval" statement in the "youtube_dl" bash variable
+	getRestrictedFilenamesFORMAT -f "($possibleFormats/$bestFormats)" $@ # because of the "eval" statement in the "youtube_dl" bash variable
 }
 function getRestrictedFilenamesFSD {
 	local height=480
 	local other_Formats=fsd/std
 	local possibleFormats="bestvideo[ext=mp4][height<=?$height]+bestaudio[ext=m4a]/$other_Formats/best[ext=mp4][height<=?$height]"
-	getRestrictedFilenamesFORMAT "($possibleFormats/$bestFormats)" $@ # because of the "eval" statement in the "youtube_dl" bash variable
+	getRestrictedFilenamesFORMAT -f "($possibleFormats/$bestFormats)" $@ # because of the "eval" statement in the "youtube_dl" bash variable
 }
 function getRestrictedFilenamesSD {
 	local height=360
 	local other_Formats=low/sd/std
 	local possibleFormats="18/bestvideo[ext=mp4][height<=?$height]+bestaudio[ext=m4a]/$other_Formats/best[ext=mp4][height<=?$height]"
-	getRestrictedFilenamesFORMAT "($possibleFormats/$bestFormats)" $@ # because of the "eval" statement in the "youtube_dl" bash variable
+	getRestrictedFilenamesFORMAT -f "($possibleFormats/$bestFormats)" $@ # because of the "eval" statement in the "youtube_dl" bash variable
 }
 function getRestrictedFilenamesLD {
 	local height=240
 	local other_Formats=ld/low
 	local possibleFormats="bestvideo[ext=mp4][height<=?$height]+bestaudio[ext=m4a]/$other_Formats/best[ext=mp4][height<=?$height]"
-	getRestrictedFilenamesFORMAT "($possibleFormats/$bestFormats)" $@ # because of the "eval" statement in the "youtube_dl" bash variable
+	getRestrictedFilenamesFORMAT -f "($possibleFormats/$bestFormats)" $@ # because of the "eval" statement in the "youtube_dl" bash variable
 }
 function getRestrictedFilenamesVLD {
 	local height=144
 	local other_Formats=vld/low
 	local possibleFormats="bestvideo[ext=mp4][height<=?$height]+bestaudio[ext=m4a]/$other_Formats/best[ext=mp4][height<=?$height]"
-	getRestrictedFilenamesFORMAT "($possibleFormats/$bestFormats)" $@ # because of the "eval" statement in the "youtube_dl" bash variable
+	getRestrictedFilenamesFORMAT -f "($possibleFormats/$bestFormats)" $@ # because of the "eval" statement in the "youtube_dl" bash variable
 }
 
 time $funcName $@
