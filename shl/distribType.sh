3800d0739ebb366fcfb4a9be2c15c06e0cfeb77f
sebma <sebma@users.noreply.github.com>
2024-08-10 18:01:25 +0200

    Updated distribType.sh

--- shl/distribType.sh
+++ shl/distribType.sh
@@ -1,43 +1,18 @@
 #!/usr/bin/env bash
 
-distribName () {
-	local OSTYPE=$(bash -c 'echo $OSTYPE')
-	local osName=unknown
-	echo $OSTYPE | grep -q android && local osFamily=Android || local osFamily=$(uname -s)
-
-	if [ $osFamily = Linux ]; then
-		if type -P lsb_release >/dev/null 2>&1; then
-			osName=$(lsb_release -si | awk '{print tolower($0)}')
-			[ $osName = "n/a" ] && osName=$(source /etc/os-release && echo $ID)
-		elif type -P hostnamectl >/dev/null 2>&1; then
-			osName=$(hostnamectl status | awk '/Operating System/{print tolower($3)}')
-		elif [ -s /etc/os-release ]; then
-			osName=$(source /etc/os-release && echo $ID)
-		fi
-	elif [ $osFamily = Darwin ]; then
-		osName="$(sw_vers -productName)"
-	elif [ $osFamily = Android ]; then
-		osName=Android
-	elif [ $osFamily = VMkernel ]; then # ESXi
-		osName=ESXi
-  	else
-		osName=$OSTYPE
-	fi
-
-	echo $osName | awk '{print tolower($0)}'
-}
-
 distribType () {
 	local OSTYPE=$(bash -c 'echo $OSTYPE')
 	local distribName=unknown
 	local distribType=unknown
 	echo $OSTYPE | grep -q android && local osFamily=Android || local osFamily=$(uname -s)
 
-	distribName=$(distribName)
+	local distribName=$(distribName.sh)
 
 	if [ $osFamily = Linux ]; then
-		if [ -s /etc/os-release ]; then
+		if grep ID_LIKE /etc/os-release -q; then
 			distribType=$(source /etc/os-release && echo $ID_LIKE)
+		elif [ ls /etc/*_version >/dev/null 2>&1 ]; then
+			distribType=$(echo /etc/*version | sed 's,/etc/\|_version,,g')
 		else
 			case $distribName in
 				sailfishos|rhel|fedora|centos) distribType=redhat ;;
