fc18b012b0fb2d0c2ad0f54555cbd617f8976d8c
sebma <sebma@users.noreply.github.com>
2024-08-10 18:22:05 +0200

    Updated gpups.sh

--- shl/gpups.sh
+++ shl/gpups.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-function gps {
+function gpups {
 	local pidList
 	test -e /dev/nvidia0 && pidList=$(\lsof -n -w -t /dev/nvidia*)
 	test -n "$pidList" && \ps $@ -p $pidList
 }
 
-gps $@
+gpups $@
