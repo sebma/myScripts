36f138769321e91d42a0a44b5f8cc99354546ac3
sebma <sebma@users.noreply.github.com>
2024-08-20 14:07:10 +0200

    Update subDirsSize.ps1

--- psh/subDirsSize.ps1
+++ psh/subDirsSize.ps1
@@ -1,3 +1,18 @@
+# vim: ft=powershell noet:
+
+function size2iec {
+	$size = $args[0]
+	$size2IEC = ""
+	switch( $size ) {
+		{ $_ -ge 1tb -and $_ -le 1pb } { $size2IEC = "{0:n2} TiB" -f ( $size / 1tb ); break }
+		{ $_ -ge 1gb -and $_ -le 1tb } { $size2IEC = "{0:n2} GiB" -f ( $size / 1gb ); break }
+		{ $_ -ge 1mb -and $_ -le 1gb } { $size2IEC = "{0:n2} MiB" -f ( $size / 1mb ); break }
+		{ $_ -ge 1kb -and $_ -le 1mb } { $size2IEC = "{0:n2} KiB" -f ( $size / 1kb ); break }
+		{ $_ -ge 0   -and $_ -le 1kb } { $size2IEC = "{0:n2} B  " -f   $size; break }
+	}
+
+	return $size2IEC
+}
 function dirSize {
 	$dirName = $args[0]
  	Write-Host -NoNewline "$dirName`t"
