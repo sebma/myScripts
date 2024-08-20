d6ce8f012f28cc91f011dd5cd9d815cb6c0a2f71
sebma <sebma@users.noreply.github.com>
2024-08-20 14:07:43 +0200

    Update subDirsSize.ps1

--- psh/subDirsSize.ps1
+++ psh/subDirsSize.ps1
@@ -17,13 +17,7 @@ function dirSize {
 	$dirName = $args[0]
  	Write-Host -NoNewline "$dirName`t"
 	$dirSize = ( dir "$dirName" -force -recurse | measure -property length -sum ).Sum
-	switch( $dirSize ) {
-		{ $_ -ge 1tb -and $_ -le 1pb } { "{0:n2} TiB" -f ( $dirSize / 1tb ); break }
-		{ $_ -ge 1gb -and $_ -le 1tb } { "{0:n2} GiB" -f ( $dirSize / 1gb ); break }
-		{ $_ -ge 1mb -and $_ -le 1gb } { "{0:n2} MiB" -f ( $dirSize / 1mb ); break }
-		{ $_ -ge 1kb -and $_ -le 1mb } { "{0:n2} KiB" -f ( $dirSize / 1kb ); break }
-		{ $_ -ge 0   -and $_ -le 1kb } { "{0:n2} B" -f $dirSize; break }
-	}
+ 	$dirSize2iec = ( size2iec($size) )
 }
 
 function time {
