6d43ec373dd3422b74a231ecce7108c0e8136d2e
sebma <sebma@users.noreply.github.com>
2024-08-23 15:04:52 +0200

    Update postInstall_Debian_Like.sh

--- shl/system/postInstall_Debian_Like.sh
+++ shl/system/postInstall_Debian_Like.sh
@@ -54,6 +54,7 @@ if $isDebianLike;then
 
 	if : < /dev/tcp/$proxyIP/http;then
 		# CONFIG PROXY
+  		egrep https?_proxy= $HOME/.profile -q || echo -e "\nexport https_proxy=$https_proxy" >> $HOME/.profile
   		sudo sed "/env_keep.*https\?_proxy/ s/^#//g" -i /etc/sudoers
 		# man apt-transport-http apt-transport-https
 		grep ^Acquire.*$http_proxy /etc/apt/apt.conf.d/*proxy -q || $sudo cat <<-EOF | sudo tee /etc/apt/apt.conf.d/00aptproxy
