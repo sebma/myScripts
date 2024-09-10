edc188ca5c3bf5e1cbd7e06c6d72825df62a3884
sebma <sebma@users.noreply.github.com>
2024-09-10 16:17:43 +0200

    Update initProxyClient.sh

--- shl/initProxyClient.sh
+++ shl/initProxyClient.sh
@@ -12,30 +12,32 @@ fi
 test $(id -u) == 0 && sudo="" || sudo=sudo
 
 if [ $# != 1 ];then
-	echo "=> Usage $scriptBaseName variableDefinitionFile" >&2
+	echo "=> Usage $scriptBaseName variablesDefinitionFile" >&2
 	exit 1
 fi
 
-variableDefinitionFile="$1"
-source "$variableDefinitionFile" || exit
+variablesDefinitionFile="$1"
+source "$variablesDefinitionFile" || exit
 
 if $isDebianLike;then
 	grep ^Acquire.*$http_proxy /etc/apt/apt.conf.d/*proxy -q  || echo "Acquire::http::proxy  \"$http_proxy\";"  | $sudo tee /etc/apt/apt.conf.d/00aptproxy
-	grep ^Acquire.*$https_proxy /etc/apt/apt.conf.d/*proxy -q || echo "Acquire::http::proxy  \"$https_proxy\";" | $sudo tee -a /etc/apt/apt.conf.d/00aptproxy
+	grep ^Acquire.*$https_proxy /etc/apt/apt.conf.d/*proxy -q || echo "Acquire::https::proxy \"$https_proxy\";" | $sudo tee -a /etc/apt/apt.conf.d/00aptproxy
+
+	# Propagation des variables "http_proxy" et "https_proxy" aux "sudoers"
 	$sudo grep '^[^#]\s*.*env_keep.*https_proxy' /etc/sudoers /etc/sudoers.d/* -q || echo 'Defaults:%sudo env_keep += "http_proxy https_proxy ftp_proxy all_proxy no_proxy"' | $sudo tee -a /etc/sudoers.d/proxy
 fi
 
-if which git &>/dev/null;then
-	git config --global http.proxy  $http_proxy
-	git config --global https.proxy $https_proxy
- 	git config --global -l | egrep https?.proxy
-fi
-
 if which snap &>/dev/null;then
-	$sudo snap set system proxy.http=$http_proxy
-	$sudo snap set system proxy.https=$https_proxy
+	$sudo snap get system proxy.http  || $sudo snap set system proxy.http=$http_proxy
+	$sudo snap get system proxy.https || $sudo snap set system proxy.https=$https_proxy
 	$sudo snap get system proxy
 	snap debug connectivity
+fi
+
+if which git &>/dev/null;then
+	git config --global http.proxy  | grep http -q || git config --global http.proxy  $http_proxy
+	git config --global https.proxy | grep http -q || git config --global https.proxy $https_proxy
+	git config --global -l | egrep https?.proxy
 fi
 
 exit
