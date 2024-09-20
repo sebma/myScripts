b88636ee70dac2d2cf21a97c3d4954b43236bc2c
sebma <sebma@users.noreply.github.com>
2024-09-20 21:11:54 +0200

    Updated update_snap_homedirs.sh

--- shl/system/update_snap_homedirs.sh
+++ shl/system/update_snap_homedirs.sh
@@ -9,6 +9,7 @@ fi
 variablesDefinitionFile="$1"
 source "$variablesDefinitionFile" || exit
 
+# getent passwd sebastien.mansfeld@$domain
 if sudo grep "_homedir.*/home/%d" /etc/sssd/sssd.conf -q && ! sudo snap get system homedirs | grep /home/$domain;then
 	sudo snap set system homedirs=/home/$domain
 	sudo snap get system homedirs
