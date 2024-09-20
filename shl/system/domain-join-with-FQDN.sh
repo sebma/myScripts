32f2c5a06689fea93b1c92c9f4b80cdfb67654ad
sebma <sebma@users.noreply.github.com>
2024-09-20 21:11:35 +0200

    Updated domain-join-with-FQDN.sh

--- shl/system/domain-join-with-FQDN.sh
+++ shl/system/domain-join-with-FQDN.sh
@@ -3,7 +3,7 @@
 scriptBaseName=${0/*\//}
 if ! which adcli &>/dev/null;then
 	echo "==> Installing realmd adcli krb5-user krb5-doc ..."
-	sudo apt-get install realmd adcli krb5-user krb5-doc -y >/dev/null
+	sudo apt-get install realmd adcli krb5-user krb5-doc -y
 fi
 
 NAME=$(source /etc/os-release;echo $NAME)
@@ -22,13 +22,17 @@ domainUppercase=${domain^^}
 
 logDIR=../log
 mkdir -p $logDIR
+
+echo "=> The hostname is : " $(hostnamectl hostname)
+hostnamectl hostname | grep -i $domain -q || sudo hostnamectl hostname $HOSTNAME.$domainLowercase
+echo "=> The hostname is now : " $(hostnamectl hostname)
+
 echo "=> Test de jonction au domain $domainLowercase ..." | tee $logDIR/$HOSTNAME-join-$(date +%Y%m%d).log
 if ! sudo adcli testjoin >/dev/null;then
 	echo "=> Jonction au domain $domainLowercase ..."
 	read -p "=> Veillez saisir votre identifiant T2: " t2USER
-	sudo realm join -v --user=$t2USER --computer-ou="$OU" --os-name=$NAME --os-version=$VERSION_ID $domainLowercase 2>&1 | tee -a $logDIR/$HOSTNAME-join-$(date +%Y%m%d).log
-	sudo pam-auth-update --enable mkhomedir 2>&1 | tee -a $logDIR/$HOSTNAME-join-$(date +%Y%m%d).log # Au cas ou le homedir n'est pas cree
-fi
+	sudo realm join -v --user=$t2USER --computer-ou="$OU" --os-name=$NAME --os-version=$VERSION_ID $domainLowercase
+fi 2>&1 | tee -a $logDIR/$HOSTNAME-join-$(date +%Y%m%d).log
 
 if sudo adcli testjoin >/dev/null;then
 	if ! grep "renew_lifetime\s*=\s*7d" /etc/krb5.conf -q;then
@@ -38,11 +42,7 @@ if sudo adcli testjoin >/dev/null;then
 	echo "=> Parametrage du homedir SSSD en /home/$domainLowercase ..."
 	sudo grep "_homedir.*/home/%d/%u$" /etc/sssd/sssd.conf -q || sudo sed -i-$(date +%Y%m%d-%H%M%S).conf "/_homedir/s|=.*|= /home/%d/%u|" /etc/sssd/sssd.conf
 	sudo sed -i 's/use_fully_qualified_names.*[Ff]alse/use_fully_qualified_names = True/' /etc/sssd/sssd.conf
-	if sudo grep "_homedir.*/home/%d" /etc/sssd/sssd.conf -q && ! sudo snap get system homedirs | grep /home/$domainLowercase -q;then
-		echo "=> Parametrage du homedir SNAP en /home/$domainLowercase ..."
-		sudo mkdir /home/$domainLowercase
-		sudo snap set system homedirs=/home/$domainLowercase
-	fi
+	sudo pam-auth-update --enable mkhomedir # Au cas ou le homedir n'est pas cree
 
 	if ! sudo grep "ad_access_filter" /etc/sssd/sssd.conf -q;then
 #		echo "=> Ajout du group autorisee dans /etc/sssd/sssd.conf ..."
@@ -67,4 +67,5 @@ if sudo adcli testjoin >/dev/null;then
 		sudo sed -i "/systemd-journal:/s/$/,$adminGroup@$domainLowercase/" /etc/group
 		grep systemd-journal: /etc/group
 	fi
-fi | tee $logDIR/$HOSTNAME-join-$(date +%Y%m%d).log
+	sudo systemctl restart sssd
+fi 2>&1 | tee -a $logDIR/$HOSTNAME-join-$(date +%Y%m%d).log
