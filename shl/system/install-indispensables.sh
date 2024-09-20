d81df3687dfec182e28789674e1335e90f5148aa
sebma <sebma@users.noreply.github.com>
2024-09-20 21:10:36 +0200

    Updated install-indispensables.sh

--- shl/system/install-indispensables.sh
+++ shl/system/install-indispensables.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-sudo apt install -V apt-file aptitude aria2 bind9-dnsutils curl dconf-editor dfc dlocate fd-find gh git glances htop hub jq lynx mc neofetch plocate ppa-purge pv rclone ripgrep rsync vim w3m wget xclip xsel
+sudo apt install -V apt-file aptitude aria2 bind9-dnsutils curl dconf-editor dfc dlocate fd-find gh git glances htop hub jq lynx mc neofetch plocate ppa-purge pv rclone ripgrep rsync vim w3m wget xclip xsel xxd
 sudo snap install yq
-sudo apt install -V doublecmd-gtk geany gnome-tweaks keepassxc libreoffice
+sudo add-apt-repository ppa:libreoffice/ppa
+sudo apt install -V doublecmd-gtk geany gnome-tweaks keepassxc libreoffice libreoffice-grammalecte libreoffice-style-breeze ttf-mscorefonts-installer
 dpkg -s dra >/dev/null || { wget -c -nv https://github.com/devmatteini/dra/releases/latest/download/dra_0.6.2-1_amd64.deb && sudo apt install -V ./dra_0.6.2-1_amd64.deb && rm ./dra_0.6.2-1_amd64.deb; }
 # sudo apt install -V vlc mpv
 sudo systemctl daemon-reload
