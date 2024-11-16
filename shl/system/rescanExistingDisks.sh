bed0b4b49e6adbc47b8a0ceca1459f7e37e3cd2b
sebma <sebma@users.noreply.github.com>
2024-11-16 11:27:02 +0100

    Updated rescanExistingDisks.sh

--- shl/system/rescanExistingDisks.sh
+++ shl/system/rescanExistingDisks.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "=> Re-scanning existing disks for changes (such as size for virtual disks), just in case ..."
-time echo 1 | $sudo tee /sys/class/block/sd?/device/rescan
+time echo 1 | $sudo tee /sys/class/block/sd?/device/rescan >/dev/null
