a0d1e4dc858734f23249c21f54de493896af6ef1
sebma <sebma@users.noreply.github.com>
2024-11-16 11:16:48 +0100

    Updated lvm_grow.sh

--- shl/system/lvm/lvm_grow.sh
+++ shl/system/lvm/lvm_grow.sh
@@ -4,7 +4,7 @@ set -u
 scriptBaseName=${0/*\//}
 
 if [ $# != 1 ] && [ $# != 2 ];then
-	echo "=> Usage $scriptBaseName filesystemPath [size]" >&2
+	echo "=> USAGE: $scriptBaseName filesystemPath [size]" >&2
 	exit 1
 fi
 
@@ -12,7 +12,7 @@ filesystemPath="$1"
 [ $# == 2 ] && size=$2 || size=-1
 vgName=$(findmnt -no source "$filesystemPath" | awk -F '[/-]' '{print$4}')
 if [ -z "$vgName" ];then
-	echo "=> Could not find the LVM VG for the <$filesystemPath> filesystem."
+	echo "=> ERROR: Could not find the LVM VG for the <$filesystemPath> filesystem."
 	exit 2
 fi
 
@@ -26,20 +26,26 @@ diskSizeBefore=$(cat /sys/block/$disk/size)
 echo "=> Rescan des disques existants ..."
 echo 1 | sudo tee /sys/class/block/sd?/device/rescan >/dev/null
 diskSizeAfter=$(cat /sys/block/$disk/size)
-if [ $diskSizeAfter == $diskSizeBefore ];then
-	echo "=> The $disk disk size has not changed, quitting."
+trap 'echo "=> SIGINT Received, cannot interrupt $scriptBaseName starting from this point, continuing ...";' INT
+set -x
+if [ $diskSizeAfter != $diskSizeBefore ];then
+	echo Fix | sudo parted ---pretend-input-tty $diskDevicePath print free
+	partNum=$(echo $partitionDevicePath | sed "s/.dev.//;s/[a-z]*//")
+	sudo parted -s $diskDevicePath resizepart $partNum 100%
+	sudo pvresize $partitionDevicePath
+fi
+
+vgFree=$(sudo vgs --noheadings $vgName -o vg_free | awk '{print$1}')
+if [ $vgFree == 0 ]
+	echo "=> ERROR: There is not enough free space on the $disk."
 	exit 3
 fi
 
-set -x
-echo Fix | sudo parted ---pretend-input-tty $diskDevicePath print free
-partNum=$(echo $partitionDevicePath | sed "s/.dev.//;s/[a-z]*//")
-sudo parted -s $diskDevicePath resizepart $partNum 100%
-sudo pvresize $partitionDevicePath
-lvName=$(findmnt -no SOURCE "$filesystemPath"" | cut -d- -f2)
+lvName=$(findmnt -no SOURCE "$filesystemPath" | cut -d- -f2)
 echo "=> lvName = $lvName"
 if [ $size == -1 ];then
 	sudo lvextend -r -l +100%FREE /dev/$vgName/$lvName
 else
 	sudo lvextend -r -L +$size /dev/$vgName/$lvName
 fi
+trap - SIGINT
