eab290fa872209076bc028566cce6e7d5daaa241
sebma <sebma@users.noreply.github.com>
2024-11-22 19:48:23 +0100

    Updated shl/test-SSH-Signature.sh

--- shl/test-SSH-Signature.sh
+++ shl/test-SSH-Signature.sh
@@ -14,13 +14,13 @@ if ssh $sshUser@$sshServer 2>&1 | grep 'CHANGED' -q;then
 You need to check with your SFTP service provider if they changed the SSH host key.
 
 Because the SFTP server SSH host key is NOW :
-$(ssh-keyscan -t rsa,ed25519 $sftpServer 2>&1 | grep -v '^#')
+$(ssh-keyscan $sftpServer 2>&1 | grep -v '^#')
 
 If they did change the SFTP server SSH host key, then you need to type these two commands as $USER to update the signature into the <$HOME/.ssh/known_hosts> file on the <$sftpClient> machine :
 
-ssh-keygen -f $HOME/.ssh/known_hosts -R $sftpServer
+ssh-keygen -R $sftpServer
 
-timeout 1s ssh -o StrictHostKeyChecking=accept-new $sftpServer : # Works since OpenSSH 7.6/Ubuntu 18.04
+timeout 1s ssh -o StrictHostKeyChecking=accept-new $sshUser@$sftpServer : # Works since OpenSSH 7.6/Ubuntu 18.04 cf.
 " | mail -s "ERROR: The $sftpServer SFTP server SSH key signature has changed !" -c "$CCs" $Recipients
 	exit 255
 fi
