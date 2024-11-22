3c8033823bf79ab4ee327c0d1b9ffc4035674bc9
sebma <sebma@users.noreply.github.com>
2024-11-22 20:15:41 +0100

    Updated shl/test-SSH-Signature.sh

--- shl/test-SSH-Signature.sh
+++ shl/test-SSH-Signature.sh
@@ -2,25 +2,25 @@
 
 set -u
 
-sshClient=$(hostname -f)
-sshServer=myRemote_SSH_SERVER
-sshUser=my_SSH_User
+sftpClient=$(hostname -f)
+sftpServer=myRemote_SSH_SERVER
+sftpUser=my_SSH_User
 
-ssh $sshUser@$sshServer : >/dev/null
-if ssh $sshUser@$sshServer 2>&1 | grep 'CHANGED' -q;then
+ssh $sftpUser@$sftpServer : >/dev/null
+if ssh $sftpUser@$sftpServer 2>&1 | grep 'CHANGED' -q;then
 	Recipients="myEmail@address.domain"
 	CCs=""
-	echo "The SFTP server SSH host key has changed, you need to update your <$HOME/.ssh/known_hosts> file on the <$sftpClient> machine !
-You need to check with your SFTP service provider if they changed the SSH host key.
+	echo "The <$sftpServer> server SSH host key has changed, you need to update your <$HOME/.ssh/known_hosts> file on the <$sftpClient> machine !
+You need to check with your <$sftpServer> server service provider if they changed the SSH host key.
 
-Because the SFTP server SSH host key is NOW :
+Because the <$sftpServer> server SSH host key is NOW :
 $(ssh-keyscan $sftpServer 2>&1 | grep -v '^#')
 
-If they did change the SFTP server SSH host key, then you need to type these two commands as $USER to update the signature into the <$HOME/.ssh/known_hosts> file on the <$sftpClient> machine :
+If they did change the <$sftpServer> server SSH host key, then you need to type these two commands as $USER to update the signature into the <$HOME/.ssh/known_hosts> file on the <$sftpClient> machine :
 
 ssh-keygen -R $sftpServer
 
-timeout 1s ssh -o StrictHostKeyChecking=accept-new $sshUser@$sftpServer : # Works since OpenSSH 7.6/Ubuntu 18.04 cf. https://serverfault.com/a/903635/312306
-" | mail -s "ERROR: The $sftpServer SFTP server SSH key signature has changed !" -c "$CCs" $Recipients
+timeout 1s ssh -o StrictHostKeyChecking=accept-new $sftpUser@$sftpServer : # Works since OpenSSH 7.6/Ubuntu 18.04 cf. https://serverfault.com/a/903635/312306
+" | mail -s "ERROR: The <$sftpServer> server SSH host key signature has changed !" -c "$CCs" $Recipients
 	exit 255
 fi
