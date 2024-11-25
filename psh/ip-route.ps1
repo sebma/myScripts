d25e24d93820d78aa8373d1a268fa176137e4902
sebma <sebma@users.noreply.github.com>
2024-11-25 14:51:05 +0100

    Update ip-route.ps1

--- psh/ip-route.ps1
+++ psh/ip-route.ps1
@@ -3,12 +3,11 @@ gsudo.exe -u t2-sma netsh interface ip delete destinationcache
 
 echo '=> Printing the route via "route print" simplifed :'
 echo ''
-route -4 print | sls "On-link|0.0.0.0|Gateway" | sls -n "224.0.0.0|255.255.255.255"
+route -4 print | sls "On-link|0.0.0.0|Gateway" | sls -n "224.0.0.0|255.255.255.255|127.0.0.0"
 echo ''
 
 echo '=> Printing default routes via "netsh int ip sh route" ...'
-echo ''
-netsh int ipv4 show route | sls -n "224.0.0.0/4|/32"
+netsh int ipv4 show route | sls -n "224.0.0.0/4|/32|127.0.0.0/8"
 
 echo '=> Printing the route via "Get-NetRoute -AddressFamily IPv4" :'
-Get-NetRoute -AddressFamily IPv4 | select DestinationPrefix,NextHop,InterfaceAlias,ifIndex,InterfaceMetric,RouteMetric | Format-Table | Out-String -stream | sls -n "224.0.0.0/4|/32"
+Get-NetRoute -AddressFamily IPv4 | select DestinationPrefix,NextHop,InterfaceAlias,ifIndex,InterfaceMetric,RouteMetric | Format-Table | Out-String -stream | sls -n "224.0.0.0/4|/32|127.0.0.0/8"
