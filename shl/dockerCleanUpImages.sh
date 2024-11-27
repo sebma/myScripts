ca063829199ede07cb7aa6f4e1abe093930244c1
sebma <sebma@users.noreply.github.com>
2024-11-27 17:09:13 +0100

    Update dockerCleanUpImages.sh

--- shl/dockerCleanUpImages.sh
+++ shl/dockerCleanUpImages.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 test $(id -u) == 0 && sudo="" || sudo=sudo
-$sudo docker images | awk '/^<none>\s+<none>/{printf" "$3}' | $sudo xargs -rt docker rmi
+$sudo docker image ls | awk '/^<none>\s+<none>/{printf" "$3}' | $sudo xargs -rt docker rmi
