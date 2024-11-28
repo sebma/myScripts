46d2c5e8cc18d42604d00c7df2f7841c73aa1253
sebma <sebma@users.noreply.github.com>
2024-11-28 12:04:23 +0100

    Update dockerBuild.sh

--- shl/dockerBuild.sh
+++ shl/dockerBuild.sh
@@ -11,22 +11,32 @@ dockerBuild () {
 
 	if [ $# = 2 ];then
 		imageName="${1,,}"
-        	dockerFile="$2"
-	 	shift 2
+		dockerFile="$2"
+		shift 2
 	elif [ $# = 3 ];then
 		imageName="${1,,}"
-        	dockerFile="$2"
-	 	dir="$3"
-   		shift 3
+		dockerFile="$2"
+		dir="$3"
+		shift 3
 	elif [ $# -lt 2 ];then
 		echo "[function $FUNCNAME] => INFO: Usage: $scriptBaseName imageName dockerFile [dir = .]" >&2
 		return -1
 	fi
 
-	( [ ! -f "$dockerFile" ] || [ ! -d "$dir" ] ) && echo "[$FUNCNAME] => ERROR: <$dockerFile> or <$dir> does not exits.">&2 && return 1
+	if [ ! -f "$dockerFile" ] || [ ! -d "$dir" ];then
+		echo "[$FUNCNAME] => ERROR: <$dockerFile> or <$dir> does not exits.">&2
+		return 1
+	fi
+
 	set -x
-	time $sudo docker build --no-cache -t "$imageName" -f "$dockerFile" "$dir" "$@" && echo && docker images "$imageName" && echo && echo "=> docker run -it -h pc1 --rm $imageName"
-	return $?
+	time $sudo docker build --no-cache -t "$imageName" -f "$dockerFile" "$dir" "$@"
+	retCode=$?
+	set +x
+
+	if [ $retCode == 0 ];then
+		echo && $sudo docker images "$imageName" && echo && echo "=> $sudo docker run -it -h pc1 --rm $imageName"
+	fi
+	return $retCode
 }
 
 dockerBuild "$@"
