11c5f3b34fc0052e02c85e41f5765409524f3aec
sebma <sebma@users.noreply.github.com>
2024-12-13 12:24:17 +0100

    Update dockerBuild.sh

--- shl/dockerBuild.sh
+++ shl/dockerBuild.sh
@@ -6,36 +6,48 @@ test $(id -u) == 0 && sudo="" || sudo=sudo
 
 dockerBuild () {
 	local imageName=""
-	local dockerFile=""
+	local imageRelease="v0.0.1"
+	local dockerFileName=""
 	local dir="."
 
-	if [ $# = 2 ];then
-		imageName="${1,,}"
-		dockerFile="$2"
+	if [ $# = 1 ];then
+		dockerFileName="$1"
+		shift 1
+	elif [ $# = 2 ];then
+		imageRelease="${1,,}"
+		dockerFileName="$2"
 		shift 2
 	elif [ $# = 3 ];then
-		imageName="${1,,}"
-		dockerFile="$2"
+		imageRelease="${1,,}"
+		dockerFileName="$2"
 		dir="$3"
 		shift 3
 	elif [ $# -lt 2 ];then
-		echo "[function $FUNCNAME] => INFO: Usage: $scriptBaseName imageName dockerFile [dir = .]" >&2
+		echo "[function $FUNCNAME] => INFO: Usage: $scriptBaseName [imageRelease=v0.0.1] [dir = .] dockerFileName" >&2
 		return -1
 	fi
 
-	if [ ! -f "$dockerFile" ] || [ ! -d "$dir" ];then
-		echo "[$FUNCNAME] => ERROR: <$dockerFile> or <$dir> does not exits.">&2
+	imageName=${dockerFileName/*\//}
+	imageName=${imageName/Dockerfile/$USER}
+	imageName=${imageName,,}:$imageRelease
+
+	if [ ! -f "$dockerFileName" ] || [ ! -d "$dir" ];then
+		echo "[$FUNCNAME] => ERROR: <$dockerFileName> or <$dir> does not exits.">&2
 		return 1
 	fi
 
-	set -x
-#	time $sudo docker build --no-cache -t "$imageName" -f "$dockerFile" "$dir" "$@"
-	time $sudo docker build -t "$imageName" -f "$dockerFile" "$dir" "$@"
+	if [ -n "$apt_proxy" ];then
+		set -x
+		time $sudo docker build --build-arg apt_proxy=$apt_proxy -t "$imageName" -f "$dockerFileName" "$dir" "$@"
+	else
+		set -x
+		time $sudo docker build -t "$imageName" -f "$dockerFileName" "$dir" "$@"
+	fi
 	retCode=$?
 	set +x
 
 	if [ $retCode == 0 ];then
-		echo && $sudo docker images "$imageName" && echo && echo "=> $sudo docker run -it -h pc1 --rm --network=host $imageName"
+		echo && $sudo docker images "$imageName" && echo && echo "=> $sudo docker run -it -h pc1 --rm --network=host $imageName bash -l"
 	fi
 	return $retCode
 }
