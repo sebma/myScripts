29f97c2d722bf155077096c20b51d0732fa0a3ef
sebma <sebma@users.noreply.github.com>
2024-12-28 18:17:38 +0100

    Updated install-libreoffice-languagues.sh

--- shl/install-libreoffice-languagues.sh
+++ shl/install-libreoffice-languagues.sh
@@ -32,6 +32,7 @@ elif [ $osFamily == Darwin ];then # https://osxdaily.com/2023/10/13/native-macos
 	packageType=mac
 	osFamily=MacOS
 	languagePackFile=LibreOffice_${libreofficeVersion}_${osFamily}_x86-64_langpack_fr.dmg
+	$brew upgrade libreoffice-language-pack
 	wget https://download.documentfoundation.org/libreoffice/stable/$libreofficeVersion/$packageType/x86_64/$languagePackFile
 	:
 fi
