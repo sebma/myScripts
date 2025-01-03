ba778dfe2e187a6c37f171802c4d7c1912318092
sebma <sebma@users.noreply.github.com>
2025-01-03 17:50:05 +0100

    Updated lsPPAs.sh

--- shl/lsPPAs.sh
+++ shl/lsPPAs.sh
@@ -2,7 +2,8 @@
 
 function lsPPAs {
 	releaseCodeName=$(lsb_release -sc 2>/dev/null)
-	awk -F "[ /]" '/^(deb|URIs:) .*launchpad.*.net/{
+	awk -F "[ /]" 'BEGINFILE{if(ERRNO)nextfile}
+	/^(deb|URIs:) .*launchpad.*.net/{
 	print"https://launchpad.net/~"$5"/+archive/ubuntu/"$6" ppa:"$5"/"$6
 }' /etc/apt/sources.list.d/*-$releaseCodeName.list /etc/apt/sources.list.d/*-$releaseCodeName.sources 2>/dev/null | sort -u
 }
