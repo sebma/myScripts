b7ac94be92275af12fdba014530d4112bfc0bae4
sebma <sebma@users.noreply.github.com>
2025-01-03 17:59:50 +0100

    Updated lsNonPPAs.sh

--- shl/lsNonPPAs.sh
+++ shl/lsNonPPAs.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
 function lsNonPPAs {
-	releaseCodeName=$(lsb_release -sc 2>/dev/null)
 	awk -F "[ /]" 'BEGINFILE{if(ERRNO)nextfile}
 	/launchpad.*.net|esm.ubuntu.com|^#|^\s*$/{next}
 	{sub("^.*https?","https")}
-	/\s#/{print$1" # "$NF} !/\s#/{print$1}' /etc/apt/sources.list.d/*-$releaseCodeName.list 2>/dev/null | sort -u
+	/\s#/{print$1" # "$NF} !/\s#/{print$1}' /etc/apt/sources.list.d/*.list 2>/dev/null | sort -u
 }
 
 lsNonPPAs
