6a9535d7371b23a2953aa386c768624ee42fdb05
sebma <sebma@users.noreply.github.com>
2025-01-07 15:04:28 +0100

    Update create_firstboot_service.sh

--- shl/system/create_firstboot_service.sh
+++ shl/system/create_firstboot_service.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 test $(id -u) == 0 && sudo="" || sudo=$(which sudo)
-
-if egrep -i "vmware|virtal" /sys/class/dmi/id/sys_vendor -q;then
+initName=$(ps -p 1 -o comm=)
+if egrep -i "vmware|virtal" /sys/class/dmi/id/sys_vendor -q && [ $initName == systemd ];then
 #	Taken from https://askubuntu.com/a/1327781
 	cat << EOF | $sudo tee /lib/systemd/system/firstboot.service
 [Unit]
