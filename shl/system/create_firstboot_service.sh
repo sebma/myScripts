f6a50a377fd3490495b7482819bb7ebd107bf0a3
sebma <sebma@users.noreply.github.com>
2025-01-07 14:44:29 +0100

    Update create_firstboot_service.sh

--- shl/system/create_firstboot_service.sh
+++ shl/system/create_firstboot_service.sh
@@ -14,34 +14,6 @@ ExecStart=/firstboot.sh
 WantedBy=multi-user.target
 EOF
 
-	cat <<-EOF | $sudo tee /firstboot.sh
-#!/bin/bash
-if egrep -i "vmware|virtal" /sys/class/dmi/id/sys_vendor -q;then
-	if netplan get network | grep ethernets -q;then
-		mkdir -pv /etc/netplan/BACKUP/
-		cp -piv /etc/netplan/00-installer-config.yaml /etc/netplan/BACKUP/00-installer-config-ORIG.yaml <<< n
-		netplan get network.ethernets | awk -F: '/^[^ ]*:$/{print$1}' | while read iface;do
-			echo "=> Removing the IP of $iface network interface ..."
-			netplan set "network.ethernets.$iface.addresses=null"
-		done
-		netplan apply
-	fi
-	echo "=> Re-generating /etc/machine-id and /var/lib/dbus/machine-id ..."
-	rm -f /etc/machine-id
-	dbus-uuidgen --ensure=/etc/machine-id
-	rm -f /var/lib/dbus/machine-id
-	dbus-uuidgen --ensure
-	echo "=> done."
-	echo "=> Re-generating ssh host keys ..."
-	for type in dsa ecdsa ed25519 rsa;do
-		ssh-keygen -q -f /etc/ssh/ssh_host_${type}_key -N '' -t $type <<< y | grep Generating
-	done
-	echo "=> done."
-fi
-systemctl disable firstboot.service
-# systemctl mask firstboot.service
-EOF
-
 	# $sudo systemctl enable firstboot.service # A saisir juste AVANT le clonage de la VM
 	$sudo chmod +x /firstboot.sh
 fi
