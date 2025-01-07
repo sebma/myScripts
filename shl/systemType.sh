362fc92df50c2cf30fd04a6bb42d292f3b8e8248
sebma <sebma@users.noreply.github.com>
2025-01-07 20:26:58 +0100

    Updated shl/systemType.sh

--- shl/systemType.sh
+++ shl/systemType.sh
@@ -13,8 +13,7 @@ fi
 function systemType {
 	local ps=unset
 	ps --help 2>&1 | $grep -q "error while loading shared libraries" && ps="busybox ps" || ps="command ps"
- 	local initName=$(ps -p 1 -o comm=)
-	local initName=$($ps -e -o comm= -o pid= | $grep "  *1$" | $cut -d" " -f1)
+ 	local initName=$($ps -p 1 -o comm=)
 	if [ -n "$initName" ];then
 		initPath=$(type -P $initName) #Needed for gentoo and maybe others
 		$strings $initPath | $egrep -o "upstart|sysvinit|systemd|launchd" | $head -1
