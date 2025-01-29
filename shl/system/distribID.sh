da9c5ee2b706c0be9e00226fa1dd5c167ce8f860
sebma <sebma@users.noreply.github.com>
2025-01-29 20:57:02 +0100

    Updated distribID.sh

---
+++ shl/system/distribID.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+scriptBaseName=${0/*\//}
+if [ $# != 0 ];then
+	echo "=> Usage $scriptBaseName" >&2
+	exit 1
+fi
+test $(id -u) == 0 && sudo="" || sudo=$(type -P sudo)
+
+declare -x {isDebian,isRedHat,isAlpine}Like=false
+distribID=$(source /etc/os-release;echo $ID)
+if   echo $distribID | egrep "centos|rhel|fedora" -q;then
+	isRedHatLike=true
+elif echo $distribID | egrep "debian|ubuntu" -q;then
+	isDebianLike=true
+elif echo $distribID | egrep "alpine" -q;then
+	isAlpineLike=true
+fi
+
+echo $distribID
