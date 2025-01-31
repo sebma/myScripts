43b942722c9a4e7953a81f7686ed9a6088332551
sebma <sebma@users.noreply.github.com>
2025-01-31 11:03:54 +0100

    Update interpreter.sh

--- shl/interpreter.sh
+++ shl/interpreter.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env zsh
 
 get_interpreter() {
-	interpreter=$(ps -o args= $$ | awk '{print sub("^/.*/","");print}')
+	if which ps >/dev/null 2>&1;then
+ 		interpreter=$(ps -o args= $$ | awk '{print sub("^/.*/","");print}')
+   	else
+    		interpreter=$(readlink -e /proc/$$/exe)
+		interpreter=$(basename $interpreter)
+    	fi
 	echo $interpreter  
 }
 
