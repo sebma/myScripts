e3f5dc762c2b83322137a3dc125698992ce11d11
sebma <sebma@users.noreply.github.com>
2025-02-01 16:55:58 +0100

    Updated interpreter.sh

--- shl/interpreter.sh
+++ shl/interpreter.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env sh
 
-get_interpreter() {
-	interpreter=$(readlink -f /proc/$$/exe)
-	interpreter=$(basename $interpreter)
-	echo $interpreter
-}
-
-interpreter=$(get_interpreter)
+interpreter=$(readlink -f /proc/$$/exe)
+interpreter=$(basename $interpreter)
 echo "=> interpreter = <$interpreter>"
