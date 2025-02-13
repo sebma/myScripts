d4a1bf7c00cd9f5ee3134055c3f60f652f80b5c2
sebma <sebma@users.noreply.github.com>
2025-02-13 22:19:35 +0100

    Updated distribID.sh

--- shl/system/distribID.sh
+++ shl/system/distribID.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+set -eu
 scriptBaseName=${0/*\//}
+scriptDirName=${0%/*}
+
 if [ $# != 0 ];then
 	echo "=> Usage $scriptBaseName" >&2
 	exit 1
@@ -8,13 +11,28 @@ fi
 test $(id -u) == 0 && sudo="" || sudo=$(type -P sudo)
 
 declare -x {isDebian,isRedHat,isAlpine}Like=false
-distribID=$(source /etc/os-release;echo $ID)
-if   echo $distribID | egrep "centos|rhel|fedora" -q;then
-	isRedHatLike=true
-elif echo $distribID | egrep "debian|ubuntu" -q;then
-	isDebianLike=true
-elif echo $distribID | egrep "alpine" -q;then
-	isAlpineLike=true
+
+ID_LIKE=""
+ID_LIKE=$(source /etc/os-release;echo $ID_LIKE)
+if [ -n "$ID_LIKE" ];then
+	if   echo $ID_LIKE | egrep "rhel|centos|fedora" -q;then
+		isRedHatLike=true
+	elif echo $ID_LIKE| egrep "debian|ubuntu" -q;then
+		isDebianLike=true
+	elif echo $ID_LIKE| egrep "alpine" -q;then
+		isAlpineLike=true
+	fi
+	distribID=$ID_LIKE
+else
+	ID=$(source /etc/os-release;echo $ID)
+	if   echo $ID_LIKE | egrep "rhel|centos|fedora" -q;then
+		isRedHatLike=true
+	elif echo $ID_LIKE| egrep "debian|ubuntu" -q;then
+		isDebianLike=true
+	elif echo $ID_LIKE| egrep "alpine" -q;then
+		isAlpineLike=true
+	fi
+	distribID=$ID
 fi
 
 echo $distribID
