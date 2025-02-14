43d3aa4cb328dac5cc56d084b91771c7da2755ad
sebma <sebma@users.noreply.github.com>
2025-02-14 17:45:38 +0100

    Update taskList.ps1

--- psh/taskList.ps1
+++ psh/taskList.ps1
@@ -1,2 +1,3 @@
 #schtasks | sls -n "^TaskName|N/A|=======" | Out-String -Stream | sls -Context 1,0 $(get-date).year
+#Get-ScheduledTask | Get-ScheduledTaskInfo  | Sort-Object Nextruntime | select Taskname, LastRunTime, NextRunTime | Out-GridView
 Get-ScheduledTask | Get-ScheduledTaskInfo  | Sort-Object Nextruntime | select Taskname, LastRunTime, NextRunTime
