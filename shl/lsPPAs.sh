fbf686acb04bfd6ec18d7a8a08e051b298f740af
sebma <sebma@users.noreply.github.com>
2025-02-17 21:51:10 +0100

    Updated lsPPAs.sh

--- shl/lsPPAs.sh
+++ shl/lsPPAs.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 function lsPPAs {
-	releaseCodeName=$(lsb_release -sc 2>/dev/null)
-	sourcesDisabledFileList=$(cd /etc/apt/sources.list.d/;grep Enabled:.no *-$releaseCodeName.sources -l | paste -sd"|" -)
-	sourcesEnabledFileList=$(ls /etc/apt/sources.list.d/*-$releaseCodeName.sources | egrep -v "$sourcesDisabledFileList")
+	local releaseCodeName=$(lsb_release -sc 2>/dev/null)
+	local sourcesDisabledFileList=$({ cd /etc/apt/sources.list.d/;grep Enabled:.no *-$releaseCodeName.sources -l || echo '^$'; } | paste -sd"|" -;)
+	local sourcesEnabledFileList=$(ls /etc/apt/sources.list.d/*-$releaseCodeName.sources | egrep -v "$sourcesDisabledFileList")
 	awk -F "[ /]" 'BEGINFILE{if(ERRNO)nextfile}
 		/^(deb|URIs:) .*launchpad.*.net/{
 		print"https://launchpad.net/~"$5"/+archive/ubuntu/"$6" ppa:"$5"/"$6
