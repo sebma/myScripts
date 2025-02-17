3484125e6d7e097cd1fbbf13a88e71e63ef8ccc9
sebma <sebma@users.noreply.github.com>
2025-02-17 22:13:08 +0100

    Updated mpv.sh

--- shl/mpv.sh
+++ shl/mpv.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 mpv=$(which mpv)
-cd ~/Pictures || cd
-#HOSTNAME=$(hostname) nohup $mpv "$@" &
-HOSTNAME=$HOSTNAME nohup $mpv "$@" &
+logDIR=$HOME/log/mpv
+timestamp=$(date +%Y%m%d-%HH%M)
+mkdir -pv $logDIR/
+#HOSTNAME=$(hostname) nohup $mpv "$@" >$logDIR/mpv-$timestamp.log 2>&1 &
+HOSTNAME=$HOSTNAME nohup $mpv "$@" >$logDIR/mpv-$timestamp.log 2>&1 &
