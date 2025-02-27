7684ff2986b2fee6a219b9ea2fba26a0b81b223e
sebma <sebma@users.noreply.github.com>
2025-02-27 10:57:50 +0100

    Update host.ps1

--- psh/host.ps1
+++ psh/host.ps1
@@ -3,12 +3,22 @@ function main {
 	if ( $argc -eq 0 ) {
 		echo USAGE
 	} elseif ( $argc -eq 1 ) {
-		$fqdn = $args[0].TrimStart('https?://').TrimEnd('/.*')
+		$fqdn = $args[0].TrimStart('https?://') -Replace('/.*$','')
 		Resolve-DnsName $fqdn
 	} elseif ( $argc -eq 2 ) {
-		$fqdn = $args[0].TrimStart('https?://').TrimEnd('/.*')
-		$server = $args[1]
-		Resolve-DnsName -name $args[0] -server $server
+		if ( $args[0] -eq "-4" ) {
+			$type = "A"
+			$fqdn = $args[1].TrimStart('https?://') -Replace('/.*$','')
+		} elseif ( $args[0] -eq "-6" ) {
+			$type = "AAAA"
+			$fqdn = $args[1].TrimStart('https?://') -Replace('/.*$','')
+			$server = $args[2]
+		} else {
+			$type = "A_AAAA"
+			$fqdn = $args[0].TrimStart('https?://') -Replace('/.*$','')
+			$server = $args[1]
+		}
+		Resolve-DnsName -type $type -name $fqdn -server $server
 	} else {
 		echo WHATEVER
 	}
