a1c51faeb26e6de0c141bae934def2137d3b24c2
sebma <sebma@users.noreply.github.com>
2025-03-04 15:11:23 +0100

    Update findUser.ps1

--- psh/findUser.ps1
+++ psh/findUser.ps1
@@ -1,4 +1,5 @@
 $myPattern = '*'+$args[0]+'*'
 $DC = $env:LOGONSERVER.Substring(2)
-Get-ADUser -Server $DC -Properties CN , CanonicalName , Created , Description , EmailAddress , Enabled , LastLogonDate, LockedOut, msDS-UserPasswordExpiryTimeComputed , PasswordExpired , PasswordLastSet , PasswordNeverExpires , SamAccountName , UserPrincipalName -Filter { name -like $myPattern }
+# ` is used for Newline escape
+Get-ADUser -Server $DC -Properties CN , CanonicalName , Created , Description , EmailAddress , Enabled , LastLogonDate, LockedOut, msDS-UserPasswordExpiryTimeComputed , PasswordExpired , PasswordLastSet , PasswordNeverExpires , SamAccountName , UserPrincipalName -Filter { name -like $myPattern -or (SamAccountName -like $myPattern) } `
 | select CN , CanonicalName , Created , Description , DistinguishedName , EmailAddress , Enabled , LastLogonDate, LockedOut, @{name="PasswordExpiryDate";expression={ [datetime]::fromfiletime($_."msDS-UserPasswordExpiryTimeComputed") } } , PasswordExpired , PasswordLastSet , PasswordNeverExpires , SamAccountName , UserPrincipalName
