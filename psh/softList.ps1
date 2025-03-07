9cdbb31c640cd19461ffbd7b009251695348335c
sebma <sebma@users.noreply.github.com>
2025-03-07 11:33:35 +0100

    Update softList.ps1

--- psh/softList.ps1
+++ psh/softList.ps1
@@ -1,3 +1,4 @@
 #gwmi Win32_Product | ? Name -notMatch "update|microsoft" | Select Name , Version , InstallDate | Sort Name | Format-Table
 #gci HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall | % { gp $_.PsPath } | ? Displayname -notMatch "update|microsoft" | Select Displayname , DisplayVersion , InstallDate | Sort -u Displayname | Format-Table | Out-String -Stream | % { $_.Trim() }
+#gp HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | ? Displayname -notMatch "update|microsoft" | Select Displayname , DisplayVersion , InstallDate | Sort -u Displayname | Format-Table
 Get-Package | ? Name -notMatch "update|microsoft" | Select Name , Version | Sort Name | Format-Table -AutoSize
