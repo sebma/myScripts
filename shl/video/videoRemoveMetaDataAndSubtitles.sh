eca8db32b8fd2e3ecd89ed3ba6e72bd53988a34c
sebma <sebma@users.noreply.github.com>
2025-03-11 20:26:19 +0100

    Updated videoRemoveMetaDataAndSubtitles.sh

--- shl/video/videoRemoveMetaDataAndSubtitles.sh
+++ shl/video/videoRemoveMetaDataAndSubtitles.sh
@@ -3,6 +3,7 @@
 function videoRemoveMetaDataAndSubtitles {
 	local extension outputVideo
 	local ffmpeg="command ffmpeg -hide_banner -probesize 400M -analyzeduration 400M"
+	local ffprobe="command ffprobe -hide_banner -probesize 400M -analyzeduration 400M"
 	for video
 	do
 		extension="${video/*./}"
