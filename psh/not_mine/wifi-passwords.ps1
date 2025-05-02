c05309759ef22ab56e97eaf205beb3ccdb975ab2
sebma <sebma@users.noreply.github.com>
2025-05-02 11:19:37 +0200

    Updated psh/not_mine/*

--- psh/not_mine/wifi-passwords.ps1
+++
@@ -1,3 +0,0 @@
-# https://stackoverflow.com/a/74034694/5649639
-# Taken from https://gist.github.com/CybersamuraiDK/6e0be5c0c47165228895079efa8d98ec
-(netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Format-Table -AutoSize
