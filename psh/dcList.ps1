949474616a929022c1ccecf2292cec0d761dd70a
sebma <sebma@users.noreply.github.com>
2025-06-05 15:04:59 +0200

    Update dcList.ps1

--- psh/dcList.ps1
+++ psh/dcList.ps1
@@ -1,12 +1,13 @@
 $scriptName = Split-Path -Leaf $PSCommandPath
 function dcList {
 	$argc=$args.Count
+	$defaultDomainController = (Get-ADDomainController -Discover).Name
 	if ( $argc -eq 0 ) {
-		$server = (Get-ADDomainController -Discover).Name
-	} else if ( $argc -eq 1 ) {
+		$server = $defaultDomainController
+	} elseif ( $argc -eq 1 ) {
 		$server = $args[0]
 	} else {
-		write-warning "Usage:$scriptName [dirName=.]"
+		write-warning "Usage:$scriptName [server=$defaultDomainController]"
 		exit 1
 	}
 	Get-ADDomainController -Filter '*' -Server $server | Select Hostname , IPv4Address , OperatingSystem , Site , IsReadOnly | sort Hostname | Format-Table
