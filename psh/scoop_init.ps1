f6ab1addf693acc48c93950c3f48421d166026a8
sebma <sebma@users.noreply.github.com>
2025-06-25 16:33:26 +0200

    Create scoop_init.ps1

---
+++ psh/scoop_init.ps1
@@ -0,0 +1,6 @@
+scoop config aria2-retry-wait 1
+scoop config aria2-options --max-concurrent-downloads=10
+scoop config aria2-min-split-size 1M
+scoop config aria2-max-connection-per-server 20
+scoop config aria2-enabled false
+scoop config use_sqlite_cache true
