3753f24c6edc83071d6e505dfc3ef319ee26b106
sebma <sebma@users.noreply.github.com>
2025-06-25 16:34:53 +0200

    Update scoop_init.ps1

--- psh/scoop_init.ps1
+++ psh/scoop_init.ps1
@@ -4,3 +4,4 @@ scoop config aria2-min-split-size 1M
 scoop config aria2-max-connection-per-server 20
 scoop config aria2-enabled false
 scoop config use_sqlite_cache true
+cat $HOME/.config/scoop/config.json
