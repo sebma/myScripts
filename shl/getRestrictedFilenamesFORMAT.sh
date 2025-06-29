8a8a9489e5ccefb3c67a06468838586a77ae5d0c
sebma <sebma@users.noreply.github.com>
2025-06-29 22:24:59 +0200

    Updated getRestrictedFilenamesFORMAT.sh

--- shl/getRestrictedFilenamesFORMAT.sh
+++ shl/getRestrictedFilenamesFORMAT.sh
@@ -310,9 +310,13 @@ function getRestrictedFilenamesFORMAT() {
 			# Les resultats ci-dessous ne dependent pas du format selectionne
 			isLIVE=$(echo "$jsonHeaders" | $jq -n -r 'first(inputs | .is_live)'| sed "s/null/false/")
 
-			title=$(echo "$jsonHeaders" | $jq -n -r 'first(inputs | .title)' | sed 's/\xf0\x9f\x9f\xa2 \s//')
+			# printf "\x1b\x5b\x30\x6d\x1b\x5b\x30\x31\x3b\x33\x35\x6d\xf0\x9f\x9f\xa2 " | \hexdump -C
+			$isLIVE && echo && echo -e "=> title = <$title>" && echo && printf "$title" | cut -c 1-20 | hexdump -C
 #			title=$(echo "$jsonHeaders" | $jq -n -r 'first(inputs | .title)' )
-			ytdlExtraOptions+=( --replace-in-metadata "title" r'\xf0\x9f\x9f\xa2\s' "" )
+			title=$(echo "$jsonHeaders" | $jq -n -r 'first(inputs | .title)' | perl -C -pe 's/[\x{1f300}-\x{1f5ff}\x{1f900}-\x{1f9ff}\x{1f600}-\x{1f64f}\x{1f680}-\x{1f6ff}\x{2600}-\x{26ff}\x{2700}-\x{27bf}\x{1f1e6}-\x{1f1ff}\x{1f191}-\x{1f251}\x{1f004}\x{1f0cf}\x{1f170}-\x{1f171}\x{1f17e}-\x{1f17f}\x{1f18e}\x{3030}\x{2b50}\x{2b55}\x{2934}-\x{2935}\x{2b05}-\x{2b07}\x{2b1b}-\x{2b1c}\x{3297}\x{3299}\x{303d}\x{00a9}\x{00ae}\x{2122}\x{23f3}\x{24c2}\x{23e9}-\x{23ef}\x{25b6}\x{23f8}-\x{23fa}\x{1f7e2}]\s*//g') # cf. https://stackoverflow.com/a/58422821/5649639
+			$isLIVE && echo && echo -e "=> title = <$title>" && echo && printf "$title" | cut -c 1-20 | hexdump -C
+
+			ytdlExtraOptions+=( --replace-in-metadata "video:title" " ?[\U00002B07-\U00002E7F]+" "" --replace-in-metadata "video:title" " ?[\U0000U+2FF0-\U0000U+2FFF]+" "" --replace-in-metadata "video:title" " ?[\U00002B00-\U0000DFFF]+" "" --replace-in-metadata "video:title" " ?[\U0000E000-\U0000F8FF]+" "" --replace-in-metadata "video:title" " ?[\U0000FE00-\U0000FE2F]+" "" --replace-in-metadata "video:title" " ?[\U0000FF00-\U0000FFFF]+" "" --replace-in-metadata "video:title" " ?[\U00010B40-\U0010ffff]+" "" ) # cf. https://github.com/yt-dlp/yt-dlp/issues/3047#issuecomment-2587024675
 
 			webpage_url=$(echo "$jsonHeaders" | $jq -n -r 'first(inputs | .webpage_url)')
 			duration=$(echo "$jsonHeaders" | $jq -n -r 'first(inputs | .duration)' | sed "s/null/0/")
