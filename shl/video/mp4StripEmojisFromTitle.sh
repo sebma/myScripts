145a4b32fc39d6f01caa655bd589d0b9e778d1f2
sebma <sebma@users.noreply.github.com>
2025-07-02 19:51:03 +0200

    Updated mp4StripEmojisFromTitle.sh

--- shl/video/mp4StripEmojisFromTitle.sh
+++ shl/video/mp4StripEmojisFromTitle.sh
@@ -9,22 +9,17 @@ mp4StripEmojisFromTitle ()
 
 	local ffprobe="command ffprobe -hide_banner"
 
+	echo >&2
 	echo "=> Starting $FUNCNAME $@ ..." >&2
 	local emojiRegExp='[\x{1f300}-\x{1f5ff}\x{1f900}-\x{1f9ff}\x{1f600}-\x{1f64f}\x{1f680}-\x{1f6ff}\x{2600}-\x{26ff}\x{2700}-\x{27bf}\x{1f1e6}-\x{1f1ff}\x{1f191}-\x{1f251}\x{1f004}\x{1f0cf}\x{1f170}-\x{1f171}\x{1f17e}-\x{1f17f}\x{1f18e}\x{3030}\x{2b50}\x{2b55}\x{2934}-\x{2935}\x{2b05}-\x{2b07}\x{2b1b}-\x{2b1c}\x{3297}\x{3299}\x{303d}\x{00a9}\x{00ae}\x{2122}\x{23f3}\x{24c2}\x{23e9}-\x{23ef}\x{25b6}\x{23f8}-\x{23fa}\x{1f7e2}]'
 
-	for mp4File in "$@"
+	for mp4File
 	do
-		echo "==> Processing $mp4File ..." >&2
-		set -o pipefail
+		echo >&2
+		echo "==> Processing <$mp4File> ..." >&2
 		title=$(bash -c "ffprobe -v error -show_format -show_streams -of json \"$mp4File\"" | jq -r .format.tags.title | sed "s/null//")
-		set +o pipefail
 
-		echo "==> title = <$title>"
-		if test -z "$title";then
-			echo "==> The title is empty, processing next file (if any) ..." >&2
-			echo >&2
-			continue
-		fi
+		test -z "$title" && echo "==> The is no title for this video, processing next file (if any) ..." >&2 && continue
 
 		if perl -e "use utf8; exit 0 if '$title' =~ /$emojiRegExp/; exit 1;";then
 			echo "==> Emojis were found in this title: <$title>." >&2
@@ -43,7 +38,6 @@ mp4StripEmojisFromTitle ()
 		touch -r $timestampFileRef "$mp4File" && \rm $timestampFileRef
 		chmod -v -w "$mp4File"
 		echo "==> DONE processing $mp4File." >&2
-		echo >&2
 	done
 	echo "=> Finished !" >&2
 }
