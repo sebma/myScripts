0d004486e881c2b229eb37a8017a211444a17f5d
sebma <sebma@users.noreply.github.com>
2025-07-03 16:36:49 +0200

    Update scoopPostInstall.ps1

--- psh/scoopPostInstall.ps1
+++ psh/scoopPostInstall.ps1
@@ -3,14 +3,14 @@ function isInstalled($cmd) { return gcm "$cmd" 2>$null }
 if( $IsWindows ) {
 	function scoopPostInstall {
 		if( ! (isInstalled("git.exe")) ) {
-			sudo scoop install -g git
+			sudo scoop install git -g
 		}
 		if( ( git config --global credential.helper ) -ne "manager-core" ) {
 			sudo git config --global credential.helper manager-core # i.e https://github.com/ScoopInstaller/Main/blob/master/bucket/git.json
 		}
 
 		if( ! (sudo scoop bucket list | sls extras) ) { sudo scoop bucket add extras }
-		sudo scoop install -g freetube kitty gow pshazz openssh psutils wget gsudo
+		sudo scoop install freetube kitty gow pshazz openssh psutils wget gsudo -g
 		sudo scoop shim rm putty plink pscp psftp peagent
 		sudo -k
 	}
