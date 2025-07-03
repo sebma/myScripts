fab46944e3bdbb397515feabd4fc21909dd024f6
sebma <sebma@users.noreply.github.com>
2025-07-03 21:07:34 +0200

    Updated videoResetExtension.sh

--- shl/video/videoResetExtension.sh
+++ shl/video/videoResetExtension.sh
@@ -16,7 +16,7 @@ videoResetExtension ()
 	do
 		echo >&2
 		echo "==> Processing <$videoFile> ..." >&2
-		format_name=$(bash -c "ffprobe -v error -show_format_name -show_streams -of json \"$videoFile\"" | jq -r .format_name.format_name | sed "s/null//")
+		format_name=$(bash -c "ffprobe -v error -show_format_name -show_streams -of json \"$videoFile\"" | jq -r .format.format_name | sed "s/null//")
 
 		test -z "$format_name" && echo "==> The is no format_name for this video, processing next file (if any) ..." >&2 && continue
 		extension="${videoFile/*./}"
