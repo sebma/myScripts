f4cd8d887dc48ec0cd5e6a96e064a1b3e35c7a70
sebma <sebma@users.noreply.github.com>
2025-07-03 21:17:21 +0200

    Updated videoResetExtension.sh

--- shl/video/videoResetExtension.sh
+++ shl/video/videoResetExtension.sh
@@ -21,16 +21,16 @@ videoResetExtension ()
 
 		test -z "$format_name" && echo "==> The is no format_name for this video, processing next file (if any) ..." >&2 && continue
 
-		echo "==> format_name = <$format_name>"
+		echo "==> format_name = <$format_name>" >&2
 		case $format_name in
 			"mov,mp4,m4a,3gp,3g2,mj2") newExtension=mp4;;
 			mpegts) newExtension=m2ts;;
 			*) echo "==> This format is not supported yet." >&2; continue;;
 		esac
-		echo "==> newExtension = <$newExtension>"
+		echo "==> newExtension = <$newExtension>" >&2
 
 		extension="${videoFile/*./}"
-		echo "==> extension = <$extension>"
+		echo "==> extension = <$extension>" >&2
 
 		if [ $newExtension != $extension ];then
 			echo "==> Renamming the video according to its format_name which is <$format_name>." >&2
