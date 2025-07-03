d267da13a3755ec63c25d6035e6fce51f20cd63c
sebma <sebma@users.noreply.github.com>
2025-07-03 07:59:32 +0200

    Updated ffprobe2JSON.sh

--- shl/ffprobe2JSON.sh
+++ shl/ffprobe2JSON.sh
@@ -5,8 +5,9 @@ ffprobe2JSON ()
 	local ffprobe="command ffprobe -hide_banner"
 	local ffprobeUserAgentOption="-user_agent \"$userAgent\""
 	local ffprobeOptions="-v error -show_format -show_streams -of json"
-	for arg in "$@"
+	for arg
 	do
+		echo "=> arg = <$arg>." >&2
 		echo "$arg" | \egrep -q "(https?|s?ftps?|ssh|rtmp|rtsp|mms)://" && $ffprobe $ffprobeUserAgentOption $ffprobeOptions "$(yt-dlp --ignore-config -g -- "$arg")" || $ffprobe $ffprobeOptions "$arg"
 	done
 }
