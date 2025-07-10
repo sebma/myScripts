caa7cf1471cd0bb72eee6d5a10580327ac1e1bae
sebma <sebma@users.noreply.github.com>
2025-07-10 14:34:35 +0200

    Update pingMyLAN.sh

--- shl/pingMyLAN.sh
+++ shl/pingMyLAN.sh
@@ -5,7 +5,7 @@ pingMyLAN ()
 	local myOutgoingInterFace=$(ip route | awk '/default/{print$5}')
 	local myLAN=$(ip -4 addr show dev $myOutgoingInterFace scope global up | awk -F " *|/" '/inet/{print$3}')
  	local prefix=$(ip -4 addr show dev $myOutgoingInterFace scope global up | awk -F " *|/" '/inet/{print$4}')
-  	local lanPrefix=""
+  	local lanPrefix=$(echo $myLAN | cut -d. -f1-3)
  	local nbHost=$((2**(32-$prefix)))
 	if [ $# = 0 ]; then
 		if type -P fping > /dev/null 2>&1; then
@@ -13,7 +13,6 @@ pingMyLAN ()
 		elif type -P nmap > /dev/null 2>&1; then
 			time \nmap -T5 -sP $myLAN/$prefix | sed -n '/Nmap scan report for /s/Nmap scan report for //p'
    		else
-     			lanPrefix=$(echo $myLAN | cut -d. -f1-3)
      			time for i in $(seq $nbHost); do ping $lanPrefix.$i -c1 -W1 & done | grep from
 		fi
 	elif [ $# = 1 ]; then
