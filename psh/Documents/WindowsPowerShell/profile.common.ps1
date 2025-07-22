bebc0788fb1cdd415523f5fad1e6f3ade5c05e4a
sebma <sebma@users.noreply.github.com>
2025-07-22 17:14:51 +0200

    Update profile.common.ps1

--- psh/Documents/WindowsPowerShell/profile.common.ps1
+++ psh/Documents/WindowsPowerShell/profile.common.ps1
@@ -1,3 +1,35 @@
+function osFamily {
+	if( !(Test-Path variable:IsWindows) ) {
+		# $IsWindows is not defined, let's define it
+		$platform = [System.Environment]::OSVersion.Platform
+		$IsWindows = $IsLinux = $IsMacOS = $false
+		$osFamily = "not defined yet"
+		$IsWindows = $platform -eq "Win32NT"
+		if( $isWindows ) {
+			$osFamily = "Windows"
+		} elseif( $platform -eq "Unix" ) {
+			$osFamily = (uname -s)
+			if( $osFamily -eq "Linux") {
+				$IsLinux = $true
+			} elseif( $osFamily -eq "Darwin" ) {
+				$IsMacOS = $true
+			} else {
+				$osFamily = "NOT_SUPPORTED"
+			}
+		} else {
+			$osFamily = "NOT_SUPPORTED"
+		}
+	} else {
+		#Using PSv>5.1 where these variables are already defined
+		if( $IsWindows )   { $osFamily = "Windows" }
+		elseif( $IsLinux ) { $osFamily = "Linux" }
+		elseif( $IsMacOS ) { $osFamily = "Darwin" }
+		else { $osFamily = "NOT_SUPPORTED" }
+	}
+
+	return $IsWindows, $IsLinux, $IsMacOS, $osFamily
+}
+
 function pow2($a,$n) {
 	return [math]::pow($a,$n)
 }
