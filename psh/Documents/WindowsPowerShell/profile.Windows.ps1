533e58dd753539ce4a30a587aa7ccc815102dac5
sebma <sebma@users.noreply.github.com>
2025-07-22 17:12:05 +0200

    Update profile.Windows.ps1

--- psh/Documents/WindowsPowerShell/profile.Windows.ps1
+++ psh/Documents/WindowsPowerShell/profile.Windows.ps1
@@ -78,3 +78,8 @@ function main {
 
 #time main
 main
+
+if( isInstalled("choco") ) {
+	"=> Sourcing Chocolatey functions ..."
+	. $profileDIR/profile.choco.ps1 # Ne peut pas etre mis dans la fonction "main", sinon les definitions seront locales
+}
