80b180bd0e8a445f0d394f0cf55cf9faa6139472
sebma <sebma@users.noreply.github.com>
2025-07-25 20:24:01 +0200

    Update findComputer.ps1

--- psh/findComputer.ps1
+++ psh/findComputer.ps1
@@ -1,11 +1,15 @@
 $DC = $env:LOGONSERVER.Substring(2)
 "=> LOGONSERVER = $DC"
-
-$firstArg = $args[0]
-if( ! $firstArg.Contains("*") ) {
-	$myPattern = '*'+$firstArg+'*'
+$argc = $args.Count
+if( $argc -eq 0 ) {
+	$myPattern = $env:COMPUTERNAME
 } else {
-	$myPattern = $firstArg
+	$firstArg = $args[0]
+	if( ! $firstArg.Contains("*") ) {
+		$myPattern = '*'+$firstArg+'*'
+	} elseif( $firstArg ) {
+		$myPattern = $firstArg
+	}
 }
 
 "=> myPattern = $myPattern"
