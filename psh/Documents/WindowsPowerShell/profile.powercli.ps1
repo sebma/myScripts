f51153d83ec456f8c5ad12d9047941274d584500
sebma <sebma@users.noreply.github.com>
2025-08-01 12:10:47 +0200

    Create profile.powercli.ps1

---
+++ psh/Documents/WindowsPowerShell/profile.powercli.ps1
@@ -0,0 +1,19 @@
+function diskEnableUUID($vm) {
+	if( $vm ) {
+		Get-AdvancedSetting -Entity $vm -Name "disk.EnableUUID"
+		New-AdvancedSetting -Entity $vm -Name "disk.EnableUUID" -Value "TRUE" -Confirm:$false
+		Get-AdvancedSetting -Entity $vm -Name "disk.EnableUUID"
+	}
+}
+function enableVMXNET3($vm) {
+	if( $vm ) {
+		$nic = Get-NetworkAdapter -VM $vm
+		$nic
+		Set-NetworkAdapter -NetworkAdapter $nic -Type "vmxnet3" -Confirm:$false
+		$nic
+	}
+}
+
+#$vm = Get-VM "Virtual Machine Name"
+#Connect-VIServer -Menu
+#Disconnect-VIServer -Confirm:$false
