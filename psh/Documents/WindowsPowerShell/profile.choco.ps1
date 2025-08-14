c4e03485510021a8ee5ca160ebaa1c963774433d
sebma <sebma@users.noreply.github.com>
2025-08-14 19:11:38 +0200

    Update profile.choco.ps1

--- psh/Documents/WindowsPowerShell/profile.choco.ps1
+++ psh/Documents/WindowsPowerShell/profile.choco.ps1
@@ -1,7 +1,9 @@
-Set-Variable -Scope global shimgen "$env:ChocolateyInstall\tools\shimgen.exe"
+"=> Sourcing Chocolatey functions ..."
+$global:shimgen = "$env:ChocolateyInstall\tools\shimgen.exe"
 function cfind { choco find @args }
 function chome { Start-Process $(choco info @args | sls "Site:").Line.Split()[-1] }
 function cinfo { choco info @args }
 function clistlocal { choco list -l @args | sort }
 function coutdated { choco outdated @args }
 function csearch { choco search @args | sort }
+
