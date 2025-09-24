9cf13e4b2afc4788579b39737f851685af201060
sebma <sebma@users.noreply.github.com>
2025-09-24 19:47:38 +0200

    Updated grub-Add-Keyboard-Layout.sh

--- shl/system/grub/grub-Add-Keyboard-Layout.sh
+++ shl/system/grub/grub-Add-Keyboard-Layout.sh
@@ -9,8 +9,9 @@ fi
 countryKeyCode=$1
 test $(id -u) == 0 && sudo="" || sudo=sudo
 $sudo mkdir -v /boot/grub/layouts
-$sudo grub-kbdcomp -o /boot/grub/layouts/$countryKeyCode.gkb $countryKeyCode
 
-grep "insmod keylayouts" -q /etc/grub.d/40_custom || echo "insmod keylayouts" | $sudo tee -a /etc/grub.d/40_custom >/dev/null
-grep $countryKeyCode -q /etc/grub.d/40_custom || echo "keymap $countryKeyCode" | $sudo tee -a /etc/grub.d/40_custom >/dev/null
-$sudo update-grub
+if $sudo grub-kbdcomp -o /boot/grub/layouts/$countryKeyCode.gkb $countryKeyCode;then
+	grep "insmod keylayouts" -q /etc/grub.d/40_custom || echo "insmod keylayouts" | $sudo tee -a /etc/grub.d/40_custom >/dev/null
+	grep $countryKeyCode -q /etc/grub.d/40_custom || echo "keymap $countryKeyCode" | $sudo tee -a /etc/grub.d/40_custom >/dev/null
+	$sudo update-grub
+fi
