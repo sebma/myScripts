1369e1858e5a68cc6e8327340f31c3501278c381
sebma <sebma@users.noreply.github.com>
2025-09-25 14:50:04 +0200

    Update aliases.Windows.ps1

--- psh/Documents/WindowsPowerShell/aliases.Windows.ps1
+++ psh/Documents/WindowsPowerShell/aliases.Windows.ps1
@@ -8,6 +8,8 @@ if ( $(alias history *>$null;$?) ) { del alias:history }
 #if ( $(alias ls *>$null;$?) ) { del alias:ls }
 #set-alias -Scope Global more less.exe
 #set-alias -Scope Global openssl "${ENV:ProgramFiles(x86)}\LogMeIn\x64\openssl.exe"
+set-alias -Scope Global winmerge "${ENV:ProgramFiles}\WinMerge\WinMergeU.exe"
+
 if ( $(alias curl *>$null;$?) ) { del alias:curl }
 if ( $(alias ip *>$null;$?) ) { del alias:ip }
 if ( $(alias rm *>$null;$?) ) { del alias:rm }
@@ -34,5 +36,6 @@ set-alias -Scope Global more less
 #	del alias:man
 #	function man { help @args | less }
 #}
+
 
 
