5d46c5b72a65f69eed56e8ab65e9fde1da7784f8
sebma <sebma@users.noreply.github.com>
2025-09-27 13:00:27 +0200

    Updated dockerCleanUpImages.sh

--- shl/dockerCleanUpImages.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-type sudo >/dev/null 2>&1 && [ $(id -u) != 0 ] && groups | egrep -wq "sudo|adm|admin|root|wheel" && sudo="sudo" || sudo=""
-groups | egrep -w docker && docker=docker || docker="$sudo docker"
-
-$docker image ls | awk '/^<none>\s+<none>/{printf" "$3}' | \xargs -rt $docker rmi
