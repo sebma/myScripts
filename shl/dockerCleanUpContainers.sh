5de7e3aab1fe837c282145ef9ac7a098f6a8854b
sebma <sebma@users.noreply.github.com>
2025-09-27 12:31:38 +0200

    Updated dockerCleanUpContainers.sh

--- shl/dockerCleanUpContainers.sh
+++ shl/dockerCleanUpContainers.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-docker ps -f status=exited | awk '/Exited/{printf" "$1}' | \xargs -rt docker rm
+type sudo >/dev/null 2>&1 && [ $(id -u) != 0 ] && groups | egrep -wq "sudo|adm|admin|root|wheel" && sudo="sudo" || sudo=""
+groups | egrep -w docker && docker=docker || docker="$sudo docker"
+
+$docker ps -f status=exited | awk '/Exited/{printf" "$1}' | \xargs -rt $docker rm
