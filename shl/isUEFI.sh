27e427e70f4836f6ec2e7074032b76f7a5736706
sebma <sebma@users.noreply.github.com>
2025-09-27 12:30:27 +0200

    Updated isUEFI.sh

--- shl/isUEFI.sh
+++ shl/isUEFI.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+set -x
 type -P efibootmgr >/dev/null 2>&1 && efibootmgr -q && echo "Session EFI" || echo "Session non-EFI"
 type -P efivar >/dev/null 2>&1 && efivar -l >/dev/null && echo "Session EFI" || echo "Session non-EFI"
 [ -d /sys/firmware/efi ] && echo "Session EFI" || echo "Session non-EFI"
