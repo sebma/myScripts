dc1a68ee2bda4fdc640090ef5ce0fe6fed3018d1
sebma <sebma@users.noreply.github.com>
2025-09-29 14:48:11 +0200

    Update firstboot.sh

--- shl/system/sysprep/firstboot.sh
+++ shl/system/sysprep/firstboot.sh
@@ -31,7 +31,11 @@ if egrep -i "vmware|virtal" /sys/class/dmi/id/sys_vendor -q;then
 			cp -piv /etc/netplan/00-installer-config.yaml /etc/netplan/BACKUP/00-installer-config-ORIG.yaml <<< n
 			netplan get ethernets | awk -F: '/^[^ ]*:$/{print$1}' | while read iface;do
 				echo "=> Removing the IP of $iface network interface ..."
-				netplan set "ethernets.$iface.addresses=null"
+				if echo $iface | grep bond -q;then
+					netplan set "bonds.$iface.addresses=null"
+				else
+					netplan set "ethernets.$iface.addresses=null"
+				fi
 			done
 			netplan apply
 		fi
