054e4098ba6b12c5f404c6a444ee9e7d2e04ddbc
sebma <sebma@users.noreply.github.com>
2025-10-14 12:26:18 +0200

    Update rename-User.sh

--- shl/system/rename-User.sh
+++ shl/system/rename-User.sh
@@ -20,8 +20,8 @@ oldUSER=$1
 if $isDebianLike;then
 	if id -u $oldUSER >/dev/null;then
 		newUSER=$2
-		sudo loginctl terminate-session $(loginctl list-sessions | awk "/$oldUSER/"'{print$1;exit}')
-	#	sudo sed -i "s/$oldUSER\>/$newUSER/g" /etc/passwd /etc/group /etc/shadow /etc/gshadow /etc/subuid /etc/subgid; sudo mv /home/$oldUSER /home/$newUSER
-		sudo usermod -l $newUSER -m -d /home/$newUSER $oldUSER && sudo groupmod -n $newUSER $oldUSER # https://serverfault.com/a/653514/312306
+		$sudo loginctl terminate-session $(loginctl list-sessions | awk "/$oldUSER/"'{print$1;exit}')
+#		$sudo sed -i "s/$oldUSER\>/$newUSER/g" /etc/passwd /etc/group /etc/shadow /etc/gshadow /etc/subuid /etc/subgid; $sudo mv /home/$oldUSER /home/$newUSER
+		$sudo usermod -l $newUSER -m -d /home/$newUSER $oldUSER && $sudo groupmod -n $newUSER $oldUSER # https://serverfault.com/a/653514/312306
 	fi
 fi
