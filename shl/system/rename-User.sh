20fcb2e5578bb90d87d387200de2d3e18490cd12
sebma <sebma@users.noreply.github.com>
2025-10-14 14:55:27 +0200

    Update rename-User.sh

--- shl/system/rename-User.sh
+++ shl/system/rename-User.sh
@@ -24,11 +24,11 @@ if $isDebianLike;then
 
 		newUSER=$2
 		oldMount=$(grep $oldUSER /etc/passwd | grep -v /home/$oldUSER | cut -d: -f6)
-		[ $oldMount ] && newMount=${oldUSER/$newUSER/} && grep "^[^#]/$oldUSER" /etc/fstab -q && $sudo umount -v $oldMount
+		[ $oldMount ] && grep "^[^#]/$oldUSER" /etc/fstab -q && $sudo umount -v $oldMount
 #		$sudo sed -i "s/$oldUSER\>/$newUSER/g" /etc/passwd /etc/group /etc/shadow /etc/gshadow /etc/subuid /etc/subgid; $sudo mv /home/$oldUSER /home/$newUSER
 		# https://serverfault.com/a/653514/312306
 		$sudo groupmod -n $newUSER $oldUSER && $sudo usermod -l $newUSER -m -d /home/$newUSER $oldUSER
 		$sudo sed -i "s/$oldUSER\>/$newUSER/g" /etc/subuid /etc/subgid
-		[ $newMount ] && $sudo mount -v $newMount
+		[ $oldMount ] && newMount=${oldUSER/$newUSER/} && $sudo mount -v $newMount
 	fi
 fi
