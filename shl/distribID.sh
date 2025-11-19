49eacfe05685200437e8248ec31c1cce74d862ee
sebma <sebma@users.noreply.github.com>
2025-11-19 21:39:37 +0100

    Updated distribID.sh

--- shl/distribID.sh
+++ shl/distribID.sh
@@ -17,20 +17,20 @@ ID_LIKE=$(source /etc/os-release;echo $ID_LIKE)
 if [ -n "$ID_LIKE" ];then
 	if   echo $ID_LIKE | egrep "rhel|centos|fedora" -q;then
 		isRedHatLike=true
-	elif echo $ID_LIKE| egrep "debian|ubuntu" -q;then
+	elif echo $ID_LIKE | egrep "debian" -q;then
 		isDebianLike=true
-	elif echo $ID_LIKE| egrep "alpine" -q;then
-		isAlpineLike=true
 	fi
 	distribID=$ID_LIKE
 else
 	ID=$(source /etc/os-release;echo $ID)
-	if   echo $ID_LIKE | egrep "rhel|centos|fedora" -q;then
-		isRedHatLike=true
-	elif echo $ID_LIKE| egrep "debian|ubuntu" -q;then
+	if   echo $ID | egrep "debian" -q;then
 		isDebianLike=true
-	elif echo $ID_LIKE| egrep "alpine" -q;then
+	elif echo $ID | egrep "alpine" -q;then
 		isAlpineLike=true
+	elif echo $ID | egrep "arch" -q;then
+		isArchLike=true
+	elif echo $ID | egrep "photon" -q;then
+		isPhotonLike=true
 	fi
 	distribID=$ID
 fi
