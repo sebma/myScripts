bc724a0bdefdc7e8e3fc5154e8efed23d0852acf
sebma <sebma@users.noreply.github.com>
2025-11-19 21:19:27 +0100

    Updated distribType.sh

--- shl/distribType.sh
+++ shl/distribType.sh
@@ -1,5 +1,32 @@
 #!/usr/bin/env bash
 
+function distribName {
+	local osName=unknown
+	echo $OSTYPE | grep -q android && local osFamily=Android || local osFamily=$(uname -s)
+
+	if [ $osFamily = Linux ]; then
+		if grep -w ID /etc/os-release -q 2>/dev/null; then
+			osName=$(source /etc/os-release && echo $ID)
+		elif [ -s /etc/issue.net ]; then
+			osName=$(awk '{print tolower($1)}' /etc/issue.net)
+		elif ! lsb_release -si 2>/dev/null | grep -i "n/a" -q; then
+			osName=$(lsb_release -si | awk '{print tolower($0)}')
+		elif type -P hostnamectl >/dev/null 2>&1; then
+			osName=$(hostnamectl status | awk '/Operating System/{print tolower($3)}')
+		fi
+	elif [ $osFamily = Darwin ]; then
+		osName="$(sw_vers -productName)"
+	elif [ $osFamily = Android ]; then
+		osName=Android
+	elif [ $osFamily = VMkernel ]; then # ESXi
+		osName=ESXi
+	else
+		test -n $OSTYPE && osName=$OSTYPE || osName=$osFamily
+	fi
+
+	echo $osName | awk '{print tolower($0)}'
+}
+
 function distribType {
 	local distribName=unknown
 	local distribType=unknown
@@ -11,9 +38,9 @@ function distribType {
 		elif [ ls /etc/*_version >/dev/null 2>&1 ]; then
 			distribType=$(echo /etc/*version | sed 's,/etc/\|_version,,g')
 		else
-			distribName=$(distribName.sh)
+			distribName=$(distribName)
 			case $distribName in
-				sailfishos|rhel|fedora|centos) distribType=redhat ;;
+				sailfishos|rhel|fedora|centos|rocky|photon) distribType=redhat ;;
 				ubuntu) distribType=debian;;
 				*) distribType=$distribName ;;
 			esac
