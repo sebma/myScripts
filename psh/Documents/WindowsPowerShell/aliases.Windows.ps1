e0cb43b70b490f89c0e20c1d1eb9961891d730e5
sebma <sebma@users.noreply.github.com>
2025-11-27 12:00:11 +0100

    Update aliases.Windows.ps1

--- psh/Documents/WindowsPowerShell/aliases.Windows.ps1
+++ psh/Documents/WindowsPowerShell/aliases.Windows.ps1
@@ -1,22 +1,23 @@
-if( $(alias cd *>$null;$?) ) {
-	del alias:cd
-}
+if( $(alias cd *>$null;echo $?) ) { del alias:cd }
+if( $(alias curl *>$null;echo $?) ) { del alias:curl }
+if( $(alias ip *>$null;echo $?) ) { del alias:ip }
+if( $(alias rm *>$null;echo $?) ) { del alias:rm }
+if( $(alias tee *>$null;echo $?) ) { del -Force alias:tee }
+if( $(alias wget *>$null;echo $?) ) { del alias:wget }
 
-set-alias historyLocal Get-History
+#if(alias man 2>$null | sls man) { del alias:man }
+#function man { help @args | less }
 
 #if ( $(alias ls *>$null;$?) ) { del alias:ls }
-#set-alias -Scope Global more less.exe
-#set-alias -Scope Global openssl "${ENV:ProgramFiles(x86)}\LogMeIn\x64\openssl.exe"
-set-alias -Scope Global winmerge "${ENV:ProgramFiles}\WinMerge\WinMergeU.exe"
-
-if ( $(alias curl *>$null;$?) ) { del alias:curl }
-if ( $(alias ip *>$null;$?) ) { del alias:ip }
-if ( $(alias rm *>$null;$?) ) { del alias:rm }
-set-alias -Scope Global id whoisUSER
 if( isInstalled("ls.exe") ) {
 	$global:ls = "ls.exe"
 	set-alias -Scope Global l $ls
 }
+
+#set-alias -Scope Global openssl "${ENV:ProgramFiles(x86)}\LogMeIn\x64\openssl.exe"
+set-alias -Scope Global winmerge "${ENV:ProgramFiles}\WinMerge\WinMergeU.exe"
+set-alias historyLocal Get-History
+set-alias -Scope Global id whoisUSER
 set-alias -Scope Global lscom lsserial
 set-alias -Scope Global np notepad
 set-alias -Scope Global np++ notepad++
@@ -30,14 +31,5 @@ set-alias ipa ipv4
 set-alias ipl iplink
 set-alias ipr iproute
 set-alias mac@ iplink
-set-alias -Scope Global more less  		
-#if( ! (alias wget 2>$null | sls wget) ) { set-alias -Scope Global wget Invoke-WebRequest }
-#if(alias man 2>$null | sls man) {
-#	del alias:man
-#	function man { help @args | less }
-#}
-
-
-
-
-
+set-alias mac iplink
+set-alias -Scope Global more less
