1c283c8bbc94e561f3ae055614ddbe5c80437d59
sebma <sebma@users.noreply.github.com>
2025-11-30 10:37:20 +0100

    Updated record_NCC_Live.sh

--- shl/record_NCC_Live.sh
+++ shl/record_NCC_Live.sh
@@ -6,5 +6,6 @@ scriptDir=$(cd $scriptDir;pwd)
 echo $PATH | grep /usr/local/bin -q || export PATH+=:/usr/local/bin
 echo $PATH | grep $scriptDir -q || export PATH+=:$scriptDir
 url=https://www.youtube.com/user/NewCreationChurch/live
-#url="$(ytdlGetLiveURL.sh "$url")"
-cd ~/Videos/ENGLISH/CHRIST/Joseph_Prince/Live_sermons/Live_NCC/ && getRestrictedFilenamesFORMAT.sh --timeout $estimatedDuration -f 94/231+233 "$url"
+#cd /multimedia
+cd $HOME
+cd ./Videos/ENGLISH/CHRIST/Joseph_Prince/Live_sermons/Live_NCC/ && getRestrictedFilenamesFORMAT.sh --timeout $estimatedDuration -f 94/231+233 "$url"
