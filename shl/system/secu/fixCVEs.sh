46c30e07bc69bb46c0e1a4ec6d245886ed87066a
sebma <sebma@users.noreply.github.com>
2025-12-04 20:55:21 +0100

    Updated fixCVEs.sh

--- shl/system/secu/fixCVEs.sh
+++ shl/system/secu/fixCVEs.sh
@@ -6,7 +6,8 @@ function fixCVEs {
 	cveListRegExp=${@#CVE-}
 	cveListRegExp="CVE-(${cveListRegExp// /|})"
 
-	#sudo apt install -V $(debsecan --suite $(cut -d/ -f2 /etc/debian_version) --only-fixed | egrep "$cveListRegExp" | cut -d" " -f2 | sort -u)
+	test $(id -u) == 0 && sudo="" || sudo=$(type -P sudo)
+	$sudo apt install -V $(debsecan --suite $(cut -d/ -f2 /etc/debian_version) --only-fixed | egrep "$cveListRegExp" | cut -d" " -f2 | sort -u)
 }
 
 fixCVEs "$@"
