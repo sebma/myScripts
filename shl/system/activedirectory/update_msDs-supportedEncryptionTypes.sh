faf3da96e8f87c21ab7ddfca368df1f7a82d7ffe
sebma <sebma@users.noreply.github.com>
2025-12-04 21:14:08 +0100

    Updated ./system/activedirectory/update_msDs-supportedEncryptionTypes.sh

--- shl/system/activedirectory/update_msDs-supportedEncryptionTypes.sh
+++ shl/system/activedirectory/update_msDs-supportedEncryptionTypes.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 man msktutil | sed -n '/^\s*--dont-change-password/,/^$/ p;/^\s*--enctypes/,/0x10/ p'
-test $(id -u) == 0 && sudo="" || sudo=sudo
+test $(id -u) == 0 && sudo="" || sudo=$(type -P sudo)
 $sudo msktutil update --dont-change-password --enctypes 0x18
