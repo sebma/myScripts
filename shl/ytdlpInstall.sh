2b8f80ac3a563f260fc905911131c85f7005b164
sebma <sebma@users.noreply.github.com>
2025-12-04 21:17:00 +0100

    Updated ./ytdlpInstall.sh

--- shl/ytdlpInstall.sh
+++ shl/ytdlpInstall.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-test $(id -u) == 0 && sudo="" || sudo=$(which sudo)
+test $(id -u) == 0 && sudo="" || sudo=$(type -P sudo)
 $sudo pipx install --global yt-dlp[default,curl-cffi]
