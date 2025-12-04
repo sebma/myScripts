645221173ee85bfd5b4f7bcb683dba8b2297e39f
sebma <sebma@users.noreply.github.com>
2025-12-04 20:54:06 +0100

    Updated system/check4BadDisks.sh

--- shl/system/check4BadDisks.sh
+++ shl/system/check4BadDisks.sh
@@ -4,7 +4,7 @@ function lsdisks {
         \ls /sys/class/block/ | sed "s|^|/dev/|" | egrep -v '/(loop|dm-|md|sr|z?ram|sd[a-z]+[0-9]+|nvme[0-9]n[0-9]p[0-9]|synoboot[0-9])'
 }
 
-test $(id -u) == 0 && sudo="" || sudo=sudo
+test $(id -u) == 0 && sudo="" || sudo=$(type -P sudo)
 for disk in $(lsdisks);do
         smartBadsectors=$($sudo smartctl -A $disk | awk '/Reallocated_Sector_Ct|Current_Pending_Sector|Offline_Uncorrectable/{bad+=$NF}END{printf bad}')
         if [ $smartBadsectors != 0 ];then
