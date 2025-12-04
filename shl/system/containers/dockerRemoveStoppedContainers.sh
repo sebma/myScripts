ccf76e9ab1f5d3824c60ea21d3fd5aa02641dd7b
sebma <sebma@users.noreply.github.com>
2025-12-04 21:14:08 +0100

    Updated ./system/containers/dockerRemoveStoppedContainers.sh

--- shl/system/containers/dockerRemoveStoppedContainers.sh
+++ shl/system/containers/dockerRemoveStoppedContainers.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-test $(id -u) == 0 && sudo="" || sudo=sudo
+test $(id -u) == 0 && sudo="" || sudo=$(type -P sudo)
 $sudo docker ps -a | awk '/Exited/{print$1}' | $sudo xargs -rt docker rm
