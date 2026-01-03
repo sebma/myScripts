a9a32bf17bf5488245614ec6c12b347d0cbf0dbd
sebma <sebma@users.noreply.github.com>
2026-01-04 00:51:29 +0100

    Updated lsPPAs.sh

--- shl/lsPPAs.sh
+++ shl/lsPPAs.sh
@@ -5,7 +5,7 @@ function lsPPAs {
 	local sourcesDisabledFileList=$({ cd /etc/apt/sources.list.d/;grep "^\s*Enabled:.no" *-$releaseCodeName.sources -l || echo '^$'; } | paste -sd"|" -;)
 	local sourcesEnabledFileList=$(ls /etc/apt/sources.list.d/*-$releaseCodeName.sources | egrep -v "$sourcesDisabledFileList")
 	awk -F "[ /]" 'BEGINFILE{if(ERRNO)nextfile}
-		/^(deb|URIs:) .*launchpad.*.net/{
+		$0 ~ /^(deb|URIs:) .*launchpad.*.net/ {
 		print"https://launchpad.net/~"$5"/+archive/ubuntu/"$6" ppa:"$5"/"$6
 }' /etc/apt/sources.list.d/*-$releaseCodeName.list $sourcesEnabledFileList 2>/dev/null | sort -u
 }
