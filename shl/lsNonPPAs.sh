19575733f90be7ab1f1589cfcef4500896f16f31
sebma <sebma@users.noreply.github.com>
2026-01-04 01:15:38 +0100

    Updated lsNonPPAs.sh

--- shl/lsNonPPAs.sh
+++ shl/lsNonPPAs.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 function lsNonPPAs {
+	local releaseCodeName=$(lsb_release -sc 2>/dev/null)
+	local sourcesDisabledFileList=$({ cd /etc/apt/sources.list.d/;grep "^\s*Enabled:.no" *-$releaseCodeName.sources -l || echo '^$'; } | paste -sd"|" -;)
+	local sourcesEnabledFileList=$(ls /etc/apt/sources.list.d/*-$releaseCodeName.sources | egrep -v "$sourcesDisabledFileList")
 	awk -F "[ /]" 'BEGINFILE{if(ERRNO)nextfile}
 	/launchpad.*.net|esm.ubuntu.com|^#|^\s*$/{next}
 	{sub("^.*https?","https")}
