2e53ad76e2b14d3255705feec8bac8eef7b2e49e
sebma <sebma@users.noreply.github.com>
2026-01-11 12:45:56 +0100

    Updated record_NCC_Live_Then_Suspend.sh

--- shl/record_NCC_Live_Then_Suspend.sh
+++ shl/record_NCC_Live_Then_Suspend.sh
@@ -6,10 +6,11 @@ scriptDir=$(cd $scriptDir;pwd)
 echo $PATH | grep /usr/local/bin -q || export PATH+=:/usr/local/bin
 echo $PATH | grep $scriptDir -q || export PATH+=:$scriptDir
 url=https://www.youtube.com/user/NewCreationChurch/live
-#cd /multimedia
-cd $HOME
-mkdir -vp /tmp/Videos/ENGLISH/CHRIST/Joseph_Prince/Live_sermons/Live_NCC/;cd /tmp/
-cd Videos/ENGLISH/CHRIST/Joseph_Prince/Live_sermons/Live_NCC/ && getRestrictedFilenamesFORMAT.sh --timeout $estimatedDuration -f 94/231+233 "$url"
+#lvmVolume=/multimedia
+lvmVolume=/tmp
+cd $lvmVolume
+live_NCC_Dir="Videos/ENGLISH/CHRIST/Joseph_Prince/Live_sermons/Live_NCC/"
+mkdir -vp $live_NCC_Dir && cd $live_NCC_Dir && getRestrictedFilenamesFORMAT.sh --timeout $estimatedDuration -f 94/231+233 "$url"
 initName=$(ps -p 1 -o comm= | cut -d" " -f1)
 initPath=$(type -P $initName)
 systemType=$(strings $initPath | grep -o -E "upstart|sysvinit|systemd|launchd" | head -1 || echo unknown)
