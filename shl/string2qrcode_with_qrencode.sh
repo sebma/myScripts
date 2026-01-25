ac33d70e2c9c417e996f90fcdf11bb4f867a92e0
sebma <sebma@users.noreply.github.com>
2026-01-25 19:55:43 +0100

    Updated string2qrcode_with_qrencode.sh

--- shl/string2qrcode_with_qrencode.sh
+++ shl/string2qrcode_with_qrencode.sh
@@ -1,9 +1,11 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-string2qrcode_with_qrencode ()
-{
+string2qrcode_with_qrencode () {
 	local dotSize=8 string=""
-	if [ $# = 1 ]; then
+	if [ $# != 1 ]; then
+		echo "=> Usage $FUNCNAME stringOrURL" >&2
+		return -1
+	else
 		string="$1"
 		qrencode -l H -s $dotSize -m 0 -o- "$string" | feh -
 	fi
