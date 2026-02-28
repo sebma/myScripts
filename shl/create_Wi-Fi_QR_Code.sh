3e221a2f131990f4ba9e48c030d5cea6d48edf75
sebma <sebma@users.noreply.github.com>
2026-02-28 17:25:55 +0100

    Updated *Wi-Fi*

--- shl/create_Wi-Fi_QR_Code.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-ssid=$(zenity --entry --text="Hidden Network name (SSID)" --title="Create WiFi QR")
-
-zenity --question --title="" --text "Is your Wi-Fi network hidden ?" --ok-label=No --cancel-label=Yes && hidden=false || hidden=true
-
-qrencode -l H -s 7 -o $ssid.png "WIFI:S:$ssid;T:WPA;P:$(zenity --password --title="Wifi Password");H:$hidden;;"
-
-nohup eog $ssid.png &
