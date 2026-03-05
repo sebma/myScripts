a1a75f066d90ac84e977a3b39881d7236955b9dd
sebma <sebma@users.noreply.github.com>
2026-03-05 21:26:29 +0100

    Updated install-Nvidia-driver.sh

--- shl/distribs/rocky/install-Nvidia-driver.sh
+++ shl/distribs/rocky/install-Nvidia-driver.sh
@@ -13,13 +13,14 @@ declare {isDebian,isRedHat,isAlpine}Like=false
 distribID=$(source /etc/os-release;echo $ID)
 if   echo $distribID | egrep "centos|rhel|fedora" -q;then
 	isRedHatLike=true
-elif echo $distribID | egrep "debian|ubuntu" -q;then
-	isDebianLike=true
-elif echo $distribID | egrep "alpine" -q;then
-	isAlpineLike=true
 fi
 
 if $isRedHatLike;then
+	if ! dnf repolist | grep cuda -q;then
+		rhelVersion=8
+		$sudo dnf config-manager --add-repo http://developer.download.nvidia.com/compute/cuda/repos/rhel$rhelVersion/$(uname -i)/cuda-rhel$rhelVersion.repo
+	fi
+
 	if dnf module list nvidia-driver | grep $nvidiaDriverVersion -q;then
 		nvidiaDriverVersionNumber=(tr -d '[a-zA-Z-_]' <<< $nvidiaDriverVersion)
 		$sudo dnf module enable $nvidiaDriverVersion -y
@@ -27,8 +28,9 @@ if $isRedHatLike;then
 			$sudo dnf install nvidia-open -y
 			nvidia-smi >/dev/null || $sudo dnf reinstall kmod-nvidia-open-dkms -y
 		else
+			nvidiaEffectiveDriverVersion=$(dnf info nvidia-driver | awk -F ':| ' '/Version/{print$NF}')
 			$sudo dnf install nvidia-driver nvidia-driver-cuda -y
-			nvidia-smi >/dev/null || $sudo dnf reinstall kmod-nvidia-driver-$(uname -r) -y
+			nvidia-smi >/dev/null || $sudo dnf reinstall kmod-nvidia-$nvidiaEffectiveDriverVersion-$(uname -r) -y
 		fi
 #		$sudo grubby --args="nouveau.modeset=0 rd.driver.blacklist=nouveau" --update-kernel=ALL
 		mokutil --sb-state | grep SecureBoot -q && $sudo mokutil --import /var/lib/dkms/mok.pub
