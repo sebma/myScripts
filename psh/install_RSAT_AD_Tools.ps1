29fd500395ff7115843ed275dac40914756102dc
sebma <sebma@users.noreply.github.com>
2026-03-09 20:40:38 +0100

    Updated install_RSAT_AD_Tools.ps1

--- psh/install_RSAT_AD_Tools.ps1
+++ psh/install_RSAT_AD_Tools.ps1
@@ -1,13 +1,13 @@
 $osVersion = $PSVersionTable.BuildVersion.Major
 if ( $osVersion -eq 10 ) {
 	function install_RSAT_AD_Tools {
-		sudo cache on
-		sudo New-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Servicing -Name RepairContentServerSource -Type dword -Value 2
+		$sudo cache on
+		$sudo New-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Servicing -Name RepairContentServerSource -Type dword -Value 2
 		$getWindowsCapabilityCommand = "& Get-WindowsCapability -Name RSAT.ActiveDirectory* -Online"
-		iex $getWindowsCapabilityCommand | Where-Object State -eq 'NotPresent' | Select-Object -Property name , displayname , state
-		iex $getWindowsCapabilityCommand | Where-Object State -eq 'NotPresent' | sudo Add-WindowsCapability -Online
-		iex $getWindowsCapabilityCommand | Where-Object State -eq 'NotPresent' | Select-Object -Property name , displayname , state
-		sudo Set-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Servicing -Name RepairContentServerSource -Type dword -Value 0
-		sudo -k
+		Invoke-Expression $getWindowsCapabilityCommand | Where-Object State -eq 'NotPresent' | Select-Object -Property name , displayname , state
+		Invoke-Expression $getWindowsCapabilityCommand | Where-Object State -eq 'NotPresent' | $sudo Add-WindowsCapability -Online
+		Invoke-Expression $getWindowsCapabilityCommand | Where-Object State -eq 'NotPresent' | Select-Object -Property name , displayname , state
+		$sudo Set-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Servicing -Name RepairContentServerSource -Type dword -Value 0
+		$sudo -k
 	}
 }
