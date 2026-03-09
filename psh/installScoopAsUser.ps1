dfbaf448eaf7bf6b69c0d53d00bc201c44a9d735
sebma <sebma@users.noreply.github.com>
2026-03-09 20:48:03 +0100

    Updated installScoopAsUser.ps1

--- psh/installScoopAsUser.ps1
+++ psh/installScoopAsUser.ps1
@@ -2,14 +2,14 @@ function isInstalled($cmd) { return gcm "$cmd" 2>$null }
 
 if( $IsWindows ) {
 	function InstallScoopAsUser {
-		sudo config CacheMode auto # i.e https://github.com/gerardog/gsudo#credentials-cache
+		$sudo config CacheMode auto # i.e https://github.com/gerardog/gsudo#credentials-cache
 		if( ! (isInstalled("scoop.ps1")) ) {
 			if( (Get-ExecutionPolicy) -ne "Unrestricted" -and (Get-ExecutionPolicy) -ne "RemoteSigned" -and (Get-ExecutionPolicy) -ne "Bypass" ) {
 				Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
 			}
-			iwr -useb get.scoop.sh | iex
+			iwr -useb get.scoop.sh | Invoke-Expression
 		}
-		sudo -k
+		$sudo -k
 	}
 	InstallScoopAsUser
 }
