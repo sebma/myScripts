b519584f33de90cb11b2c9149346f345a6eda528
sebma <sebma@users.noreply.github.com>
2026-03-09 20:41:30 +0100

    Updated installScoopAsAdmin.ps1

--- psh/installScoopAsAdmin.ps1
+++ psh/installScoopAsAdmin.ps1
@@ -2,14 +2,14 @@ function isInstalled($cmd) { return gcm "$cmd" 2>$null }
 
 if( $IsWindows ) {
 	function InstallScoopAsAdmin {
-		sudo config CacheMode auto # i.e https://github.com/gerardog/gsudo#credentials-cache
+		$sudo config CacheMode auto # i.e https://github.com/gerardog/gsudo#credentials-cache
 		if( ! (isInstalled("scoop.ps1")) ) {
 			if( (Get-ExecutionPolicy) -ne "Unrestricted" -and (Get-ExecutionPolicy) -ne "RemoteSigned" -and (Get-ExecutionPolicy) -ne "Bypass" ) {
-				sudo Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
+				$sudo Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
 			}
-			sudo iex "& {$(irm get.scoop.sh)} -RunAsAdmin -ScoopDir $env:ProgramData\scoop"
+			$sudo iex "& {$(irm get.scoop.sh)} -RunAsAdmin -ScoopDir $env:ProgramData\scoop"
 		}
-		sudo -k
+		$sudo -k
 	}
 	InstallScoopAsAdmin
 }
