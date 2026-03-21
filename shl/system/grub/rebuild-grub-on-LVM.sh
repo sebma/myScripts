187cbdb5c2e82e14dde538cd8c303aa91a037944
sebma <sebma@users.noreply.github.com>
2026-03-21 22:49:14 +0100

    Updated rebuild-grub-on-LVM.sh

--- shl/system/grub/rebuild-grub-on-LVM.sh
+++ shl/system/grub/rebuild-grub-on-LVM.sh
@@ -14,7 +14,7 @@ currentRootFS_VG=$($sudo lvs --noheadings $(findmnt / -o source -n) -o vg_name)
 rootFSLogicalVolume=$($sudo lvs | awk  "/$currentRootFS_VG/{next}"'/root/{if($2 ~ /-/){print$2"--"$1}else{print$2"-"$1}}')
 
 set -x
-mount | grep -q $rootFSLogicalVolume || $sudo mount -v /dev/mapper/$rootFSLogicalVolume /mnt          # montage de celle-ci en remplacant le X par le bon numero de partition
+mount | grep -q $rootFSLogicalVolume || $sudo mount -v /dev/mapper/$rootFSLogicalVolume /mnt # montage de celle-ci en remplacant le X par le bon numero de partition
 for special in dev dev/pts proc sys ; do $sudo mkdir -pv /mnt/$special;$sudo mount -v --bind /$special /mnt/$special ; done
 
 set +o errexit
