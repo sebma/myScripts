038cd38821b08eed149ac6de735999872a90fc63
sebma <sebma@users.noreply.github.com>
2026-03-21 22:23:07 +0100

    Updated chroot-root-volume-on-LVM.sh

--- shl/system/grub/chroot-root-volume-on-LVM.sh
+++ shl/system/grub/chroot-root-volume-on-LVM.sh
@@ -11,7 +11,7 @@ $sudo lvs | grep root -q || {
 }
 
 currentRootFS_VG=$($sudo lvs --noheadings $(findmnt / -o source -n) -o vg_name)
-rootFSLogicalVolume=$($sudo lvs | awk  "/$currentRootFS_VG/{next}"'/root/{if($2 ~ /-/){print$2"--"$1}else{print$2"-"$1)}')
+rootFSLogicalVolume=$($sudo lvs | awk  "/$currentRootFS_VG/{next}"'/root/{if($2 ~ /-/){print$2"--"$1}else{print$2"-"$1}}')
 
 mount | grep -q $rootFSLogicalVolume || $sudo mount /dev/mapper/$rootFSLogicalVolume /mnt          # montage de celle-ci en remplacant le X par le bon numero de partition
 for special in dev dev/pts proc sys ; do $sudo mkdir -pv /mnt/$special;$sudo mount -v --bind /$special /mnt/$special ; done
