d9e5dee69e71d7d85d839708a391cbabf88b4e6c
sebma <sebma@users.noreply.github.com>
2026-03-22 17:06:22 +0100

    Updated chroot-root-volume-on-LVM.sh

--- shl/system/grub/chroot-root-volume-on-LVM.sh
+++ shl/system/grub/chroot-root-volume-on-LVM.sh
@@ -10,12 +10,14 @@ $sudo lvs | grep root -q || {
 	$sudo lvscan
 }
 
-currentRootFS_VG=$($sudo lvs --noheadings $(findmnt / -o source -n) -o vg_name)
-rootFSLogicalVolume=$($sudo lvs | awk  "/$currentRootFS_VG/{next}"'/root/{if($2 ~ /-/){print$2"--"$1}else{print$2"-"$1}}')
+currentRootFS_VGName=$($sudo lvs --noheadings $(findmnt / -o source -n) -o vg_name)
+rootFS_VGName=$($sudo lvs | awk  "/$currentRootFS_VGName/{next}"'/root/{print$2;exit}')
+rootFS_LVName=$($sudo lvs $rootFS_VGName | awk '/root/{print$1;exit}')
 
 set -x
-mount | grep -q $rootFSLogicalVolume || $sudo mount -v /dev/mapper/$rootFSLogicalVolume /mnt
+mount | grep ${rootFS_VGName}-*$rootFS_LVName -q || $sudo mount -v /dev/$rootFS_VGName/$rootFS_LVName /mnt
 for special in dev dev/pts proc sys ; do $sudo mkdir -pv /mnt/$special;$sudo mount -v --bind /$special /mnt/$special ; done
 
 set +o errexit
 $sudo chroot /mnt bash
+$sudo umount -v /mnt/{usr,sys,proc,dev/pts,dev,}
