26248b73122bf352b0231d5218aa4d9c077df5d6
sebma <sebma@users.noreply.github.com>
2026-03-23 17:03:20 +0100

    Update install-Nvidia-driver.sh

--- shl/distribs/rocky/install-Nvidia-driver.sh
+++ shl/distribs/rocky/install-Nvidia-driver.sh
@@ -31,7 +31,8 @@ if $isRedHatLike;then
 	if ! dnf repolist | grep cuda -q;then
 		rhelMajorVersion=$(source /etc/os-release;echo ${VERSION_ID/.*})
 		# See https://docs.rockylinux.org/8/desktop/display/installing_nvidia_gpu_drivers/
-		$sudo rm /etc/yum.repos.d/cuda.repo -f
+		#$sudo rm /etc/yum.repos.d/cuda.repo -f
+		$sudo dnf config-manager --disable cuda
 		$sudo dnf remove *nvidia* cuda-drivers -y
 		dnf repolist | grep cuda-rhel$rhelMajorVersion-$(uname -i) -q || $sudo dnf config-manager --add-repo http://developer.download.nvidia.com/compute/cuda/repos/rhel$rhelMajorVersion/$(uname -i)/cuda-rhel$rhelMajorVersion.repo
 	fi
