37216da18daa75f9be8df78cb6a418fa0069732d
sebma <sebma@users.noreply.github.com>
2026-03-28 18:31:00 +0100

    Updated install-systemd-serivce.sh

--- shl/system/systemd/install-systemd-serivce.sh
+++ shl/system/systemd/install-systemd-serivce.sh
@@ -4,19 +4,19 @@
 set -o nounset
 scriptBaseName=${0/*\//}
 if [ $# != 1 ];then
-        echo "=> Usage $scriptBaseName SERVICE_NAME" >&2
+        echo "=> Usage $scriptBaseName serviceFileName" >&2
         exit 1
 fi
 test $(id -u) == 0 && sudo="" || sudo=$(type -P sudo)
 
-SERVICE_NAME=$1
+serviceFileName=$1
 
-# Check if the /etc/systemd/system/$SERVICE_NAME exists
-if [ ! -f /etc/systemd/system/$SERVICE_NAME ]; then
-    $sudo cp -piv $SERVICE_NAME /etc/systemd/system/$SERVICE_NAME
+# Check if the /etc/systemd/system/$serviceFileName exists
+if [ ! -f /etc/systemd/system/$serviceFileName ]; then
+    $sudo cp -piv $serviceFileName /etc/systemd/system/$SERVICE_NAME
 fi
 
 $sudo systemctl daemon-reload
-$sudo systemctl enable $SERVICE_NAME
-$sudo systemctl start $SERVICE_NAME
-$sudo systemctl status $SERVICE_NAME
+$sudo systemctl enable $serviceFileName
+$sudo systemctl start $serviceFileName
+$sudo systemctl status $serviceFileName
