63edcea8ba8a4c1489e0ce2debd83bf22c6c0702
sebma <sebma@users.noreply.github.com>
2026-03-30 17:36:20 +0200

    Update install-Nvidia-driver.sh

--- shl/distribs/rocky/install-Nvidia-driver.sh
+++ shl/distribs/rocky/install-Nvidia-driver.sh
@@ -25,7 +25,7 @@ if $isRedHatLike;then
 
 	$sudo systemctl stop puppet.service
 	$sudo sed -i '/^exclude=/s/^/#/' $(readlink -e /etc/yum.conf)
-	$sudo sed -i '/^proxy=/s/^/#/' $(readlink -e /etc/yum.conf)
+	$sudo sed -i.bak '/^proxy=/s/^/#/' $(readlink -e /etc/yum.conf)
 	dnf config-manager --dump | grep ^exclude
 	dnf repolist | grep powertools -wq || $sudo dnf config-manager --set-enabled powertools
 
