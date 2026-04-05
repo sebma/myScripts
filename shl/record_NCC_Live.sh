4996af7f54b3e59a20a33915c779c3f309ad7565
sebma <sebma@users.noreply.github.com>
2026-04-06 01:05:09 +0200

    Updated record_NCC_Live.sh

--- shl/record_NCC_Live.sh
+++ shl/record_NCC_Live.sh
@@ -10,4 +10,4 @@ url=https://www.youtube.com/user/NewCreationChurch/live
 lvmVolume=/tmp
 cd $lvmVolume
 live_NCC_Dir="Videos/ENGLISH/CHRIST/Joseph_Prince/Live_sermons/Live_NCC"
-mkdir -vp $live_NCC_Dir && cd $live_NCC_Dir && getRestrictedFilenamesFORMAT.sh --timeout $estimatedDuration -f 94/231+233 "$url"
+mkdir -vp $live_NCC_Dir && cd $live_NCC_Dir && getRestrictedFilenamesFORMAT.sh --timeout $estimatedDuration -f 93/230+233 "$url"
