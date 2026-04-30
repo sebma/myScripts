78f602dbc3233d40478f872e3ad86bca1f8740ad
sebma <sebma@users.noreply.github.com>
2026-04-30 20:59:40 +0200

    Update rocklinux-9-postInstall.sh

--- shl/distribs/rocky/rocklinux-9-postInstall.sh
+++ shl/distribs/rocky/rocklinux-9-postInstall.sh
@@ -19,14 +19,14 @@ timedatectl status | grep Time.zone:.Europe/Paris -q || $sudo timedatectl set-ti
 swapSize=32.6GiB # car / trop petit pour le moment
 dataDir=/data
 if [ ! -e $swapFilePath ];then
-        $sudo mkdir -p $dataDir
-        swapFilePath=$dataDir/swapfile
-        $sudo fallocate -l $swapSize $swapFilePath
-        $sudo chmod 0600 $swapFilePath
-        $sudo mkswap $swapFilePath
-        $sudo swapon $swapFilePath
-        grep $swapFilePath /etc/fstab -q || echo "$swapFilePath none swap sw 0 0" | $sudo tee -a /etc/fstab
-        $sudo systemctl daemon-reload
+	$sudo mkdir -p $dataDir
+	swapFilePath=$dataDir/swapfile
+	$sudo fallocate -l $swapSize $swapFilePath
+	$sudo chmod 0600 $swapFilePath
+	$sudo mkswap $swapFilePath
+	$sudo swapon $swapFilePath
+	grep $swapFilePath /etc/fstab -q || echo "$swapFilePath none swap sw 0 0" | $sudo tee -a /etc/fstab
+	$sudo systemctl daemon-reload
 fi
 
 $sudo dnf install realmd krb5-workstation -y
@@ -60,23 +60,23 @@ dnf repolist | grep cuda-rhel -q || $sudo dnf config-manager --add-repo http://d
 
 dnf clean expire-cache
 if dnf module list nvidia-driver | grep $nvidiaDriverVersion -q;then
- $sudo dnf clean expire-cache
- $sudo dnf module enable nvidia-driver:$nvidiaDriverVersion -y
- $sudo dnf install nvidia-open kmod-nvidia-open-dkms nvidia-driver-cuda -y --allowerasing
- $sudo dnf install docker-ce docker-compose-plugin -y
- $sudo systemctl enable --now docker.service
- $sudo dnf install nvidia-container-toolkit -y
- $sudo systemctl restart docker.service
+	$sudo dnf clean expire-cache
+	$sudo dnf module enable nvidia-driver:$nvidiaDriverVersion -y
+	$sudo dnf install nvidia-open kmod-nvidia-open-dkms nvidia-driver-cuda -y --allowerasing
+	$sudo dnf install docker-ce docker-compose-plugin -y
+	$sudo systemctl enable --now docker.service
+	$sudo dnf install nvidia-container-toolkit -y
+	$sudo systemctl restart docker.service
 else
- echo "=> There is no $nvidiaDriverVersion available in the nvidia-driver DNF modules list." >&2
- exit 2
+	echo "=> There is no $nvidiaDriverVersion available in the nvidia-driver DNF modules list." >&2
+	exit 2
 fi
 
 if ! grep -r vm.max_map_count /etc/sysctl.conf /etc/sysctl.d/ -q 2>/dev/null;then
- echo "vm.max_map_count = $((2**18))" | $sudo tee -a /etc/sysctl.d/98-comfyui.conf >/dev/null
- $sudo systemctl restart systemd-sysctl.service
- sysctl vm.max_map_count
- cat /proc/sys/vm/max_map_count
+	echo "vm.max_map_count = $((2**18))" | $sudo tee -a /etc/sysctl.d/98-comfyui.conf >/dev/null
+	$sudo systemctl restart systemd-sysctl.service
+	sysctl vm.max_map_count
+	cat /proc/sys/vm/max_map_count
 fi
 
 $sudo sed -i.bak -r '/#\s*greeter-show-manual-login\s*=\s*\w+$/s/^.*$/greeter-show-manual-login = true/' /etc/lightdm/lightdm.conf
