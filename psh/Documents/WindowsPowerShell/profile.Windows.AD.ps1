46a3f6f25881fd88619a2b73b42daa3ad6489802
sebma <sebma@users.noreply.github.com>
2026-05-19 14:08:30 +0200

    Update profile.Windows.AD.ps1

--- psh/Documents/WindowsPowerShell/profile.Windows.AD.ps1
+++ psh/Documents/WindowsPowerShell/profile.Windows.AD.ps1
@@ -1,4 +1,25 @@
 "=> Sourcing $scriptPrefix.$osFamily.AD.ps1 functions ..."
+function findComputer {
+	$DC = $env:LOGONSERVER.Substring(2)
+	"=> LOGONSERVER = $DC"
+	$argc = $args.Count
+	if( $argc -eq 0 ) {
+		$myPattern = $env:COMPUTERNAME
+	} else {
+		$firstArg = $args[0]
+		if( ! $firstArg.Contains("*") ) {
+			$myPattern = '*'+$firstArg+'*'
+		} elseif( $firstArg ) {
+			$myPattern = $firstArg
+		}
+	}
+
+	"=> myPattern = $myPattern"
+
+	Get-ADComputer -Server $DC -Properties CN , CanonicalName , Description , IPv4Address , lastLogon, msDS-SupportedEncryptionTypes , OperatingSystem , ServicePrincipalNames , whenCreated , whenChanged -Filter { Name -like $myPattern }
+
+	# Get-ADComputer -Server $DC -Properties CN , CanonicalName , Description , IPv4Address , lastLogon , msDS-SupportedEncryptionTypes , OperatingSystem , ServicePrincipalNames , whenCreated , whenChanged -Filter { Name -like $myPattern } | ? Name -match $myRegExp
+}
 function findGroup {
 	$myPattern = '*'+$args[0]+'*'
 	$DC = $env:LOGONSERVER.Substring(2)
