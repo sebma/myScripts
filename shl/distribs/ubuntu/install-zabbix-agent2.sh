2298b019b55348b85088451606b5a18aef138e1d
sebma <sebma@users.noreply.github.com>
2026-06-17 18:16:12 +0200

    Update install-zabbix-agent2.sh

--- shl/distribs/ubuntu/install-zabbix-agent2.sh
+++ shl/distribs/ubuntu/install-zabbix-agent2.sh
@@ -13,4 +13,5 @@ if [ $majorNumber -ge 18 ];then
 	systemctl --quiet is-active postgresql || sudo apt install -V zabbix-agent2-plugin-postgresql
 	systemctl --quiet is-active mongodb || sudo apt install -V zabbix-agent2-plugin-mongodb
 	systemctl --quiet is-active mssql-server || sudo apt install -V zabbix-agent2-plugin-mssql
+	systemctl --quiet is-active mariadb || sudo apt install -V zabbix-agent2-plugin-mssql
 fi
