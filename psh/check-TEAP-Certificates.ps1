b45ff3d7a4665d9b01fa63da01cba4c62a171f7a
sebma <sebma@users.noreply.github.com>
2026-06-19 12:38:02 +0200

    Create check-TEAP-Certificates.ps1

---
+++ psh/check-TEAP-Certificates.ps1
@@ -0,0 +1,2 @@
+ls Cert:\CurrentUser\My  | ? Subject -IMatch CN=.*media-participations.com | select Subject , Issuer , NotBefore , NotAfter | fl
+ls Cert:\LocalMachine\My | ? Subject -IMatch CN=$env:COMPUTERNAME | select Subject , Issuer , NotBefore , NotAfter | fl
