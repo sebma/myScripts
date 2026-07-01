73f70c8bbc73d945276f86e38b455fa71dc41967
sebma <sebma@users.noreply.github.com>
2026-07-01 17:45:24 +0200

    Update robocopy-PS5.ps1

--- psh/system/robocopy-PS5.ps1
+++ psh/system/robocopy-PS5.ps1
@@ -11,12 +11,11 @@ function robocopyPS {
 	$sourceBaseName = $sourceDIR.Split('\')[-1]
 	$destinationDIR = Join-Path $destinationDIR $sourceBaseName
 	$logDIR = "C:\TEMP\Robocopy\Logs"
-#	$logFile = $logDIR + '\' + $sourceBaseName + '.log'
+	$logFile = $logDIR + '\' + $sourceBaseName + '.log'
 	$robocopyOptions = "/COPY:DATSO /MIR /r:0 /np /v /tee" -split '\s+'
 	#$robocopyDryRUN = "/L"
 
 	gci $sourceDIR | foreach {
-		$logFile = $logDIR + '\' + $sourceBaseName + '.log'
 		Write-Host robocopy $_.FullName $destinationDIR $robocopyDryRUN @robocopyOptions /log+:$logFile
 		robocopy $_.FullName $destinationDIR $robocopyDryRUN @robocopyOptions /log+:$logFile
 	}
