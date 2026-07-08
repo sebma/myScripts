5c915c2a7d68a95884ff94bce5ac128c4bec6bb6
sebma <sebma@users.noreply.github.com>
2026-07-08 22:01:05 +0200

    Updated pingMyLANSimple.sh

--- shl/pingMyLANSimple.sh
+++ shl/pingMyLANSimple.sh
@@ -7,7 +7,7 @@ function pingMyLANSimple {
     local lanPrefix=$(echo $myLAN | cut -d. -f1-3)
     local nbHost=$((2**(32-$prefix)-1))
 
-    for i in $(seq $(($nbHost-1)));do
+    time for i in $(seq $(($nbHost-1)));do
         ping $lanPrefix.$i -c1 -W1 &
     done | grep from | sort -t . -k 4n
 }
