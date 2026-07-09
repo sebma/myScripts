e08c8a9893215fe10af63e1148f7dbac0607fa33
sebma <sebma@users.noreply.github.com>
2026-07-09 17:11:26 +0200

    Create createSwapFile.sh

---
+++ shl/system/createSwapFile.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+swapFilePath=$1
+if [ ! -e $swapFilePath ];then
+	$sudo fallocate -l $swapSize $swapFilePath
+	$sudo chmod 0600 $swapFilePath
+	$sudo mkswap $swapFilePath
+	$sudo swapon $swapFilePath
+	grep $swapFilePath /etc/fstab -q || echo "$swapFilePath none swap sw 0 0" | $sudo tee -a /etc/fstab
+	$sudo systemctl daemon-reload
+fi
