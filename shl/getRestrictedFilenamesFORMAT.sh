2e81f53f3978b1d60e8ee7bf4633ce096e4cddf5
sebma <sebma@users.noreply.github.com>
2026-07-09 19:16:41 +0200

    Updated getRestrictedFilenamesFORMAT.sh

--- shl/getRestrictedFilenamesFORMAT.sh
+++ shl/getRestrictedFilenamesFORMAT.sh
@@ -725,6 +725,29 @@ function addThumbnail2media() {
 	fi
 	return $retCode
 }
+function videoLocalInfo {
+	local size=0
+	type -P ffprobe >/dev/null && {
+		local ffprobe='command ffprobe -hide_banner'
+		local ffprobeOptions="-probesize 400M -analyzeduration 400M"
+		for urlOrFile;do
+			echo "=> urlOrFile = $urlOrFile"
+			if echo "$urlOrFile" | egrep -q "(https?|s?ftps?|ssh|rtmp|rtsp|mms)://";then
+				#remote stream
+				echo "=> Cannot handle remote stream, next ..."
+				continue
+			else
+				#Local file
+				[ ! -s "$urlOrFile" ] && echo "=> ERROR: The file <$urlOrFile> is empty or does not exist." 1>&2 && continue
+				echo "=> This file is local to this machine."
+				size="$(\ls -l "$urlOrFile" | awk '/[0-9]+/{printf "%8.3f MiB\n",$5/1024^2}')"
+				echo "Size: $size"
+				$ffprobe "$urlOrFile" || $ffprobe $ffprobeOptions "$urlOrFile"
+			fi
+			echo
+		done 2>&1 | \egrep -vi "^ +(:\s+$|comment|description +: [^/]+$)" | uniq | egrep --color -iw "^$|description.*:/|PURL.*:/|stream|local|urlOrFile|kb/s|Input|Size:|Title\s*:|Duration:|Channel.*:|Stream|Chapter|Invalid|error|bad| no such file|^\[.* not"
+	}
+}
 
 bestFormats="bestvideo[vcodec^=avc1]+bestaudio[ext=m4a]/best[ext=mp4]/best[ext=webm]/best[ext=avi]/best[ext=mov]/best[ext=flv]"
 function getRestrictedFilenamesBEST() {
@@ -771,29 +794,6 @@ function getRestrictedFilenamesVLD() {
 	local other_Formats=vld/low/${height}p
 	local possibleFormats="best[vcodec^=avc1][height<=?$height]/bestvideo[vcodec^=avc1][height<=?$height]+bestaudio[ext=m4a]/$other_Formats"
 	getRestrictedFilenamesFORMAT -f "($possibleFormats/$bestFormats)" $@ # because of the "eval" statement in the "youtube_dl" bash variable
-}
-function videoLocalInfo {
-	local size=0
-	type -P ffprobe >/dev/null && {
-		local ffprobe='command ffprobe -hide_banner'
-		local ffprobeOptions="-probesize 400M -analyzeduration 400M"
-		for urlOrFile;do
-			echo "=> urlOrFile = $urlOrFile"
-			if echo "$urlOrFile" | egrep -q "(https?|s?ftps?|ssh|rtmp|rtsp|mms)://";then
-				#remote stream
-				echo "=> Cannot handle remote stream, next ..."
-				continue
-			else
-				#Local file
-				[ ! -s "$urlOrFile" ] && echo "=> ERROR: The file <$urlOrFile> is empty or does not exist." 1>&2 && continue
-				echo "=> This file is local to this machine."
-				size="$(\ls -l "$urlOrFile" | awk '/[0-9]+/{printf "%8.3f MiB\n",$5/1024^2}')"
-				echo "Size: $size"
-				$ffprobe "$urlOrFile" || $ffprobe $ffprobeOptions "$urlOrFile"
-			fi
-			echo
-		done 2>&1 | \egrep -vi "^ +(:\s+$|comment|description +: [^/]+$)" | uniq | egrep --color -iw "^$|description.*:/|PURL.*:/|stream|local|urlOrFile|kb/s|Input|Size:|Title\s*:|Duration:|Channel.*:|Stream|Chapter|Invalid|error|bad| no such file|^\[.* not"
-	}
 }
 
 function main() {
