68d18d588072022c5fc60948d85b341d19245c10
sebma <sebma@users.noreply.github.com>
2026-07-15 15:33:36 +0200

    Update scoopPostInstall.ps1

--- psh/scoop/scoopPostInstall.ps1
+++ psh/scoop/scoopPostInstall.ps1
@@ -24,3 +24,5 @@ if( $IsWindows ) {
 	}
 	scoopPostInstall
 }
+# Si lenteurs "scoop search" cf. https://github.com/ScoopInstaller/Scoop/issues/4491#issuecomment-2605528197
+# scoop config use_sqlite_cache true
