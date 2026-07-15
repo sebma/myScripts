679e2efedf8e78546c39fcec2dbda7474968d0b6
sebma <sebma@users.noreply.github.com>
2026-07-15 15:09:05 +0200

    Update scoopPostInstall.ps1

--- psh/scoop/scoopPostInstall.ps1
+++ psh/scoop/scoopPostInstall.ps1
@@ -2,6 +2,7 @@ function isInstalled($cmd) { return gcm "$cmd" 2>$null }
 
 if( $IsWindows ) {
 	function scoopPostInstall {
+		if( ! $(gcm "scoop" 2>$null | % Name) ) { & $env:SCOOP\shims\scoop.ps1 shim add scoop $env:SCOOP\shims\scoop.ps1 }
 		if( ! (isInstalled("git.exe")) ) {
 			sudo scoop install git -g
 		}
