60550b1c86f6f232c3ba87f87bf8055351338651
sebma <sebma@users.noreply.github.com>
2026-07-15 23:51:06 +0200

    Updated yt-dlpChapters2ffmetadata.sh

--- shl/video/yt-dlpChapters2ffmetadata.sh
+++ shl/video/yt-dlpChapters2ffmetadata.sh
@@ -6,11 +6,11 @@ function yt-dlpChapters2ffmetadata {
 		jq -r '
 ";FFMETADATA1",
 .chapters[]? as $c
-| "[CHAPTER]\n"
-+ "TIMEBASE=1/1000\n"
-+ "START=" + ($c.start_time * 1000 | floor | tostring) + "\n"
-+ "END="   + ($c.end_time   * 1000 | floor | tostring) + "\n"
-+ "title=" + $c.title // ""
+| "[CHAPTER]"
+, "TIMEBASE=1/1000"
+, "START=" + ($c.start_time * 1000 | floor | tostring)
+, "END="   + ($c.end_time   * 1000 | floor | tostring)
+, "title=" + $c.title // ""
 ' "$jsonFile" > "${jsonFile/.json/.ffmeta}"
 		ls -l "${jsonFile/.json}"*
 	done
