3f164f01b384ac35334ed8c3082ff8a75cb3bf34
sebma <sebma@users.noreply.github.com>
2026-07-15 16:57:15 +0200

    Update scoopPostInstall.ps1

--- psh/scoop/scoopPostInstall.ps1
+++ psh/scoop/scoopPostInstall.ps1
@@ -17,7 +17,7 @@ if( $IsWindows ) {
 		}
 
 		# Buckets of softwares
-		'main' , 'extras' , 'nirsoft' , 'versions' | % { scoop bucket list | Out-String -Stream | sls ^$_ || scoop bucket add $_ }
+		'main' , 'extras' , 'nirsoft' , 'versions' | % { if( ! (scoop bucket list | Out-String -Stream | sls ^$_) ) { scoop bucket add $_ } }
 #		'main' , 'extras' , 'nirsoft' , 'versions' | % { git config --global --add safe.directory `"$env:ProgramFiles/scoop/buckets/$_`" }
 
 		sudo scoop install freetube kitty gow pshazz openssh openssl-lts-light psutils wget gsudo -g
