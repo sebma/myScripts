3df3279318365ca6f17a4a7655fafe34d6993d56
sebma <sebma@users.noreply.github.com>
2026-07-15 16:55:01 +0200

    Update scoop_init_admin.ps1

--- psh/scoop/scoop_init_admin.ps1
+++ psh/scoop/scoop_init_admin.ps1
@@ -1,2 +1,4 @@
-scoop config aria2-enabled false
+# scoop config aria2-enabled false
+# Si lenteurs "scoop search" cf. https://github.com/ScoopInstaller/Scoop/issues/4491#issuecomment-2605528197
+# scoop config use_sqlite_cache true
 cat $HOME/.config/scoop/config.json
