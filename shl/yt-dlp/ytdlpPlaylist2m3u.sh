c4d7eb28de659356a11550cd8474a3fc023faf8b
sebma <sebma@users.noreply.github.com>
2026-07-16 22:44:33 +0200

    Updated ytdlpPlaylist2m3u.sh

--- shl/yt-dlp/ytdlpPlaylist2m3u.sh
+++ shl/yt-dlp/ytdlpPlaylist2m3u.sh
@@ -3,7 +3,14 @@
 function ytdlpPlaylist2m3u {
 	local playListURL="$1"
 	local ytdlpnoconfig="yt-dlp --ignore-config"
-	$ytdlpnoconfig --ignore-errors --flat-playlist -J "$playListURL" | jq -r '"#EXTM3U", .entries[]? as $e | "#EXTINF:" + ($e.duration | tostring) + "," + $e.title , $e.url'
+	local playListTitle=""
+	local tmpJsonFile=$(mktemp).json
+	$ytdlpnoconfig --ignore-errors --flat-playlist -J "$playListURL" > "$tmpJsonFile"
+	playListTitle="$(jq -r .title "$tmpJsonFile" | sed -E "s/ /_/g;s/'/_/g;s/\|/-/g;s/[éè]/e/g" | tr -d "[:\-]")"
+	playListFile="$playListTitle.m3u"
+	jq -r '"#EXTM3U", .entries[]? as $e | "#EXTINF:" + ($e.duration | tostring) + "," + $e.title , $e.url' "$tmpJsonFile" | tee "$playListFile"
+	echo "=> playListFile = <$playListFile>."
+	rm -f "$tmpJsonFile"
 }
 
 ytdlpPlaylist2m3u "$@"
