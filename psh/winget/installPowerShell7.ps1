b0cf46793be88feea606b51a9ab9530563da094d
sebma <sebma@users.noreply.github.com>
2026-07-17 16:20:49 +0200

    Update installPowerShell7.ps1

--- psh/winget/installPowerShell7.ps1
+++ psh/winget/installPowerShell7.ps1
@@ -1,3 +1,6 @@
 winget search Microsoft.PowerShell --source winget
+winget show Microsoft.PowerShell --verbose | Select-String "Installer Type|Scope"
 winget install --id Microsoft.PowerShell # local to the current user
-winget install --id Microsoft.PowerShell --scope machine --accept-package-agreements
+if( $(winget show Microsoft.PowerShell --verbose | Select-String "Scope") ) {
+	winget install --id Microsoft.PowerShell --scope machine --accept-package-agreements
+}
