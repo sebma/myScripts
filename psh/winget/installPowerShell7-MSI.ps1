96c6676ac67cc85f6c56ec553c665aa5abe2dc44
sebma <sebma@users.noreply.github.com>
2026-07-17 19:42:24 +0200

    Update installPowerShell7-MSI.ps1

--- psh/winget/installPowerShell7-MSI.ps1
+++ psh/winget/installPowerShell7-MSI.ps1
@@ -1,4 +1,5 @@
+# See https://learn.microsoft.com/en-gb/powershell/scripting/install/install-powershell-on-windows?view=powershell-7.6#install-powershell-using-winget
 winget search Microsoft.PowerShell --source winget
-winget install --id Microsoft.PowerShell --installer-type wix
+winget install --id Microsoft.PowerShell --source winget --installer-type wix
 gcm pwsh | % Source
 pwsh -c "gcm pwsh | % Source"
