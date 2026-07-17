04d10702c8a381c57c4b0b12134f26e7b13c6592
sebma <sebma@users.noreply.github.com>
2026-07-17 11:21:44 +0200

    Update scaldiskReplaceNewDisk.sh

--- shl/system/products/Scality-RING/scaldiskReplaceNewDisk.sh
+++ shl/system/products/Scality-RING/scaldiskReplaceNewDisk.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 disk=$(time scaldisk iods list | awk '/OOS_PERM/{print$1;exit}')
-while [ -n "$disk" ]
-do
+while [ -n "$disk" ];do
 	scaldisk replace -d $disk
 	disk=$(time scaldisk iods list | awk '/OOS_PERM/{print$1;exit}')
 done
