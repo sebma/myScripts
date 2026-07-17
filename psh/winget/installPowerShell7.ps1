a2d92ae7266973b93b477f1a04510c4ffdc91c23
sebma <sebma@users.noreply.github.com>
2026-07-17 16:02:40 +0200

    Create installPowerShell7.ps1

---
+++ psh/winget/installPowerShell7.ps1
@@ -0,0 +1,3 @@
+winget search Microsoft.PowerShell --source winget
+winget install --id Microsoft.PowerShell # local to the current user
+winget install --id Microsoft.PowerShell --scope machine --accept-package-agreements
