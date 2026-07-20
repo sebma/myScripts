a76495199fa5784c1bfbd4098bf82939360c15a1
sebma <sebma@users.noreply.github.com>
2026-07-20 11:10:10 +0200

    Update profile.Windows.ps1

--- psh/Documents/WindowsPowerShell/profile.Windows.ps1
+++ psh/Documents/WindowsPowerShell/profile.Windows.ps1
@@ -173,6 +173,24 @@ function getServiceTag {
 	}
 }
 
+function get-Service-Info {
+	$ErrorActionPreference = 'Stop'
+	$argc=$args.Count
+	if ( $argc -gt 0) {
+		for($i=0;$i -lt $argc;$i++) {
+			$serviceName = $args[$i]
+			echo "=> serviceName = <$serviceName> :`n"
+			try {
+				Get-Service $serviceName >$null 2>&1
+				Get-CimInstance win32_service | ? Name -eq $serviceName | Select Name , Caption , State , ServiceType , StartMode , ErrorControl , PathName , TagId , StartName , DelayedAutoStart
+			} catch {
+				echo "=> WARNING: The service <$serviceName> does not exist."
+				echo ""
+			}
+		}
+	}
+}
+
 function osName {
 	echo $(gwmi Win32_OperatingSystem).Caption
 }
