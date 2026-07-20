2a79566436d1d08c320f664482aa653ae421b693
sebma <sebma@users.noreply.github.com>
2026-07-20 10:46:54 +0200

    Update findService.ps1

--- psh/findService.ps1
+++ psh/findService.ps1
@@ -10,7 +10,6 @@ function findService {
 		write-warning "Usage:$scriptName [regexp=.]"
 		exit 1
 	}
-#	Get-WmiObject win32_service | select Name , DisplayName , ServiceType , StartMode , State , ProcessId , PathName | ogv
 	Get-WmiObject win32_service | ? { $_.Description -Match "$regexp" -or $_.Name -Match "$regexp" -or $_.DisplayName -Match "$regexp" } | Format-Table Name , DisplayName , ServiceType , StartMode , State , ProcessId , PathName
 }
 
