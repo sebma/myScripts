fc7154ddbcc360ade1b5aecf28e3b2e59e27649c
sebma <sebma@users.noreply.github.com>
2026-07-21 16:18:14 +0200

    Update robocopy-PS5.ps1

--- psh/system/robocopy-PS5.ps1
+++ psh/system/robocopy-PS5.ps1
@@ -10,7 +10,7 @@ function robocopyPS {
 	$sourceDIR = $args[0]
 	$destinationDIR = $args[1]
 	$sourceBaseName = $sourceDIR.Split('\')[-1]
-	$destinationDIR =+ $dirSep + $sourceBaseName
+	$destinationDIR += $dirSep + $sourceBaseName
 	$logDIR = "C:\TEMP\Robocopy\Logs"
 	$logFile = $logDIR + '\' + $sourceBaseName + '.log'
 	$nbThreads = $(Get-WmiObject Win32_Processor).NumberOfLogicalProcessors
