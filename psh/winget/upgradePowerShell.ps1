574618467bb5a4f18d8a6f79e9c2919facd4f2e4
sebma <sebma@users.noreply.github.com>
2026-07-21 16:50:56 +0200

    Update upgradePowerShell.ps1

--- psh/winget/upgradePowerShell.ps1
+++ psh/winget/upgradePowerShell.ps1
@@ -1,5 +1,5 @@
 # winget list --scope user --id Microsoft.PowerShell
-if ( (winget list --scope machine --id Microsoft.PowerShell) ) {
+if ( (winget list --scope machine --id Microsoft.PowerShell --accept-source-agreements) ) {
 	winget upgrade --scope machine Microsoft.PowerShell
 } else {
 	winget upgrade --scope user Microsoft.PowerShell
