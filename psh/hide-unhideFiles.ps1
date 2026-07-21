7e2e2130716ef660e3f49567f8a9f2a2fd6387d8
sebma <sebma@users.noreply.github.com>
2026-07-21 17:29:34 +0200

    Update hide-unhideFiles.ps1

--- psh/hide-unhideFiles.ps1
+++ psh/hide-unhideFiles.ps1
@@ -4,8 +4,8 @@ function hide-unhideFiles {
         if ( $argc -gt 0 ) {
                 for($i=0;$i -lt $argc;$i++) {
                         $file = $args[$i]
-                        (Get-ItemProperty $file).Attributes = (Get-ItemProperty $file).Attributes -bxor [io.fileattributes]::Hidden
-                        "=> Attributes of $file : " + (Get-ItemProperty $file).Attributes
+                        $(Get-ItemProperty $file).Attributes = $(Get-ItemProperty $file).Attributes -bxor [io.fileattributes]::Hidden
+                        "=> Attributes of $file : " + $(Get-ItemProperty $file).Attributes
                 }
         }
 }
