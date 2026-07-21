9a3aa78d212d751b92dd43c3781f3fc3ad222610
sebma <sebma@users.noreply.github.com>
2026-07-21 15:41:17 +0200

    Update lsgroup.ps1

--- psh/lsgroup.ps1
+++ psh/lsgroup.ps1
@@ -1,7 +1,7 @@
 function lsgroup {
 	$DC = $env:LOGONSERVER.Substring(2)
 	$argc=$args.Count
-	if ( $argc -gt 0) {
+	if ( $argc -gt 0 ) {
 		for($i=0;$i -lt $argc;$i++) {
   			$group = $args[$i]
 			echo "=> Members of group $group :"
