0cfef8141b6abbb6728fed14cc385b3b96bafdfe
sebma <sebma@users.noreply.github.com>
2026-07-21 15:39:46 +0200

    Create hide-unhideFiles.ps1

---
+++ psh/hide-unhideFiles.ps1
@@ -0,0 +1,14 @@
+$scriptName = Split-Path -Leaf $PSCommandPath
+function hide-unhideFiles {
+        $argc=$args.Count
+        $regexp = "."
+        if ( $argc -gt 0) {
+                for($i=0;$i -lt $argc;$i++) {
+                        $file = $args[$i]
+                        (Get-ItemProperty $file).Attributes = (Get-ItemProperty $file).Attributes -bxor [io.fileattributes]::Hidden
+                        "=> Attributes of $file : " + (Get-ItemProperty $file).Attributes
+                }
+        }
+}
+
+hide-unhideFiles @args
