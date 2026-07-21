2b73af2a81528e464435d9ec095446cfa663e487
sebma <sebma@users.noreply.github.com>
2026-07-21 17:01:32 +0200

    Create downloadPowerShell.ps1

---
+++ psh/winget/downloadPowerShell.ps1
@@ -0,0 +1,4 @@
+$PS7DirectDownloadURL = "https://github.com/PowerShell/PowerShell/releases/latest/download/PowerShell-7.6.4-win-x64.msi"
+$downloadedFile = $PS7DirectDownloadURL.split('/')[-1]
+Invoke-WebRequest $PS7DirectDownloadURL -OutFile $downloadedFile
+echo "=> downloadedFile = $downloadedFile"
