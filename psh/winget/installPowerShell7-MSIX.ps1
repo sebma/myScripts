2423ecdb043b790a8e8a4d73fe4f36f3d41c5644
sebma <sebma@users.noreply.github.com>
2026-07-21 16:31:43 +0200

    Update installPowerShell7-MSIX.ps1

--- psh/winget/installPowerShell7-MSIX.ps1
+++ psh/winget/installPowerShell7-MSIX.ps1
@@ -1,12 +1,15 @@
 # See https://learn.microsoft.com/en-gb/powershell/scripting/install/install-powershell-on-windows#install-powershell-using-winget
 winget search Microsoft.PowerShell --source winget
 winget show Microsoft.PowerShell --verbose | Select-String "Installer Type|Scope"
+
 if( $(winget show Microsoft.PowerShell --verbose | Select-String "Scope") ) {
 	winget install --id Microsoft.PowerShell --scope machine --accept-package-agreements
 } else {
 	# local to the current user
 	winget install --id Microsoft.PowerShell
 }
+
+winget list Microsoft.PowerShell
 gcm pwsh | % Source
 pwsh -c "gcm pwsh | % Source"
 Get-AppxPackage -AllUsers Microsoft.PowerShell
