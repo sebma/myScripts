dcba676b67fa881295787dfef624fadd5aae1d4b
sebma <sebma@users.noreply.github.com>
2026-07-21 17:06:08 +0200

    Update downloadPowerShell7.ps1

--- psh/winget/downloadPowerShell7.ps1
+++ psh/winget/downloadPowerShell7.ps1
@@ -1,3 +1,4 @@
+# https://aka.ms/PowerShell-Release?release=v7.6.4
 $PS7DirectDownloadURL = "https://github.com/PowerShell/PowerShell/releases/latest/download/PowerShell-7.6.4-win-x64.msi"
 $downloadedFile = $PS7DirectDownloadURL.split('/')[-1]
 Invoke-WebRequest $PS7DirectDownloadURL -OutFile $downloadedFile
