0fad10f2b6b96fbb3088b7dbdd81411c9651a55d
sebma <sebma@users.noreply.github.com>
2026-07-21 16:53:38 +0200

    Update upgradePowerShell.ps1

--- psh/winget/upgradePowerShell.ps1
+++ psh/winget/upgradePowerShell.ps1
@@ -1,6 +1,8 @@
 # winget list --scope user --id Microsoft.PowerShell
 if ( (winget list --scope machine --id Microsoft.PowerShell --accept-source-agreements) ) {
 	winget upgrade --scope machine Microsoft.PowerShell
-} else {
+} elseif ( (winget list --scope user --id Microsoft.PowerShell --accept-source-agreements) ) {
 	winget upgrade --scope user Microsoft.PowerShell
+} else {
+	echo "=> Microsoft.PowerShell is not installed with winget."
 }
