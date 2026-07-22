4ccd0846714a6a44c0c7f4f8aa279441632a6c24
sebma <sebma@users.noreply.github.com>
2026-07-22 22:25:45 +0200

    Updated check-TEAP-Certificates.ps1

--- psh/check-TEAP-Certificates.ps1
+++ psh/check-TEAP-Certificates.ps1
@@ -1,2 +1,3 @@
+# vim: ft=ps1 noet:
 ls Cert:\CurrentUser\My  | ? Subject -IMatch CN=.*media-participations.com | select Subject , Issuer , NotBefore , NotAfter | fl
 ls Cert:\LocalMachine\My | ? Subject -IMatch CN=$env:COMPUTERNAME | select Subject , Issuer , NotBefore , NotAfter | fl
