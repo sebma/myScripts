908f89035ec82f0292bc1115312c8f08e6d6190b
sebma <sebma@users.noreply.github.com>
2026-07-30 20:42:14 +0200

    Updated updateThunderbird.sh

--- shl/updateThunderbird.sh
+++ shl/updateThunderbird.sh
@@ -68,7 +68,7 @@ function main {
 	for package in $wgetThunderbirdPackageList
 	do
 		echo "=> Downloading $package ..."
-		wget -nv -P $tmpDir/ -nd -nH -A "${package}_*$LTSReleaseNumber*$arch.deb" -rl1 $baseUrl
+		wget -nv -P $tmpDir/ -nd -nH -A "${package}_*$LTSReleaseNumber*$arch.deb" -r -l1 $baseUrl
 		ls -v $tmpDir/$package* >/dev/null || exit
 		remoteVersion=$(ls -v $tmpDir/${package}_* | cut -d_ -f2 | tail -1)
 		localVersion=$(dpkg-query --showformat='${Version}' -W $package | cut -d: -f2-)
