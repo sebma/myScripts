c3396705ec1d485cb9a1220c79af891192dcd667
sebma <sebma@users.noreply.github.com>
2026-07-30 21:09:49 +0200

    Updated getFiles.sh

--- shl/wget/getFiles.sh
+++ shl/wget/getFiles.sh
@@ -7,7 +7,7 @@ test $# = 0 && {
 
 lastArg="$(eval echo \${$#})"
 baseUrl="$(echo $lastArg | \sed -r "s,(https?|s?ftps?)://,," | cut -d/ -f1)"
-wget="command wget"
+wget="command wget2"
 
 #time $wget --output-file=$0-$(date +%Y%m%d-%H_%M_%S).log --no-verbose --no-parent --continue --timestamping --server-response --random-wait --no-directories --directory-prefix=$baseUrl/ --user-agent=Mozilla --content-disposition --convert-links --page-requisites --recursive --level=1 --reject index.html --accept $@
 #time $wget --no-verbose --no-parent --continue --timestamping --server-response --random-wait --no-directories --directory-prefix=$baseUrl/ --user-agent=Mozilla --content-disposition --convert-links --page-requisites --recursive --level=1 --reject index.html --accept "$@"
