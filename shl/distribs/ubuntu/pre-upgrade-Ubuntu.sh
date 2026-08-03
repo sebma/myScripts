6e287866932dd2ff56ea7e7ce36c60ec18fbd7cc
sebma <sebma@users.noreply.github.com>
2026-08-03 17:58:10 +0200

    Update pre-upgrade-Ubuntu.sh

--- shl/distribs/ubuntu/pre-upgrade-Ubuntu.sh
+++ shl/distribs/ubuntu/pre-upgrade-Ubuntu.sh
@@ -25,37 +25,37 @@ fi
 grep ::proxy /etc/apt/apt.conf.d/*proxy
 
 ################## DEPLACEMENT ES CONF DANS DES SOUS REPERTOIRES #####################
-sudo mkdir -p /etc/systemd/timesyncd.conf.d/
+$sudo mkdir -p /etc/systemd/timesyncd.conf.d/
 if egrep 'NTP=[0-9.]+' /etc/systemd/timesyncd.conf -q 2>/dev/null;then
-	sudo mv -v /etc/systemd/timesyncd.conf /etc/systemd/timesyncd.conf.d/
-	sudo apt -V install --reinstall -o Dpkg::Options::="--force-confask,confnew,confmiss" systemd-timesyncd
-	sudo systemctl restart systemd-timesyncd.service
+	$sudo mv -v /etc/systemd/timesyncd.conf /etc/systemd/timesyncd.conf.d/
+	$sudo apt -V install --reinstall -o Dpkg::Options::="--force-confask,confnew,confmiss" systemd-timesyncd
+	$sudo systemctl restart systemd-timesyncd.service
 fi
 
-sudo mkdir -p /etc/snmp/snmpd.conf.d/
-if sudo grep -i '^agentAddress' /etc/snmp/snmpd.conf -q 2>/dev/null;then
-	sudo mv -v /etc/snmp/snmpd.conf /etc/snmp/snmpd.conf.d/
-	sudo apt -V install --reinstall -o Dpkg::Options::="--force-confask,confnew,confmiss" snmpd
-	sudo systemctl restart snmpd.service
+$sudo mkdir -p /etc/snmp/snmpd.conf.d/
+if $sudo grep -i '^agentAddress' /etc/snmp/snmpd.conf -q 2>/dev/null;then
+	$sudo mv -v /etc/snmp/snmpd.conf /etc/snmp/snmpd.conf.d/
+	$sudo apt -V install --reinstall -o Dpkg::Options::="--force-confask,confnew,confmiss" snmpd
+	$sudo systemctl restart snmpd.service
 fi
 
-sudo apt install -V open-vm-tools aptitude deborphan ripgrep htop dfc pv ncdu fd-find jq -y
-[ $majorNumber -ge 22 ] && sudo apt install -V plocate -y
+$sudo apt install -V open-vm-tools aptitude deborphan ripgrep htop dfc pv ncdu fd-find jq -y
+[ $majorNumber -ge 22 ] && $sudo apt install -V plocate -y
 
-[ $http_proxy  ] && sudo snap get system proxy.http  -l 2>/dev/null | grep proxy.http  -wq || time sudo snap set system proxy.http=$http_proxy
-[ $https_proxy ] && sudo snap get system proxy.https -l 2>/dev/null | grep proxy.https -wq || time sudo snap set system proxy.https=$https_proxy
-sudo snap get system proxy -l
+[ $http_proxy  ] && $sudo snap get system proxy.http  -l 2>/dev/null | grep proxy.http  -wq || time $sudo snap set system proxy.http=$http_proxy
+[ $https_proxy ] && $sudo snap get system proxy.https -l 2>/dev/null | grep proxy.https -wq || time $sudo snap set system proxy.https=$https_proxy
+$sudo snap get system proxy -l
 snap debug connectivity
 
-sudo grep '^\s*Defaults:%sudo env_keep.*https_proxy' /etc/sudoers /etc/sudoers.d/* 2>/dev/null -q || echo 'Defaults:%sudo env_keep += "http_proxy https_proxy ftp_proxy all_proxy no_proxy HTTP_PROXY HTTPS_PROXY FTP_PROXY ALL_PROXY NO_PROXY"' | sudo tee -a /etc/sudoers.d/proxy_env
-test -s /etc/sudoers.d/proxy_env && sudo chmod 640 /etc/sudoers.d/proxy_env
+$sudo grep '^\s*Defaults:%sudo env_keep.*https_proxy' /etc/sudoers /etc/sudoers.d/* 2>/dev/null -q || echo 'Defaults:%sudo env_keep += "http_proxy https_proxy ftp_proxy all_proxy no_proxy HTTP_PROXY HTTPS_PROXY FTP_PROXY ALL_PROXY NO_PROXY"' | $sudo tee -a /etc/sudoers.d/proxy_env
+test -s /etc/sudoers.d/proxy_env && $sudo chmod 640 /etc/sudoers.d/proxy_env
 
 if ! which ppa-purge >/dev/null 2>&1;then
-	sudo apt install -V ppa-purge -y
+	$sudo apt install -V ppa-purge -y
 fi
 
 ls /etc/apt/sources.list.d/ | awk -F- "/$(lsb_release -sc).list$/"'{print$1"/"$3}' | while read repo;do
 	unset http_proxy https_proxy
-	sudo ppa-purge ppa:$repo -y
-	sudo add-apt-repository ppa:$repo -r -y
+	$sudo ppa-purge ppa:$repo -y
+	$sudo add-apt-repository ppa:$repo -r -y
 done
