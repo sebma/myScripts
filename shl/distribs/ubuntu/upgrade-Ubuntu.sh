6dd4652c368cadf1d1b066d96107816c4b314420
sebma <sebma@users.noreply.github.com>
2026-08-03 16:16:22 +0200

    Update upgrade-Ubuntu.sh

--- shl/distribs/ubuntu/upgrade-Ubuntu.sh
+++ shl/distribs/ubuntu/upgrade-Ubuntu.sh
@@ -4,8 +4,7 @@ set -u
 scriptBaseName=${0/*\//}
 
 if do-release-upgrade -c | grep New.release.*LTS.*available.;then
-	sudo apt update
-	sudo apt upgrade -Vy
+	sudo apt update && sudo apt upgrade -Vy
 	sudo aptitude upgrade -Vy || sudo apt install -V $(apt list --upgradable 2>/dev/null | awk -F"," "/$(lsb_release -sc)/"'{print$1}') -y
 	if ! do-release-upgrade;then
 		sudo apt install --reinstall -V ubuntu-keyring
