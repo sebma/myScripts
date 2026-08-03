20b971eef1843f314b62f3912e0f87ae49960b89
sebma <sebma@users.noreply.github.com>
2026-08-03 16:29:16 +0200

    Update upgrade-Ubuntu.sh

--- shl/distribs/ubuntu/upgrade-Ubuntu.sh
+++ shl/distribs/ubuntu/upgrade-Ubuntu.sh
@@ -3,12 +3,17 @@
 set -u
 scriptBaseName=${0/*\//}
 
+if ! do-release-upgrade;then
+	sudo apt install --reinstall -V ubuntu-keyring
+fi
+
 if do-release-upgrade -c | grep New.release.*LTS.*available.;then
 	sudo apt update && sudo apt upgrade -Vy
-	sudo aptitude upgrade -Vy || sudo apt install -V $(apt list --upgradable 2>/dev/null | awk -F"," "/$(lsb_release -sc)/"'{print$1}') -y
-	if ! do-release-upgrade;then
-		sudo apt install --reinstall -V ubuntu-keyring
+	if sudo apt-get upgrade -V -s | grep 'and [^0][0-9]* not upgraded';then
+		sudo aptitude install -V $(apt list --upgradable 2>/dev/null | awk -F"/" "/$(lsb_release -sc)/"'{print$1}') -y
+		sudo apt install -V $(apt list --upgradable 2>/dev/null | awk -F"/" "/$(lsb_release -sc)/"'{print$1}') -y || exit
 	fi
+
 	do-release-upgrade
 	sudo apt autoremove -Vy
 fi
