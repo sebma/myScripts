a612008993131fbc6b243361ecd9ab1cdd5fc741
sebma <sebma@users.noreply.github.com>
2026-08-03 12:07:15 +0200

    Update findGroup.ps1

--- psh/findGroup.ps1
+++ psh/findGroup.ps1
@@ -1,3 +1,3 @@
-$myPattern = '*'+$args[0]+'*'
+$myRegExp = $args[0]
 $DC = $env:LOGONSERVER.Substring(2)
-Get-ADGroup -Properties CN , CanonicalName , Created , Description -Filter { name -like $myPattern }
+Get-ADGroup -Server $DC -Properties CN , CanonicalName , Created, Modified , Description -Filter '*' | ? Name -iMatch $myRegExp
