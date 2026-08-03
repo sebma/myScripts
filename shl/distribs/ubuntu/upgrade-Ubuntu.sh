ddb8196975781c58d4aa095dc45a4dfc28711b4a
sebma <sebma@users.noreply.github.com>
2026-08-03 20:50:29 +0200

    Updated upgrade-Ubuntu.sh

--- shl/distribs/ubuntu/upgrade-Ubuntu.sh
+++ shl/distribs/ubuntu/upgrade-Ubuntu.sh
@@ -3,22 +3,33 @@
 set -u
 scriptBaseName=${0/*\//}
 
-sudo apt autoremove -V
+test $(id -u) == 0 && sudo="" || sudo=$(type -P sudo)
+distribID=$(source /etc/os-release;echo $ID)
+if   echo $distribID | egrep "centos|rhel|fedora" -q;then
+	isRedHatLike=true
+elif echo $distribID | egrep "debian|ubuntu" -q;then
+	isDebianLike=true
+	if echo $distribID | egrep "ubuntu" -q;then
+		isUbuntuLike=true
+	fi
+fi
+
+$sudo apt autoremove -V
 if ! do-release-upgrade -c;then
-	sudo apt install --reinstall -V ubuntu-keyring
+	$sudo apt install --reinstall -V ubuntu-keyring
 fi
 
 if do-release-upgrade -c | grep New.release.*LTS.*available.;then
-	sudo apt update && sudo apt upgrade -Vy
-	if sudo apt-get upgrade -V -s | grep 'and [^0][0-9]* not upgraded';then
+	$sudo apt update && $sudo apt upgrade -Vy
+	if $sudo apt-get upgrade -V -s | grep 'and [^0][0-9]* not upgraded';then
 		packagesList=$(apt list --upgradable 2>/dev/null | awk -F"/" "/$(lsb_release -sc)/"'{print$1}' | paste -sd " ")
 		packagesRegExp=${packagesList/ /|}
-		sudo aptitude install -V $packagesList -y
-		if ! sudo apt install -V $packagesList -y;then
-			sudo apt purge -V $(deborphan | egrep "$packagesRegExp")
+		$sudo aptitude install -V $packagesList -y
+		if ! $sudo apt install -V $packagesList -y;then
+			$sudo apt purge -V $(deborphan | egrep "$packagesRegExp")
 		fi
 	fi
 
 	do-release-upgrade
 fi
-sudo apt autoremove -V
+$sudo apt autoremove -V
