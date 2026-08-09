c86c4d56547cc60d11accbeb4aaa915b4bd8b1cf
sebma <sebma@users.noreply.github.com>
2026-08-09 13:11:34 +0200

    Updated ./findBigFiles.ps1

--- psh/findBigFiles.ps1
+++ psh/findBigFiles.ps1
@@ -2,9 +2,4 @@
 $nbLines = 48
 if( $args.Count -eq 0 ) { $dirName = "." } else { $dirName = $args[0] }
 # " => $dirName = " + $dirName
-
-#dir -Force -r -file $dirName | Sort-Object Length -desc | select @{ n= "RelativePath"; e={ Resolve-Path -Relative $_.fullname } } , @{ n="Size"; e={ "{0,5:n3} MiB" -f ($_.length / 1mb) } } | select -f $nbLines
-#dir -Force -r -file $dirName | Sort-Object Length -desc | select -f $nbLines | select @{ n= "RelativePath"; e={ Resolve-Path -Relative $_.fullname } } , @{ n="Size"; e={ "{0,5:n3} MiB" -f ($_.length / 1mb) } }
-#dir -Force -r -file $dirName | ? Length -gt 10mb | select -Property fullName,Length | Sort-Object Length -desc | select -f $nbLines | select @{ n= "RelativePath"; e={ Resolve-Path -Relative $_.fullname } } , @{ n="Size"; e={ "{0,5:n3} MiB" -f ($_.length / 1mb) } }
-
-dir -Force -r $dirName 2>$null | ? Length -gt 1mb | select fullname,length | Sort-Object Length -desc | select @{ n="Size"; e={ "{0,5:n3} MiB" -f ($_.length / 1mb) } } , @{ n= "RelativePath"; e={ Resolve-Path -Relative $_.fullname } } | select -f $nbLines
+dir -Force -r -file $dirName 2>$null | ? Length -gt 10mb | select Length , FullName | Sort-Object Length -desc | select -f $nbLines | select @{ n="Size"; e={ "{0,5:n3} MiB" -f ($_.length / 1mb) } } , @{ n= "RelativePath"; e={ Resolve-Path -Relative -LiteralPath $_.fullname } }
