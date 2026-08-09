66ab0bfeae60f71dae29423a19ce144e3bd2f81d
sebma <sebma@users.noreply.github.com>
2026-08-09 11:57:54 +0200

    Updated topflines.sh

--- shl/topflines.sh
+++ shl/topflines.sh
@@ -2,8 +2,9 @@
 
 topflines () { 
 	local find="command find"
-	local findExcludedPathList="./proc ./sys ./dev"
-	local findPrunePathsExpression="( -type d -a ( -name .git $(printf -- " -o -path %s" $findExcludedPathList) ) ) -prune -o"
+	local findExcludedPathList="/proc /sys /dev"
+	local findPrunePathsExpression="( -type d -a ( -path .git/ $(printf -- " -o -path %s" $findExcludedPathList) ) ) -prune -o"
+	local -i LINES=$(tput lines)
 
 	$find "$@" -xdev $findPrunePathsExpression -type f -size +10M -exec ls -l --block-size=M --time-style=+"%Y-%m-%d %T" {} \; 2> /dev/null | sort -nrk5 | head -n $(($LINES-4)) | numfmt --field 5 --from=iec --to=iec-i --suffix=B
 }
