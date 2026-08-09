faad19ec79540bd6a24150f8fe0d2a02cb86a792
sebma <sebma@users.noreply.github.com>
2026-08-09 11:57:54 +0200

    Updated topf.sh

--- shl/topf.sh
+++ shl/topf.sh
@@ -2,11 +2,12 @@
 
 topf () { 
 	local find="command find"
-	local findExcludedPathList="./proc ./sys ./dev"
-	local findPrunePathsExpression="( -type d -a ( -name .git $(printf -- " -o -path %s" $findExcludedPathList) ) ) -prune -o"
-	[ $# = 0 ] && local lines=10 || local lines=$1
+	local findExcludedPathList="/proc /sys /dev"
+	local findPrunePathsExpression="( -type d -a ( -path .git/ $(printf -- " -o -path %s" $findExcludedPathList) ) ) -prune -o"
+	local -i lines=10
+	[ $# -ge 1 ] && local lines=$1 && shift
 
-    $find . -xdev $findPrunePathsExpression -type f -size +10M -exec ls -l --block-size=M --time-style=+"%Y-%m-%d %T" {} \; 2> /dev/null | sort -nrk5 | head -n $lines | numfmt --field 5 --from=iec --to=iec-i --suffix=B
+    $find "$@" -xdev $findPrunePathsExpression -type f -size +10M -exec ls -l --block-size=M --time-style=+"%Y-%m-%d %T" {} \; 2> /dev/null | sort -nrk5 | head -n $lines | numfmt --field 5 --from=iec --to=iec-i --suffix=B
 }
 
 topf "$@"
