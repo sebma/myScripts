a99401dbbff6740073436a49b0925d699b4a557f
sebma <sebma@users.noreply.github.com>
2026-08-19 00:35:17 +0200

    Updated str2qrcode_with_qrencode.sh

--- shl/str2qrcode_with_qrencode.sh
+++ shl/str2qrcode_with_qrencode.sh
@@ -2,12 +2,14 @@
 
 string2qrcode_with_qrencode () {
 	local dotSize=8 string=""
-	local width=$(xrandr | awk '-Fx| *' '/\+([^0]|$)/{printf$2;exit}')
-	local heigth=$(xrandr | awk '-Fx| *' '/\+([^0]|$)/{printf$3;exit}')
+	local width=0
+	local heigth=0
+
 	if [ $# != 1 ]; then
 		echo "=> Usage $FUNCNAME stringOrURL" >&2
 		return -1
 	else
+		read width heigth <<< $(xrandr | awk '-Fx| *' '/\+([^0]|$)/{print$2" "$3;exit}')
 		string="$1"
 		qrencode -l H -s $dotSize -m 0 -o- "$string" | feh -g +$((($width-$dotSize)/2))+$((($heigth-$dotSize)/2)) -
 	fi
