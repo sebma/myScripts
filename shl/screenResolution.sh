5759ad10595b133abd288b43ca2b8eb4cfcd3b3a
sebma <sebma@users.noreply.github.com>
2026-08-18 23:59:19 +0200

    Updated screenResolution.sh

--- shl/screenResolution.sh
+++ shl/screenResolution.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-xrandr | awk -F'x| *' '/[^s]connected/{printf$1":\t"} /\+([^0]|$)/{print $2"x"$3}'
+xrandr | awk -F'x| *' '/[^s]connected/{printf$1":\t"}/\+([^0]|$)/{print $2"x"$3}'
