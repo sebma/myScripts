05bb4734560829dcf2a9886c631cb3d7027e07ea
sebma <sebma@users.noreply.github.com>
2026-08-19 14:15:14 +0200

    Update vmGet-Snapshot-List.ps1

--- psh/system/vsphere/vmGet-Snapshot-List.ps1
+++ psh/system/vsphere/vmGet-Snapshot-List.ps1
@@ -5,7 +5,7 @@ $days = $($args[1]).ToInt16()
 $CutoffDate = (Get-Date).AddDays($day)
 
 Get-Cluster "$clusterName" | Get-VM | Get-Snapshot | Where-Object { $_.Created -lt $CutoffDate } | Sort-Object `
-@{Expression='Created'; Descending=$false}, @{Expression='SizeGB'; Descending=$true} | Format-Table `
+@{Expression='SizeGB'; Descending=$true} , @{Expression='Created'; Descending=$false} | Format-Table `
 @{Label='VM';Expression={$_.VM.Name}}, Name, @{Label='Created';Expression={$_.Created.ToString('yyyy-MM-dd HH:mm:ss')}},
 @{Label='AgeDays';Expression={(New-TimeSpan $_.Created (Get-Date)).Days}},
 @{Label='SizeGB';Expression={"{0,12:N3}" -f $_.SizeGB}} -AutoSize
