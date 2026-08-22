b91041c6171cd76acb452c18d4bb184f95e99cbf
sebma <sebma@users.noreply.github.com>
2026-08-22 11:54:29 +0200

    Updated upgrade-libreoffice-language-pack.sh

--- shl/mac/upgrade-libreoffice-language-pack.sh
+++ shl/mac/upgrade-libreoffice-language-pack.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+test $(id -u) == 0 && sudo="" || sudo=$(type -P sudo)
+
 if [ $(uname -s) == Darwin ];then
-	# brew reinstall --cask libreoffice-language-pack
+	# brew reinstall --cask libreoffice-language-pack --language=fr
 	brew fetch --cask libreoffice-language-pack
 	open -W -a libreoffice
-	open -W $(brew --cache --cask libreoffice-language-pack)
-	read
+	#open -W $(brew --cache --cask libreoffice-language-pack)
+	dmgMountDir=$(hdiutil attach $(brew --cache --cask libreoffice-language-pack) | awk  '/Volumes.LibreOffice/{for(i=3;i<NF;++i)printf$i" ";print$NF}')
+	ls "$dmgMountDir/LibreOffice Language Pack.app" && $sudo open -W "$dmgMountDir/LibreOffice Language Pack.app" || exit
 	vdisk=$(df | awk '/LibreOffice/{print$1}')
 	diskutil eject $vdisk
 fi
