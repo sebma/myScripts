ca79880b00c24a3133373fb2f4fe207cd77c98e3
sebma <sebma@users.noreply.github.com>
2026-08-22 17:37:16 +0200

    Updated str2qrcode_with_qrencode.sh

--- shl/str2qrcode_with_qrencode.sh
+++ shl/str2qrcode_with_qrencode.sh
@@ -10,13 +10,9 @@ string2qrcode_with_qrencode () {
 		echo "=> Usage $FUNCNAME stringOrURL" >&2
 		return -1
 	else
-		string="$1"
-		if [ $osFamily == Linux ];then
-			read width heigth <<< $(xrandr 2>/dev/null | awk -F'x| *' '/\+([^0]|$)/{printf$2" "$3;exit}')
-		elif [ $osFamily == Darwin ];then
-			read width heigth <<< $(xrandr 2>/dev/null | awk -F'[x+ ]' '/connected/{print$3" "$4;exit}')
-		fi
-		qrencode -l H -s $dotSize -m 0 -o- "$string" | feh -g +$((($width-$dotSize)/2))+$((($heigth-$dotSize)/2)) -
+		[ $osFamily == Linux ] &&  read width heigth <<< $(xrandr 2>/dev/null | awk -F'x| *' '/\+([^0]|$)/{printf$2" "$3;exit}')
+		[ $osFamily == Darwin ] && read width heigth <<< $(xrandr 2>/dev/null | awk -F'[x+ ]' '/connected/{print$3" "$4;exit}')
+		qrencode -l H -s $dotSize -m 0 -o- "$1" | feh -g +$((($width-$dotSize)/2))+$((($heigth-$dotSize)/2)) -
 	fi
 }
 
