9d296efc45cc6549a9a0c1aff5bd34398fb4f826
sebma <sebma@users.noreply.github.com>
2026-08-22 17:30:26 +0200

    Updated str2qrcode_with_qrencode.sh

--- shl/str2qrcode_with_qrencode.sh
+++ shl/str2qrcode_with_qrencode.sh
@@ -4,13 +4,18 @@ string2qrcode_with_qrencode () {
 	local dotSize=8 string=""
 	local width=0
 	local heigth=0
+	local osFamily=$(uname -s)
 
 	if [ $# != 1 ]; then
 		echo "=> Usage $FUNCNAME stringOrURL" >&2
 		return -1
 	else
-		read width heigth <<< $(xrandr | awk '-Fx| *' '/\+([^0]|$)/{printf$2" "$3;exit}')
 		string="$1"
+		if [ $osFamily == Linux ];then
+			read width heigth <<< $(xrandr 2>/dev/null | awk -F'x| *' '/\+([^0]|$)/{printf$2" "$3;exit}')
+		elif [ $osFamily == Darwin ];then
+			:
+		fi
 		qrencode -l H -s $dotSize -m 0 -o- "$string" | feh -g +$((($width-$dotSize)/2))+$((($heigth-$dotSize)/2)) -
 	fi
 }
