8da369c33113133a4178d22f071213993bc46907
sebma <sebma@users.noreply.github.com>
2026-08-24 12:14:53 +0200

    Updated str2qrcode_with_qrencode.sh

--- shl/str2qrcode_with_qrencode.sh
+++ shl/str2qrcode_with_qrencode.sh
@@ -11,7 +11,7 @@ string2qrcode_with_qrencode () {
 		return -1
 	else
 		[ $osFamily == Linux ] &&  read width heigth <<< $(xrandr 2>/dev/null | awk -F'x| *' '/\+([^0]|$)/{printf$2" "$3;exit}')
-		[ $osFamily == Darwin ] && read width heigth <<< $(xrandr 2>/dev/null | awk -F'[x+ ]' '/connected/{print$3" "$4;exit}')
+		[ $osFamily == Darwin ] && read width heigth <<< $(xdpyinfo | awk -F 'x| *' '/dimensions/{print$3" "$4}')
 		qrencode -l H -s $dotSize -m 0 -o- "$1" | feh -g +$((($width-$dotSize)/2))+$((($heigth-$dotSize)/2)) -
 	fi
 }
