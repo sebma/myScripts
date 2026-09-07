8f9222a90e25a46b21568be5dd8ea161230e171a
sebma <sebma@users.noreply.github.com>
2026-09-07 14:43:09 +0200

    Update clear_swap.sh

--- shl/system/clear_swap.sh
+++ shl/system/clear_swap.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-
+[ $(id -u) != 0 ] && sudo=$(type -P sudo) || sudo=""
 swapUsage=$(swapon -s | awk 'NR==2{printf "%d", 100*$4/$3}')
 swapLimit=55
 if [ $swapUsage -gt $swapLimit ];then
-        if swapoff -va;then
-                swapon -va;
+        if $sudo swapoff -va;then
+                $sudo swapon -va;
         fi
 fi
