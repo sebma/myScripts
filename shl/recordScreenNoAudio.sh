e3a846b59a7556226f081b9bb9f8df18e6818093
sebma <sebma@users.noreply.github.com>
2026-09-17 20:45:06 +0200

    Updated recordScreenNoAudio.sh

--- shl/recordScreenNoAudio.sh
+++ shl/recordScreenNoAudio.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 ffmpeg="command ffmpeg -hide_banner"
-time $ffmpeg -video_size $(xrandr | awk '/\*/{print$1}') -framerate 15 -f x11grab -i $DISPLAY.0
 outFile=recordedScreenNoAudio.webp
-#time $ffmpeg -video_size $(xrandr | awk '/\*/{print$1}') -framerate 15 -f x11grab -i $DISPLAY.0 $outFile
+time $ffmpeg -video_size $(xrandr | awk '/\*/{print$1}') -framerate 15 -f x11grab -i $DISPLAY.0 $outFile
