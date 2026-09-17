1c5cff558016294363e6497615df2f3e64e995ba
sebma <sebma@users.noreply.github.com>
2026-09-17 20:45:06 +0200

    Updated recordWindowNoAudio.sh

--- shl/recordWindowNoAudio.sh
+++ shl/recordWindowNoAudio.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 ffmpeg="command ffmpeg -hide_banner"
-time $ffmpeg -window_id $(xwininfo | awk '/Window id:/{printf$4}') -framerate 15 -f x11grab -i $DISPLAY.0
 outFile=recordedWindowNoAudio.webp
-#time $ffmpeg -window_id $(xwininfo | awk '/Window id:/{printf$4}') -framerate 15 -f x11grab -i $DISPLAY.0 $outFile
+time $ffmpeg -window_id $(xwininfo | awk '/Window id:/{printf$4}') -framerate 15 -f x11grab -i $DISPLAY.0 $outFile
